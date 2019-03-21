6cbd4f4c991ee3f8d3494de59b32bd4c1a557f9c
Remi Lehe <remi.lehe@normalesup.org>
2019-03-20 17:12:53 -0700

    Started deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="dev"
+TARGET_REPO="ecp-warpx.github.io"
+SHA=`git rev-parse --verify HEAD`
+
+# Pull requests and commits to other branches shouldn't try to deploy
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    exit 0
+fi
+
+# Install sphinx and build the documentation
+pip install sphinx sphinx_rtd_theme
+cd Docs
+make html
+
+# Clone the documentation repository
+git clone https://github.com/ECP-WarpX/$(TARGET_REPO).git
+# Remove the previous `dev` documentation
+cd $(TARGET_REPO)/doc_versions/dev
+git rm -r ./*
+# Copy and add the new documentation
+cp -r ../../../Docs/build/html/* ./
+git add ./*
+
