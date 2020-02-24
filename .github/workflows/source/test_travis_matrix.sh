03be1f81572b50375be916ef9332f70f42d63c01
MaxThevenet <mthevenet@lbl.gov>
2020-02-24 13:08:38 -0800

    use fewer &&

--- .github/workflows/source/test_travis_matrix.sh
+++ .github/workflows/source/test_travis_matrix.sh
@@ -1,18 +1,27 @@
 #! /usr/bin/env sh
+
 set -eu -o pipefail
+
 cp .github/workflows/source/travis_matrix.py Regression/
 cd Regression/
 
 # Put the name of all travis tests into a text file
-python prepare_file_travis.py && grep "\[" travis-tests.ini > travis_all_tests.txt
+python prepare_file_travis.py
+grep "\[" travis-tests.ini > travis_all_tests.txt
 
 # Concatenate the names of all elements in Travis matrix into another test file
-WARPX_CI_REGULAR_CARTESIAN=TRUE python prepare_file_travis.py && grep "\[" travis-tests.ini >  travis_matrix_elements.txt
-WARPX_CI_PSATD=TRUE             python prepare_file_travis.py && grep "\[" travis-tests.ini >> travis_matrix_elements.txt
-WARPX_CI_PYTHON_MAIN=TRUE       python prepare_file_travis.py && grep "\[" travis-tests.ini >> travis_matrix_elements.txt
-WARPX_CI_SINGLE_PRECISION=TRUE  python prepare_file_travis.py && grep "\[" travis-tests.ini >> travis_matrix_elements.txt
-WARPX_CI_RZ_OR_NOMPI=TRUE      python prepare_file_travis.py && grep "\[" travis-tests.ini >> travis_matrix_elements.txt
-WARPX_CI_QED=TRUE               python prepare_file_travis.py && grep "\[" travis-tests.ini >> travis_matrix_elements.txt
+WARPX_CI_REGULAR_CARTESIAN=TRUE python prepare_file_travis.py
+grep "\[" travis-tests.ini >  travis_matrix_elements.txt
+WARPX_CI_PSATD=TRUE             python prepare_file_travis.py
+grep "\[" travis-tests.ini >> travis_matrix_elements.txt
+WARPX_CI_PYTHON_MAIN=TRUE       python prepare_file_travis.py
+grep "\[" travis-tests.ini >> travis_matrix_elements.txt
+WARPX_CI_SINGLE_PRECISION=TRUE  python prepare_file_travis.py
+grep "\[" travis-tests.ini >> travis_matrix_elements.txt
+WARPX_CI_RZ_OR_NOMPI=TRUE      python prepare_file_travis.py
+grep "\[" travis-tests.ini >> travis_matrix_elements.txt
+WARPX_CI_QED=TRUE               python prepare_file_travis.py
+grep "\[" travis-tests.ini >> travis_matrix_elements.txt
 
 # Check that the resulting lists are equal
 {
