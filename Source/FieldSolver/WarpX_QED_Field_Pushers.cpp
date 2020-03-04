0223aeef924c55f1f905af655094ed5cdc7ae8c1
gtrichardson <gtrichardson@berkeley.edu>
2020-03-04 12:55:57 -0800

    adjusted more indentations that were accidently changed from the origional

--- Source/FieldSolver/WarpX_QED_Field_Pushers.cpp
+++ Source/FieldSolver/WarpX_QED_Field_Pushers.cpp
@@ -66,27 +66,27 @@ WarpX::Hybrid_QED_Push (int lev, PatchType patch_type, Real a_dt)
     MultiFab *Ex, *Ey, *Ez, *Bx, *By, *Bz, *Jx, *Jy, *Jz;
     if (patch_type == PatchType::fine)
     {
-	    Ex = Efield_fp[lev][0].get();
-	    Ey = Efield_fp[lev][1].get();
-	    Ez = Efield_fp[lev][2].get();
-	    Bx = Bfield_fp[lev][0].get();
-	    By = Bfield_fp[lev][1].get();
-	    Bz = Bfield_fp[lev][2].get();
-	    Jx = Bfield_fp[lev][0].get();
-	    Jy = Bfield_fp[lev][1].get();
-	    Jz = Bfield_fp[lev][2].get();
+        Ex = Efield_fp[lev][0].get();
+        Ey = Efield_fp[lev][1].get();
+        Ez = Efield_fp[lev][2].get();
+        Bx = Bfield_fp[lev][0].get();
+        By = Bfield_fp[lev][1].get();
+        Bz = Bfield_fp[lev][2].get();
+        Jx = Bfield_fp[lev][0].get();
+        Jy = Bfield_fp[lev][1].get();
+        Jz = Bfield_fp[lev][2].get();
     }
     else
     {
-	    Ex = Efield_cp[lev][0].get();
-	    Ey = Efield_cp[lev][1].get();
-	    Ez = Efield_cp[lev][2].get();
-	    Bx = Bfield_cp[lev][0].get();
-	    By = Bfield_cp[lev][1].get();
-	    Bz = Bfield_cp[lev][2].get();
-	    Jx = Bfield_cp[lev][0].get();
-	    Jy = Bfield_cp[lev][1].get();
-	    Jz = Bfield_cp[lev][2].get();
+        Ex = Efield_cp[lev][0].get();
+        Ey = Efield_cp[lev][1].get();
+        Ez = Efield_cp[lev][2].get();
+        Bx = Bfield_cp[lev][0].get();
+        By = Bfield_cp[lev][1].get();
+        Bz = Bfield_cp[lev][2].get();
+        Jx = Bfield_cp[lev][0].get();
+        Jy = Bfield_cp[lev][1].get();
+        Jz = Bfield_cp[lev][2].get();
     }
 
     MultiFab* cost = WarpX::getCosts(lev);
