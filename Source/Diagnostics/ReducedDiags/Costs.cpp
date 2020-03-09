712fc18e5baa78360b754f06047e2dc74d5266ca
Michael Rowan <mrowan137@gmail.com>
2020-03-09 14:53:10 -0400

    WIP costs reduced diagnostic

--- Source/Diagnostics/ReducedDiags/Costs.cpp
+++ Source/Diagnostics/ReducedDiags/Costs.cpp
@@ -7,13 +7,13 @@
 
 #include "Costs.H"
 #include "WarpX.H"
-#include "Utils/WarpXConst.H"
+//#include "Utils/WarpXConst.H"
 
 #include <AMReX_REAL.H>
-#include <AMReX_ParticleReduce.H>
+//#include <AMReX_ParticleReduce.H>
 
 #include <iostream>
-#include <cmath>
+//#include <cmath>
 
 
 using namespace amrex;
@@ -37,21 +37,10 @@ Costs::Costs (std::string rd_name)
     ParmParse pp("amr");
     pp.query("max_level", nLevels);
     nLevels += 1;
-    
-    // get number of boxes over all levels
-    int nBoxes = 0;
-    for (int lev = 0; lev < nLevels; ++lev)
-    {
-        //const amrex::Vector<amrex::Real>* cost_heuristic = warpx.getCostsHeuristic(lev);
-        nBoxes += 0; //??
-    }
 
+    // Does it make sense to get nBoxes at t=0?  What if it changes?
     // resize data array (for each box on each level we will save 6 data fields:
     // [cost, proc, lev, i, j, k])
-    int nDataFields = 6;
-    amrex::Print() << "nboxes: " << nBoxes << "\n";
-    amrex::Print() << "nlevels: " << nLevels << "\n";
-    m_data.resize(nDataFields*nBoxes,0.0);
 
     if (ParallelDescriptor::IOProcessor())
     {
@@ -67,7 +56,7 @@ Costs::Costs (std::string rd_name)
             ofs << "[1]step()";
             ofs << m_sep;
             ofs << "[2]time(s)";
-            for (int boxNumber = 0; boxNumber < nBoxes; ++boxNumber)
+            for (int boxNumber = 0; boxNumber < 1; ++boxNumber)
             {
                 ofs << m_sep;
                 ofs << "[" + std::to_string(3 + nDataFields*boxNumber) + "]";
@@ -100,13 +89,33 @@ Costs::Costs (std::string rd_name)
 // function that computes costs
 void Costs::ComputeDiags (int step)
 {
-    
-    // Judge if the diags should be done
-    if ( ((step+1) % m_freq != 0) ) { return; }
 
     // get WarpX class object
     auto & warpx = WarpX::GetInstance();
+    
+    // Judge if the diags should be done
+    // costs is initialized only if we're doing load balance
+    if ( ((step+1) % m_freq != 0)
+         || step <= 0
+         || warpx.get_load_balance_int() < 1 ) { return; }
 
+    // get number of boxes over all levels
+    auto nLevels = warpx.finestLevel() + 1;
+    int nBoxes = 0;
+    for (int lev = 0; lev < nLevels; ++lev)
+    {
+        const amrex::Vector<amrex::Real>* cost_heuristic = warpx.getCostsHeuristic(lev);
+        const MultiFab & Ex = warpx.getEfield(lev,0);
+        // we need to compute the number of boxes
+        nBoxes += Ex.size();
+        //amrex::Print() << "nBoxes: " << nBoxes << "step: " << step << "\n";
+    }
+    //amrex::AllPrint() << "proc: " << ParallelDescriptor::MyProc() << " nDataFields*nBoxes" << nDataFields*nBoxes << "\n";    
+    m_data.resize(nDataFields*nBoxes,0.0);
+    // you just got to clear it
+    m_data.assign(nDataFields*nBoxes,0.0);
+    //amrex::AllPrint() << "proc: " << ParallelDescriptor::MyProc() << " m_data.size()" << m_data.size() << "\n";
+    
     // compute the costs if it's not a load balance step
     if ((step+1) % warpx.get_load_balance_int() != 0)
     {
@@ -123,8 +132,13 @@ void Costs::ComputeDiags (int step)
     // keeps track of correct index in array over all boxes on all levels
     int shift = 0;
 
+    // amrex::Print()  << " m_data before";
+    // for (int iter=0; iter<m_data.size(); ++iter) {
+    //     amrex::Print() << " " << m_data[iter];
+    // }
+    // amrex::Print()  << "\n";
+    
     // save data
-    auto nLevels = warpx.finestLevel() + 1;
     for (int lev = 0; lev < nLevels; ++lev)
     {
         const amrex::DistributionMapping& dm = warpx.DistributionMap(lev);
@@ -133,26 +147,37 @@ void Costs::ComputeDiags (int step)
         for (MFIter mfi(Ex, false); mfi.isValid(); ++mfi)
         {
             const Box& tbx = mfi.tilebox();
-            //m_data[mfi.index()] = 0.;
-            // m_data[shift + mfi.index()*nDataFields + 0] = (*cost_heuristic)[mfi.index()];
-            // m_data[shift + mfi.index()*nDataFields + 1] = dm[mfi.index()];
-            // m_data[shift + mfi.index()*nDataFields + 2] = lev;
-            // m_data[shift + mfi.index()*nDataFields + 3] = tbx.loVect()[0];
-            // m_data[shift + mfi.index()*nDataFields + 4] = tbx.loVect()[1];
-            // m_data[shift + mfi.index()*nDataFields + 5] = tbx.loVect()[2];                
+            m_data[shift + mfi.index()*nDataFields + 0] = (*cost_heuristic)[mfi.index()];
+            m_data[shift + mfi.index()*nDataFields + 1] = dm[mfi.index()];
+            m_data[shift + mfi.index()*nDataFields + 2] = lev;
+            m_data[shift + mfi.index()*nDataFields + 3] = tbx.loVect()[0];
+            m_data[shift + mfi.index()*nDataFields + 4] = tbx.loVect()[1];
+            m_data[shift + mfi.index()*nDataFields + 5] = tbx.loVect()[2];
         }
         
         // we looped through all the boxes on level lev, update the shift index
-        shift += nDataFields*(cost_heuristic->size());
+        shift += nDataFields*(Ex.size());
     }
-        
+
     // parallel reduce to IO proc and get data over all procs
-    if (ParallelDescriptor::IOProcessor())
-    {
-        //amrex::Vector<Real>::iterator it_m_data = m_data.begin();
-        //amrex::Real* addr_it_m_data = &(*it_m_data);
-        //ParallelAllReduce::Sum(addr_it_m_data, m_data.size(), ParallelContext::CommunicatorSub());
-    }
+    //if (ParallelDescriptor::IOProcessor())
+    //{
+    // amrex::Print()  << " m_data after";
+    // for (int iter=0; iter<m_data.size(); ++iter) {
+    //     amrex::Print() << " " << m_data[iter];
+    // }
+    // amrex::Print()  << "\n";
+        
+    amrex::Vector<amrex::Real>::iterator it_m_data = m_data.begin();
+    amrex::Real* addr_it_m_data = &(*it_m_data);
+    // Be sure, this does what you want
+    //if (ParallelDescriptor::IOProcessor())
+    //{
+    // How I can do this only for IOproc?
+    ParallelAllReduce::Sum(addr_it_m_data,
+                           m_data.size(),
+                           ParallelContext::CommunicatorSub());
+        //}
     /* m_data now contains up-to-date values for:
      *  [cost, proc, lev, i, j, k] of box 0 at level 0,
      *   cost, proc, lev, i, j, k] of box 1 at level 0,
