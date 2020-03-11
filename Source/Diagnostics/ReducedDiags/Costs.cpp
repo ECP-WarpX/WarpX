5d21588b172524ba37d7f52468a9ec692cd561b2
Michael Rowan <mrowan137@gmail.com>
2020-03-11 16:19:56 -0400

    Member variable naming convention

--- Source/Diagnostics/ReducedDiags/Costs.cpp
+++ Source/Diagnostics/ReducedDiags/Costs.cpp
@@ -46,11 +46,11 @@ void Costs::ComputeDiags (int step)
 
     // keep track of the max number of boxes, this is needed later on to fill
     // the jagged array (in case each step does not have the same number of boxes)
-    nBoxesMax = std::max(nBoxesMax, nBoxes);
+    m_nBoxesMax = std::max(m_nBoxesMax, nBoxes);
 
     // resize and clear data array
-    m_data.resize(nDataFields*nBoxes, 0.0);
-    m_data.assign(nDataFields*nBoxes, 0.0);
+    m_data.resize(m_nDataFields*nBoxes, 0.0);
+    m_data.assign(m_nDataFields*nBoxes, 0.0);
 
     // if not a load balance step, we must compute costs
     if ((step+1) % warpx.get_load_balance_int() != 0)
@@ -77,16 +77,16 @@ void Costs::ComputeDiags (int step)
         for (MFIter mfi(Ex, false); mfi.isValid(); ++mfi)
         {
             const Box& tbx = mfi.tilebox();
-            m_data[shift + mfi.index()*nDataFields + 0] = (*cost_heuristic)[mfi.index()];
-            m_data[shift + mfi.index()*nDataFields + 1] = dm[mfi.index()];
-            m_data[shift + mfi.index()*nDataFields + 2] = lev;
-            m_data[shift + mfi.index()*nDataFields + 3] = tbx.loVect()[0];
-            m_data[shift + mfi.index()*nDataFields + 4] = tbx.loVect()[1];
-            m_data[shift + mfi.index()*nDataFields + 5] = tbx.loVect()[2];
+            m_data[shift + mfi.index()*m_nDataFields + 0] = (*cost_heuristic)[mfi.index()];
+            m_data[shift + mfi.index()*m_nDataFields + 1] = dm[mfi.index()];
+            m_data[shift + mfi.index()*m_nDataFields + 2] = lev;
+            m_data[shift + mfi.index()*m_nDataFields + 3] = tbx.loVect()[0];
+            m_data[shift + mfi.index()*m_nDataFields + 4] = tbx.loVect()[1];
+            m_data[shift + mfi.index()*m_nDataFields + 5] = tbx.loVect()[2];
         }
 
         // we looped through all the boxes on level lev, update the shift index
-        shift += nDataFields*(cost_heuristic->size());
+        shift += m_nDataFields*(cost_heuristic->size());
     }
 
     // parallel reduce to IO proc and get data over all procs
@@ -134,24 +134,24 @@ void Costs::WriteToFile (int step) const
             ofs << m_sep;
             ofs << "[2]time(s)";
             ofs << m_sep;
-            for (int boxNumber=0; boxNumber<nBoxesMax; ++boxNumber)
+            for (int boxNumber=0; boxNumber<m_nBoxesMax; ++boxNumber)
             {
-                ofs << "[" + std::to_string(3 + nDataFields*boxNumber) + "]";
+                ofs << "[" + std::to_string(3 + m_nDataFields*boxNumber) + "]";
                 ofs << "cost_box_"+std::to_string(boxNumber)+"()";
                 ofs << m_sep;
-                ofs << "[" + std::to_string(4 + nDataFields*boxNumber) + "]";
+                ofs << "[" + std::to_string(4 + m_nDataFields*boxNumber) + "]";
                 ofs << "proc_box_"+std::to_string(boxNumber)+"()";
                 ofs << m_sep;
-                ofs << "[" + std::to_string(5 + nDataFields*boxNumber) + "]";
+                ofs << "[" + std::to_string(5 + m_nDataFields*boxNumber) + "]";
                 ofs << "lev_box_"+std::to_string(boxNumber)+"()";
                 ofs << m_sep;
-                ofs << "[" + std::to_string(6 + nDataFields*boxNumber) + "]";
+                ofs << "[" + std::to_string(6 + m_nDataFields*boxNumber) + "]";
                 ofs << "i_low_box_"+std::to_string(boxNumber)+"()";
                 ofs << m_sep;
-                ofs << "[" + std::to_string(7 + nDataFields*boxNumber) + "]";
+                ofs << "[" + std::to_string(7 + m_nDataFields*boxNumber) + "]";
                 ofs << "j_low_box_"+std::to_string(boxNumber)+"()";
                 ofs << m_sep;
-                ofs << "[" + std::to_string(8 + nDataFields*boxNumber) + "]";
+                ofs << "[" + std::to_string(8 + m_nDataFields*boxNumber) + "]";
                 ofs << "k_low_box_"+std::to_string(boxNumber)+"()";
                 ofs << m_sep;
             }
@@ -176,10 +176,10 @@ void Costs::WriteToFile (int step) const
                 }
 
                 // 2 columns for step, time; then nBoxes*nDatafields columns for data;
-                // then fill the remaining columns (i.e., up to 2 + nBoxesMax*nDataFields)
+                // then fill the remaining columns (i.e., up to 2 + m_nBoxesMax*m_nDataFields)
                 // with NaN, so the array is not jagged
                 ofs << lineIn;
-                for (int i=0; i<(nBoxesMax*nDataFields - (cnt - 2)); ++i)
+                for (int i=0; i<(m_nBoxesMax*m_nDataFields - (cnt - 2)); ++i)
                 {
                     ofs << m_sep << "NaN";
                 }
