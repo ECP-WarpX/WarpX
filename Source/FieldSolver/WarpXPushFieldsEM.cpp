7769726cd68362311d7d6ba3334d017a986aa6a2
David Grote <dpgrote@lbl.gov>
2020-03-13 21:34:21 -0700

    Add assert ensuring that Jr is never node-centered
    Co-Authored-By: MaxThevenet <mthevenet@lbl.gov>

--- Source/FieldSolver/WarpXPushFieldsEM.cpp
+++ Source/FieldSolver/WarpXPushFieldsEM.cpp
@@ -493,6 +493,8 @@ WarpX::ApplyInverseVolumeScalingToCurrentDensity (MultiFab* Jx, MultiFab* Jy, Mu
     const Real dr = dx[0];
 
     constexpr int NODE = amrex::IndexType::NODE;
+    AMREX_ALWAYS_ASSERT_WITH_MESSAGE(Jx->ixType().ixType()[0] != NODE,
+        "Jr should never node-centered in r");
 
     Box tilebox;
 
