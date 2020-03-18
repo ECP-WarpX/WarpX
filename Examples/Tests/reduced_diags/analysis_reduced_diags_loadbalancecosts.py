5a162597cb489a7a48069bcee603269accd2a5de
Michael Rowan <mrowan137@gmail.com>
2020-03-17 18:11:39 -0700

    Add test for reduced_diagnostics_cost

--- Examples/Tests/reduced_diags/analysis_reduced_diags_loadbalancecosts.py
+++ Examples/Tests/reduced_diags/analysis_reduced_diags_loadbalancecosts.py
@@ -19,20 +19,39 @@
 
 # Possible running time: ~ 2 s
 
+import numpy as np
 
-# PART1: load costs data
-LBCdata = np.genfromtxt("./diags/reducedfiles/LBC.txt")
+# Load costs data
+data = np.genfromtxt("./diags/reducedfiles/LBC.txt")
+data = data[:,2:]
 
-# PART2: compute efficiency before/after load balance and check it is improved
+# From data header, data layout is:
+#     [step, time,
+#      cost_box_0, proc_box_0, lev_box_0, i_low_box_0, j_low_box_0, k_low_box_0,
+#      cost_box_1, proc_box_1, lev_box_1, i_low_box_1, j_low_box_1, k_low_box_1,
+#      ...
+#      cost_box_n, proc_box_n, lev_box_n, i_low_box_n, j_low_box_n, k_low_box_n]
 
-print('LoadBalanceCostsTest: ')
-#print('difference of field energy:', abs(EFyt-EF))
-#print('tolerance of field energy:', 1.0e-3)
-#print('difference of particle energy:', abs(EPyt-EP))
-#print('tolerance of particle energy:', 1.0e-8)
+# Function to get efficiency at an iteration i
+def get_efficiency(i):
+    # First get the unique ranks
+    costs, ranks = data[i,0::6], data[i,1::6].astype(int)
+    rank_to_cost_map = {r:0. for r in set(ranks)}
+    
+    # compute efficiency before/after load balance and check it is improved
+    for c, r in zip(costs, ranks):
+        rank_to_cost_map[r] += c
 
-assert(True)
-assert(True)
+    # Normalize the costs
+    efficiencies = np.array(list(rank_to_cost_map.values()))
+    efficiencies /= efficiencies.max()
 
-#assert(abs(EFyt-EF) < 1.0e-3)
-#assert(abs(EPyt-EP) < 1.0e-8)
+    return efficiencies.mean()
+
+# The iteration i=2 is load balanced; examine before/after load balance
+efficiency_before, efficiency_after = get_efficiency(1), get_efficiency(2)
+print('load balance efficiency (before load balance): ', efficiency_before)
+print('load balance efficiency (after load balance): ', efficiency_after)
+
+# The load balanced case is expcted to be more efficient then non-load balanced case
+assert(efficiency_before < efficiency_after)
