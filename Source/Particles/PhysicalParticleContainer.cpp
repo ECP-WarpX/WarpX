AMReX-Codes/amrex

commit 24aef3703b91c74c9cf049ee863415bb3ce4fbe1

Bulk momentum (#831)
* Adding a method to query the bulk rather than total momentum

* update copyright

* use bulk momentum for ballistic correction in add plasma

* also include the ballistic correction for lab frame simulations

* EOL

* bug fixes pointed out in code review

* append bulk to variable names

* in the Langmuir case, the momentum profile does depend on z

* some const

* clarify units of u_over_r in docstring

Source/Particles/PhysicalParticleContainer.cpp
@@ -672,12 +672,20 @@ PhysicalParticleContainer::AddPlasma (int lev, RealBox part_realbox)
                 // If the particle is not within the species's
                 // xmin, xmax, ymin, ymax, zmin, zmax, go to
                 // the next generated particle.
-                if (!inj_pos->insideBounds(xb, yb, z)) {
+
+                // include ballistic correction for plasma species with bulk motion
+                const XDim3 u_bulk = inj_mom->getBulkMomentum(x, y, z);
+                const Real gamma_bulk = std::sqrt(1.+(u_bulk.x*u_bulk.x+u_bulk.y*u_bulk.y+u_bulk.z*u_bulk.z));
+                const Real betaz_bulk = u.z/gamma_bulk;
+                const Real z0 = z - PhysConst::c*t*betaz_bulk;
+
+                if (!inj_pos->insideBounds(xb, yb, z0)) {
                     p.id() = -1;
                     return;
                 }
-                u = inj_mom->getMomentum(x, y, z);
-                dens = inj_rho->getDensity(x, y, z);
+
+                u = inj_mom->getMomentum(x, y, z0);
+                dens = inj_rho->getDensity(x, y, z0);
                 // Remove particle if density below threshold
                 if ( dens < density_min ){
                     p.id() = -1;
@@ -697,13 +705,15 @@ PhysicalParticleContainer::AddPlasma (int lev, RealBox part_realbox)
                 //
                 // In order for this equation to be solvable, betaz_lab
                 // is explicitly assumed to have no dependency on z0_lab
-                u = inj_mom->getMomentum(x, y, 0.); // No z0_lab dependency
+                //
+                // Note that we use the bulk momentum to perform the ballastic correction
+                const XDim3 u_bulk = inj_mom->getBulkMomentum(x, y, 0.); // No z0_lab dependency
                 // At this point u is the lab-frame momentum
                 // => Apply the above formula for z0_lab
-                Real gamma_lab = std::sqrt( 1.+(u.x*u.x+u.y*u.y+u.z*u.z) );
-                Real betaz_lab = u.z/(gamma_lab);
-                Real z0_lab = gamma_boost * ( z*(1-beta_boost*betaz_lab)
-                                              - PhysConst::c*t*(betaz_lab-beta_boost) );
+                const Real gamma_lab_bulk = std::sqrt(1.+(u_bulk.x*u_bulk.x+u_bulk.y*u_bulk.y+u_bulk.z*u_bulk.z));
+                const Real betaz_lab_bulk = u.z/(gamma_lab_bulk);
+                const Real z0_lab = gamma_boost * ( z*(1-beta_boost*betaz_lab_bulk)
+                                              - PhysConst::c*t*(betaz_lab_bulk-beta_boost) );
                 // If the particle is not within the lab-frame zmin, zmax, etc.
                 // go to the next generated particle.
                 if (!inj_pos->insideBounds(xb, yb, z0_lab)) {
@@ -719,6 +729,12 @@ PhysicalParticleContainer::AddPlasma (int lev, RealBox part_realbox)
                 }
                 // Cut density if above threshold
                 dens = amrex::min(dens, density_max);
+
+                // get the full momentum, including thermal motion
+                u = inj_mom->getMomentum(x, y, 0.);
+                const Real gamma_lab = std::sqrt( 1.+(u.x*u.x+u.y*u.y+u.z*u.z) );
+                const Real betaz_lab = u.z/(gamma_lab);
+
                 // At this point u and dens are the lab-frame quantities
                 // => Perform Lorentz transform
                 dens = gamma_boost * dens * ( 1.0 - beta_boost*betaz_lab );

Source/Initialization/CustomMomentumProb.H
@@ -28,6 +28,14 @@ struct InjectorMomentumCustom
         return {0., 0., 0.};
     }
 
+    // Return momentum at given position (illustration: momentum=0).
+    AMREX_GPU_HOST_DEVICE
+    amrex::XDim3
+    getBulkMomentum (amrex::Real, amrex::Real, amrex::Real) const noexcept
+    {
+        return {0., 0., 0.};
+    }
+
     // Note that we are not allowed to have non-trivial destructor.
     // So we rely on clear() to free memory if needed.
     void clear () { }

Source/Initialization/InjectorMomentum.H
@@ -1,5 +1,5 @@
-/* Copyright 2019 Axel Huebl, Cameron Yang, Maxence Thevenet
- * Weiqun Zhang
+/* Copyright 2019-2020 Andrew Myers, Axel Huebl, Cameron Yang,
+ * Maxence Thevenet, Weiqun Zhang
  *
  * This file is part of WarpX.
  *
@@ -10,6 +10,7 @@
 
 #include "CustomMomentumProb.H"
 #include "Parser/GpuParser.H"
+#include "Utils/WarpXConst.H"
 
 #include <AMReX_Gpu.H>
 #include <AMReX_Dim3.H>
@@ -26,6 +27,14 @@ struct InjectorMomentumConstant
     {
         return amrex::XDim3{m_ux,m_uy,m_uz};
     }
+
+    AMREX_GPU_HOST_DEVICE
+    amrex::XDim3
+    getBulkMomentum (amrex::Real, amrex::Real, amrex::Real) const noexcept
+    {
+        return amrex::XDim3{m_ux,m_uy,m_uz};
+    }
+
 private:
     amrex::Real m_ux, m_uy, m_uz;
 };
@@ -49,6 +58,14 @@ struct InjectorMomentumGaussian
                             amrex::RandomNormal(m_uy_m, m_uy_th),
                             amrex::RandomNormal(m_uz_m, m_uz_th)};
     }
+
+    AMREX_GPU_HOST_DEVICE
+    amrex::XDim3
+    getBulkMomentum (amrex::Real x, amrex::Real y, amrex::Real z) const noexcept
+    {
+        return amrex::XDim3{m_ux_m, m_uy_m, m_uz_m};
+    }
+
 private:
     amrex::Real m_ux_m, m_uy_m, m_uz_m;
     amrex::Real m_ux_th, m_uy_th, m_uz_th;
@@ -106,6 +123,18 @@ struct InjectorMomentumBoltzmann
         // Note that if beta = 0 then the flipping method and Lorentz transform
         // have no effect on the u[dir] direction.
         return amrex::XDim3 {u[0],u[1],u[2]};
+    }
+
+    AMREX_GPU_HOST_DEVICE
+    amrex::XDim3
+    getBulkMomentum (amrex::Real x, amrex::Real y, amrex::Real z) const noexcept
+    {
+        using namespace amrex;
+        Real u[3];
+        for (int idim = 0; idim < 3; ++idim) u[idim] = 0.0_rt;
+        const Real gamma = 1./sqrt(1+beta*beta);
+        u[dir] = gamma*beta;
+        return XDim3 {u[0],u[1],u[2]};
     }
 
 private:
@@ -182,14 +211,30 @@ struct InjectorMomentumJuttner
         return amrex::XDim3 {u[0],u[1],u[2]};
     }
 
+    AMREX_GPU_HOST_DEVICE
+    amrex::XDim3
+    getBulkMomentum (amrex::Real x, amrex::Real y, amrex::Real z) const noexcept
+    {
+        using namespace amrex;
+        Real u[3];
+        for (int idim = 0; idim < 3; ++idim) u[idim] = 0.0_rt;
+        const Real gamma = 1./sqrt(1+beta*beta);
+        u[dir] = gamma*beta;
+        return XDim3 {u[0],u[1],u[2]};
+    }
+
 private:
     int dir;
     amrex::Real beta, theta;
 };
 
-
-// struct whose getMomentum returns momentum for 1 particle, for
-// radial expansion
+/**
+ * \brief struct whose getMomentum returns momentum for 1 particle, for
+ * radial expansion.
+ *
+ * Note - u_over_r is expected to be the normalized momentum gamma*beta
+ * divided by the physical position in SI units.
+**/
 struct InjectorMomentumRadialExpansion
 {
     InjectorMomentumRadialExpansion (amrex::Real a_u_over_r) noexcept
@@ -199,6 +244,13 @@ struct InjectorMomentumRadialExpansion
     AMREX_GPU_HOST_DEVICE
     amrex::XDim3
     getMomentum (amrex::Real x, amrex::Real y, amrex::Real z) const noexcept
+    {
+        return {x*u_over_r, y*u_over_r, z*u_over_r};
+    }
+
+    AMREX_GPU_HOST_DEVICE
+    amrex::XDim3
+    getBulkMomentum (amrex::Real x, amrex::Real y, amrex::Real z) const noexcept
     {
         return {x*u_over_r, y*u_over_r, z*u_over_r};
     }
@@ -219,6 +271,13 @@ struct InjectorMomentumParser
     AMREX_GPU_HOST_DEVICE
     amrex::XDim3
     getMomentum (amrex::Real x, amrex::Real y, amrex::Real z) const noexcept
+    {
+        return amrex::XDim3{m_ux_parser(x,y,z),m_uy_parser(x,y,z),m_uz_parser(x,y,z)};
+    }
+
+    AMREX_GPU_HOST_DEVICE
+    amrex::XDim3
+    getBulkMomentum (amrex::Real x, amrex::Real y, amrex::Real z) const noexcept
     {
         return amrex::XDim3{m_ux_parser(x,y,z),m_uy_parser(x,y,z),m_uz_parser(x,y,z)};
     }
@@ -336,6 +395,50 @@ struct InjectorMomentum
         case Type::custom:
         {
             return object.custom.getMomentum(x,y,z);
+        }
+        default:
+        {
+            amrex::Abort("InjectorMomentum: unknown type");
+            return {0.0,0.0,0.0};
+        }
+        }
+    }
+
+    // call getBulkMomentum from the object stored in the union
+    // (the union is called Object, and the instance is called object).
+    AMREX_GPU_HOST_DEVICE
+    amrex::XDim3
+    getBulkMomentum (amrex::Real x, amrex::Real y, amrex::Real z) const noexcept
+    {
+        switch (type)
+        {
+        case Type::parser:
+        {
+            return object.parser.getBulkMomentum(x,y,z);
+        }
+        case Type::gaussian:
+        {
+            return object.gaussian.getBulkMomentum(x,y,z);
+        }
+        case Type::boltzmann:
+        {
+            return object.boltzmann.getBulkMomentum(x,y,z);
+        }
+        case Type::juttner:
+        {
+            return object.juttner.getBulkMomentum(x,y,z);
+        }
+        case Type::constant:
+        {
+            return object.constant.getBulkMomentum(x,y,z);
+        }
+        case Type::radial_expansion:
+        {
+            return object.radial_expansion.getBulkMomentum(x,y,z);
+        }
+        case Type::custom:
+        {
+            return object.custom.getBulkMomentum(x,y,z);
         }
         default:
         {

Source/Initialization/InjectorMomentum.cpp
@@ -1,5 +1,5 @@
-/* Copyright 2019-2020 Axel Huebl, Maxence Thevenet, Revathi Jambunathan
- * Weiqun Zhang
+/* Copyright 2019-2020 Andrew Myers, Axel Huebl, Maxence Thevenet,
+ * Revathi Jambunathan, Weiqun Zhang
  *
  * This file is part of WarpX.
  *

ECP-WarpX/WarpX

commit 0e7ecce241e9ddb7c020253d113a9c14fc7c6893

Fix bug introduced in PR #831

Source/Particles/PhysicalParticleContainer.cpp
@@ -676,7 +676,7 @@ PhysicalParticleContainer::AddPlasma (int lev, RealBox part_realbox)
                 // include ballistic correction for plasma species with bulk motion
                 const XDim3 u_bulk = inj_mom->getBulkMomentum(x, y, z);
                 const Real gamma_bulk = std::sqrt(1.+(u_bulk.x*u_bulk.x+u_bulk.y*u_bulk.y+u_bulk.z*u_bulk.z));
-                const Real betaz_bulk = u.z/gamma_bulk;
+                const Real betaz_bulk = u_bulk.z/gamma_bulk;
                 const Real z0 = z - PhysConst::c*t*betaz_bulk;
 
                 if (!inj_pos->insideBounds(xb, yb, z0)) {
@@ -711,7 +711,7 @@ PhysicalParticleContainer::AddPlasma (int lev, RealBox part_realbox)
                 // At this point u is the lab-frame momentum
                 // => Apply the above formula for z0_lab
                 const Real gamma_lab_bulk = std::sqrt(1.+(u_bulk.x*u_bulk.x+u_bulk.y*u_bulk.y+u_bulk.z*u_bulk.z));
-                const Real betaz_lab_bulk = u.z/(gamma_lab_bulk);
+                const Real betaz_lab_bulk = u_bulk.z/(gamma_lab_bulk);
                 const Real z0_lab = gamma_boost * ( z*(1-beta_boost*betaz_lab_bulk)
                                               - PhysConst::c*t*(betaz_lab_bulk-beta_boost) );
                 // If the particle is not within the lab-frame zmin, zmax, etc.