2e48e44256488c10c05e5cdf95bfeb9c0975e42a
Edoardo Zoni <ezoni@lbl.gov>
2020-03-25 18:44:39 -0700

    Write Average::FineToCoarse for cell-centered and nodal data

--- Source/Utils/Average.cpp
+++ Source/Utils/Average.cpp
@@ -42,10 +42,10 @@ Average::FineToCoarse ( const MultiFab& S_fine,
 
     bool is_cell_centered = S_crse.is_cell_centered();
 
-    //
     // Coarsen() the fine stuff on processors owning the fine data.
-    //
     BoxArray crse_S_fine_BA = S_fine.boxArray(); crse_S_fine_BA.coarsen(ratio);
+
+    const IntVect stag = S_fine.boxArray().ixType().ixType();
 
     if (crse_S_fine_BA == S_crse.boxArray() and S_fine.DistributionMap() == S_crse.DistributionMap())
     {
@@ -54,22 +54,15 @@ Average::FineToCoarse ( const MultiFab& S_fine,
 #endif
         for (MFIter mfi(S_crse,TilingIfNotGPU()); mfi.isValid(); ++mfi)
         {
-            //  NOTE: The tilebox is defined at the coarse level.
+            // NOTE: the tilebox is defined at the coarse level.
             const Box& bx = mfi.tilebox();
             Array4<Real> const& crsearr = S_crse.array(mfi);
             Array4<Real const> const& finearr = S_fine.const_array(mfi);
 
-            if (is_cell_centered) {
-                AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( bx, tbx,
-                {
-                    amrex_avgdown(tbx,crsearr,finearr,scomp,scomp,ncomp,ratio);
-                });
-            } else {
-                AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( bx, tbx,
-                {
-                    amrex_avgdown_nodes(tbx,crsearr,finearr,scomp,scomp,ncomp,ratio);
-                });
-            }
+            AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( bx, tbx,
+            {
+                Average::FineToCoarse( tbx, crsearr, finearr, stag, scomp, scomp, ncomp, ratio );
+            });
         }
     }
     else
@@ -81,26 +74,19 @@ Average::FineToCoarse ( const MultiFab& S_fine,
 #endif
         for (MFIter mfi(crse_S_fine,TilingIfNotGPU()); mfi.isValid(); ++mfi)
         {
-            //  NOTE: The tilebox is defined at the coarse level.
+            // NOTE: the tilebox is defined at the coarse level.
             const Box& bx = mfi.tilebox();
             Array4<Real> const& crsearr = crse_S_fine.array(mfi);
             Array4<Real const> const& finearr = S_fine.const_array(mfi);
 
-            //  NOTE: We copy from component scomp of the fine fab into component 0 of the crse fab
-            //        because the crse fab is a temporary which was made starting at comp 0, it is
-            //        not part of the actual crse multifab which came in.
+            // NOTE: we copy from component scomp of the fine fab into component 0 of the crse fab
+            //       because the crse fab is a temporary which was made starting at comp 0, it is
+            //       not part of the actual crse multifab which came in.
 
-            if (is_cell_centered) {
-                AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( bx, tbx,
-                {
-                    amrex_avgdown(tbx,crsearr,finearr,0,scomp,ncomp,ratio);
-                });
-            } else {
-                AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( bx, tbx,
-                {
-                    amrex_avgdown_nodes(tbx,crsearr,finearr,0,scomp,ncomp,ratio);
-                });
-            }
+            AMREX_LAUNCH_HOST_DEVICE_LAMBDA ( bx, tbx,
+            {
+                Average::FineToCoarse( tbx, crsearr, finearr, stag, 0, scomp, ncomp, ratio );
+            });
         }
 
         S_crse.copy(crse_S_fine,0,scomp,ncomp);
