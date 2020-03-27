5f7dad1643b7ad64e503ac4ece307cdb409f6bb9
Dave Grote <dpgrote@lbl.gov>
2020-03-26 17:08:27 -0700

    Reduced sizes of PICMI plasma_acceleration tests

--- Examples/Physics_applications/plasma_acceleration/PICMI_inputs_plasma_acceleration.py
+++ Examples/Physics_applications/plasma_acceleration/PICMI_inputs_plasma_acceleration.py
@@ -1,22 +1,25 @@
 from pywarpx import picmi
 #from warp import picmi
 
+# Note: Very low resolution is used in this test case since it's main
+# purpose is testing the operation of the code.
+
 constants = picmi.constants
 
-nx = 64
-ny = 64
-nz = 64
+nx = 32
+ny = 32
+nz = 16
 
-xmin = -200.e-6
-xmax = +200.e-6
-ymin = -200.e-6
-ymax = +200.e-6
-zmin = -200.e-6
-zmax = +200.e-6
+xmin = -100.e-6
+xmax = +100.e-6
+ymin = -100.e-6
+ymax = +100.e-6
+zmin = -100.e-6
+zmax = +0.e-6
 
 moving_window_velocity = [0., 0., constants.c]
 
-number_per_cell_each_dim = [2, 2, 1]
+number_per_cell_each_dim = [1, 1, 1]
 
 grid = picmi.Cartesian3DGrid(number_of_cells = [nx, ny, nz],
                              lower_bound = [xmin, ymin, zmin],
@@ -29,13 +32,13 @@ grid = picmi.Cartesian3DGrid(number_of_cells = [nx, ny, nz],
 solver = picmi.ElectromagneticSolver(grid=grid, cfl=1)
 
 beam_distribution = picmi.UniformDistribution(density = 1.e23,
-                                              lower_bound = [-20.e-6, -20.e-6, -150.e-6],
-                                              upper_bound = [+20.e-6, +20.e-6, -100.e-6],
+                                              lower_bound = [-20.e-6, -20.e-6, -75.e-6],
+                                              upper_bound = [+20.e-6, +20.e-6, -25.e-6],
                                               directed_velocity = [0., 0., 1.e9])
 
 plasma_distribution = picmi.UniformDistribution(density = 1.e22,
-                                                lower_bound = [-200.e-6, -200.e-6, 0.],
-                                                upper_bound = [+200.e-6, +200.e-6, None],
+                                                lower_bound = [None, None, 0.],
+                                                upper_bound = [None, None, None],
                                                 fill_in = True)
 
 beam = picmi.Species(particle_type='electron', name='beam', initial_distribution=beam_distribution)
@@ -44,7 +47,7 @@ plasma = picmi.Species(particle_type='electron', name='plasma', initial_distribu
 sim = picmi.Simulation(solver = solver,
                        max_steps = 10,
                        verbose = 1,
-                       warpx_plot_int = 2,
+                       warpx_plot_int = 10,
                        warpx_current_deposition_algo = 'esirkepov')
 
 sim.add_species(beam, layout=picmi.GriddedLayout(grid=grid, n_macroparticle_per_cell=number_per_cell_each_dim))
--- Examples/Physics_applications/plasma_acceleration/PICMI_inputs_plasma_acceleration_mr.py
+++ Examples/Physics_applications/plasma_acceleration/PICMI_inputs_plasma_acceleration_mr.py
@@ -1,22 +1,25 @@
 from pywarpx import picmi
 #from warp import picmi
 
+# Note: Very low resolution is used in this test case since it's main
+# purpose is testing the operation of the code.
+
 constants = picmi.constants
 
-nx = 64
-ny = 64
-nz = 64
+nx = 32
+ny = 32
+nz = 16
 
-xmin = -200.e-6
-xmax = +200.e-6
-ymin = -200.e-6
-ymax = +200.e-6
-zmin = -200.e-6
-zmax = +200.e-6
+xmin = -100.e-6
+xmax = +100.e-6
+ymin = -100.e-6
+ymax = +100.e-6
+zmin = -100.e-6
+zmax = +0.e-6
 
 moving_window_velocity = [0., 0., constants.c]
 
-number_per_cell_each_dim = [4, 4, 4]
+number_per_cell_each_dim = [1, 1, 1]
 
 grid = picmi.Cartesian3DGrid(number_of_cells = [nx, ny, nz],
                              lower_bound = [xmin, ymin, zmin],
@@ -37,13 +40,13 @@ solver = picmi.ElectromagneticSolver(grid=grid, cfl=1,
                                      warpx_pml_ncell = 10)
 
 beam_distribution = picmi.UniformDistribution(density = 1.e23,
-                                              lower_bound = [-20.e-6, -20.e-6, -150.e-6],
-                                              upper_bound = [+20.e-6, +20.e-6, -100.e-6],
+                                              lower_bound = [-20.e-6, -20.e-6, -75.e-6],
+                                              upper_bound = [+20.e-6, +20.e-6, -25.e-6],
                                               directed_velocity = [0., 0., 1.e9])
 
 plasma_distribution = picmi.UniformDistribution(density = 1.e22,
-                                                lower_bound = [-200.e-6, -200.e-6, 0.],
-                                                upper_bound = [+200.e-6, +200.e-6, None],
+                                                lower_bound = [None, None, 0.],
+                                                upper_bound = [None, None, None],
                                                 fill_in = True)
 
 beam = picmi.Species(particle_type='electron', name='beam', initial_distribution=beam_distribution)
