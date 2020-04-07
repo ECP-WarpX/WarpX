9ff9d52b71806a965f912e34b76d26305765981d
Edoardo Zoni <ezoni@lbl.gov>
2020-04-07 10:46:24 -0700

    New names: mf_src, arr_src, stag_src, mf_dst, arr_dst, stag_dst

--- Source/Utils/Average.cpp
+++ Source/Utils/Average.cpp
@@ -29,37 +29,40 @@ Average::ToCellCenter ( MultiFab& mf_out,
 }
 
 void
-Average::CoarsenAndInterpolateLoop ( MultiFab& mf_cp,
-                                     const MultiFab& mf_fp,
+Average::CoarsenAndInterpolateLoop ( MultiFab& mf_dst,
+                                     const MultiFab& mf_src,
                                      const int dcomp,
                                      const int scomp,
                                      const int ncomp,
                                      const IntVect crse_ratio )
 {
     // Staggerings of input fine MultiFab and output coarse MultiFab
-    const IntVect stag_fp = mf_fp.boxArray().ixType().toIntVect();
-    const IntVect stag_cp = mf_cp.boxArray().ixType().toIntVect();
+    const IntVect stag_src = mf_src.boxArray().ixType().toIntVect();
+    const IntVect stag_dst = mf_dst.boxArray().ixType().toIntVect();
 
-    AMREX_ALWAYS_ASSERT_WITH_MESSAGE( mf_fp.nGrowVect() >= IntVect(stag_cp-stag_fp),
+    AMREX_ALWAYS_ASSERT_WITH_MESSAGE( mf_src.nGrowVect() >= IntVect(stag_dst-stag_src),
         "input MultiFab does not have enough guard cells for this interpolation" );
 
-    // Auxiliary integer arrays (always 3D unlike IntVect objects)
+    // Auxiliary integer arrays (always 3D unlike IntVect objects):
+    // sf staggering of source MultiFab (fine)
+    // sc staggering of destination MultiFab (coarse)
+    // cr coarsening ratio
     int sf[3], sc[3], cr[3];
 
-    sf[0] = stag_fp[0];
-    sf[1] = stag_fp[1];
+    sf[0] = stag_src[0];
+    sf[1] = stag_src[1];
 #if   (AMREX_SPACEDIM == 2)
     sf[2] = 0;
 #elif (AMREX_SPACEDIM == 3)
-    sf[2] = stag_fp[2];
+    sf[2] = stag_src[2];
 #endif
 
-    sc[0] = stag_cp[0];
-    sc[1] = stag_cp[1];
+    sc[0] = stag_dst[0];
+    sc[1] = stag_dst[1];
 #if   (AMREX_SPACEDIM == 2)
     sc[2] = 0;
 #elif (AMREX_SPACEDIM == 3)
-    sc[2] = stag_cp[2];
+    sc[2] = stag_dst[2];
 #endif
 
     cr[0] = crse_ratio[0];
@@ -74,24 +77,24 @@ Average::CoarsenAndInterpolateLoop ( MultiFab& mf_cp,
 #pragma omp parallel if (Gpu::notInLaunchRegion())
 #endif
     // Loop over boxes (or tiles if not on GPU)
-    for (MFIter mfi( mf_cp, TilingIfNotGPU() ); mfi.isValid(); ++mfi)
+    for (MFIter mfi( mf_dst, TilingIfNotGPU() ); mfi.isValid(); ++mfi)
     {
         // Tiles defined at the coarse level
         const Box& bx = mfi.tilebox();
-        Array4<Real> const& mf_cp_arr = mf_cp.array( mfi );
-        Array4<Real const> const& mf_fp_arr = mf_fp.const_array( mfi );
+        Array4<Real> const& arr_dst = mf_dst.array( mfi );
+        Array4<Real const> const& arr_src = mf_src.const_array( mfi );
         ParallelFor( bx, ncomp,
                      [=] AMREX_GPU_DEVICE( int i, int j, int k, int n )
                      {
-                         mf_cp_arr(i,j,k,n+dcomp) = Average::CoarsenAndInterpolateKernel(
-                             mf_fp_arr, sf, sc, cr, i, j, k, n+scomp );
+                         arr_dst(i,j,k,n+dcomp) = Average::CoarsenAndInterpolateKernel(
+                             arr_src, sf, sc, cr, i, j, k, n+scomp );
                      } );
     }
 }
 
 void
-Average::CoarsenAndInterpolate ( MultiFab& mf_cp,
-                                 const MultiFab& mf_fp,
+Average::CoarsenAndInterpolate ( MultiFab& mf_dst,
+                                 const MultiFab& mf_src,
                                  const int dcomp,
                                  const int scomp,
                                  const int ncomp,
@@ -99,24 +102,24 @@ Average::CoarsenAndInterpolate ( MultiFab& mf_cp,
 {
     BL_PROFILE( "Average::CoarsenAndInterpolate" );
 
-    AMREX_ALWAYS_ASSERT_WITH_MESSAGE( mf_fp.boxArray().coarsenable( crse_ratio ),
+    AMREX_ALWAYS_ASSERT_WITH_MESSAGE( mf_src.boxArray().coarsenable( crse_ratio ),
         "input MultiFab is not coarsenable" );
 
     // Coarsen fine data
-    BoxArray coarsened_mf_fp_ba = mf_fp.boxArray();
-    coarsened_mf_fp_ba.coarsen( crse_ratio );
+    BoxArray coarsened_mf_src_ba = mf_src.boxArray();
+    coarsened_mf_src_ba.coarsen( crse_ratio );
 
-    if (coarsened_mf_fp_ba == mf_cp.boxArray() and mf_fp.DistributionMap() == mf_cp.DistributionMap())
-        Average::CoarsenAndInterpolateLoop( mf_cp, mf_fp, dcomp, scomp, ncomp, crse_ratio );
+    if (coarsened_mf_src_ba == mf_dst.boxArray() and mf_src.DistributionMap() == mf_dst.DistributionMap())
+        Average::CoarsenAndInterpolateLoop( mf_dst, mf_src, dcomp, scomp, ncomp, crse_ratio );
     else
     {
         // Cannot coarsen directly into a MultiFab with different BoxArray or DistributionMapping.
         // Hence, it is done in two steps:
-        // temporary MultiFab on coarsened version of mf_fp.boxArray(), with same distribution mapping
-        MultiFab coarsened_mf_fp( coarsened_mf_fp_ba, mf_fp.DistributionMap(), ncomp, 0, MFInfo(), FArrayBoxFactory() );
-        // 1) do the interpolation from mf_fp to coarsened_mf_fp (start writing into component 0)
-        Average::CoarsenAndInterpolateLoop( coarsened_mf_fp, mf_fp, 0, scomp, ncomp, crse_ratio );
-        // 2) copy from coarsened_mf_fp to mf_cp (with different BoxArray or DistributionMapping)
-        mf_cp.copy( coarsened_mf_fp, 0, dcomp, ncomp );
+        // temporary MultiFab on coarsened version of mf_src.boxArray(), with same distribution mapping
+        MultiFab coarsened_mf_src( coarsened_mf_src_ba, mf_src.DistributionMap(), ncomp, 0, MFInfo(), FArrayBoxFactory() );
+        // 1) do the interpolation from mf_src to coarsened_mf_src (start writing into component 0)
+        Average::CoarsenAndInterpolateLoop( coarsened_mf_src, mf_src, 0, scomp, ncomp, crse_ratio );
+        // 2) copy from coarsened_mf_src to mf_dst (with different BoxArray or DistributionMapping)
+        mf_dst.copy( coarsened_mf_src, 0, dcomp, ncomp );
     }
 }
