AMReX-Codes/amrex

commit c055a2f46b71d0a0fb0a7f83757e434bc56dbca6

Charge in BeamRelevant Redu. Diagnostic (#880)
Adds the beam parameter "charge" to our reduced diagnostics,
type "BeamRelevant".

Source/Diagnostics/ReducedDiags/BeamRelevant.cpp
@@ -42,7 +42,8 @@ BeamRelevant::BeamRelevant (std::string rd_name)
     // 10,11,12: rms px,py,pz
     //       13: rms gamma
     // 14,15,16: emittance x,y,z
-    m_data.resize(17,0.0);
+    //       17: charge
+    m_data.resize(18,0.0);
 #elif (AMREX_SPACEDIM == 2)
     //     0, 1: mean x,z
     //  2, 3, 4: mean px,py,pz
@@ -51,7 +52,8 @@ BeamRelevant::BeamRelevant (std::string rd_name)
     //  8, 9,10: rms px,py,pz
     //       11: rms gamma
     //    12,13: emittance x,z
-    m_data.resize(14,0.0);
+    //       14: charge
+    m_data.resize(15,0.0);
 #endif
 
     if (ParallelDescriptor::IOProcessor())
@@ -83,7 +85,8 @@ BeamRelevant::BeamRelevant (std::string rd_name)
             ofs << "[16]gamma_rms()";     ofs << m_sep;
             ofs << "[17]emittance_x(m)";  ofs << m_sep;
             ofs << "[18]emittance_y(m)";  ofs << m_sep;
-            ofs << "[19]emittance_z(m)";  ofs << std::endl;
+            ofs << "[19]emittance_z(m)";  ofs << m_sep;
+            ofs << "[20]charge(C)";       ofs << std::endl;
 #elif (AMREX_SPACEDIM == 2)
             ofs << "#";
             ofs << "[1]step()";           ofs << m_sep;
@@ -101,7 +104,8 @@ BeamRelevant::BeamRelevant (std::string rd_name)
             ofs << "[13]pz_rms(kg*m/s)";  ofs << m_sep;
             ofs << "[14]gamma_rms()";     ofs << m_sep;
             ofs << "[15]emittance_x(m)";  ofs << m_sep;
-            ofs << "[16]emittance_z(m)";  ofs << std::endl;
+            ofs << "[16]emittance_x(m)";  ofs << m_sep;
+            ofs << "[17]charge(C)";       ofs << std::endl;
 #endif
             // close file
             ofs.close();
@@ -147,8 +151,9 @@ void BeamRelevant::ComputeDiags (int step)
         // get WarpXParticleContainer class object
         auto const & myspc = mypc.GetParticleContainer(i_s);
 
-        // get mass (Real)
+        // get mass and charge (Real), FIXME actually all here are ParticleReal
         Real const m = myspc.getMass();
+        Real const q = myspc.getCharge();
 
         using PType = typename WarpXParticleContainer::SuperParticleType;
 
@@ -312,6 +317,14 @@ void BeamRelevant::ComputeDiags (int step)
             return a * p.rdata(PIdx::w);
         });
 
+        // charge
+        Real charge = ReduceSum( myspc,
+        [=] AMREX_GPU_HOST_DEVICE (const PType& p) -> Real
+        {
+            Real const w = p.rdata(PIdx::w);
+            return q * w;
+        });
+
         // reduced sum over mpi ranks
         ParallelDescriptor::ReduceRealSum
             ( x_ms, ParallelDescriptor::IOProcessorNumber());
@@ -347,6 +360,8 @@ void BeamRelevant::ComputeDiags (int step)
         ParallelDescriptor::ReduceRealSum
             (   zuz, ParallelDescriptor::IOProcessorNumber());
         zuz /= w_sum;
+        ParallelDescriptor::ReduceRealSum
+            ( charge, ParallelDescriptor::IOProcessorNumber());
 
         // save data
 #if (AMREX_SPACEDIM == 3)
@@ -367,6 +382,7 @@ void BeamRelevant::ComputeDiags (int step)
         m_data[14] = std::sqrt(x_ms*ux_ms-xux*xux) / PhysConst::c;
         m_data[15] = std::sqrt(y_ms*uy_ms-yuy*yuy) / PhysConst::c;
         m_data[16] = std::sqrt(z_ms*uz_ms-zuz*zuz) / PhysConst::c;
+        m_data[17] = charge;
 #elif (AMREX_SPACEDIM == 2)
         m_data[0]  = x_mean;
         m_data[1]  = z_mean;
@@ -382,6 +398,7 @@ void BeamRelevant::ComputeDiags (int step)
         m_data[11] = std::sqrt(gm_ms);
         m_data[12] = std::sqrt(x_ms*ux_ms-xux*xux) / PhysConst::c;
         m_data[13] = std::sqrt(z_ms*uz_ms-zuz*zuz) / PhysConst::c;
+        m_data[14] = charge;
 #endif
 
     }

Examples/Tests/initial_distribution/analysis_distribution.py
@@ -16,7 +16,7 @@
 import numpy as np
 import scipy.constants as scc
 import scipy.special as scs
-from read_raw_data import read_reduced_diags_histogram
+from read_raw_data import read_reduced_diags_histogram, read_reduced_diags
 
 # print tolerance
 tolerance = 0.01
@@ -93,6 +93,8 @@ assert(f3_error < tolerance)
 bin_value, h4x = read_reduced_diags_histogram("h4x.txt")[2:]
 h4y = read_reduced_diags_histogram("h4y.txt")[3]
 h4z = read_reduced_diags_histogram("h4z.txt")[3]
+_, bmmntr = read_reduced_diags("bmmntr.txt")
+charge = bmmntr['charge'][0]
 
 # parameters of theory
 x_rms = 0.25
@@ -107,9 +109,11 @@ f_xy = npart*db * np.exp(-0.5*(bin_value/x_rms)**2)/(x_rms*np.sqrt(2.0*scc.pi)) 
 f_z = npart*db * np.exp(-0.5*(bin_value/x_rms)**2)/(x_rms*np.sqrt(2.0*scc.pi))
 f_z[ np.absolute(bin_value) > z_cut * x_rms ] = 0.
 f_peak = np.amax(f_z)
+q_tot_cut = q_tot * scs.erf(z_cut/np.sqrt(2.))
 
 # compute error
 f4_error = np.sum(np.abs(f_xy-h4x)+np.abs(f_xy-h4y)+np.abs(f_z-h4z))/bin_value.size / f_peak
+charge_error = np.abs((q_tot_cut - charge) / q_tot)
 
 do_plot = False
 if do_plot:
@@ -126,5 +130,7 @@ if do_plot:
     plt.savefig('toto.pdf', bbox_inches='tight')
 
 print('Gaussian position distribution difference:', f4_error)
-
 assert(f4_error < tolerance)
+
+print('Relative beam charge difference:', charge_error)
+assert(charge_error < tolerance)

Examples/Tests/initial_distribution/inputs
@@ -85,7 +85,7 @@ beam.focused                    = false
 # 2 for maxwell-boltzmann
 # 3 for maxwell-juttner
 # 4 for beam
-warpx.reduced_diags_names              = h1x h1y h1z h2x h2y h2z h3 h4x h4y h4z
+warpx.reduced_diags_names              = h1x h1y h1z h2x h2y h2z h3 h4x h4y h4z bmmntr
 
 h1x.type                                 = ParticleHistogram
 h1x.frequency                            = 1
@@ -176,3 +176,9 @@ h4z.bin_number                           = 50
 h4z.bin_min                              = -1.0
 h4z.bin_max                              = +1.0
 h4z.histogram_function(t,x,y,z,ux,uy,uz) = "z"
+
+# our little beam monitor
+bmmntr.type                              = BeamRelevant
+bmmntr.frequency                         = 1
+bmmntr.path                              = "./"
+bmmntr.species                           = beam

ECP-WarpX/WarpX

commit dd9289fe6c81a3ecb0899c06d90a1b5867b65290

Reduced Diags: Fix 2D Emittance Label
Fix bug introduced in #880.

Source/Diagnostics/ReducedDiags/BeamRelevant.cpp
@@ -104,7 +104,7 @@ BeamRelevant::BeamRelevant (std::string rd_name)
             ofs << "[13]pz_rms(kg*m/s)";  ofs << m_sep;
             ofs << "[14]gamma_rms()";     ofs << m_sep;
             ofs << "[15]emittance_x(m)";  ofs << m_sep;
-            ofs << "[16]emittance_x(m)";  ofs << m_sep;
+            ofs << "[16]emittance_z(m)";  ofs << m_sep;
             ofs << "[17]charge(C)";       ofs << std::endl;
 #endif
             // close file