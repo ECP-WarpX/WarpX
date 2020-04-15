d8d992588eb97eb6e3cde3c6c099c3fc3456ded8
RevathiJambunathan <revanathan@gmail.com>
2020-04-14 21:51:29 -0700

    eol fix

--- Source/Diagnostics/ComputeDiagFunctors/PartPerCellFunctor.cpp
+++ Source/Diagnostics/ComputeDiagFunctors/PartPerCellFunctor.cpp
@@ -17,7 +17,7 @@ void
 PartPerCellFunctor::operator()(amrex::MultiFab& mf_dst, const int dcomp) const
 {
     auto& warpx = WarpX::GetInstance();
-    // Guard cell is set to 1 for generality. However, for a cell-centered 
+    // Guard cell is set to 1 for generality. However, for a cell-centered
     // output Multifab, mf_avg, the guard-cell data is not needed especially considering
     // the operations performend in the CoarsenAndInterpolate function.
     constexpr int ng = 1;
