cedbb4726c81dc857e3a140f4e30f100c797120b
Olga Shapoval <oshapoval@lbl.gov>
2020-04-27 16:01:07 -0700

    Guard cells update of the averaged E,B fields

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -115,6 +115,9 @@ WarpX::Evolve (int numsteps)
             FillBoundaryB(guard_cells.ng_FieldGather, guard_cells.ng_Extra);
             // E and B: enough guard cells to update Aux or call Field Gather in fp and cp
             // Need to update Aux on lower levels, to interpolate to higher levels.
+            FillBoundaryE_avg(guard_cells.ng_FieldGather, guard_cells.ng_Extra);
+            FillBoundaryB_avg(guard_cells.ng_FieldGather, guard_cells.ng_Extra);
+
 #ifndef WARPX_USE_PSATD
             FillBoundaryAux(guard_cells.ng_UpdateAux);
 #endif
