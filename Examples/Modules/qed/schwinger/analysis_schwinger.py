658daeb68a96ad07a8717fbf9316f6b10ac1eae0
Neil Zaim <neilzaim@hotmail.com>
2020-05-08 21:01:06 +0200

    Combine all analysis scripts into a single one

--- Examples/Modules/qed/schwinger/analysis_schwinger.py
+++ Examples/Modules/qed/schwinger/analysis_schwinger.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 # Copyright 2020 Luca Fedeli, Neil Zaim
 #
 # This file is part of WarpX.
@@ -11,6 +13,8 @@
 
 import yt
 import numpy as np
+import sys
+import re
 
 # define some parameters
 
@@ -23,6 +27,33 @@ E_S = m_e**2*c**3/e/hbar # Schwinger field
 dV = (1.e-6)**3 # total simulation volume
 dt = 2.407291502e-16
 filename = sys.argv[1]
+
+Ex_test = 0.
+Ey_test = 0.
+Ez_test = 0.
+Bx_test = 0.
+By_test = 0.
+Bz_test = 0.
+
+# Find which test we are doing
+test_number = re.search( 'qed_schwinger([1234])', filename ).group(1)
+if test_number == '1':
+    Ex_test = 1.e16
+    Bx_test = 16792888.570516706
+    By_test = 5256650.141557486
+    Bz_test = 18363530.799561853
+elif test_number == '2':
+    Ex_test = 1.e18
+    Bx_test = 1679288857.0516706
+    By_test = 525665014.1557486
+    Bz_test = 1836353079.9561853
+elif test_number == '3':
+    Ey_test = 1.0321239524474501e+17
+elif test_number == '4':
+    Ez_test = 2.5e+20
+    By_test = 833910154604.3563
+else:
+    assert(False)
 
 def calculate_rate(Ex,Ey,Ez,Bx,By,Bz):
 ## Calculate theoretical pair production rate from EM field value
@@ -70,3 +101,4 @@ def do_analysis(Ex,Ey,Ez,Bx,By,Bz):
         # 5 sigma test that has an intrisic probability to fail of 1 over ~2 millions
         assert(np.abs(np.sum(ele_data)-expected_total_physical_pairs_created)<5*std_total_physical_pairs_created)
 
+do_analysis(Ex_test, Ey_test, Ez_test, Bx_test, By_test, Bz_test)
