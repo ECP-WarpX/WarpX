a5f9f53618572fb5d0e018963eabef7ca4807a28
MaxThevenet <mthevenet@lbl.gov>
2020-05-27 14:27:11 -0700

    eol

--- Source/FieldSolver/SpectralSolver/WrapCuFFT.cpp
+++ Source/FieldSolver/SpectralSolver/WrapCuFFT.cpp
@@ -66,7 +66,7 @@ namespace AnyFFT
                 cufftErrorToString(result) << "\n";
         }
     }
-    
+
     /** \brief This method converts a cufftResult
      * into the corresponding string
      *
