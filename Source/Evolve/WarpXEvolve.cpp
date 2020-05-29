36c337d1dd00dc4945802a4a4da6e23aee86355e
Revathi  Jambunathan <41089244+RevathiJambunathan@users.noreply.github.com>
2020-05-29 10:53:33 -0700

    Macroscopic EvolveE with constant properties (#991)
    * Adding macro-E Push and new file
    
    * Add macroEvolveE, call it, and include algo selection in utils
    
    * fix eol
    
    * Fixing bug in macroE for sigma method 1
    
    * changing MacroEvolveE to MacroscopicEvolveE
    
    * add class for macroscopicproperties and an object in WarpX class
    
    * fix eol
    
    * adding templated ckc call with comment that EvolveE is same for yee and ckc
    
    * add header file pointing to ckc algorithm
    
    * adding obejct m_macroscopic_properties to access sigma,eps,mu
    
    * some cleaning
    
    * Adding comments
    
    * adding documentation
    
    * spelling wandroff to wendroff
    
    * fixing eol
    
    * eol
    
    * const in the right place. Thanks bot!
    
    * profiler for macroscopic evolveE
    
    * re-introduced deleted line when fixing conflict to declare templated EvolveBPMLCartesian function
    
    * fixing if-else statement for RZ in MAcroscopicEvolveE.cpp
    
    * adding suggestions and corrections made in PR review.
    
    * fixing grammar in docs
    
    * fix eol whitespace
    
    * fix typo

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -411,7 +411,15 @@ WarpX::OneStep_nosub (Real cur_time)
         EvolveB(0.5*dt[0]); // We now have B^{n+1/2}
 
         FillBoundaryB(guard_cells.ng_FieldSolver, IntVect::TheZeroVector());
-        EvolveE(dt[0]); // We now have E^{n+1}
+        if (WarpX::em_solver_medium == MediumForEM::Vacuum) {
+            // vacuum medium
+            EvolveE(dt[0]); // We now have E^{n+1}
+        } else if (WarpX::em_solver_medium == MediumForEM::Macroscopic) {
+            // macroscopic medium
+            MacroscopicEvolveE(dt[0]); // We now have E^{n+1}
+        } else {
+            amrex::Abort(" Medium for EM is unknown \n");
+        }
 
         FillBoundaryE(guard_cells.ng_FieldSolver, IntVect::TheZeroVector());
         EvolveF(0.5*dt[0], DtType::SecondHalf);
