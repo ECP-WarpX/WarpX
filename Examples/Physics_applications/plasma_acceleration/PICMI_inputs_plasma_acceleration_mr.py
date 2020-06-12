17aa449daa38ae05ef39097c329b6b61248bc029
Andrew Myers <atmyers2@gmail.com>
2020-06-12 13:48:35 -0700

    turn off field IO for a few python tests I missed

--- Examples/Physics_applications/plasma_acceleration/PICMI_inputs_plasma_acceleration_mr.py
+++ Examples/Physics_applications/plasma_acceleration/PICMI_inputs_plasma_acceleration_mr.py
@@ -67,7 +67,7 @@ field_diag = picmi.FieldDiagnostic(name = 'diag1',
 part_diag = picmi.ParticleDiagnostic(name = 'diag1',
                                      period = 2,
                                      species = [beam, plasma],
-                                     data_list = ['ux', 'uy', 'uz', 'weighting', 'Ex', 'Ey', 'Ez'])
+                                     data_list = ['ux', 'uy', 'uz', 'weighting')
 
 sim.add_diagnostic(field_diag)
 sim.add_diagnostic(part_diag)
