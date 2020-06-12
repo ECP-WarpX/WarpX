ec841df2f4e3033546aaa3f3f018b6d079860715
Revathi  Jambunathan <41089244+RevathiJambunathan@users.noreply.github.com>
2020-06-12 15:17:39 -0700

    Apply suggestions from code review
    add pr suggestions
    
    Co-authored-by: MaxThevenet <mthevenet@lbl.gov>

--- Source/Diagnostics/ComputeDiagFunctors/BackTransformFunctor.cpp
+++ Source/Diagnostics/ComputeDiagFunctors/BackTransformFunctor.cpp
@@ -1,8 +1,8 @@
 #include "BackTransformFunctor.H"
 
-using namespace amrex;
-
 BackTransformFunctor::BackTransformFunctor(amrex::MultiFab const * mf_src, int lev,
                                            int ncomp, amrex::IntVect crse_ratio)
     : ComputeDiagFunctor(ncomp, crse_ratio), m_mf_src(mf_src), m_lev(lev)
-{}
+{
+    // Get the right slice of each field in the CC MultiFab, transform it and store it in the output.
+}
