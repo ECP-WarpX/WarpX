6fbd0cfceb08a47cbf7406964618c08416ea15e9
Edoardo Zoni <ezoni@lbl.gov>
2020-06-17 14:52:39 -0700

    Small clean-up

--- Source/Diagnostics/ComputeDiagFunctors/RhoFunctor.cpp
+++ Source/Diagnostics/ComputeDiagFunctors/RhoFunctor.cpp
@@ -30,14 +30,14 @@ RhoFunctor::operator() ( amrex::MultiFab& mf_dst, const int dcomp ) const
     const amrex::BoxArray& ba = amrex::convert( warpx.boxArray(m_lev), amrex::IntVect::TheUnitVector() );
     std::unique_ptr<amrex::MultiFab> rho_ptr;
     rho_ptr = std::unique_ptr<amrex::MultiFab>
-              (new amrex::MultiFab( ba, warpx.DistributionMap(m_lev), 2*warpx.n_rz_azimuthal_modes-1, ng) );
+              (new amrex::MultiFab( ba, warpx.DistributionMap(m_lev), 2*warpx.n_rz_azimuthal_modes-1, ng ));
     rho_ptr = mypc.GetChargeDensity( m_lev );
     amrex::MultiFab& rho = *rho_ptr;
 
 #ifdef WARPX_DIM_RZ
     if (m_convertRZmodes2cartesian) {
         // In cylindrical geometry, sum real part of all modes of rho in
-        // temporary multifab mf_dst_stag, and cell-center it to mf_dst
+        // temporary MultiFab mf_dst_stag, and cell-center it to mf_dst
         AMREX_ALWAYS_ASSERT_WITH_MESSAGE(
             nComp()==1,
             "The RZ averaging over modes must write into 1 single component");
@@ -46,14 +46,14 @@ RhoFunctor::operator() ( amrex::MultiFab& mf_dst, const int dcomp ) const
         amrex::MultiFab::Copy( mf_dst_stag, rho, 0, 0, 1, rho.nGrowVect() );
         for (int ic=1 ; ic < rho.nComp() ; ic += 2) {
             // Real part of all modes > 0
-            amrex::MultiFab::Add(mf_dst_stag, rho, ic, 0, 1, rho.nGrowVect() );
+            amrex::MultiFab::Add( mf_dst_stag, rho, ic, 0, 1, rho.nGrowVect() );
         }
         CoarsenIO::Coarsen( mf_dst, mf_dst_stag, dcomp, 0, nComp(), 0, m_crse_ratio );
     } else {
         CoarsenIO::Coarsen( mf_dst, rho, dcomp, 0, nComp(), 0, m_crse_ratio );
     }
 #else
-    // In cartesian geometry, coarsen and interpolate from simulation MultiFab rho
+    // In Cartesian geometry, coarsen and interpolate from temporary MultiFab rho
     // to output diagnostic MultiFab mf_dst
     CoarsenIO::Coarsen( mf_dst, rho, dcomp, 0, nComp(), 0, m_crse_ratio );
 #endif
