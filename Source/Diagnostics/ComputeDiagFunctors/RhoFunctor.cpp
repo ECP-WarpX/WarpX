0b1b54a5939e7a6f6fcf802454a7b8ef1546ad1c
Edoardo Zoni <ezoni@lbl.gov>
2020-06-18 08:32:03 -0700

    Simplify use of unique pointer to rho

--- Source/Diagnostics/ComputeDiagFunctors/RhoFunctor.cpp
+++ Source/Diagnostics/ComputeDiagFunctors/RhoFunctor.cpp
@@ -25,14 +25,8 @@ RhoFunctor::operator() ( amrex::MultiFab& mf_dst, const int dcomp ) const
     // the operations performed in CoarsenIO::Coarsen
     constexpr int ng = 1;
 
-    // For staggered and nodal calculations, rho is computed on the nodes:
-    // thus, the temporary MultiFab rho is generated with nodal index type
-    const amrex::BoxArray& ba = amrex::convert( warpx.boxArray(m_lev), amrex::IntVect::TheUnitVector() );
-    std::unique_ptr<amrex::MultiFab> rho_ptr;
-    rho_ptr = std::unique_ptr<amrex::MultiFab>
-              (new amrex::MultiFab( ba, warpx.DistributionMap(m_lev), 2*warpx.n_rz_azimuthal_modes-1, ng ));
-    rho_ptr = mypc.GetChargeDensity( m_lev );
-    amrex::MultiFab& rho = *rho_ptr;
+    // Deposit charge density
+    std::unique_ptr<amrex::MultiFab> rho = mypc.GetChargeDensity( m_lev );
 
 #ifdef WARPX_DIM_RZ
     if (m_convertRZmodes2cartesian) {
@@ -41,20 +35,20 @@ RhoFunctor::operator() ( amrex::MultiFab& mf_dst, const int dcomp ) const
         AMREX_ALWAYS_ASSERT_WITH_MESSAGE(
             nComp()==1,
             "The RZ averaging over modes must write into 1 single component");
-        amrex::MultiFab mf_dst_stag( rho.boxArray(), warpx.DistributionMap(m_lev), 1, rho.nGrowVect() );
+        amrex::MultiFab mf_dst_stag( rho->boxArray(), warpx.DistributionMap(m_lev), 1, rho->nGrowVect() );
         // Mode 0
-        amrex::MultiFab::Copy( mf_dst_stag, rho, 0, 0, 1, rho.nGrowVect() );
-        for (int ic=1 ; ic < rho.nComp() ; ic += 2) {
+        amrex::MultiFab::Copy( mf_dst_stag, *rho, 0, 0, 1, rho->nGrowVect() );
+        for (int ic=1 ; ic < rho->nComp() ; ic += 2) {
             // Real part of all modes > 0
-            amrex::MultiFab::Add( mf_dst_stag, rho, ic, 0, 1, rho.nGrowVect() );
+            amrex::MultiFab::Add( mf_dst_stag, *rho, ic, 0, 1, rho->nGrowVect() );
         }
         CoarsenIO::Coarsen( mf_dst, mf_dst_stag, dcomp, 0, nComp(), 0, m_crse_ratio );
     } else {
-        CoarsenIO::Coarsen( mf_dst, rho, dcomp, 0, nComp(), 0, m_crse_ratio );
+        CoarsenIO::Coarsen( mf_dst, *rho, dcomp, 0, nComp(), 0, m_crse_ratio );
     }
 #else
     // In Cartesian geometry, coarsen and interpolate from temporary MultiFab rho
     // to output diagnostic MultiFab mf_dst
-    CoarsenIO::Coarsen( mf_dst, rho, dcomp, 0, nComp(), 0, m_crse_ratio );
+    CoarsenIO::Coarsen( mf_dst, *rho, dcomp, 0, nComp(), 0, m_crse_ratio );
 #endif
 }
