0e071c5b0970622f091d2915603f926a66ee42ae
NeilZaim <49716072+NeilZaim@users.noreply.github.com>
2020-06-22 23:03:21 +0200

    Use new github picsar repo (#1111)
    * Use new picsar repo on github
    
    * Add new repo in PICSAR.cmake

--- cmake/dependencies/PICSAR.cmake
+++ cmake/dependencies/PICSAR.cmake
@@ -40,7 +40,7 @@ endfunction()
 
 if(WarpX_QED)
     option(WarpX_picsar_internal   "Download & build PICSAR" ON)
-    set(WarpX_picsar_repo "https://bitbucket.org/berkeleylab/picsar.git"
+    set(WarpX_picsar_repo "https://github.com/ECP-WarpX/picsar.git"
         CACHE STRING
         "Repository URI to pull and build PICSAR from if(WarpX_picsar_internal)")
     set(WarpX_picsar_branch "master"
