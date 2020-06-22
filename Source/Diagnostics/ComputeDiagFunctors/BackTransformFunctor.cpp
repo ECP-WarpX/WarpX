4159db94a4458bc213f1ce4fce691ec08dcdcf8e
RevathiJambunathan <revanathan@gmail.com>
2020-06-22 14:20:21 -0700

    add empty operator

--- Source/Diagnostics/ComputeDiagFunctors/BackTransformFunctor.cpp
+++ Source/Diagnostics/ComputeDiagFunctors/BackTransformFunctor.cpp
@@ -1,8 +1,15 @@
 #include "BackTransformFunctor.H"
 
-BackTransformFunctor::BackTransformFunctor(amrex::MultiFab const * mf_src, int lev,
+BackTransformFunctor::BackTransformFunctor (amrex::MultiFab const * mf_src, int lev,
                                            const int ncomp, const amrex::IntVect crse_ratio)
     : ComputeDiagFunctor(ncomp, crse_ratio), m_mf_src(mf_src), m_lev(lev)
 {
     // Get the right slice of each field in the CC MultiFab, transform it and store it in the output.
 }
+
+void
+BackTransformFunctor::operator ()(amrex::MultiFab& mf_dst, int dcomp) const
+{
+    amrex::Print() << " we are in BTFunctor operator \n";
+}
+
