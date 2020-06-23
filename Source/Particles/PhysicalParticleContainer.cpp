7eac39fc54a57e27c76bae78c574f98da8310bc2
LDAmorim <LDianaAmorim@lbl.gov>
2020-06-23 15:45:35 -0700

    Moved doxygen to header file for consistency

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -464,16 +464,6 @@ PhysicalParticleContainer::AddParticles (int lev)
     }
 }
 
-/**
- * Create new macroparticles for this species, with a fixed
- * number of particles per cell (in the cells of `part_realbox`).
- * The new particles are only created inside the intersection of `part_realbox`
- * with the local grid for the current proc.
- * @param lev the index of the refinement level
- * @param part_realbox the box in which new particles should be created
- * (this box should correspond to an integer number of cells in each direction,
- * but its boundaries need not be aligned with the actual cells of the simulation)
- */
 void
 PhysicalParticleContainer::AddPlasma (int lev, RealBox part_realbox)
 {
