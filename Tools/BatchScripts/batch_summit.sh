bc497ec745003e49a4882ea34de78a9da497253e
Axel Huebl <axel.huebl@plasma.ninja>
2020-06-22 19:21:31 -0700

    Summit: no `-n` in jsrun
    The calculation of `-n` does not seem to work anymore. Luckily, the
    parameter seems to be automatically taken by some LFS setting in the
    job.

--- Tools/BatchScripts/batch_summit.sh
+++ Tools/BatchScripts/batch_summit.sh
@@ -19,5 +19,4 @@ module load cuda
 omp=1
 export OMP_NUM_THREADS=${omp}
 
-num_nodes=$(( $(printf '%s\n' ${LSB_HOSTS} | sort -u | wc -l) - 1 ))
-jsrun -n ${num_nodes} -a 6 -g 6 -c 6 --bind=packed:${omp} <path/to/executable> <input file> > output.txt
+jsrun -a 6 -g 6 -c 6 --bind=packed:${omp} <path/to/executable> <input file> > output.txt
