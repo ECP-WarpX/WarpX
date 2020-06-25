7bb6ea7bb6ef891cc03b7d44c9dc03f83caf7aae
Axel Huebl <axel.huebl@plasma.ninja>
2020-06-25 09:26:55 -0700

    CMake: MPI, Asserts and Advanced AMReX (#1120)
    * CMake: WarpX_MPI
    
    Wrap the last user-facing AMReX option into a WarpX option. This
    reduces confusion and improves consistency further.
    
    * CMake: Assertations
    
    Enable AMReX' `ENABLE_ASSERTIONS` with build type `Debug` and allow
    to overwrite on the command line with `-DENABLE_ASSERTIONS=ON` for
    manual activation with other build types.
    
    * CMake: Advanced AMReX Options
    
    Declutter user-facing options further.
    
    * CMake: Toggle for CMAKE_BUILD_TYPE
    
    Add allowed options for toggling the `CMAKE_BUILD_TYPE`.
    
    * CMake: Floating Point Exceptions
    
    Enable AMReX' `ENABLE_FPE` with build type `Debug` and allow
    to overwrite on the command line with `-DENABLE_FPE=ON` for
    manual activation with other build types.

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -9,6 +9,14 @@ macro(find_amrex)
             set(ENABLE_CONDUIT ON CACHE INTERNAL "")
         endif()
 
+        if("${CMAKE_BUILD_TYPE}" MATCHES "Debug")
+            set(ENABLE_ASSERTIONS ON CACHE INTERNAL "")
+            set(ENABLE_FPE ON CACHE INTERNAL "")
+        else()
+            set(ENABLE_ASSERTIONS OFF CACHE INTERNAL "")
+            set(ENABLE_FPE OFF CACHE INTERNAL "")
+        endif()
+
         if(WarpX_COMPUTE STREQUAL CUDA)
             set(ENABLE_ACC   OFF CACHE INTERNAL "")
             set(ENABLE_CUDA  ON  CACHE INTERNAL "")
@@ -26,6 +34,12 @@ macro(find_amrex)
             set(ENABLE_OMP   OFF CACHE INTERNAL "")
         endif()
 
+        if(WarpX_MPI)
+            set(ENABLE_MPI ON CACHE INTERNAL "")
+        else()
+            set(ENABLE_MPI OFF CACHE INTERNAL "")
+        endif()
+
         if(WarpX_PRECISION STREQUAL "double")
             set(ENABLE_DP ON CACHE INTERNAL "")
             set(ENABLE_DP_PARTICLES ON CACHE INTERNAL "")
@@ -40,8 +54,6 @@ macro(find_amrex)
         set(ENABLE_PARTICLES ON CACHE INTERNAL "")
         set(ENABLE_TINY_PROFILE ON CACHE INTERNAL "")
 
-        # ENABLE_ASCENT
-        # ENABLE_CONDUIT
         # ENABLE_SENSEI_IN_SITU
         # we'll need this for Python bindings
         #set(ENABLE_PIC ON CACHE INTERNAL "")
@@ -80,17 +92,30 @@ macro(find_amrex)
         # AMReX options not relevant to WarpX users
         mark_as_advanced(AMREX_BUILD_DATETIME)
         mark_as_advanced(DIM)
+        mark_as_advanced(ENABLE_ACC)
+        mark_as_advanced(ENABLE_ASSERTIONS)
+        mark_as_advanced(ENABLE_AMRDATA)
+        mark_as_advanced(ENABLE_BACKTRACE) # BT files are written anyway
+        mark_as_advanced(ENABLE_BASE_PROFILE) # mutually exclusive to tiny profile
+        mark_as_advanced(ENABLE_CONDUIT)
+        mark_as_advanced(ENABLE_CUDA)
         mark_as_advanced(ENABLE_DP)
         mark_as_advanced(ENABLE_DP_PARTICLES)
+        mark_as_advanced(ENABLE_DPCPP)
+        mark_as_advanced(ENABLE_EB)
+        mark_as_advanced(ENABLE_FPE)
         mark_as_advanced(ENABLE_FORTRAN)
         mark_as_advanced(ENABLE_FORTRAN_INTERFACES)
+        mark_as_advanced(ENABLE_HDF5)  # we do HDF5 I/O (and more) via openPMD-api
         mark_as_advanced(ENABLE_LINEAR_SOLVERS)
+        mark_as_advanced(ENABLE_MEM_PROFILE)
+        mark_as_advanced(ENABLE_MPI)
+        mark_as_advanced(ENABLE_OMP)
         mark_as_advanced(ENABLE_PIC)
+        mark_as_advanced(ENABLE_SENSEI_INSITU)
+        mark_as_advanced(ENABLE_TINY_PROFILE)
+        mark_as_advanced(TP_PROFILE)
         mark_as_advanced(USE_XSDK_DEFAULTS)
-        # ENABLE_ACC
-        # ENABLE_CUDA
-        # ENABLE_DPCPP
-        # ENABLE_OMP
 
         message(STATUS "AMReX: Using INTERNAL version '${AMREX_PKG_VERSION}' (${AMREX_GIT_VERSION})")
     else()
