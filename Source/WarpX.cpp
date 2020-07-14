ad6d242e490690f183dc58aa15389a6f3bf00261
Daniel Belkin <dbelkin1@swarthmore.edu>
2020-07-14 13:21:50 -0700

    Adding new input parameter

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -116,6 +116,7 @@ int WarpX::n_field_gather_buffer = -1;
 int WarpX::n_current_deposition_buffer = -1;
 
 int WarpX::do_nodal = false;
+std::string WarpX::stagger_mode = "yee";
 
 #ifdef AMREX_USE_GPU
 bool WarpX::do_device_synchronize_before_profile = true;
@@ -561,7 +562,7 @@ WarpX::ReadParameters ()
         pp.query("stagger_mode", stagger_mode);
         if (do_nodal) stagger_mode = "nodal";
         if (stagger_mode == "nodal") do_nodal = true; // enforce consistency
-        
+
         // Use same shape factors in all directions, for gathering
         if (do_nodal) l_lower_order_in_v = false;
 
