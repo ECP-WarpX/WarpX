0eb7c7ff20f8c5bc2e6730f0eb8a2932c17d5741
Daniel Belkin <dbelkin1@swarthmore.edu>
2020-07-14 21:07:03 -0700

    Remove whitespace

--- Source/FieldSolver/SpectralSolver/SpectralKSpace.cpp
+++ Source/FieldSolver/SpectralSolver/SpectralKSpace.cpp
@@ -249,37 +249,37 @@ SpectralKSpace::getModifiedKComponent( const DistributionMapping& dm,
  * `nodal` indicates whether this finite-difference approximation is
  * taken on a nodal grid or a staggered grid.
  */
- Vector<Real>
- getFonbergStencilCoefficients( const int n_order, const bool nodal )
- {
-     AMREX_ALWAYS_ASSERT_WITH_MESSAGE( n_order%2 == 0,
-                                       "n_order should be even.");
-     const int m = n_order/2;
-     Vector<Real> coefs;
-     coefs.resize( m+1 );
+Vector<Real>
+getFonbergStencilCoefficients( const int n_order, const bool nodal )
+{
+   AMREX_ALWAYS_ASSERT_WITH_MESSAGE( n_order%2 == 0,
+                                     "n_order should be even.");
+   const int m = n_order/2;
+   Vector<Real> coefs;
+   coefs.resize( m+1 );
 
-     // Note: there are closed-form formula for these coefficients,
-     // but they result in an overflow when evaluated numerically.
-     // One way to avoid the overflow is to calculate the coefficients
-     // by recurrence.
+   // Note: there are closed-form formula for these coefficients,
+   // but they result in an overflow when evaluated numerically.
+   // One way to avoid the overflow is to calculate the coefficients
+   // by recurrence.
 
-     // Coefficients for nodal (a.k.a. centered) finite-difference
-     if (nodal == true) {
-         coefs[0] = -2.; // First coefficient
-         for (int n=1; n<m+1; n++){ // Get the other coefficients by recurrence
-             coefs[n] = - (m+1-n)*1./(m+n)*coefs[n-1];
-         }
-     }
-     // Coefficients for staggered finite-difference
-     else {
-         Real prod = 1.;
-         for (int k=1; k<m+1; k++){
-             prod *= (m+k)*1./(4*k);
-         }
-         coefs[0] = 4*m*prod*prod; // First coefficient
-         for (int n=1; n<m+1; n++){ // Get the other coefficients by recurrence
-             coefs[n] = - ((2*n-3)*(m+1-n))*1./((2*n-1)*(m-1+n))*coefs[n-1];
-         }
-     }
-     return coefs;
- }
+   // Coefficients for nodal (a.k.a. centered) finite-difference
+   if (nodal == true) {
+       coefs[0] = -2.; // First coefficient
+       for (int n=1; n<m+1; n++){ // Get the other coefficients by recurrence
+           coefs[n] = - (m+1-n)*1./(m+n)*coefs[n-1];
+       }
+   }
+   // Coefficients for staggered finite-difference
+   else {
+       Real prod = 1.;
+       for (int k=1; k<m+1; k++){
+           prod *= (m+k)*1./(4*k);
+       }
+       coefs[0] = 4*m*prod*prod; // First coefficient
+       for (int n=1; n<m+1; n++){ // Get the other coefficients by recurrence
+           coefs[n] = - ((2*n-3)*(m+1-n))*1./((2*n-1)*(m-1+n))*coefs[n-1];
+       }
+   }
+   return coefs;
+}
