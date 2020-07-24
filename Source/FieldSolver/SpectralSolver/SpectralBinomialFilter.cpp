7e8642889516b2b2172f57d5ec214aef17e8e371
David Grote <grote1@llnl.gov>
2020-07-23 18:25:21 -0700

    Added SpectralBinomialFilter::InitFilterArray

--- Source/FieldSolver/SpectralSolver/SpectralBinomialFilter.cpp
+++ Source/FieldSolver/SpectralSolver/SpectralBinomialFilter.cpp
@@ -12,27 +12,38 @@
 
 using namespace amrex::literals;
 
+/* \brief Initialize the general filter array */
+void
+SpectralBinomialFilter::InitFilterArray (HankelTransform::RealVector const & kvec,
+                                         amrex::Real const dels,
+                                         int const npasses,
+                                         bool const compensation,
+                                         KFilterArray & filter)
+{
+
+    filter.resize(kvec.size());
+
+    for (int i=0 ; i < kvec.size() ; i++) {
+        amrex::Real const ss = std::sin(0.5_rt*kvec[i]*dels);
+        amrex::Real const ss2 = ss*ss;
+        amrex::Real filt = std::pow(1._rt - ss2, npasses);
+        if (compensation) {
+            filt *= (1._rt + npasses*ss2);
+        }
+        filter[i] = filt;
+    }
+
+}
+
 /* \brief Initialize the radial filter array */
 void
-SpectralBinomialFilter::InitFilterArrayR (HankelTransform::RealVector const & kr,
+SpectralBinomialFilter::InitFilterArrayR (RealKVector const & kr,
                                           amrex::Real const dr,
                                           int const npasses,
                                           bool const compensation)
 {
-
     // Note that this includes the kr values for all modes
-    filter_r.resize(kr.size());
-
-    for (int i=0 ; i < kr.size() ; i++) {
-        amrex::Real const sr = std::sin(0.5_rt*kr[i]*dr);
-        amrex::Real const sr2 = sr*sr;
-        amrex::Real filt_r = std::pow(1._rt - sr2, npasses);
-        if (compensation) {
-            filt_r *= (1._rt + npasses*sr2);
-        }
-        filter_r[i] = filt_r;
-    }
-
+    InitFilterArray(kr, dr, npasses, compensation, filter_r);
 }
 
 /* \brief Initialize the longitudinal filter array */
@@ -42,16 +53,5 @@ SpectralBinomialFilter::InitFilterArrayZ (RealKVector const & kz,
                                           int const npasses,
                                           bool const compensation)
 {
-
-    filter_z.resize(kz.size());
-
-    for (int i=0 ; i < kz.size() ; i++) {
-        amrex::Real const sz = std::sin(0.5_rt*kz[i]*dz);
-        amrex::Real const sz2 = sz*sz;
-        amrex::Real filt_z = std::pow(1._rt - sz2, npasses);
-        if (compensation) {
-            filt_z *= (1._rt + npasses*sz2);
-        }
-        filter_z[i] = filt_z;
-    }
+    InitFilterArray(kz, dz, npasses, compensation, filter_z);
 }
