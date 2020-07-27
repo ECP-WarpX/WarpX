59f74a99104b2919ab2218598eb178f84e40ccfe
Luca Fedeli <luca.fedeli@cea.fr>
2020-07-27 15:58:43 +0200

    changed position of evolution of optical depth

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -1886,14 +1886,6 @@ PhysicalParticleContainer::PushPX (WarpXParIter& pti,
 
         scaleFields(xp, yp, zp, Exp, Eyp, Ezp, Bxp, Byp, Bzp);
 
-#ifdef WARPX_QED
-    if (local_has_quantum_sync) {
-        evolve_opt(ux[ip], uy[ip], uz[ip],
-                   Exp, Eyp, Ezp,Bxp, Byp, Bzp,
-                   dt, p_optical_depth_QSR[ip]);
-    }
-#endif
-
         doParticlePush(getPosition, setPosition, copyAttribs, ip,
                        ux[ip+offset], uy[ip+offset], uz[ip+offset],
                        Exp, Eyp, Ezp, Bxp, Byp, Bzp,
@@ -1904,6 +1896,15 @@ PhysicalParticleContainer::PushPX (WarpXParIter& pti,
                        t_chi_max,
 #endif
                        dt);
+
+#ifdef WARPX_QED
+    if (local_has_quantum_sync) {
+        evolve_opt(ux[ip], uy[ip], uz[ip],
+                   Exp, Eyp, Ezp,Bxp, Byp, Bzp,
+                   dt, p_optical_depth_QSR[ip]);
+    }
+#endif
+
     });
 }
 
