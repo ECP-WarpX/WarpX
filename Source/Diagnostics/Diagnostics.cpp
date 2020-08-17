f6059b26eda595b4c17a56be8a70cb6de33039d9
MaxThevenet <mthevenet@lbl.gov>
2020-08-17 15:44:04 +0200

    fix performance IO automated test (#1248)

--- Source/Diagnostics/Diagnostics.cpp
+++ Source/Diagnostics/Diagnostics.cpp
@@ -212,6 +212,7 @@ Diagnostics::ComputeAndPack ()
 void
 Diagnostics::FilterComputePackFlush (int step, bool force_flush)
 {
+    WARPX_PROFILE("Diagnostics::FilterComputePackFlush()");
     if ( DoComputeAndPack (step, force_flush) ) {
         ComputeAndPack();
 
