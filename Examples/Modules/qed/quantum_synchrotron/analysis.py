e73a21ce247702c3869d56466c2fede810e0aa91
Luca Fedeli <luca.fedeli.88@gmail.com>
2020-09-18 17:02:06 +0200

    Update Examples/Modules/qed/quantum_synchrotron/analysis.py
    Co-authored-by: NeilZaim <49716072+NeilZaim@users.noreply.github.com>

--- Examples/Modules/qed/quantum_synchrotron/analysis.py
+++ Examples/Modules/qed/quantum_synchrotron/analysis.py
@@ -25,7 +25,7 @@ import checksumAPI
 # Specifically the script checks that:
 #
 # - The expected number of generated photons n_phot is in agreement with theory
-#   (the maximum tolerated error is 6*sqrt(n_phot)
+#   (the maximum tolerated error is 5*sqrt(n_phot)
 # - The weight of the generated particles is equal to the weight of the photon
 # - The generated particles are emitted in the right direction
 # - The energy distribution of the generated particles is in agreement with theory
