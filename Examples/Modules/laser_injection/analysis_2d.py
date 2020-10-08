3eb4a91ace43da370d9974d97eaa7e03ec8325a7
Luca Fedeli <luca.fedeli.88@gmail.com>
2020-10-08 14:02:13 +0200

    correct typo in documentation

--- Examples/Modules/laser_injection/analysis_2d.py
+++ Examples/Modules/laser_injection/analysis_2d.py
@@ -9,7 +9,7 @@
 
 # This file is part of the WarpX automated test suite. Its purpose is to test the
 # injection of a Gaussian laser pulse from an antenna in a 2D simulation.
-# In order to avoid priviledged directions, the laser is injected at
+# In order to avoid privileged directions, the laser is injected at
 # ~ 27° with respect to the x axis. Moreover the polarization axis is neither
 # parallel nor perpendicular to the xz plane. Finally moving window along the
 # x axis is enabled.
