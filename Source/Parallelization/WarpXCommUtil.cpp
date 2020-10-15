cc9176b794b9577ba6885150ecd5adba6a28a81c
Andrew Myers <atmyers2@gmail.com>
2020-10-14 17:42:48 -0700

    don't override valid cells

--- Source/Parallelization/WarpXCommUtil.cpp
+++ Source/Parallelization/WarpXCommUtil.cpp
@@ -12,6 +12,8 @@ void ParallelCopy (amrex::MultiFab&            dst,
                    const amrex::Periodicity&   period,
                    amrex::FabArrayBase::CpOp   op)
 {
+    BL_PROFILE("WarpXCommUtil::ParallelCopy");
+
     if (WarpX::do_single_precision_comms)
     {
         amrex::FabArray<amrex::BaseFab<comm_float_type> > src_tmp(src.boxArray(),
@@ -27,10 +29,15 @@ void ParallelCopy (amrex::MultiFab&            dst,
 
         mixedCopy(dst_tmp, dst, 0, 0, dst.nComp(), dst.nGrowVect());
 
+        amrex::MultiFab orig_dst(dst.boxArray(), dst.DistributionMap(),
+                                 dst.nComp(), dst.nGrowVect());
+        amrex::MultiFab::Copy(orig_dst, dst, 0, 0, dst.nComp(), dst.nGrowVect());
+
         dst_tmp.ParallelCopy(src_tmp, src_comp, dst_comp, num_comp,
                              src_nghost, dst_nghost, period, op);
 
         mixedCopy(dst, dst_tmp, 0, 0, dst.nComp(), dst_nghost);
+        amrex::MultiFab::Copy(dst, orig_dst, 0, 0, dst.nComp(), 0);
     }
     else
     {
@@ -53,18 +60,24 @@ void ParallelAdd (amrex::MultiFab&            dst,
 
 void FillBoundary (amrex::MultiFab& mf, const amrex::Periodicity& period)
 {
+    BL_PROFILE("WarpXCommUtil::FillBoundary");
+
     if (WarpX::do_single_precision_comms)
     {
         amrex::FabArray<amrex::BaseFab<comm_float_type> > mf_tmp(mf.boxArray(),
-                                                            mf.DistributionMap(),
-                                                            mf.nComp(),
-                                                            mf.nGrowVect());
+                                                                 mf.DistributionMap(),
+                                                                 mf.nComp(),
+                                                                 mf.nGrowVect());
+
+        amrex::MultiFab orig(mf.boxArray(), mf.DistributionMap(), mf.nComp(), mf.nGrowVect());
+        amrex::MultiFab::Copy(orig, mf, 0, 0, mf.nComp(), mf.nGrowVect());
 
         mixedCopy(mf_tmp, mf, 0, 0, mf.nComp(), mf.nGrowVect());
 
         mf_tmp.FillBoundary(period);
 
         mixedCopy(mf, mf_tmp, 0, 0, mf.nComp(), mf.nGrowVect());
+        amrex::MultiFab::Copy(mf, orig, 0, 0, mf.nComp(), 0);
     }
     else
     {
@@ -76,6 +89,8 @@ void FillBoundary (amrex::MultiFab&          mf,
                    amrex::IntVect            ng,
                    const amrex::Periodicity& period)
 {
+    BL_PROFILE("WarpXCommUtil::FillBoundary");
+
     if (WarpX::do_single_precision_comms)
     {
         amrex::FabArray<amrex::BaseFab<comm_float_type> > mf_tmp(mf.boxArray(),
@@ -83,11 +98,15 @@ void FillBoundary (amrex::MultiFab&          mf,
                                                             mf.nComp(),
                                                             mf.nGrowVect());
 
+        amrex::MultiFab orig(mf.boxArray(), mf.DistributionMap(), mf.nComp(), mf.nGrowVect());
+        amrex::MultiFab::Copy(orig, mf, 0, 0, mf.nComp(), mf.nGrowVect());
+
         mixedCopy(mf_tmp, mf, 0, 0, mf.nComp(), mf.nGrowVect());
 
         mf_tmp.FillBoundary(ng, period);
 
         mixedCopy(mf, mf_tmp, 0, 0, mf.nComp(), mf.nGrowVect());
+        amrex::MultiFab::Copy(mf, orig, 0, 0, mf.nComp(), 0);
     }
     else
     {
@@ -117,10 +136,14 @@ void OverrideSync (amrex::MultiFab&          mf,
 
         mixedCopy(mf_tmp, mf, 0, 0, mf.nComp(), mf.nGrowVect());
 
+        amrex::MultiFab orig(mf.boxArray(), mf.DistributionMap(), mf.nComp(), mf.nGrowVect());
+        amrex::MultiFab::Copy(orig, mf, 0, 0, mf.nComp(), mf.nGrowVect());
+
         auto msk = mf.OwnerMask(period);
         amrex::OverrideSync(mf_tmp, *msk, period);
 
         mixedCopy(mf, mf_tmp, 0, 0, mf.nComp(), mf.nGrowVect());
+        amrex::MultiFab::Copy(mf, orig, 0, 0, mf.nComp(), 0);
     }
     else
     {
