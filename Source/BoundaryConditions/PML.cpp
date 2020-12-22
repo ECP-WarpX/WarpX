3cf0a25b1a70bd9202428206941ba6afe808f2a1
Edoardo Zoni <ezoni@lbl.gov>
2020-12-22 14:49:11 -0800

    Apply @RemiLehe's suggestions and simplify allocations

--- Source/BoundaryConditions/PML.cpp
+++ Source/BoundaryConditions/PML.cpp
@@ -495,24 +495,15 @@ PML::PML (const BoxArray& grid_ba, const DistributionMapping& /*grid_dm*/,
         ngf = ngFFT;
     }
 
-    if (do_dive_cleaning)
-    {
-        pml_E_fp[0] = std::make_unique<MultiFab>(amrex::convert( ba,
-            WarpX::GetInstance().getEfield_fp(0,0).ixType().toIntVect() ), dm, 3, nge );
-        pml_E_fp[1] = std::make_unique<MultiFab>(amrex::convert( ba,
-            WarpX::GetInstance().getEfield_fp(0,1).ixType().toIntVect() ), dm, 3, nge );
-        pml_E_fp[2] = std::make_unique<MultiFab>(amrex::convert( ba,
-            WarpX::GetInstance().getEfield_fp(0,2).ixType().toIntVect() ), dm, 3, nge );
-    }
-    else // diagonal field components (xx,yy,zz) not needed when do_dive_cleaning = false
-    {
-        pml_E_fp[0] = std::make_unique<MultiFab>(amrex::convert( ba,
-            WarpX::GetInstance().getEfield_fp(0,0).ixType().toIntVect() ), dm, 2, nge );
-        pml_E_fp[1] = std::make_unique<MultiFab>(amrex::convert( ba,
-            WarpX::GetInstance().getEfield_fp(0,1).ixType().toIntVect() ), dm, 2, nge );
-        pml_E_fp[2] = std::make_unique<MultiFab>(amrex::convert( ba,
-            WarpX::GetInstance().getEfield_fp(0,2).ixType().toIntVect() ), dm, 2, nge );
-    }
+    // Allocate diagonal components (xx,yy,zz) only with divergence cleaning
+    const int ncomp = (do_dive_cleaning) ? 3 : 2;
+
+    pml_E_fp[0] = std::make_unique<MultiFab>(amrex::convert( ba,
+        WarpX::GetInstance().getEfield_fp(0,0).ixType().toIntVect() ), dm, ncomp, nge );
+    pml_E_fp[1] = std::make_unique<MultiFab>(amrex::convert( ba,
+        WarpX::GetInstance().getEfield_fp(0,1).ixType().toIntVect() ), dm, ncomp, nge );
+    pml_E_fp[2] = std::make_unique<MultiFab>(amrex::convert( ba,
+        WarpX::GetInstance().getEfield_fp(0,2).ixType().toIntVect() ), dm, ncomp, nge );
 
     pml_B_fp[0] = std::make_unique<MultiFab>(amrex::convert( ba,
         WarpX::GetInstance().getBfield_fp(0,0).ixType().toIntVect() ), dm, 2, ngb );
@@ -520,7 +511,6 @@ PML::PML (const BoxArray& grid_ba, const DistributionMapping& /*grid_dm*/,
         WarpX::GetInstance().getBfield_fp(0,1).ixType().toIntVect() ), dm, 2, ngb );
     pml_B_fp[2] = std::make_unique<MultiFab>(amrex::convert( ba,
         WarpX::GetInstance().getBfield_fp(0,2).ixType().toIntVect() ), dm, 2, ngb );
-
 
     pml_E_fp[0]->setVal(0.0);
     pml_E_fp[1]->setVal(0.0);
@@ -535,6 +525,7 @@ PML::PML (const BoxArray& grid_ba, const DistributionMapping& /*grid_dm*/,
         WarpX::GetInstance().getcurrent_fp(0,1).ixType().toIntVect() ), dm, 1, ngb );
     pml_j_fp[2] = std::make_unique<MultiFab>(amrex::convert( ba,
         WarpX::GetInstance().getcurrent_fp(0,2).ixType().toIntVect() ), dm, 1, ngb );
+
     pml_j_fp[0]->setVal(0.0);
     pml_j_fp[1]->setVal(0.0);
     pml_j_fp[2]->setVal(0.0);
@@ -603,24 +594,12 @@ PML::PML (const BoxArray& grid_ba, const DistributionMapping& /*grid_dm*/,
             MakeBoxArray(*cgeom, grid_cba, ncell, do_pml_in_domain, do_pml_Lo, do_pml_Hi);
         DistributionMapping cdm{cba};
 
-        if (do_dive_cleaning)
-        {
-            pml_E_cp[0] = std::make_unique<MultiFab>(amrex::convert( cba,
-                WarpX::GetInstance().getEfield_cp(1,0).ixType().toIntVect() ), cdm, 3, nge );
-            pml_E_cp[1] = std::make_unique<MultiFab>(amrex::convert( cba,
-                WarpX::GetInstance().getEfield_cp(1,1).ixType().toIntVect() ), cdm, 3, nge );
-            pml_E_cp[2] = std::make_unique<MultiFab>(amrex::convert( cba,
-                WarpX::GetInstance().getEfield_cp(1,2).ixType().toIntVect() ), cdm, 3, nge );
-        }
-        else // diagonal field components (xx,yy,zz) not needed when do_dive_cleaning = false
-        {
-            pml_E_cp[0] = std::make_unique<MultiFab>(amrex::convert( cba,
-                WarpX::GetInstance().getEfield_cp(1,0).ixType().toIntVect() ), cdm, 2, nge );
-            pml_E_cp[1] = std::make_unique<MultiFab>(amrex::convert( cba,
-                WarpX::GetInstance().getEfield_cp(1,1).ixType().toIntVect() ), cdm, 2, nge );
-            pml_E_cp[2] = std::make_unique<MultiFab>(amrex::convert( cba,
-                WarpX::GetInstance().getEfield_cp(1,2).ixType().toIntVect() ), cdm, 2, nge );
-        }
+        pml_E_cp[0] = std::make_unique<MultiFab>(amrex::convert( cba,
+            WarpX::GetInstance().getEfield_cp(1,0).ixType().toIntVect() ), cdm, ncomp, nge );
+        pml_E_cp[1] = std::make_unique<MultiFab>(amrex::convert( cba,
+            WarpX::GetInstance().getEfield_cp(1,1).ixType().toIntVect() ), cdm, ncomp, nge );
+        pml_E_cp[2] = std::make_unique<MultiFab>(amrex::convert( cba,
+            WarpX::GetInstance().getEfield_cp(1,2).ixType().toIntVect() ), cdm, ncomp, nge );
 
         pml_B_cp[0] = std::make_unique<MultiFab>(amrex::convert( cba,
             WarpX::GetInstance().getBfield_cp(1,0).ixType().toIntVect() ), cdm, 2, ngb );
@@ -642,12 +621,14 @@ PML::PML (const BoxArray& grid_ba, const DistributionMapping& /*grid_dm*/,
             pml_F_cp->setVal(0.0);
 
         }
+
         pml_j_cp[0] = std::make_unique<MultiFab>(amrex::convert( cba,
             WarpX::GetInstance().getcurrent_cp(1,0).ixType().toIntVect() ), cdm, 1, ngb );
         pml_j_cp[1] = std::make_unique<MultiFab>(amrex::convert( cba,
             WarpX::GetInstance().getcurrent_cp(1,1).ixType().toIntVect() ), cdm, 1, ngb );
         pml_j_cp[2] = std::make_unique<MultiFab>(amrex::convert( cba,
             WarpX::GetInstance().getcurrent_cp(1,2).ixType().toIntVect() ), cdm, 1, ngb );
+
         pml_j_cp[0]->setVal(0.0);
         pml_j_cp[1]->setVal(0.0);
         pml_j_cp[2]->setVal(0.0);
