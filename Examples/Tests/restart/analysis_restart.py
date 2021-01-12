179aae4299093d1b6386955625665fa30a76dcc1
Yin-YinjianZhao <yinjianzhao@lbl.gov>
2021-01-12 14:50:43 -0700

    minor

--- Examples/Tests/restart/analysis_restart.py
+++ Examples/Tests/restart/analysis_restart.py
@@ -14,20 +14,19 @@ ds  = yt.load( filename )
 ad  = ds.all_data()
 xb  = ad['beam',     'particle_position_x'].to_ndarray()
 xe  = ad['plasma_e', 'particle_position_x'].to_ndarray()
-xp  = ad['plasma_p', 'particle_position_x'].to_ndarray()
 zb  = ad['beam',     'particle_position_z'].to_ndarray()
 ze  = ad['plasma_e', 'particle_position_z'].to_ndarray()
-zp  = ad['plasma_p', 'particle_position_z'].to_ndarray()
 
 filename = 'orig_restart_plt00010'
 ds  = yt.load( filename )
 ad  = ds.all_data()
 xb0 = ad['beam',     'particle_position_x'].to_ndarray()
 xe0 = ad['plasma_e', 'particle_position_x'].to_ndarray()
-xp0 = ad['plasma_p', 'particle_position_x'].to_ndarray()
 zb0 = ad['beam',     'particle_position_z'].to_ndarray()
 ze0 = ad['plasma_e', 'particle_position_z'].to_ndarray()
-zp0 = ad['plasma_p', 'particle_position_z'].to_ndarray()
+
+# The loops are because particle arrays may be reordered,
+# so the same index may not lead to the same particle.
 
 num = 0
 for i in range(len(xb)):
@@ -38,6 +37,39 @@ for i in range(len(xb)):
             num = num + 1
     if num == len(xb0):
         assert( num < len(xb0) )
+        num = 0
+
+num = 0
+for i in range(len(zb)):
+    for j in range(len(zb0)):
+        if abs(zb[i]-zb0[j]) < tolerance:
+            break
+        else:
+            num = num + 1
+    if num == len(zb0):
+        assert( num < len(zb0) )
+        num = 0
+
+num = 0
+for i in range(len(xe)):
+    for j in range(len(xe0)):
+        if abs(xe[i]-xe0[j]) < tolerance:
+            break
+        else:
+            num = num + 1
+    if num == len(xe0):
+        assert( num < len(xe0) )
+        num = 0
+
+num = 0
+for i in range(len(ze)):
+    for j in range(len(ze0)):
+        if abs(ze[i]-ze0[j]) < tolerance:
+            break
+        else:
+            num = num + 1
+    if num == len(ze0):
+        assert( num < len(ze0) )
         num = 0
 
 filename = sys.argv[1]
