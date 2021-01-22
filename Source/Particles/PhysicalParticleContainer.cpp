06282356c303fa47bce5a51bca540b8f4cd4576b
Neil Zaim <neilzaim@hotmail.com>
2021-01-22 19:50:38 +0100

    Remove unused lines about particle filters

--- Source/Particles/PhysicalParticleContainer.cpp
+++ Source/Particles/PhysicalParticleContainer.cpp
@@ -171,19 +171,6 @@ PhysicalParticleContainer::PhysicalParticleContainer (AmrCore* amr_core, int isp
     // Scale the velocity by the speed of light
     for (int i=0; i<3; i++) m_v_galilean[i] *= PhysConst::c;
 
-    // build filter functors
-    m_do_random_filter  = queryWithParser(pp, "random_fraction", m_random_fraction);
-    m_do_uniform_filter = pp.query("uniform_stride",  m_uniform_stride);
-    std::string buf;
-    m_do_parser_filter  = pp.query("plot_filter_function(t,x,y,z,ux,uy,uz)", buf);
-    if (m_do_parser_filter) {
-        std::string function_string = "";
-        Store_parserString(pp,"plot_filter_function(t,x,y,z,ux,uy,uz)",
-                           function_string);
-        m_particle_filter_parser = std::make_unique<ParserWrapper<7>>(
-            makeParser(function_string,{"t","x","y","z","ux","uy","uz"}));
-    }
-
 }
 
 PhysicalParticleContainer::PhysicalParticleContainer (AmrCore* amr_core)
