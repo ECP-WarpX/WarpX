a72c2076a0f0e9556e4d76bbe9ba4fa4378bccf8
Dave Grote <grote1@llnl.gov>
2021-02-25 14:45:15 -0800

    Added RZ Poison solver

--- Source/FieldSolver/ElectrostaticSolver.cpp
+++ Source/FieldSolver/ElectrostaticSolver.cpp
@@ -7,7 +7,11 @@
 
 #include <AMReX_ParallelDescriptor.H>
 #include <AMReX_MLMG.H>
+#ifdef WARPX_DIM_RZ
+#include <AMReX_MLNodeLaplacian.H>
+#else
 #include <AMReX_MLNodeTensorLaplacian.H>
+#endif
 #include <AMReX_REAL.H>
 
 #include <WarpX.H>
@@ -54,7 +58,8 @@ WarpX::AddSpaceChargeField (WarpXParticleContainer& pc)
 {
 
 #ifdef WARPX_DIM_RZ
-    amrex::Abort("The initialization of space-charge field has not yet been implemented in RZ geometry.");
+    AMREX_ALWAYS_ASSERT_WITH_MESSAGE(n_rz_azimuthal_modes == 1,
+                                     "Error: RZ electrostatic only implemented for a single mode");
 #endif
 
     // Allocate fields for charge and potential
@@ -96,7 +101,8 @@ WarpX::AddSpaceChargeFieldLabFrame ()
 {
 
 #ifdef WARPX_DIM_RZ
-    amrex::Abort("The calculation of space-charge field has not yet been implemented in RZ geometry.");
+    AMREX_ALWAYS_ASSERT_WITH_MESSAGE(n_rz_azimuthal_modes == 1,
+                                     "Error: RZ electrostatic only implemented for a single mode");
 #endif
 
     // Allocate fields for charge
@@ -124,6 +130,11 @@ WarpX::AddSpaceChargeFieldLabFrame ()
     for (int lev = 0; lev <= max_level; lev++) {
         ApplyFilterandSumBoundaryRho (lev, lev, *rho[lev], 0, 1);
     }
+#ifdef WARPX_DIM_RZ
+    for (int lev = 0; lev <= max_level; lev++) {
+        ApplyInverseVolumeScalingToChargeDensity(rho[lev].get(), lev);
+    }
+#endif
 
     // beta is zero in lab frame
     // Todo: use simpler finite difference form with beta=0
@@ -158,6 +169,96 @@ WarpX::computePhi (const amrex::Vector<std::unique_ptr<amrex::MultiFab> >& rho,
                    Real const required_precision,
                    int const max_iters) const
 {
+#ifdef WARPX_DIM_RZ
+
+    // Create a new geometry with the z coordinate scaled by gamma
+    amrex::Real const gamma = std::sqrt(1._rt/(1. - beta[2]*beta[2]));
+
+    amrex::Vector<amrex::Geometry> geom_scaled(max_level + 1);
+    for (int lev = 0; lev <= max_level; ++lev) {
+        const amrex::Geometry & geom_lev = Geom(lev);
+        const amrex::Real* current_lo = geom_lev.ProbLo();
+        const amrex::Real* current_hi = geom_lev.ProbHi();
+        amrex::Real scaled_lo[AMREX_SPACEDIM];
+        amrex::Real scaled_hi[AMREX_SPACEDIM];
+        scaled_lo[0] = current_lo[0];
+        scaled_hi[0] = current_hi[0];
+        scaled_lo[1] = current_lo[1]*gamma;
+        scaled_hi[1] = current_hi[1]*gamma;
+        amrex::RealBox rb = RealBox(scaled_lo, scaled_hi);
+        geom_scaled[lev].define(geom_lev.Domain(), &rb);
+    }
+
+    // Setup the sigma = radius
+    // sigma must be cell centered
+    amrex::Vector<std::unique_ptr<amrex::MultiFab> > sigma(max_level+1);
+    for (int lev = 0; lev <= max_level; ++lev) {
+        const amrex::Real * problo = geom_scaled[lev].ProbLo();
+        const amrex::Real * dx = geom_scaled[lev].CellSize();
+        const amrex::Real rmin = problo[0];
+        const amrex::Real dr = dx[0];
+
+        amrex::BoxArray nba = boxArray(lev);
+        nba.enclosedCells(); // Get cell centered array (correct?)
+        sigma[lev] = std::make_unique<MultiFab>(nba, dmap[lev], 1, 0);
+        for ( MFIter mfi(*sigma[lev], TilingIfNotGPU()); mfi.isValid(); ++mfi )
+        {
+            const amrex::Box& tbx = mfi.tilebox();
+            const amrex::Dim3 lo = amrex::lbound(tbx);
+            const int irmin = lo.x;
+            Array4<amrex::Real> const& sigma_arr = sigma[lev]->array(mfi);
+            amrex::ParallelFor( tbx,
+                [=] AMREX_GPU_DEVICE (int i, int j, int /*k*/) {
+                    sigma_arr(i,j,0) = rmin + (i - irmin + 0.5_rt)*dr;
+                }
+            );
+        }
+
+        // Also, multiply rho by radius (rho is node centered)
+        // Note that this multiplication is not undone since rho is
+        // a temporary array.
+        for ( MFIter mfi(*rho[lev], TilingIfNotGPU()); mfi.isValid(); ++mfi )
+        {
+            const amrex::Box& tbx = mfi.tilebox();
+            const amrex::Dim3 lo = amrex::lbound(tbx);
+            const int irmin = lo.x;
+            int const ncomp = rho[lev]->nComp(); // This should be 1!
+            Array4<Real> const& rho_arr = rho[lev]->array(mfi);
+            amrex::ParallelFor(tbx, ncomp,
+            [=] AMREX_GPU_DEVICE (int i, int j, int /*k*/, int icomp)
+            {
+                amrex::Real r = rmin + (i - irmin)*dr;
+                if (r == 0.) {
+                    rho_arr(i,j,0,icomp) *= dr/3._rt;
+                } else {
+                    rho_arr(i,j,0,icomp) *= r;
+                }
+            });
+        }
+    }
+
+    // Define the boundary conditions
+    Array<LinOpBCType,AMREX_SPACEDIM> lobc, hibc;
+    lobc[0] = LinOpBCType::Neumann;
+    hibc[0] = LinOpBCType::Dirichlet;
+    if ( Geom(0).isPeriodic(1) ) {
+        lobc[1] = LinOpBCType::Periodic;
+        hibc[1] = LinOpBCType::Periodic;
+    } else {
+        // Use Dirichlet boundary condition by default.
+        // Ideally, we would often want open boundary conditions here.
+        lobc[1] = LinOpBCType::Dirichlet;
+        hibc[1] = LinOpBCType::Dirichlet;
+    }
+
+    // Define the linear operator (Poisson operator)
+    MLNodeLaplacian linop( geom_scaled, boxArray(), dmap );
+    for (int lev = 0; lev <= max_level; ++lev) {
+        linop.setSigma( lev, *sigma[lev] );
+    }
+
+#else
+
     // Define the boundary conditions
     Array<LinOpBCType,AMREX_SPACEDIM> lobc, hibc;
     for (int idim=0; idim<AMREX_SPACEDIM; idim++){
@@ -174,7 +275,6 @@ WarpX::computePhi (const amrex::Vector<std::unique_ptr<amrex::MultiFab> >& rho,
 
     // Define the linear operator (Poisson operator)
     MLNodeTensorLaplacian linop( Geom(), boxArray(), DistributionMap() );
-    linop.setDomainBC( lobc, hibc );
     // Set the value of beta
     amrex::Array<amrex::Real,AMREX_SPACEDIM> beta_solver =
 #if (AMREX_SPACEDIM==2)
@@ -184,7 +284,10 @@ WarpX::computePhi (const amrex::Vector<std::unique_ptr<amrex::MultiFab> >& rho,
 #endif
     linop.setBeta( beta_solver );
 
+#endif
+
     // Solve the Poisson equation
+    linop.setDomainBC( lobc, hibc );
     MLMG mlmg(linop);
     mlmg.setVerbose(2);
     mlmg.setMaxIter(max_iters);
