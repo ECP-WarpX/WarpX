ECP-WarpX/WarpX

commit 00aae4ea8364dbf6d8158dabc585aedda57798c2

AMReX: CMake Install (if shared) (#1755)

cmake/dependencies/AMReX.cmake
@@ -61,6 +61,7 @@ macro(find_amrex)
             set(AMReX_PRECISION_PARTICLES "SINGLE" CACHE INTERNAL "")
         endif()
 
+        set(AMReX_INSTALL ${BUILD_SHARED_LIBS} CACHE INTERNAL "")
         set(AMReX_ENABLE_TESTS OFF CACHE INTERNAL "")
         set(AMReX_FORTRAN OFF CACHE INTERNAL "")
         set(AMReX_FORTRAN_INTERFACES OFF CACHE INTERNAL "")
@@ -185,7 +186,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "21.03"
+set(WarpX_amrex_branch "dd2ec76a5cc5db01caa17bdf51efb603c019346d"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 

run_test.sh
@@ -51,7 +51,7 @@ echo "cd $PWD"
 
 # Clone PICSAR and AMReX
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout 21.03 && cd -
+cd amrex && git checkout dd2ec76a5cc5db01caa17bdf51efb603c019346d && cd -
 # Use QED brach for QED tests
 git clone https://github.com/ECP-WarpX/picsar.git
 cd picsar && git checkout b35f07243c51ac35d47857fe36f0aafb6b517955 && cd -

commit 37ae2c5017e862202f78e37a10b62e92857b95ef

Update AMReX: Install (Regression)
Update AMReX to the latest `development` branch. Fix a
regression introduced in #1755.

cmake/dependencies/AMReX.cmake
@@ -186,7 +186,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "dd2ec76a5cc5db01caa17bdf51efb603c019346d"
+set(WarpX_amrex_branch "2fb82bf64838aba949f7a641988a0c58774e5a88"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 

run_test.sh
@@ -51,7 +51,7 @@ echo "cd $PWD"
 
 # Clone PICSAR and AMReX
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout dd2ec76a5cc5db01caa17bdf51efb603c019346d && cd -
+cd amrex && git checkout 2fb82bf64838aba949f7a641988a0c58774e5a88 && cd -
 # Use QED brach for QED tests
 git clone https://github.com/ECP-WarpX/picsar.git
 cd picsar && git checkout b35f07243c51ac35d47857fe36f0aafb6b517955 && cd -