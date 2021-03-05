91f8927750a9c12ffd21493a97cdf9debd21ad33
Axel Huebl <axel.huebl@plasma.ninja>
2021-03-04 17:26:37 -0800

    Update AMReX: Install (Regression) (#1762)

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -186,7 +186,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "dd2ec76a5cc5db01caa17bdf51efb603c019346d"
+set(WarpX_amrex_branch "2fb82bf64838aba949f7a641988a0c58774e5a88"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
