5c452bcc27a2adbd3d45f6a49cf0d44e9654a3dc
roelof.groenewald <roelof.groenewald@modernelectron.com>
2021-03-10 15:08:42 -0800

    switched to using yt for data analysis since h5py is not available

--- Examples/Tests/ElectrostaticDirichletBC/analysis.py
+++ Examples/Tests/ElectrostaticDirichletBC/analysis.py
@@ -13,25 +13,25 @@
 
 # Possible running time: ~ 19 s
 
-import h5py
 import numpy as np
 
+import yt
 import glob
 
-dt = 7.5e-10
-
-files = sorted(glob.glob('dirichletbc_plt/openpmd_*.h5'))[1:]
+files = sorted(glob.glob('dirichletbc_plt*'))[1:]
 
 times = np.zeros(len(files))
 potentials = np.zeros(len(files))
 
 for ii, file in enumerate(files):
-    with h5py.File(file, 'r') as f:
-        step = str(int(file.split('_')[-1][:-3]))
-        times[ii] = (float(step) - 1.0) * dt
-        potentials[ii] = np.mean(
-            np.array(f['data'][step]['fields']['phi']), axis=0
-        )[-1]
+    ds = yt.load( file )
+    times[ii] = (
+        ds.current_time.item() - float(ds.parameters.get('warpx.const_dt'))
+    )
+    data = ds.covering_grid(
+        level=0, left_edge=ds.domain_left_edge, dims=ds.domain_dimensions
+    )
+    potentials[ii] = np.mean(data['phi'].to_ndarray()[-1][:])
 
 expected_potentials = 450.0 * np.sin(2.0 * np.pi * 13.56e6 * times)
 assert np.allclose(potentials, expected_potentials, rtol=0.01)
