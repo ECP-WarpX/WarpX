2cc2c5e61f593e5ffeb3989ae125c0473443b733
Michael E Rowan <38045958+mrowan137@users.noreply.github.com>
2021-03-18 19:45:12 -0700

    LoadBalanceCosts reduced diagnostic set spatial indices to zero depending on dims (#1785)
    * Fix for diff spatial dims
    
    * eol
    
    * Update LoadBalanceCosts.cpp
    
    * Update LoadBalanceCosts.cpp
    
    * Restart CI

--- Source/Diagnostics/ReducedDiags/LoadBalanceCosts.cpp
+++ Source/Diagnostics/ReducedDiags/LoadBalanceCosts.cpp
@@ -83,8 +83,16 @@ void LoadBalanceCosts::ComputeDiags (int step)
             m_data[shift_m_data + mfi.index()*m_nDataFields + 1] = dm[mfi.index()];
             m_data[shift_m_data + mfi.index()*m_nDataFields + 2] = lev;
             m_data[shift_m_data + mfi.index()*m_nDataFields + 3] = tbx.loVect()[0];
+#if (AMREX_SPACEDIM >= 2)
             m_data[shift_m_data + mfi.index()*m_nDataFields + 4] = tbx.loVect()[1];
+#else
+            m_data[shift_m_data + mfi.index()*m_nDataFields + 4] = 0.;
+#endif
+#if (AMREX_SPACEDIM == 3)
             m_data[shift_m_data + mfi.index()*m_nDataFields + 5] = tbx.loVect()[2];
+#else
+            m_data[shift_m_data + mfi.index()*m_nDataFields + 5] = 0.;
+#endif
 #ifdef AMREX_USE_GPU
             m_data[shift_m_data + mfi.index()*m_nDataFields + 6] = amrex::Gpu::Device::deviceId();
 #endif
