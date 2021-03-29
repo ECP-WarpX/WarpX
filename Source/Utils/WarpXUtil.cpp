f9032d6344c37ea43bdac988a37e07689c48c786
RevathiJambunathan <revanathan@gmail.com>
2021-03-29 09:44:41 -0700

    resolving commit conflict

--- Source/Utils/WarpXUtil.cpp
+++ Source/Utils/WarpXUtil.cpp
@@ -405,10 +405,6 @@ void ReadBCParams ()
                 // set particle boundary to periodic
                 WarpX::particle_boundary_lo[idim] = ParticleBoundaryType::Periodic;
                 WarpX::particle_boundary_hi[idim] = ParticleBoundaryType::Periodic;
-<<<<<<< HEAD
-=======
-                amrex::Warning("Particle boundary is set to periodic to be consistent with the fields.");
->>>>>>> 77e70c05... separate particle and field boudnary structs
             }
 
         }
