99acee0e0e36e3d6d1a66d07c19f3f3818f6e0f9
Remi Lehe <remi.lehe@normalesup.org>
2021-03-30 20:25:02 -0700

    Remove the avg_aux fields (#1849)
    * Remove the avg_aux fields
    
    * Update 3D benchmark
    
    * Update benchmark in 2D
    
    * Initialize Bfield_avg_cp
    
    * Fix two whitespaces left out
    
    * Fix two more whitespaces left out
    
    Co-authored-by: Edoardo Zoni <59625522+EZoni@users.noreply.github.com>

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -612,8 +612,6 @@ WarpX::PushParticlesandDepose (int lev, amrex::Real cur_time, DtType a_dt_type)
     mypc->Evolve(lev,
                  *Efield_aux[lev][0],*Efield_aux[lev][1],*Efield_aux[lev][2],
                  *Bfield_aux[lev][0],*Bfield_aux[lev][1],*Bfield_aux[lev][2],
-                 *Efield_avg_aux[lev][0],*Efield_avg_aux[lev][1],*Efield_avg_aux[lev][2],
-                 *Bfield_avg_aux[lev][0],*Bfield_avg_aux[lev][1],*Bfield_avg_aux[lev][2],
                  *current_x, *current_y, *current_z,
                  current_buf[lev][0].get(), current_buf[lev][1].get(), current_buf[lev][2].get(),
                  rho_fp[lev].get(), charge_buf[lev].get(),
