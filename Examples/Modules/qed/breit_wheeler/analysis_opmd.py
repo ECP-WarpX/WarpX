cf5317f4a74c48b7022e727a5cb13c457400fc64
Luca Fedeli <luca.fedeli@cea.fr>
2021-04-13 17:43:53 +0200

    comment out checksum calculation for openPMD output

--- Examples/Modules/qed/breit_wheeler/analysis_opmd.py
+++ Examples/Modules/qed/breit_wheeler/analysis_opmd.py
@@ -10,8 +10,8 @@
 
 import sys
 import openpmd_api as io
-sys.path.insert(1, '../../../../warpx/Regression/Checksum/')
-import checksumAPI
+#sys.path.insert(1, '../../../../warpx/Regression/Checksum/')
+#import checksumAPI
 
 import analysis_core as ac
 
@@ -61,8 +61,8 @@ def main():
 
     ac.check(sim_time, particle_data)
 
-    test_name = filename_end[:-9] # Could also be os.path.split(os.getcwd())[1]
-    checksumAPI.evaluate_checksum(test_name, filename_end)
+    #test_name = filename_end[:-9] # Could also be os.path.split(os.getcwd())[1]
+    #checksumAPI.evaluate_checksum(test_name, filename_end)
 
 if __name__ == "__main__":
     main()
