002a1f4afb510480c593a88ef6caea3f4164e115
Lorenzo Giacomel <lorenzo.giacomel@cern.ch>
2021-04-22 23:46:22 +0200

    Fixed the case of all_regular geometries

--- Source/EmbeddedBoundary/WarpXInitEB.cpp
+++ Source/EmbeddedBoundary/WarpXInitEB.cpp
@@ -15,7 +15,8 @@ WarpX::InitEB ()
 
     amrex::ParmParse pp_eb2("eb2");
     if (!pp_eb2.contains("geom_type")) {
-        pp_eb2.add("geom_type", "all_regular"); // use all_regular by default
+        std::string geom_type = "all_regular";
+        pp_eb2.add("geom_type", geom_type); // use all_regular by default
     }
     amrex::EB2::Build(Geom(maxLevel()), maxLevel(), maxLevel());
 
@@ -37,22 +38,37 @@ WarpX::ComputeEdgeLengths () {
     auto const &edge_centroid = eb_fact.getEdgeCent();
     for (amrex::MFIter mfi(flags); mfi.isValid(); ++mfi){
         amrex::Box const &box = mfi.validbox();
+        amrex::FabType fab_type = flags[mfi].getType(box);
         for (int idim = 0; idim < AMREX_SPACEDIM; ++idim){
-            auto const &edge_cent = edge_centroid[idim]->const_array(mfi);
             auto const &edge_lengths_dim = m_edge_lengths[maxLevel()][idim]->array(mfi);
-            amrex::LoopOnCpu(amrex::convert(box, amrex::Box(edge_cent).ixType()),
-                            [=](int i, int j, int k) {
-                if (edge_cent(i, j, k) == amrex::Real(-1.0)) {
-                    // This edge is all covered
-                    edge_lengths_dim(i, j, k) = 0.;
-                } else if (edge_cent(i, j, k) == amrex::Real(1.0)) {
-                    // This edge is all open
+            if (fab_type == amrex::FabType::regular) {
+                // every cell in box is all regular
+                amrex::LoopOnCpu(amrex::convert(box, amrex::Box(edge_lengths_dim).ixType()),
+                                 [=](int i, int j, int k) {
                     edge_lengths_dim(i, j, k) = 1.;
-                } else {
-                    // This edge is cut.
-                    edge_lengths_dim(i, j, k) = 1 - abs(amrex::Real(2.0)*edge_cent(i, j, k));
-                }
-            });
+                });
+            } else if (fab_type == amrex::FabType::covered) {
+                // every cell in box is all covered
+                amrex::LoopOnCpu(amrex::convert(box, amrex::Box(edge_lengths_dim).ixType()),
+                                 [=](int i, int j, int k) {
+                    edge_lengths_dim(i, j, k) = 1.;
+                });
+            } else {
+                auto const &edge_cent = edge_centroid[idim]->const_array(mfi);
+                amrex::LoopOnCpu(amrex::convert(box, amrex::Box(edge_cent).ixType()),
+                                [=](int i, int j, int k) {
+                    if (edge_cent(i, j, k) == amrex::Real(-1.0)) {
+                        // This edge is all covered
+                        edge_lengths_dim(i, j, k) = 0.;
+                    } else if (edge_cent(i, j, k) == amrex::Real(1.0)) {
+                        // This edge is all open
+                        edge_lengths_dim(i, j, k) = 1.;
+                    } else {
+                        // This edge is cut.
+                        edge_lengths_dim(i, j, k) = 1 - abs(amrex::Real(2.0)*edge_cent(i, j, k));
+                    }
+                });
+            }
         }
     }
 #endif
@@ -73,13 +89,28 @@ WarpX::ComputeFaceAreas () {
 
     for (amrex::MFIter mfi(flags); mfi.isValid(); ++mfi) {
         amrex::Box const &box = mfi.validbox();
+        amrex::FabType fab_type = flags[mfi].getType(box);
         for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
-              auto const &face = area_frac[idim]->const_array(mfi);
-              auto const &face_areas_dim = m_face_areas[maxLevel()][idim]->array(mfi);
-              amrex::LoopOnCpu(amrex::convert(box, amrex::Box(face).ixType()),
-                               [=](int i, int j, int k) {
-                  face_areas_dim(i, j, k) = face(i, j, k);
-              });
+            auto const &face_areas_dim = m_face_areas[maxLevel()][idim]->array(mfi);
+            if (fab_type == amrex::FabType::regular) {
+                // every cell in box is all regular
+                amrex::LoopOnCpu(amrex::convert(box, amrex::Box(face_areas_dim).ixType()),
+                                [=](int i, int j, int k) {
+                    face_areas_dim(i, j, k) = amrex::Real(1.);
+                });
+            } else if (fab_type == amrex::FabType::covered) {
+                // every cell in box is all covered
+                amrex::LoopOnCpu(amrex::convert(box, amrex::Box(face_areas_dim).ixType()),
+                                 [=](int i, int j, int k) {
+                    face_areas_dim(i, j, k) = amrex::Real(0.);
+                });
+            } else {
+                auto const &face = area_frac[idim]->const_array(mfi);
+                amrex::LoopOnCpu(amrex::convert(box, amrex::Box(face).ixType()),
+                                [=](int i, int j, int k) {
+                    face_areas_dim(i, j, k) = face(i, j, k);
+                });
+            }
         }
     }
 #endif
@@ -91,20 +122,20 @@ WarpX::ComputeFaceAreas () {
 void
 WarpX::ScaleEdges () {
 #ifdef AMREX_USE_EB
+    BL_PROFILE("ScaleEdges");
+
     auto const &cell_size = CellSize(maxLevel());
     auto const eb_fact = fieldEBFactory(maxLevel());
     auto const &flags = eb_fact.getMultiEBCellFlagFab();
-    auto const &edge_centroid = eb_fact.getEdgeCent();
 
     for (amrex::MFIter mfi(flags); mfi.isValid(); ++mfi) {
         amrex::Box const &box = mfi.validbox();
         for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
-            auto const &edge = edge_centroid[idim]->const_array(mfi);
             auto const &edge_lengths_dim = m_edge_lengths[maxLevel()][idim]->array(mfi);
-            amrex::LoopOnCpu(amrex::convert(box, amrex::Box(edge).ixType()),
+            amrex::LoopOnCpu(amrex::convert(box, amrex::Box(edge_lengths_dim).ixType()),
                              [=](int i, int j, int k) {
-                                 edge_lengths_dim(i, j, k) *= cell_size[idim];
-              });
+                edge_lengths_dim(i, j, k) *= cell_size[idim];
+            });
         }
     }
 #endif
@@ -116,12 +147,13 @@ WarpX::ScaleEdges () {
 void
 WarpX::ScaleAreas() {
 #ifdef AMREX_USE_EB
+    BL_PROFILE("ScaleAreas");
+
     auto const& cell_size = CellSize(maxLevel());
     amrex::Real full_area;
 
     auto const eb_fact = fieldEBFactory(maxLevel());
     auto const &flags = eb_fact.getMultiEBCellFlagFab();
-    auto const &area_frac = eb_fact.getAreaFrac();
 
     for (amrex::MFIter mfi(flags); mfi.isValid(); ++mfi) {
         amrex::Box const &box = mfi.validbox();
@@ -133,13 +165,11 @@ WarpX::ScaleAreas() {
             } else {
                 full_area = cell_size[0]*cell_size[1];
             }
-            auto const &face = area_frac[idim]->const_array(mfi);
             auto const &face_areas_dim = m_face_areas[maxLevel()][idim]->array(mfi);
-            amrex::LoopOnCpu(amrex::convert(box, amrex::Box(face).ixType()),
+            amrex::LoopOnCpu(amrex::convert(box, amrex::Box(face_areas_dim).ixType()),
                              [=](int i, int j, int k) {
                                 face_areas_dim(i, j, k) *= full_area;
             });
-
         }
     }
 #endif
