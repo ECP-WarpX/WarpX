33f7a96168cdb5a8e85cecd8abb56f111001ef98
Dave Grote <grote1@llnl.gov>
2021-05-07 16:29:42 -0700

    Bug fix in DampFieldsInGuards (for tiling)

--- Source/FieldSolver/WarpXPushFieldsEM.cpp
+++ Source/FieldSolver/WarpXPushFieldsEM.cpp
@@ -438,14 +438,17 @@ WarpX::DampFieldsInGuards(std::array<std::unique_ptr<amrex::MultiFab>,3>& Efield
         int const zero_fields_nz_temp = zero_fields_nz;
         int const damp_fields_nz_temp = damp_fields_nz;
 
-        if (tilebox.smallEnd(zdir) < 0) {
+        if (tilebox.smallEnd(zdir) < zero_fields_nz + damp_fields_nz) {
 
             // Apply damping factor in guards cells below the lower end of the domain
-            int const nz_guard = zero_fields_nz + damp_fields_nz; // -tilebox.smallEnd(zdir);
+            int nz_guard = zero_fields_nz + damp_fields_nz; // -tilebox.smallEnd(zdir);
+            if (nz_guard > tilebox.bigEnd(zdir)) {
+                nz_guard = tilebox.bigEnd(zdir);
+            }
 
             // Set so the box only covers the lower half of the guard cells
             amrex::Box tilebox_guard = tilebox;
-            tilebox_guard.setBig(zdir, nz_guard-1);
+            tilebox_guard.setBig(zdir, nz_guard);
 
             amrex::ParallelFor(tilebox_guard, Efield[0]->nComp(),
             [=] AMREX_GPU_DEVICE (int i, int j, int k, int icomp)
@@ -470,14 +473,17 @@ WarpX::DampFieldsInGuards(std::array<std::unique_ptr<amrex::MultiFab>,3>& Efield
             });
 
         }
-        if (nz_tile > nz_domain) {
+        if (nz_tile > nz_domain - zero_fields_nz - damp_fields_nz) {
 
             // Apply damping factor in guards cells above the upper end of the domain
             int nz_guard = damp_fields_nz + zero_fields_nz; //nz_tile - nz_domain;
+            if (nz_domain - nz_guard < tilebox.smallEnd(zdir)) {
+                nz_guard = nz_domain - tilebox.smallEnd(zdir);
+            }
 
             // Set so the box only covers the upper half of the guard cells
             amrex::Box tilebox_guard = tilebox;
-            tilebox_guard.setSmall(zdir, nz_domain - nz_guard + 1);
+            tilebox_guard.setSmall(zdir, nz_domain - nz_guard);
 
             amrex::ParallelFor(tilebox_guard, Efield[0]->nComp(),
             [=] AMREX_GPU_DEVICE (int i, int j, int k, int icomp)
