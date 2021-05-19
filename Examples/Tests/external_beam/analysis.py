2b5bf952eba3dfc73a1c667caf5bb39a6799e83b
Maxence Thevenet <maxence.thevenet@desy.de>
2021-05-19 21:15:25 +0200

    include new test in CI

--- Examples/Tests/external_beam/analysis.py
+++ Examples/Tests/external_beam/analysis.py
@@ -1,5 +1,15 @@
 #! /usr/bin/env python
 
+# Copyright 2021 Maxence Thevenet
+#
+# This file is part of WarpX.
+#
+# License: BSD-3-Clause-LBNL
+
+# This script reads beam from two output folders, and checkS whether theis
+# x and z positions are equal. Typically used to test the capability to
+# read a beam from file and use it in a simulation.
+
 import numpy as np
 from openpmd_viewer import OpenPMDTimeSeries
 
