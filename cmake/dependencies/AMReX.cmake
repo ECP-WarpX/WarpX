49ecacc73be2757d46a37f96e02a1806667fe448
Axel Huebl <axel.huebl@plasma.ninja>
2021-06-02 13:26:21 -0700

    Update AMReX
    Update AMReX to f7fd082bc8ab099d42784f69d6fe49dea3d388a7

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -198,7 +198,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "21.06"
+set(WarpX_amrex_branch "f7fd082bc8ab099d42784f69d6fe49dea3d388a7"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
