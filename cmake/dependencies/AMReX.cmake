4d6166e0294bca0a8dbcd66cf0ca06eb5582f22e
Luca Fedeli <luca.fedeli@cea.fr>
2021-06-15 04:23:11 +0200

    use right version of amrex

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -231,7 +231,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "fc9bc1e3efdaead0253d0a55dcf1abb7abc06bd7"
+set(WarpX_amrex_branch "659ab43420a2c4a918cda2702910771fbbc395d3"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
