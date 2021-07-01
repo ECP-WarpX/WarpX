f3b4c0e2a93327530d4d1f5bf15eaa7797eed682
Peter Scherpelz <peter.scherpelz@modernelectron.com>
2021-07-01 11:42:19 -0700

    Eliminate duplicate functionality, doc tweaks
    - initialize_criteria calls add_checker to avoid repeating it
    - max_steps is still required, but now implemented as a criteria similar
      to the rest
    - Note this also means max_steps doesn't short-circuit all other
      criteria being run
    - Add types to Arguments docs; break long line

--- mewarpx/mewarpx/sim_control.py
+++ mewarpx/mewarpx/sim_control.py
@@ -16,11 +16,12 @@ class SimControl:
 
         Arguments:
 
-            max_steps: Maximum number of steps to perform in the simulation,
-                when step == max_steps check_criteria returns False
+            max_steps (int): Maximum number of steps to perform in the
+                simulation, when step == max_steps check_criteria returns False
 
-            criteria: list of user defined functions or list of user defined
-                tuples(function, kwargs) that each return a True or False value
+            criteria (list): list of user defined functions or list of user
+                defined tuples(function, kwargs) that each return a True or
+                False value
 
         Functions:
 
@@ -39,38 +40,44 @@ class SimControl:
         self.max_steps = max_steps
         if self.max_steps < 1:
             raise AssertionError("max_steps must be >= 1")
+        self.add_checker(self.eval_max_steps)
         self.initialize_criteria(criteria)
 
-    def initialize_criteria(self, criteria):
-        if criteria:
-            for crit in criteria:
-                if callable(crit):
-                    self.crit_list.append(crit)
-                    self.crit_args_list.append({})
-                else:
-                    self.crit_list.append(crit[0])
-                    self.crit_args_list.append(crit[1])
+    def add_checker(self, criterion):
+        """Install a single function to check.
 
-    def add_checker(self, criteria):
-        if callable(criteria):
-            self.crit_list.append(criteria)
+        Arguments:
+            criterion (func or tuple): Either a function or a tuple of (func,
+                kwargs_dict) where the kwargs_dict will be passed to the func.
+        """
+        if callable(criterion):
+            self.crit_list.append(criterion)
             self.crit_args_list.append({})
         else:
-            self.crit_list.append(criteria[0])
-            self.crit_args_list.append(criteria[1])
+            self.crit_list.append(criterion[0])
+            self.crit_args_list.append(criterion[1])
 
-    def check_criteria(self):
-        """Return True if all installed criteria are True, else False."""
+    def initialize_criteria(self, criteria_list):
+        """Install the full initial list of criteria."""
+        if criteria_list:
+            for crit in criteria_list:
+                self.add_checker(crit)
+
+    def eval_max_steps(self):
         if mwxrun.get_it() >= self.max_steps:
             print('SimControl: Max steps reached!')
             return False
+        return True
 
+    def check_criteria(self):
+        """Return True if all installed criteria are True, else False."""
         all_good = True
         terminate_statement = 'SimControl: Termination from criteria: '
         for i, criteria in enumerate(self.crit_list):
             all_good = all_good and criteria(**self.crit_args_list[i])
             if not criteria(**self.crit_args_list[i]):
-                add_statement = "{criteria_name} ".format(criteria_name=criteria.__name__)
+                add_statement = "{criteria_name} ".format(
+                    criteria_name=criteria.__name__)
                 terminate_statement += add_statement
 
         if not all_good:
