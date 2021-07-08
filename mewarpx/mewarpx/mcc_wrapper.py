49af8e3ac2066ba760dff173dae0e49086cad1da
kzhu-ME <kevin.zhu@modernelectron.com>
2021-07-08 16:06:53 -0700

    Added MCC wrapper to facilitate initializing MCC in PICMI script

--- mewarpx/mewarpx/mcc_wrapper.py
+++ mewarpx/mewarpx/mcc_wrapper.py
@@ -1,50 +1,90 @@
+import glob, os
 from pywarpx import picmi
+from mewarpx import util as mwxutil
 
-def init_mcc(electrons, ions, N_INERT, T_INERT):
-    """Initializes MCC objects."""
-    cross_sec_direc = '../../../warpx-data/MCC_cross_sections/He/'
-    mcc_electrons = picmi.MCCCollisions(
-        name='coll_elec',
-        species=electrons,
-        background_density=N_INERT,
-        background_temperature=T_INERT,
-        background_mass=ions.mass,
-        scattering_processes={
-            'elastic' : {
-                'cross_section' : cross_sec_direc+'electron_scattering.dat'
-            },
-            'excitation1' : {
-                'cross_section': cross_sec_direc+'excitation_1.dat',
-                'energy' : 19.82
-            },
-            'excitation2' : {
-                'cross_section': cross_sec_direc+'excitation_2.dat',
-                'energy' : 20.61
-            },
-            'ionization' : {
-                'cross_section' : cross_sec_direc+'ionization.dat',
-                'energy' : 24.55,
-                'species' : ions
-            },
-        }
-    )
+# For use later to sync with diode test template and access sim object in mwxrun
+from mewarpx import mwxrun
 
-    mcc_ions = picmi.MCCCollisions(
-        name='coll_ion',
-        species=ions,
-        background_density=N_INERT,
-        background_temperature=T_INERT,
-        scattering_processes={
-            'elastic' : {
-                'cross_section' : cross_sec_direc+'ion_scattering.dat'
-            },
-            'back' : {
-                'cross_section' : cross_sec_direc+'ion_back_scatter.dat'
-            },
-            # 'charge_exchange' : {
-            #    'cross_section' : cross_sec_direc+'charge_exchange.dat'
-            # }
-        }
-    )
 
-    return mcc_electrons, mcc_ions
+class MCC():
+    """Wrapper used to initialize MCC parameters"""
+
+    def __init__(self, electron_species, ion_species, P_INERT,
+                 T_INERT, scraper=None, **kwargs):
+        """Initialize MCC"""
+        self.electron_species = electron_species
+        self.ion_species = ion_species
+        self.P_INERT = P_INERT
+        self.T_INERT = T_INERT
+        self.N_INERT = mwxutil.ideal_gas_density(self.P_INERT, self.T_INERT)
+        self.scraper = scraper
+
+        # include all collision processes that match species
+        path_name = os.path.join("../../../warpx-data/MCC_cross_sections", self.ion_species.particle_type)
+
+        file_paths = glob.glob(path_name + "/*.dat")
+
+        elec_collision_types = {
+                            "electron_scattering.dat": "elastic",
+                            "excitation_1.dat": "excitation1",
+                            "excitation_2.dat": "excitation2",
+                            "ionization.dat": "ionization",
+                           }
+        ion_collision_types = {
+                            "ion_scattering.dat": "elastic",
+                            "ion_back_scatter.dat": "back",
+                            "charge_exchange.dat": "charge_exchange"
+                            }
+        requires_energy = {
+                            "Ar": {
+                                "excitation_1.dat": 11.5,
+                                "ionization.dat": 15.7596112
+                                },
+                            "He": {
+                                "excitation_1.dat": 19.82,
+                                "excitation_2.dat": 20.61,
+                                "ionization.dat": 24.55
+                                },
+                            "Xe": {
+                                "excitation_1.dat": 8.315,
+                                "ionization.dat": 12.1298431
+                                }
+                        }
+
+        # build scattering process dictionaries
+        elec_scattering_processes = {}
+        ion_scattering_processes = {}
+
+        for path in range(len(file_paths)):
+            file_name = os.path.absename(path)
+            # if electron process
+            if file_name in elec_collision_types:
+                scatter_dict = {"cross_section": path}
+                # add energy if needed
+                if file_name in requires_energy[self.ion_species.name]:
+                    scatter_dict["energy"] = requires_energy[self.ion_species.name][file_name]
+                # specify species for ionization
+                if file_name == "ionization.dat":
+                    scatter_dict["species"] = self.ion_species
+                elec_scattering_processes[elec_collision_types[file_name]] = scatter_dict
+            # if ion process
+            elif file_name in ion_collision_types:
+                scatter_dict = {"cross_section": path}
+                ion_scattering_processes[ion_collision_types[file_name]] = scatter_dict
+
+        self.mcc_electrons = picmi.MCCCollisions(
+            name='coll_elec',
+            species=self.electron_species,
+            background_density=self.N_INERT,
+            background_temperature=self.T_INERT,
+            background_mass=self.ion_species.mass,
+            scattering_processes=elec_scattering_processes
+        )
+
+        self.mcc_ions = picmi.MCCCollisions(
+            name='coll_ion',
+            species=self.ion_species,
+            background_density=self.N_INERT,
+            background_temperature=self.T_INERT,
+            scattering_processes=ion_scattering_processes
+        )
--- mewarpx/mewarpx/util.py
+++ mewarpx/mewarpx/util.py
@@ -3,8 +3,18 @@ Utility functions for mewarpx.
 """
 import inspect
 import os
+from scipy import constants
 
 from pywarpx import geometry
+
+# CONSTANTS - SI
+kb_J = constants.value("Boltzmann constant") # J/K
+torr_SI = constants.value("standard atmosphere")/760 # 1 torr in Pa
+erg_SI = 1e-7 # 1 erg in J
+
+# CONSTANTS - CGS
+kb_cgs = kb_J / erg_SI # erg/K
+torr_cgs = torr_SI * 10 # 1 torr in dyne/cm^2
 
 # http://stackoverflow.com/questions/50499/in-python-how-do-i-get-the-path-and-name-of-the-file-t
 mewarpx_dir = os.path.dirname(os.path.abspath(
@@ -30,3 +40,16 @@ def init_libwarpx(ndim, rz):
     # This just quiets linters like pyflakes by using the otherwise-unused
     # variable
     assert pywarpx._libwarpx
+
+
+def ideal_gas_density(p, T):
+    """Calculate neutral gas density from the ideal gas law.
+
+    Arguments:
+        p (float): Gas pressure (Torr)
+        T (float): Mean gas temperature (K)
+
+    Returns:
+        N (float): Number density of gas atoms/molecules (1/cm^3)
+    """
+    return (p * torr_cgs) / (kb_cgs * T)
