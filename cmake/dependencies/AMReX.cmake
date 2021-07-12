ce32ee5071b5a9e81e2ff29d5c7992a5c889292d
Axel Huebl <axel.huebl@plasma.ninja>
2021-07-12 09:06:27 -0700

    AMReX/PICSAR: Weekly Update
    Update AMReX to AMReX-Codes/amrex@cd1f5430be29aed0a85348669394d7fbb8355dba
    
    No changes im PICSAR since last update.
    
    ```
    ./Tools/Release/updatePICSAR.py
    ./Tools/Release/updateAMReX.py
    ```

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -231,7 +231,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "21.07"
+set(WarpX_amrex_branch "cd1f5430be29aed0a85348669394d7fbb8355dba"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
