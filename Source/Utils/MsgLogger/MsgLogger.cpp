e7fb19ea206269b1112fca5eeb1968123d5c1d38
Luca Fedeli <luca.fedeli@cea.fr>
2021-07-23 15:55:34 +0200

    improved MsgLogger

--- Source/Utils/MsgLogger/MsgLogger.cpp
+++ Source/Utils/MsgLogger/MsgLogger.cpp
@@ -1,9 +1,42 @@
 
 #include "MsgLogger.H"
 
+#include "MsgLoggerSerialization.H"
+
 #include <iostream> //DEBUG
+#include <sstream>
 
 using namespace Utils::MsgLogger;
+
+std::vector<char> MsgWithCounter::serialize() const
+{
+    std::vector<char> serialized_msg;
+
+    put_in(this->msg.topic, serialized_msg);
+
+    put_in(this->msg.text, serialized_msg);
+
+    const int int_priority = static_cast<int>(this->msg.priority);
+    put_in(int_priority, serialized_msg);
+
+    put_in(this->counter, serialized_msg);
+
+    return serialized_msg;
+}
+
+MsgWithCounter MsgWithCounter::deserialize (const std::vector<char>& serialized)
+{
+    MsgWithCounter msg_with_counter;
+
+    auto it = serialized.begin();
+
+    msg_with_counter.msg.topic = get_out<std::string> (it);
+    msg_with_counter.msg.text = get_out<std::string> (it);
+    msg_with_counter.msg.priority = static_cast<Priority> (get_out<int> (it));
+    msg_with_counter.counter = get_out<typeof(counter)> (it);
+
+    return msg_with_counter;
+}
 
 Logger::Logger(){}
 
@@ -21,4 +54,33 @@ std::vector<MsgWithCounter> Logger::get_msg_list() const
     }
 
     return res;
+}
+
+std::vector<MsgWithCounterAndRanks> Logger::collective_gather_msg_lists() const
+{
+
+    const auto my_serialized_list =
+        serialize_msg_list(this->get_msg_list());
+
+    return std::vector<MsgWithCounterAndRanks>{};
+}
+
+        aux_broadcast_io_rank_list();
+
+
+std::vector<char> Logger::serialize_msg_list(
+    const std::vector<MsgWithCounter>& msg_list) const
+{
+    std::vector<char> serialized;
+
+    const auto how_many = static_cast<int> (msg_list.size());
+    put_in (how_many, serialized);
+
+    for (auto msg_w_counter : msg_list){
+        const auto serialized_msg_w_counter = msg_w_counter.serialize();
+        serialized.insert(serialized.end(),
+            serialized_msg_w_counter.begin(),
+            serialized_msg_w_counter.end());
+    }
+    return serialized;
 }
