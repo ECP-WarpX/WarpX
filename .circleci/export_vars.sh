7296b5400c159c5b96510050c257c03918ef212b
Michael Kieburtz <michaelkieburtz@gmail.com>
2021-07-29 11:37:11 -0700

    Refactor CircleCI compile parameters (#38)
    * Attempt at refactoring CircleCI config
    
    * Moved bash script to what I think is the right directory
    
    * Try 'source' instead of 'bash'
    
    * Moved export_vars.sh into /.circleci

---
+++ .circleci/export_vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export WarpX_DIMS=2
+export WarpX_MPI=ON
+export WarpX_OPENPMD=OFF
+export WarpX_PSATD=OFF
+export WarpX_QED=OFF
+export WarpX_EB=ON
+export CC=$(which clang)
+export CXX=$(which clang++)
