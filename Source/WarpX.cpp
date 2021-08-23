e33bae14c9b5a3b11158061476f03b7566530b94
Remi Lehe <remi.lehe@normalesup.org>
2021-08-23 16:58:43 -0700

    Use the function `SyncRho` and the array `rho_fp` in the Electrostatic solver (#1811)
    * Use `SyncRho` in Electrostatic solver
    
    * Reuse rho_fp arrays
    
    * Allocate arrays for rho
    
    * Fix unused variable
    
    * Fix bug in rho deposition
    
    * Only use rho_fp in lab-frame Poisson solver
    
    * Update test cases
    
    * Incorporate PR comments

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -1500,7 +1500,7 @@ WarpX::AllocLevelMFs (int lev, const BoxArray& ba, const DistributionMapping& dm
     }
 #endif
 
-    bool deposit_charge = do_dive_cleaning;
+    bool deposit_charge = do_dive_cleaning || (do_electrostatic == ElectrostaticSolverAlgo::LabFrame);
     if (WarpX::maxwell_solver_id == MaxwellSolverAlgo::PSATD) {
         deposit_charge = do_dive_cleaning || update_with_rho || current_correction;
     }
