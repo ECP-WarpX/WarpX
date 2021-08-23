3fe49d2c0693af0651d2357675130566aeab14c5
Axel Huebl <axel.huebl@plasma.ninja>
2021-08-23 16:13:30 -0700

    Ascent/SENSEI: Add Profiler (#2204)
    * Ascent/SENSEI: Add Profiler
    
    Add overall write profilers to Ascent & SENSEI.
    Add detailed profile for sub-operaions (blueprint, publish, execute)
    to Ascent as well.
    
    * Simplify One-Time Profiler Vars

--- Source/Diagnostics/FlushFormats/FlushFormatAscent.cpp
+++ Source/Diagnostics/FlushFormats/FlushFormatAscent.cpp
@@ -19,29 +19,40 @@ FlushFormatAscent::WriteToFile (
     bool /*isBTD*/, int /*snapshotID*/, const amrex::Geometry& /*full_BTD_snapshot*/, bool /*isLastBTDFlush*/) const
 {
 #ifdef AMREX_USE_ASCENT
+    WARPX_PROFILE("FlushFormatAscent::WriteToFile()");
+
     auto & warpx = WarpX::GetInstance();
 
     // wrap mesh data
+    WARPX_PROFILE_VAR("FlushFormatAscent::WriteToFile::MultiLevelToBlueprint", prof_ascent_mesh_blueprint);
     conduit::Node bp_mesh;
     amrex::MultiLevelToBlueprint(
         nlev, amrex::GetVecOfConstPtrs(mf), varnames, geom, time, iteration, warpx.refRatio(), bp_mesh);
+    WARPX_PROFILE_VAR_STOP(prof_ascent_mesh_blueprint);
 
+    WARPX_PROFILE_VAR("FlushFormatAscent::WriteToFile::WriteParticles", prof_ascent_particles);
     WriteParticles(particle_diags, bp_mesh);
+    WARPX_PROFILE_VAR_STOP(prof_ascent_particles);
 
     // If you want to save blueprint HDF5 files w/o using an Ascent
     // extract, you can call the following AMReX helper:
     // const auto step = istep[0];
     // WriteBlueprintFiles(bp_mesh,"bp_export",step,"hdf5");
 
+    WARPX_PROFILE_VAR("FlushFormatAscent::WriteToFile::publish", prof_ascent_publish);
     ascent::Ascent ascent;
     conduit::Node opts;
     opts["exceptions"] = "catch";
     opts["mpi_comm"] = MPI_Comm_c2f(ParallelDescriptor::Communicator());
     ascent.open(opts);
     ascent.publish(bp_mesh);
+    WARPX_PROFILE_VAR_STOP(prof_ascent_publish);
+
+    WARPX_PROFILE_VAR("FlushFormatAscent::WriteToFile::execute", prof_ascent_execute);
     conduit::Node actions;
     ascent.execute(actions);
     ascent.close();
+    WARPX_PROFILE_VAR_STOP(prof_ascent_execute);
 
 #else
     amrex::ignore_unused(varnames, mf, geom, iteration, time,
@@ -55,6 +66,8 @@ FlushFormatAscent::WriteToFile (
 void
 FlushFormatAscent::WriteParticles(const amrex::Vector<ParticleDiag>& particle_diags, conduit::Node& a_bp_mesh) const
 {
+    WARPX_PROFILE("FlushFormatAscent::WriteParticles()");
+
     // wrap particle data for each species
     // we prefix the fields with "particle_{species_name}" b/c we
     // want to to uniquely name all the fields that can be plotted
