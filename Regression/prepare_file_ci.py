e3e00d1dbbad554e0229b8ab328af6bc5144f86d
Axel Huebl <axel.huebl@plasma.ninja>
2021-08-30 00:04:37 -0700

    CI: Rename Travis -> CI
    Rename scripts and docs since our CI uses several providers now
    (currently: GitHub actions, Azure pipelines and other providers).

--- Regression/prepare_file_ci.py
+++ Regression/prepare_file_ci.py
@@ -6,8 +6,8 @@
 # License: BSD-3-Clause-LBNL
 
 # This script modifies `WarpX-test.ini` (which is used for nightly builds)
-# and creates the file `travis-test.ini` (which is used for continous
-# integration on TravisCI (https://travis-ci.org/)
+# and creates the file `ci-test.ini` (which is used for continous
+# integration)
 # The subtests that are selected are controlled by WARPX_TEST_DIM
 # The architecture (CPU/GPU) is selected by WARPX_TEST_ARCH
 import re
@@ -150,5 +150,5 @@ if ci_eb:
 # - Add the selected test blocks to the text
 text = text + '\n' + '\n'.join(test_blocks)
 
-with open('travis-tests.ini', 'w') as f:
+with open('ci-tests.ini', 'w') as f:
     f.write(text)
