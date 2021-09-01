e790052c2d6285fb03a479ce82c5ce8abec391ed
Phil Miller <unmobile+gh@gmail.com>
2021-08-31 22:50:40 -0400

    Add profiling to electrostatic charge deposition and solver (#62)
    * Add profiling for charge deposition
    
    * Add profiling for ME-specific charge deposition code

--- Source/FieldSolver/ElectrostaticSolver.cpp
+++ Source/FieldSolver/ElectrostaticSolver.cpp
@@ -13,6 +13,7 @@
 #include "Utils/WarpXAlgorithmSelection.H"
 #include "Utils/WarpXConst.H"
 #include "Utils/WarpXUtil.H"
+#include "Utils/WarpXProfilerWrapper.H"
 
 #include <AMReX_Array.H>
 #include <AMReX_Array4.H>
@@ -66,6 +67,7 @@ WarpX::DepositChargeDensity (WarpXParticleContainer& species, const bool local,
 void
 WarpX::ChargeDensityGridProcessing ()
 {
+    WARPX_PROFILE("WarpX::ChargeDensityGridProcessing");
     for (int lev = 0; lev <= max_level; lev++) {
         ApplyFilterandSumBoundaryRho (lev, lev, *rho_fp[lev], 0, 1);
     }
@@ -79,8 +81,10 @@ WarpX::ChargeDensityGridProcessing ()
 void
 WarpX::ComputeSpaceChargeField (bool const reset_fields)
 {
+    WARPX_PROFILE("WarpX::ComputeSpaceChargeField");
     if (reset_fields) {
         // Reset all E and B fields to 0, before calculating space-charge fields
+        WARPX_PROFILE("WarpX::ComputeSpaceChargeField::reset_fields");
         for (int lev = 0; lev <= max_level; lev++) {
             for (int comp=0; comp<3; comp++) {
                 Efield_fp[lev][comp]->setVal(0);
@@ -112,6 +116,7 @@ WarpX::ComputeSpaceChargeField (bool const reset_fields)
 void
 WarpX::AddSpaceChargeField (WarpXParticleContainer& pc)
 {
+    WARPX_PROFILE("WarpX::AddSpaceChargeField");
 
 #ifdef WARPX_DIM_RZ
     AMREX_ALWAYS_ASSERT_WITH_MESSAGE(n_rz_azimuthal_modes == 1,
@@ -155,6 +160,7 @@ WarpX::AddSpaceChargeField (WarpXParticleContainer& pc)
 void
 WarpX::AddSpaceChargeFieldLabFrame ()
 {
+    WARPX_PROFILE("WarpX::AddSpaceChargeFieldLabFrame");
 
 #ifdef WARPX_DIM_RZ
     AMREX_ALWAYS_ASSERT_WITH_MESSAGE(n_rz_azimuthal_modes == 1,
