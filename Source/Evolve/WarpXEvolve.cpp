9623ce9301b52327190452f2abf6b9303d7d83dc
RevathiJambunathan <revanathan@gmail.com>
2021-09-13 14:37:32 -0700

    clean and move time-update

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -239,7 +239,6 @@ WarpX::Evolve (int numsteps)
             ++istep[lev];
         }
 
-        cur_time += dt[0];
 
         ShiftGalileanBoundary();
 
@@ -252,21 +251,13 @@ WarpX::Evolve (int numsteps)
         }
 
 
-        // sync up time
-        for (int i = 0; i <= max_level; ++i) {
-            t_new[i] = cur_time;
-        }
-        //if (cur_time + dt[0] > stop_time || step == numsteps_max-1) {
-        //    // perform BTD, force_flush=true, BackTransform=true
-        //    amrex::Print() << " compulsory : " << step << "  " << numsteps_max-1 << "  " << cur_time + dt[0] << " " << stop_time << "\n";
-        //    multi_diags->FilterComputePackFlush( step, true, true );
-        //} else {
-        //    // perform BTD, force_flush=false, BackTransform=true
-        //    amrex::Print() << " filter compuite pack and flush : " << step << "  " << numsteps_max-1 << "  " << cur_time + dt[0] << " " << stop_time - 1.e-3*dt[0] << "\n";
-        //    multi_diags->FilterComputePackFlush( step, false, true );
+        //// sync up time
+        //for (int i = 0; i <= max_level; ++i) {
+        //    t_new[i] = cur_time;
         //}
         multi_diags->FilterComputePackFlush( step, false, true );
 
+        cur_time += dt[0];
         bool move_j = is_synchronized;
         // If is_synchronized we need to shift j too so that next step we can evolve E by dt/2.
         // We might need to move j because we are going to make a plotfile.
