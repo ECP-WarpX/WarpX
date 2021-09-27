e407907c69140d91e5efd6e71f0deed9c38fbfc1
Hannah Klion <hannah.klion@gmail.com>
2021-09-27 14:15:56 -0700

    Split GetTemperature implementations implementations into .cpp

---
+++ Source/Initialization/GetTemperature.cpp
@@ -0,0 +1,40 @@
+/* Copyright 2021 Hannah Klion
+ *
+ *
+ * This file is part of WarpX.
+ *
+ * License: BSD-3-Clause-LBNL
+ */
+
+#include <Initialization/GetTemperature.H>
+
+GetTemperature::GetTemperature (TemperatureProperties const& temp) noexcept {
+    m_type = temp.m_type;
+    if (m_type == ConstantValue) {
+        m_temperature = temp.m_temperature;
+    }
+    else if (m_type == ParserFunction) {
+        m_temperature_parser = temp.m_ptr_temperature_parser->compile<3>();
+    }
+}
+
+amrex::Real GetTemperature::operator() (amrex::Real x, amrex::Real y, amrex::Real z) const noexcept
+{
+    switch (m_type)
+    {
+        case (ConstantValue):
+        {
+            return m_temperature;
+        }
+        case (ParserFunction):
+        {
+            return m_temperature_parser(x,y,z);
+        }
+        default:
+        {
+            amrex::Abort("Get initial temperature: unknown type");
+            return 0.0;
+        }
+    }
+
+}
