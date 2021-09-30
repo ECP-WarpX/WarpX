a1cff56f967f60684cf410d9ad15a79214cc2ffa
Andrew Myers <atmyers2@gmail.com>
2021-09-29 18:22:21 -0700

    bump tol again

--- Examples/analysis_default_regression.py
+++ Examples/analysis_default_regression.py
@@ -13,7 +13,7 @@ test_name = fn[:-9] # Could also be os.path.split(os.getcwd())[1]
 
 # Run checksum regression test
 if re.search( 'single_precision', fn ):
-    checksumAPI.evaluate_checksum(test_name, fn, rtol=1.e-7)
+    checksumAPI.evaluate_checksum(test_name, fn, rtol=2.e-6)
 else:
     checksumAPI.evaluate_checksum(test_name, fn)
 
