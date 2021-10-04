90d664a765aa46f53851302f6ef26648c08cbbf9
NeilZaim <neilzaim@hotmail.com>
2021-10-04 12:06:01 +0200

    Cleaner python script

--- Regression/Checksum/update_benchmarks_from_azure_output.py
+++ Regression/Checksum/update_benchmarks_from_azure_output.py
@@ -1,51 +1,38 @@
 import re
 import sys
+import json
 
 azure_output_filename = sys.argv[1]
 
-first_char = 29 # first non prefix character
+first_char = 29 # first non prefix character, found empirically
 test_name_str = "working on test:"
-test_name_first_char = 50 # start of test name
-test_name_last_char = -5 # end of test name
+test_name_first_char = 50 # start of test name, found empirically
+test_name_last_char = -5 # end of test name, found empirically
 plotfile_str = "Plotfile : "
 benchmark_path = "benchmarks_json/"
 benchmark_suffix = ".json"
 
 current_test = ""
 
-f = open(azure_output_filename, "r")
-
-for line in f:
-    if (line.find(test_name_str)!= -1):
-        current_test = line[test_name_first_char:test_name_last_char]
-    line_noprefix = line[first_char:]
-    # Here we parse lines that read, for example,
-    # "Plotfile : [lev=0,jx] 1.012345678901234e+16"
-    # and we set key1 = "lev=0", key2 = "jx", value = 1.012345678901234e+16
-    if (line_noprefix.startswith(plotfile_str)):
-        key1 = re.search('\[(.*),', line_noprefix).group(1)
-        key2 = re.search(',(.*)\]', line_noprefix).group(1)
-        value = re.search('\](.*)', line_noprefix).group(1)[1:]
-        f_json = open(benchmark_path+current_test+benchmark_suffix, "r")
-        json_data = f_json.readlines()
-        f_json.close()
-        key1_found = False
-        for i, line2 in enumerate(json_data):
-            if (line2.find(key1) != -1):
-                key1_found = True
-            if (key1_found and (line2.find(key2) != -1)):
-                prefix = re.search('(.*):', line2).group(1)
-                add_comma = True
-                if (json_data[i+1].find("}") != -1):
-                    add_comma = False
-                if (add_comma):
-                    json_data[i] = prefix+": "+value+",\n"
-                else:
-                    json_data[i] = prefix+": "+value+"\n"
-                break
-        f_json = open(benchmark_path+current_test+benchmark_suffix, "w")
-        f_json.writelines(json_data)
-        f_json.close()
-
-f.close()
-
+with open(azure_output_filename, "r") as f:
+    for line in f:
+        # Here we parse lines that read, for example,
+        # "working on test: bilinear_filter"
+        # and we set current_test = "bilinear_filter"
+        if (line.find(test_name_str)!= -1):
+            current_test = line[test_name_first_char:test_name_last_char]
+        line_noprefix = line[first_char:]
+        # Here we parse lines that read, for example,
+        # "Plotfile : [lev=0,jx] 1.012345678901234e+16"
+        # and we set key1 = "lev=0", key2 = "jx", value = 1.012345678901234e+16
+        if (line_noprefix.startswith(plotfile_str)):
+            key1 = re.search('\[(.*),', line_noprefix).group(1)
+            key2 = re.search(',(.*)\]', line_noprefix).group(1)
+            value = float(re.search('\](.*)', line_noprefix).group(1)[1:])
+            # Update json file with the new value
+            json_filepath = benchmark_path+current_test+benchmark_suffix
+            with open(json_filepath, "r") as f_json:
+                json_data = json.load(f_json)
+            json_data[key1][key2] = value
+            with open(json_filepath, "w") as f_json:
+                json.dump(json_data, f_json, indent=2)
