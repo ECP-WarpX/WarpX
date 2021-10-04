6cd03c47ab1926075b38f5514583b54cc2f731c4
Edoardo Zoni <ezoni@lbl.gov>
2021-10-04 11:57:43 -0700

    Single Loop over Particles and Fields

--- Examples/Tests/restart/analysis_restart.py
+++ Examples/Tests/restart/analysis_restart.py
@@ -6,54 +6,30 @@ import numpy as np
 sys.path.insert(1, '../../../../warpx/Regression/Checksum/')
 import checksumAPI
 
+# Load output data generated after restart
 filename = sys.argv[1]
 ds_restart = yt.load( filename )
-
-benchmark = 'orig_' + filename
-ds_benchmark = yt.load( benchmark )
-
-# Check particle output
-ad_restart = ds_restart.all_data()
-ad_benchmark = ds_benchmark.all_data()
-
-all_species = ['beam', 'plasma_e', 'plasma_p', 'driver', 'driverback']
-all_attrs = ['particle_position_x', 'particle_position_y', 'particle_position_z',
-             'particle_momentum_x', 'particle_momentum_y', 'particle_momentum_z',
-             'particle_weight']
-
-tolerance = 1e-15
-print('\ntolerance (particle output) = {:g}'.format(tolerance))
-for species in all_species:
-    print()
-    print('species: {:s}'.format(species))
-    for attr in all_attrs:
-        dr = ad_restart[species, attr].to_ndarray()
-        db = ad_benchmark[species, attr].to_ndarray()
-        dr.sort()
-        db.sort()
-        error = np.max(np.abs(dr - db))
-        print('attribute: {:s}; error = {:g}'.format(attr, error))
-        assert(error < tolerance)
-print()
-
-# Check field output
 ad_restart = ds_restart.covering_grid(level = 0,
         left_edge = ds_restart.domain_left_edge, dims = ds_restart.domain_dimensions)
+
+# Load output data generated from initial run
+benchmark = 'orig_' + filename
+ds_benchmark = yt.load( benchmark )
 ad_benchmark = ds_benchmark.covering_grid(level = 0,
         left_edge = ds_benchmark.domain_left_edge, dims = ds_benchmark.domain_dimensions)
 
-all_fields = ['Ex', 'Ey', 'Ez', 'Bx', 'By', 'Bz', 'jx', 'jy', 'jz']
-
+# Loop over all fields (all particle species, all particle attributes, all grid fields)
+# and compare output data generated from initial run with output data generated after restart
 tolerance = 1e-12
-print('tolerance (field output) = {:g}'.format(tolerance))
+print('\ntolerance = {:g}'.format(tolerance))
 print()
-for field in all_fields:
-    dr = ad_restart['boxlib', field].squeeze().v
-    db = ad_benchmark['boxlib', field].squeeze().v
+for field in ds_benchmark.field_list:
+    dr = ad_restart[field].squeeze().v
+    db = ad_benchmark[field].squeeze().v
     error = np.amax(np.abs(dr - db))
     if (np.amax(np.abs(db)) != 0.):
         error /= np.amax(np.abs(db))
-    print('field: {:s}; relative error = {:g}'.format(field, error))
+    print('field: {}; error = {:g}'.format(field, error))
     assert(error < tolerance)
 print()
 
