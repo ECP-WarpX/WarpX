5093b763674892296519a391b3a70f2ebeabdd10
Edoardo Zoni <59625522+EZoni@users.noreply.github.com>
2021-10-13 14:36:19 -0700

    Multi-J Algo: Synchronize Nodal Points of E,B (#2406)
    * Multi-J Algo: Synchronize Nodal Points of E,B
    
    * Update Benchmark of multi_J_2d_psatd

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -585,6 +585,10 @@ WarpX::OneStep_multiJ (const amrex::Real cur_time)
         FillBoundaryB(guard_cells.ng_alloc_EB);
         if (WarpX::do_dive_cleaning) FillBoundaryF(guard_cells.ng_alloc_F);
         if (WarpX::do_divb_cleaning) FillBoundaryG(guard_cells.ng_alloc_G);
+
+        // Synchronize E and B fields on nodal points
+        NodalSync(Efield_fp, Efield_cp);
+        NodalSync(Bfield_fp, Bfield_cp);
     }
     else
     {
