3ca928743fdeb5969a7575ca6f4a5a6fc444e3c8
Luca Fedeli <luca.fedeli@cea.fr>
2021-10-15 12:07:52 +0200

    fix bug

--- Source/Laser/LaserProfilesImpl/LaserProfileGaussian.cpp
+++ Source/Laser/LaserProfilesImpl/LaserProfileGaussian.cpp
@@ -71,7 +71,7 @@ WarpXLaserProfiles::GaussianLaserProfile::init (
     auto arg = m_params.stc_direction[0]*m_common_params.p_X[0] +
         m_params.stc_direction[1]*m_common_params.p_X[1] +
         m_params.stc_direction[2]*m_common_params.p_X[2];
-    targmp = std::max(-1.0_rt,std::min(1.0_rt, arg)); //limit arg between -1 and 1
+    arg = std::max(-1.0_rt,std::min(1.0_rt, arg)); //limit arg between -1 and 1
     m_params.theta_stc = std::acos(arg);
 #else
     m_params.theta_stc = 0.;
