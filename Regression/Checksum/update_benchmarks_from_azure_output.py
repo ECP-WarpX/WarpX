1eacb3b421e6ca834b27ba20e4655f15682d7992
NeilZaim <neilzaim@hotmail.com>
2021-10-21 14:36:42 +0200

    Remove empirical numbers and read and write the json files once

--- Regression/Checksum/update_benchmarks_from_azure_output.py
+++ Regression/Checksum/update_benchmarks_from_azure_output.py
@@ -4,35 +4,47 @@ import json
 
 azure_output_filename = sys.argv[1]
 
-first_char = 29 # first non prefix character, found empirically
-test_name_str = "working on test:"
-test_name_first_char = 50 # start of test name, found empirically
-test_name_last_char = -5 # end of test name, found empirically
-plotfile_str = "Plotfile : "
+pattern_test_name = 'working on test: (?P<testname>\w*)'
+pattern_keys_value = 'Plotfile : \[(?P<key1>.*),(?P<key2>.*)\] (?P<value>\w.*)'
 benchmark_path = "benchmarks_json/"
 benchmark_suffix = ".json"
 
+json_file_updated = False
 current_test = ""
+
+def dump_json_data(json_filepath, json_data):
+    with open(json_filepath, "w") as f_json:
+        json.dump(json_data, f_json, indent=2)
 
 with open(azure_output_filename, "r") as f:
     for line in f:
-        # Here we parse lines that read, for example,
+        # Here we search lines that read, for example,
         # "working on test: bilinear_filter"
         # and we set current_test = "bilinear_filter"
-        if (line.find(test_name_str)!= -1):
-            current_test = line[test_name_first_char:test_name_last_char]
-        line_noprefix = line[first_char:]
-        # Here we parse lines that read, for example,
+        match_test_name = re.search(pattern_test_name, line)
+        if match_test_name:
+            # If needed, update json file
+            if json_file_updated:
+                dump_json_data(json_filepath, json_data)
+            current_test = match_test_name.group('testname')
+            json_file_updated = False
+
+        # Here we search lines that read, for example,
         # "Plotfile : [lev=0,jx] 1.012345678901234e+16"
-        # and we set key1 = "lev=0", key2 = "jx", value = 1.012345678901234e+16
-        if (line_noprefix.startswith(plotfile_str)):
-            key1 = re.search('\[(.*),', line_noprefix).group(1)
-            key2 = re.search(',(.*)\]', line_noprefix).group(1)
-            value = float(re.search('\](.*)', line_noprefix).group(1)[1:])
-            # Update json file with the new value
-            json_filepath = benchmark_path+current_test+benchmark_suffix
-            with open(json_filepath, "r") as f_json:
-                json_data = json.load(f_json)
+        # and we update the corresponding json data with the new value
+        match_keys_value = re.search(pattern_keys_value, line)
+        if match_keys_value:
+            # If not already done, read the json file and store it in json_data
+            if not json_file_updated:
+                json_filepath = benchmark_path+current_test+benchmark_suffix
+                with open(json_filepath, "r") as f_json:
+                    json_data = json.load(f_json)
+            key1 = match_keys_value.group('key1') # "lev=0" in the example
+            key2 = match_keys_value.group('key2') # "jx" in the example
+            value = float(match_keys_value.group('value')) # "1.012345678901234e+16" in the example
             json_data[key1][key2] = value
-            with open(json_filepath, "w") as f_json:
-                json.dump(json_data, f_json, indent=2)
+            json_file_updated = True
+
+# If needed, update json file for the last test
+if json_file_updated:
+    dump_json_data(json_filepath, json_data)
