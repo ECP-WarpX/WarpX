cd9ad330e2eb7e239618637bfd08ed5792b92eea
Remi Lehe <remi.lehe@normalesup.org>
2021-10-26 12:47:09 -0700

    Revert latest amrex updates in order to fix CI (#2494)
    * Revert "Update AMReX to include EB solver fix (#2484)"
    
    This reverts commit 105e127b95db5707d17cadc77fe68f1d68453d06.
    
    * Revert "AMReX/PICSAR: Weekly Update (#2478)"
    
    This reverts commit 7341fa0eda39368b1c6f2cf6d6dc20462fd6c3fc.

--- run_test.sh
+++ run_test.sh
@@ -51,7 +51,7 @@ echo "cd $PWD"
 
 # Clone PICSAR, AMReX and warpx-data
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach 652af10ae992e2cf5c1812e0908d9b88ac22126c && cd -
+cd amrex && git checkout --detach 09a0ec56378f9f6a5f68f3ac025318af746432d5 && cd -
 # Use QED brach for QED tests
 git clone https://github.com/ECP-WarpX/picsar.git
 cd picsar && git checkout --detach a78be127f66adc1558f527edc8964e37e3a055ff && cd -
