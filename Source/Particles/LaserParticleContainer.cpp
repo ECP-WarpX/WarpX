619ac132237180ee724541c552acacfc61370531
Neil Zaim <neilzaim@hotmail.com>
2021-10-28 09:17:17 +0200

    Revert "Use parser for input parameters of type long"
    This reverts commit 9573bb3f693f1247e77faa433fd96dc294e68361.

--- Source/Particles/LaserParticleContainer.cpp
+++ Source/Particles/LaserParticleContainer.cpp
@@ -113,7 +113,7 @@ LaserParticleContainer::LaserParticleContainer (AmrCore* amr_core, int ispecies,
         );
 
     pp_laser_name.query("do_continuous_injection", do_continuous_injection);
-    queryWithParser(pp_laser_name, "min_particles_per_mode", m_min_particles_per_mode);
+    pp_laser_name.query("min_particles_per_mode", m_min_particles_per_mode);
 
     if (m_e_max == amrex::Real(0.)){
         WarpX::GetInstance().RecordWarning("Laser",
