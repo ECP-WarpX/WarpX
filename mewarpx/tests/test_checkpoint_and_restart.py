5c558f6aed7c6f9a80713717feb0ca9a3cccacdd
Kevin Z. Zhu <86268612+KZhu-ME@users.noreply.github.com>
2021-10-28 13:44:23 -0700

    Inject neutral plasma in checkpoint restart tests (#93)
    * Added plasma injection to checkpoint test
    
    * Remove ignore deprecation warning
    
    * Changed all paths to use os.path.join
    
    * Wrapped long lines

--- mewarpx/tests/test_checkpoint_and_restart.py
+++ mewarpx/tests/test_checkpoint_and_restart.py
@@ -1,11 +1,13 @@
 import os
 import pytest
 import logging
+import numpy as np
 
 from mewarpx.utils_store import util as mwxutil
 
 
 def test_create_checkpoints():
+    np.random.seed(47239475)
 
     # Initialize and import only when we know dimension
     mwxutil.init_libwarpx(ndim=2, rz=False)
@@ -24,6 +26,8 @@ def test_create_checkpoints():
     NZ = 64
     DT = 7.5e-10
 
+    P_INERT = 10
+
     MAX_STEPS = 10
 
     nx = 64
@@ -40,14 +44,19 @@ def test_create_checkpoints():
         DT=DT,
         TOTAL_TIMESTEPS=MAX_STEPS,
         DIAG_STEPS=DIAG_STEPS,
-        FIELD_DIAG_DATA_LIST=['phi']
+        P_INERT=P_INERT,
+        T_ELEC=1100 + 273.15,
+        INERT_GAS_TYPE="Ar",
+        FIELD_DIAG_DATA_LIST=['phi'],
+        PLASMA_DENSITY=10.2e12,
+        SEED_NPPC=10
     )
     # Only the functions we change from defaults are listed here
     run.setup_run(
         init_conductors=False,
         init_injectors=False,
-        init_inert_gas=False,
-        init_neutral_plasma=False,
+        init_inert_gas=True,
+        init_neutral_plasma=True,
         init_mcc=False,
         init_field_diag=True,
         init_simcontrol=False,
@@ -137,16 +146,19 @@ def test_restart_from_checkpoint(caplog, force, files_exist):
     else:
         restart = None
     try:
+
         mwxrun.init_run(
             restart=restart,
-            checkpoint_dir="../../test_files/checkpoint",
+            checkpoint_dir=os.path.join(testing_util.test_dir, "checkpoint"),
             checkpoint_prefix=prefix
         )
     except RuntimeError as e:
         if "There were no checkpoint directories starting with nonexistent_prefix!" in str(e):
-            assert force # There should only be an exception if this restart was forced
+            # There should only be an exception if this restart was forced
+            assert force
 
-    # if the files didn't exist then we didn't restart, so there's no need to verify the correct number of steps passed
+    # if the files didn't exist then we didn't restart,
+    # so there's no need to verify the correct number of steps passed
     if files_exist:
         new_max_steps = mwxrun.simulation.max_steps
 
@@ -155,7 +167,8 @@ def test_restart_from_checkpoint(caplog, force, files_exist):
         if not force and not files_exist:
             log_lines = [r.msg for r in caplog.records]
             assert any(
-                [f"There were no checkpoint directories starting with {prefix}!" in l for l in log_lines]
+                [f"There were no checkpoint directories starting with {prefix}!"
+                in l for l in log_lines]
             )
 
         mwxrun.simulation.step()
@@ -165,6 +178,7 @@ def test_restart_from_checkpoint(caplog, force, files_exist):
         assert end_step - start_step == new_max_steps
 
 def test_extra_steps_after_restart():
+    np.random.seed(47239475)
     mwxutil.init_libwarpx(ndim=2, rz=False)
     from mewarpx.setups_store import diode_setup
     from mewarpx.mwxrun import mwxrun
@@ -177,6 +191,8 @@ def test_extra_steps_after_restart():
     NX = 64
     NZ = 64
     DT = 7.5e-10
+
+    P_INERT = 10
 
     MAX_STEPS = 10
 
@@ -194,14 +210,19 @@ def test_extra_steps_after_restart():
         DT=DT,
         TOTAL_TIMESTEPS=MAX_STEPS,
         DIAG_STEPS=DIAG_STEPS,
-        FIELD_DIAG_DATA_LIST=['phi']
+        P_INERT=P_INERT,
+        T_ELEC=1100 + 273.15,
+        INERT_GAS_TYPE="Ar",
+        FIELD_DIAG_DATA_LIST=['phi'],
+        PLASMA_DENSITY=10.2e12,
+        SEED_NPPC=10
     )
 
     # Only the functions we change from defaults are listed here
     run.setup_run(
         init_conductors=False,
         init_injectors=False,
-        init_inert_gas=False,
+        init_inert_gas=True,
         init_neutral_plasma=False,
         init_mcc=False,
         init_field_diag=True,
@@ -212,7 +233,7 @@ def test_extra_steps_after_restart():
 
     mwxrun.init_run(
         restart=True,
-        checkpoint_dir="../../test_files/checkpoint",
+        checkpoint_dir=os.path.join(testing_util.test_dir, "checkpoint"),
         additional_steps=additional_steps
     )
 
@@ -223,3 +244,13 @@ def test_extra_steps_after_restart():
     end_step = mwxrun.get_it()
 
     assert start_step + additional_steps == end_step
+
+    restart_net_charge_density = np.load(os.path.join(
+        "diags", "fields", "Net_charge_density_0000000006.npy"
+        ))
+    original_net_charge_density = np.load(os.path.join(
+        testing_util.test_dir, "checkpoint", "Net_charge_density_0000000006.npy"
+        ))
+
+    assert np.allclose(restart_net_charge_density,
+                       original_net_charge_density, rtol=0.01)
