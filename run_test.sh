2a1b4a4a40cba3476bcd0a76f7d9033aa1e355d3
Axel Huebl <axel.huebl@plasma.ninja>
2021-11-02 08:31:02 -0700

    Release 21.11 (#2518)
    * Update: AMReX/PICSAR to 21.11
    
    AMReX is a post 21.11 release, due to a needed bugfix in compGrad
    with EB that landed 4hrs after release.
    
    * WarpX: 21.11

--- run_test.sh
+++ run_test.sh
@@ -51,7 +51,7 @@ echo "cd $PWD"
 
 # Clone PICSAR, AMReX and warpx-data
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach 614b9ebbbf04eb5b4532029ff4a7ae40e6663042 && cd -
+cd amrex && git checkout --detach aca6926e0a85a0fad4087bd47016edad0885252f && cd -
 # Use QED brach for QED tests
 git clone https://github.com/ECP-WarpX/picsar.git
 cd picsar && git checkout --detach a78be127f66adc1558f527edc8964e37e3a055ff && cd -
