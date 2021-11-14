196f033e5a4d5a04b7d795e718f50a2b96bc7a9d
Weiqun Zhang <weiqunzhang@lbl.gov>
2021-11-14 12:49:46 -0800

    Fix Instability in PML with PSATD
    Damping in PML should be applied before the communications between the
    regular grids and PML, and between PML grids take place, otherwise the ghost
    cells are filled with lagged information, which results in an instability.
    
    Closes #2525

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -408,6 +408,10 @@ WarpX::OneStep_nosub (Real cur_time)
         }
         PushPSATD();
 
+        if (do_pml) {
+            DampPML();
+        }
+
         if (use_hybrid_QED) {
             FillBoundaryE(guard_cells.ng_alloc_EB);
             FillBoundaryB(guard_cells.ng_alloc_EB);
@@ -428,7 +432,6 @@ WarpX::OneStep_nosub (Real cur_time)
         NodalSync(Bfield_fp, Bfield_cp);
 
         if (do_pml) {
-            DampPML();
             NodalSyncPML();
         }
     } else {
