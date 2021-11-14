7f4fc88e61f503559934fcc8313ef8df3fdc7d90
Revathi  Jambunathan <41089244+RevathiJambunathan@users.noreply.github.com>
2021-11-13 21:34:58 -0800

    Update Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveE.cpp
    Co-authored-by: Edoardo Zoni <59625522+EZoni@users.noreply.github.com>

--- Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveE.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveE.cpp
@@ -100,9 +100,9 @@ void FiniteDifferenceSolver::MacroscopicEvolveECartesian (
     std::unique_ptr<MacroscopicProperties> const& macroscopic_properties)
 {
 
-    auto& sigma_mf = macroscopic_properties->getsigma_mf();
-    auto& epsilon_mf = macroscopic_properties->getepsilon_mf();
-    auto& mu_mf = macroscopic_properties->getmu_mf();
+    amrex::MultiFab& sigma_mf = macroscopic_properties->getsigma_mf();
+    amrex::MultiFab& epsilon_mf = macroscopic_properties->getepsilon_mf();
+    amrex::MultiFab& mu_mf = macroscopic_properties->getmu_mf();
 
     // Index type required for calling CoarsenIO::Interp to interpolate macroscopic
     // properties from their respective staggering to the Ex, Ey, Ez locations
