6d92f32b360ce26f0fdc41f1689a63a3cc95049c
Hannah Klion <hannah.klion@gmail.com>
2021-11-29 18:46:24 -0800

    Add ParticleReductionFunctor
    * Adds a by-cell particle average diagnostic
    * Averaged quantity is given by an input parser in (x,y,z,ux,uy,uz)
    * Coordinates in these parsers are in the WarpX convention
    * Users specify which species the averages are calculated for. The
    averages are calculated for each given species individually.
    * If there are no particles in a cell, the average will be zero.

---
+++ Source/Diagnostics/ComputeDiagFunctors/ParticleReductionFunctor.cpp
@@ -0,0 +1,132 @@
+
+#include "ParticleReductionFunctor.H"
+
+#include "Diagnostics/ComputeDiagFunctors/ComputeDiagFunctor.H"
+#include "Particles/MultiParticleContainer.H"
+#include "Particles/WarpXParticleContainer.H"
+#include "Utils/CoarsenIO.H"
+#include "WarpX.H"
+
+#include <AMReX_Array.H>
+#include <AMReX_BLassert.H>
+#include <AMReX_IntVect.H>
+#include <AMReX_MultiFab.H>
+#include <AMReX_REAL.H>
+
+using namespace amrex::literals;
+
+ParticleReductionFunctor::ParticleReductionFunctor (const amrex::MultiFab* mf_src, const int lev,
+        amrex::IntVect crse_ratio, const std::string fn_str, const int ispec, const int ncomp)
+    : ComputeDiagFunctor(ncomp, crse_ratio), m_lev(lev), m_ispec(ispec)
+{
+    // mf_src will not be used, let's make sure it's null.
+    AMREX_ALWAYS_ASSERT(mf_src == nullptr);
+    // Write only in one output component.
+    AMREX_ALWAYS_ASSERT(ncomp == 1);
+
+    // Allocate and compile a parser based on the input string fn_str
+    m_map_fn_parser = std::make_unique<amrex::Parser>(makeParser(
+                fn_str, {"x", "y", "z", "ux", "uy", "uz"}));
+    m_map_fn = m_map_fn_parser->compile<6>();
+}
+
+void
+ParticleReductionFunctor::operator() (amrex::MultiFab& mf_dst, const int dcomp, const int /*i_buffer*/) const
+{
+    auto& warpx = WarpX::GetInstance();
+    // Guard cell is set to 1 for generality. However, for a cell-centered
+    // output Multifab, mf_dst, the guard-cell data is not needed especially considering
+    // the operations performend in the CoarsenAndInterpolate function.
+    constexpr int ng = 1;
+    // Temporary cell-centered, multi-component MultiFab for storing particles per cell.
+    amrex::MultiFab red_mf(warpx.boxArray(m_lev), warpx.DistributionMap(m_lev), 1, ng);
+    amrex::MultiFab ppc_mf(warpx.boxArray(m_lev), warpx.DistributionMap(m_lev), 1, ng);
+    // Set value to 0, and increment the value in each cell with ppc.
+    red_mf.setVal(0._rt);
+    ppc_mf.setVal(0._rt);
+    auto& pc = warpx.GetPartContainer().GetParticleContainer(m_ispec);
+    // Copy over compiled parser function so that it can be captured by value in the lambda
+    auto map_fn = m_map_fn;
+    ParticleToMesh(pc, red_mf, m_lev,
+            [=] AMREX_GPU_DEVICE (const WarpXParticleContainer::SuperParticleType& p,
+                amrex::Array4<amrex::Real> const& out_array,
+                amrex::GpuArray<amrex::Real,AMREX_SPACEDIM> const& plo,
+                amrex::GpuArray<amrex::Real,AMREX_SPACEDIM> const& dxi)
+            {
+                // Get position in WarpX convention to use in parser. Will be different from
+                // p.pos() for 2D simulations.
+                amrex::ParticleReal xw = 0, yw = 0, zw = 0;
+                get_particle_position(p, xw, yw, zw);
+
+                // Get position in AMReX convention to calculate corresponding index.
+                // Ideally this will be replaced with the AMReX NGP interpolator
+                amrex::ParticleReal x = 0, y = 0, z = 0;
+                amrex::Real lx, ly, lz;
+                int ii = 0, jj = 0, kk = 0;
+                // Always do x direction
+                x = p.pos(0);
+                lx = (x - plo[0]) * dxi[0];
+                ii = static_cast<int>(amrex::Math::floor(lx));
+                if (AMREX_SPACEDIM >= 2) {
+                    y = p.pos(1);
+                    ly = (y - plo[1]) * dxi[1];
+                    jj = static_cast<int>(amrex::Math::floor(ly));
+                }
+                if (AMREX_SPACEDIM == 3) {
+                    z = p.pos(2);
+                    lz = (z - plo[2]) * dxi[2];
+                    kk = static_cast<int>(amrex::Math::floor(lz));
+                }
+
+                // Fix dimensions since parser assumes u = gamma * v / c
+                amrex::ParticleReal ux = p.rdata(PIdx::ux) / PhysConst::c;
+                amrex::ParticleReal uy = p.rdata(PIdx::uy) / PhysConst::c;
+                amrex::ParticleReal uz = p.rdata(PIdx::uz) / PhysConst::c;
+                amrex::Real value = map_fn(xw, yw, zw, ux, uy, uz);
+                amrex::Gpu::Atomic::AddNoRet(&out_array(ii, jj, kk, 0), p.rdata(PIdx::w) * value);
+            });
+    // Add the weight for each particle -- total number of particles of this species
+    ParticleToMesh(pc, ppc_mf, m_lev,
+            [=] AMREX_GPU_DEVICE (const WarpXParticleContainer::SuperParticleType& p,
+                amrex::Array4<amrex::Real> const& out_array,
+                amrex::GpuArray<amrex::Real,AMREX_SPACEDIM> const& plo,
+                amrex::GpuArray<amrex::Real,AMREX_SPACEDIM> const& dxi)
+            {
+
+                // Get position in AMReX convention to calculate corresponding index.
+                // Ideally this will be replaced with the AMReX NGP interpolator
+                amrex::ParticleReal x = 0, y = 0, z = 0;
+                amrex::Real lx, ly, lz;
+                int ii = 0, jj = 0, kk = 0;
+
+                x = p.pos(0);
+                lx = (x - plo[0]) * dxi[0];
+                ii = static_cast<int>(amrex::Math::floor(lx));
+                if (AMREX_SPACEDIM >= 2) {
+                    y = p.pos(1);
+                    ly = (y - plo[1]) * dxi[1];
+                    jj = static_cast<int>(amrex::Math::floor(ly));
+                }
+                if (AMREX_SPACEDIM == 3) {
+                    z = p.pos(2);
+                    lz = (z - plo[2]) * dxi[2];
+                    kk = static_cast<int>(amrex::Math::floor(lz));
+                }
+                amrex::Gpu::Atomic::AddNoRet(&out_array(ii, jj, kk, 0), p.rdata(PIdx::w));
+            });
+    // Divide value by number of particles for average. Set average to zero if there are no particles
+    for (amrex::MFIter mfi(red_mf, amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi)
+    {
+        const amrex::Box& box = mfi.tilebox();
+        amrex::Array4<amrex::Real> const& a_red = red_mf.array(mfi);
+        amrex::Array4<amrex::Real const> const& a_ppc = ppc_mf.const_array(mfi);
+        amrex::ParallelFor(box,
+                [=] AMREX_GPU_DEVICE (int i, int j, int k) {
+                    if (a_ppc(i,j,k,0) == 0) a_red(i,j,k,0) = 0;
+                    else a_red(i,j,k,0) = a_red(i,j,k,0) / a_ppc(i,j,k,0);
+                });
+    }
+
+    // Coarsen and interpolate from ppc_mf to the output diagnostic MultiFab, mf_dst.
+    CoarsenIO::Coarsen(mf_dst, red_mf, dcomp, 0, nComp(), 0, m_crse_ratio);
+}
