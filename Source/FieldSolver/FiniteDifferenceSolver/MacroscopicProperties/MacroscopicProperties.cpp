74af6918125938247e5e1b7ae9f8eba1e01161f7
Prabhat Kumar <89051199+prkkumar@users.noreply.github.com>
2021-12-08 15:27:38 -0800

    Replace AMREX_SPACEDIM: Evolve & FieldSolver (#2642)
    * AMREX_SPACEDIM : Boundary Conditions
    
    * AMREX_SPACEDIM : Parallelization
    
    * Fix compilation
    
    * AMREX_SPACEDIM : Initialization
    
    * Fix Typo
    
    * space
    
    * AMREX_SPACEDIM : Particles
    
    * AMREX_SPACEDIM : Evolve and FieldSolver

--- Source/FieldSolver/FiniteDifferenceSolver/MacroscopicProperties/MacroscopicProperties.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/MacroscopicProperties/MacroscopicProperties.cpp
@@ -177,7 +177,7 @@ MacroscopicProperties::InitData ()
         Ez_IndexType[idim]      = Ez_stag[idim];
         macro_cr_ratio[idim]    = 1;
     }
-#if (AMREX_SPACEDIM==2)
+#if defined(WARPX_DIM_XZ) || defined(WARPX_DIM_RZ)
         sigma_IndexType[2]   = 0;
         epsilon_IndexType[2] = 0;
         mu_IndexType[2]      = 0;
@@ -208,7 +208,7 @@ MacroscopicProperties::InitializeMacroMultiFabUsingParser (
                 // Shift x, y, z position based on index type
                 amrex::Real fac_x = (1._rt - iv[0]) * dx_lev[0] * 0.5_rt;
                 amrex::Real x = i * dx_lev[0] + real_box.lo(0) + fac_x;
-#if (AMREX_SPACEDIM==2)
+#if defined(WARPX_DIM_XZ) || defined(WARPX_DIM_RZ)
                 amrex::Real y = 0._rt;
                 amrex::Real fac_z = (1._rt - iv[1]) * dx_lev[1] * 0.5_rt;
                 amrex::Real z = j * dx_lev[1] + real_box.lo(1) + fac_z;
