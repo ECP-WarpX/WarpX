Fix for removing checkpoints in wrong order (#122)
* Correctly sort list of checkpoints to remove

A previous PR sorted the list to load correctly, but not when removing
checkpoints. It's now refactored into a separate function that both use.

Some style edits are included too.

* Update version and changelog.

* Add date/time/version printout at init

* Style fix - line break

Co-authored-by: Kevin Z. Zhu <86268612+KZhu-ME@users.noreply.github.com>

mewarpx/tests/test_checkpoint_and_restart.py
@@ -19,6 +19,7 @@ DT = 7.5e-10
 
 P_INERT = 10
 
+
 def get_run():
     """Utility function to get the same run setup for all tests below."""
     from mewarpx.setups_store import diode_setup
@@ -57,6 +58,7 @@ def get_run():
         init_warpx=False
     )
     return run
+
 
 def test_create_checkpoints():
 
@@ -196,6 +198,7 @@ def test_restart_from_checkpoint(caplog, force, files_exist):
 
         assert end_step - start_step == new_max_steps
 
+
 def test_extra_steps_after_restart():
 
     mwxutil.init_libwarpx(ndim=2, rz=False)
@@ -237,11 +240,11 @@ def test_extra_steps_after_restart():
     assert np.allclose(restart_net_charge_density,
                        original_net_charge_density, rtol=0.1)
 
+
 def test_checkpoints_fluxdiag():
 
     mwxutil.init_libwarpx(ndim=2, rz=False)
     from mewarpx.mwxrun import mwxrun
-    from mewarpx.diags_store.checkpoint_diagnostic import CheckPointDiagnostic
     from mewarpx.diags_store.flux_diagnostic import FluxDiagFromFile
     from mewarpx.utils_store import testing_util
 