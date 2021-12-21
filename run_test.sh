1c8935ac0cf9d9b826ed5e02796ff8ba631f9fc7
Axel Huebl <axel.huebl@plasma.ninja>
2021-12-21 11:00:47 -0800

    Update AMReX (RZ Poisson) (#2699)
    Update AMReX to the latest `development`.

--- run_test.sh
+++ run_test.sh
@@ -66,7 +66,7 @@ python3 -m pip install --upgrade -r warpx/Regression/requirements.txt
 
 # Clone PICSAR, AMReX and warpx-data
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach 134d33e4b7f0c9999ab9cfc2f1c129c47be31cee && cd -
+cd amrex && git checkout --detach 85284ea15a3c5ca7d6d8e3f1045ac1e5a48c6a19 && cd -
 # Use QED brach for QED tests
 git clone https://github.com/ECP-WarpX/picsar.git
 cd picsar && git checkout --detach 7b5449f92a4b30a095cc4a67f0a8b1fc69680e15 && cd -
