351796d83d2ca2a0e48353479f2c1c0b01447186
Axel Huebl <axel.huebl@plasma.ninja>
2021-12-28 01:55:11 -0800

    CI: mpi4py breaking setuptools API
    setuptools keeps breaking its API, which currently breaks `mpi4py`
    installs. Until a new mpi4py release is cut, this will serve as a
    work-around

--- run_test.sh
+++ run_test.sh
@@ -62,6 +62,10 @@ rm -rf py-venv
 python3 -m venv py-venv
 source py-venv/bin/activate
 python3 -m pip install --upgrade pip setuptools wheel
+# setuptools/mp4py work-around, see
+#   https://github.com/mpi4py/mpi4py/pull/159
+#   https://github.com/mpi4py/mpi4py/issues/157#issuecomment-1001022274
+export SETUPTOOLS_USE_DISTUTILS="stdlib"
 python3 -m pip install --upgrade -r warpx/Regression/requirements.txt
 
 # Clone PICSAR, AMReX and warpx-data
