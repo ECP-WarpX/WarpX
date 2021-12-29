1d5f2087c1667277722e2a592e3e300abd651555
Axel Huebl <axel.huebl@plasma.ninja>
2021-12-29 03:02:54 -0800

    AMReX 3D: BTD

--- Source/Diagnostics/BTDiagnostics.cpp
+++ Source/Diagnostics/BTDiagnostics.cpp
@@ -329,7 +329,7 @@ BTDiagnostics::InitializeBufferData ( int i_buffer , int lev)
                                    / dz_lab(warpx.getdt(lev), ref_ratio[m_moving_window_dir])                               ) );
     // Take the max of 0 and num_zcells_lab
     int Nz_lab = std::max( 0, num_zcells_lab );
-#if (AMREX_SPACEDIM >= 2)
+
     // Number of lab-frame cells in x-direction at level, lev
     const int num_xcells_lab = static_cast<int>( floor (
                                   ( diag_dom.hi(0) - diag_dom.lo(0) )
@@ -337,8 +337,7 @@ BTDiagnostics::InitializeBufferData ( int i_buffer , int lev)
                               ) );
     // Take the max of 0 and num_ycells_lab
     int Nx_lab = std::max( 0, num_xcells_lab);
-#endif
-#if defined(WARPX_DIM_3D)
+
     // Number of lab-frame cells in the y-direction at level, lev
     const int num_ycells_lab = static_cast<int>( floor (
                                    ( diag_dom.hi(1) - diag_dom.lo(1) )
@@ -347,11 +346,6 @@ BTDiagnostics::InitializeBufferData ( int i_buffer , int lev)
     // Take the max of 0 and num_xcells_lab
     int Ny_lab = std::max( 0, num_ycells_lab );
     m_snapshot_ncells_lab[i_buffer] = {Nx_lab, Ny_lab, Nz_lab};
-#elif defined(WARPX_DIM_XZ) || defined(WARPX_DIM_RZ)
-    m_snapshot_ncells_lab[i_buffer] = {Nx_lab, Nz_lab};
-#else
-    m_snapshot_ncells_lab[i_buffer] = amrex::IntVect(Nz_lab);
-#endif
 }
 
 void
