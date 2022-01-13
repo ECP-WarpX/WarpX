8f21186e74188b98f7b0d1c70b0978b833158663
lgiacome <lorenzo.giacome@cern.ch>
2022-01-13 12:34:47 +0100

    Initialize areas anyways for the initialization

--- Source/Initialization/WarpXInitData.cpp
+++ Source/Initialization/WarpXInitData.cpp
@@ -889,10 +889,10 @@ void WarpX::InitializeEBGridData (int lev)
 
             ComputeEdgeLengths(m_edge_lengths[lev], eb_fact);
             ScaleEdges(m_edge_lengths[lev], CellSize(lev));
-
+            ComputeFaceAreas(m_face_areas[lev], eb_fact);
+            ScaleAreas(m_face_areas[lev], CellSize(lev));
+            
             if (WarpX::maxwell_solver_id == MaxwellSolverAlgo::ECT) {
-                ComputeFaceAreas(m_face_areas[lev], eb_fact);
-                ScaleAreas(m_face_areas[lev], CellSize(lev));
                 MarkCells();
                 ComputeFaceExtensions();
             }
