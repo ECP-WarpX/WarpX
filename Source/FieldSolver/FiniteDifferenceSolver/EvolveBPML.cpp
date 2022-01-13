47577e4c8d06a49b05cfd3fd2f5ba5f9eea9f71d
lgiacome <lorenzo.giacome@cern.ch>
2022-01-13 11:37:27 +0100

    Another fix

--- Source/FieldSolver/FiniteDifferenceSolver/EvolveBPML.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/EvolveBPML.cpp
@@ -48,7 +48,7 @@ void FiniteDifferenceSolver::EvolveBPML (
    // Select algorithm (The choice of algorithm is a runtime option,
    // but we compile code for each algorithm, using templates)
 #ifdef WARPX_DIM_RZ
-    amrex::ignore_unused(Bfield, Efield, dt, dive_cleaning, face_areas);
+    amrex::ignore_unused(Bfield, Efield, dt, dive_cleaning);
     amrex::Abort("PML are not implemented in cylindrical geometry.");
 #else
     if (m_do_nodal) {
