e7c7d3f2bb859c25299e8676461f81e61c94cd0c
Neïl Zaim <49716072+NeilZaim@users.noreply.github.com>
2022-02-03 18:59:14 +0100

    Fix typo in laser assert (#2824)

--- Source/Particles/LaserParticleContainer.cpp
+++ Source/Particles/LaserParticleContainer.cpp
@@ -138,7 +138,7 @@ LaserParticleContainer::LaserParticleContainer (AmrCore* amr_core, int ispecies,
     AMREX_ALWAYS_ASSERT_WITH_MESSAGE(m_nvec[0] == amrex::Real(0),
         "Laser propagation direction must be 0 along x in 1D");
     AMREX_ALWAYS_ASSERT_WITH_MESSAGE(m_nvec[1] == amrex::Real(0),
-        "Laser propagation direction must be 0 along y in 2D");
+        "Laser propagation direction must be 0 along y in 1D");
 #endif
 
     // Plane normal
