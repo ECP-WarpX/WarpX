4ff558da801ead04b4461796874019b28c1d9d30
Dave Grote <grote1@llnl.gov>
2022-02-22 20:16:49 -0800

    Updated background stopping CI test

--- Examples/Tests/ion_stopping/analysis_ion_stopping.py
+++ Examples/Tests/ion_stopping/analysis_ion_stopping.py
@@ -53,37 +53,60 @@ def stopping_from_electrons(ne, Te, Zb, ion_mass):
 # Fetch background parameters and inital particle data
 ds0 = yt.load(f'{prefix}{len(last_it)*"0"}')
 ad0 = ds0.all_data()
-ne = float(ds0.parameters['ion_stopping.background_density'])
-Te = float(ds0.parameters['ion_stopping.background_temperature'])
+ne = float(ds0.parameters['ion_stopping_constant.background_density'])
+Te = float(ds0.parameters['ion_stopping_constant.background_temperature'])
 Zb = 1.  # Ion charge state
 ion_mass = m_p
 
-vx = ad0[('ions', 'particle_momentum_x')].to_ndarray()/m_p
-vy = ad0[('ions', 'particle_momentum_y')].to_ndarray()/m_p
-vz = ad0[('ions', 'particle_momentum_z')].to_ndarray()/m_p
-EE = 0.5*m_p*(vx**2 + vy**2 + vz**2)/e
+# For ions1, the background parameters are constants
+vx = ad0[('ions1', 'particle_momentum_x')].to_ndarray()/m_p
+vy = ad0[('ions1', 'particle_momentum_y')].to_ndarray()/m_p
+vz = ad0[('ions1', 'particle_momentum_z')].to_ndarray()/m_p
+EE1 = 0.5*m_p*(vx**2 + vy**2 + vz**2)/e
+
+# For ions2, the background parameters are parsed
+xx = ad0[('ions2', 'particle_position_x')].to_ndarray()/m_p
+yy = ad0[('ions2', 'particle_position_y')].to_ndarray()/m_p
+ne2 = np.where(xx > 0., 1.e20, 1.e21)
+Te2 = np.where(yy > 0., 1., 2.)
+
+vx = ad0[('ions2', 'particle_momentum_x')].to_ndarray()/m_p
+vy = ad0[('ions2', 'particle_momentum_y')].to_ndarray()/m_p
+vz = ad0[('ions2', 'particle_momentum_z')].to_ndarray()/m_p
+EE2 = 0.5*m_p*(vx**2 + vy**2 + vz**2)/e
 
 ds = yt.load(last_filename)
 ad = ds.all_data()
 dt = ds.current_time.to_value()/int(last_it)
 
 # Step through the same number of time steps
-a_EE = EE
+a_EE1 = EE1
+a_EE2 = EE2
 for it in range(int(last_it)):
-    dEdt = stopping_from_electrons(ne, Te, Zb, ion_mass)
-    a_EE *= np.exp(dEdt*dt)
+    dEdt1 = stopping_from_electrons(ne, Te, Zb, ion_mass)
+    a_EE1 *= np.exp(dEdt1*dt)
+    dEdt2 = stopping_from_electrons(ne2, Te2, Zb, ion_mass)
+    a_EE2 *= np.exp(dEdt2*dt)
 
 # Fetch the final particle data
-vx = ad[('ions', 'particle_momentum_x')].to_ndarray()/m_p
-vy = ad[('ions', 'particle_momentum_y')].to_ndarray()/m_p
-vz = ad[('ions', 'particle_momentum_z')].to_ndarray()/m_p
-EE = 0.5*m_p*(vx**2 + vy**2 + vz**2)/e
+vx = ad[('ions1', 'particle_momentum_x')].to_ndarray()/m_p
+vy = ad[('ions1', 'particle_momentum_y')].to_ndarray()/m_p
+vz = ad[('ions1', 'particle_momentum_z')].to_ndarray()/m_p
+EE1 = 0.5*m_p*(vx**2 + vy**2 + vz**2)/e
 
-error = np.abs(EE - a_EE)
-print('error = ', error)
+vx = ad[('ions2', 'particle_momentum_x')].to_ndarray()/m_p
+vy = ad[('ions2', 'particle_momentum_y')].to_ndarray()/m_p
+vz = ad[('ions2', 'particle_momentum_z')].to_ndarray()/m_p
+EE2 = 0.5*m_p*(vx**2 + vy**2 + vz**2)/e
+
+error1 = np.abs(EE1 - a_EE1)
+error2 = np.abs(EE2 - a_EE2)
+print('error with constant = ', error1)
+print('error with parsed = ', error2)
 print('tolerance = ', tolerance)
 
-assert np.all(error < tolerance)
+assert np.all(error1 < tolerance)
+assert np.all(error2 < tolerance)
 
 test_name = os.path.split(os.getcwd())[1]
-checksumAPI.reset_benchmark(test_name, last_filename)
+checksumAPI.evaluate_checksum(test_name, last_filename)
