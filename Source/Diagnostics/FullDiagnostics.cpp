29874ea647677f53f66708ee5e104be4d4436fbb
Hannah Klion <klion@lbl.gov>
2022-03-02 11:35:29 -0800

    use static_cast<int> pattern

--- Source/Diagnostics/FullDiagnostics.cpp
+++ Source/Diagnostics/FullDiagnostics.cpp
@@ -408,9 +408,9 @@ FullDiagnostics::InitializeFieldFunctors (int lev)
     // Species index to loop over species that dump rho per species
     int i = 0;
 
-    const int nvar = m_varnames_fields.size();
-    const int nspec = m_pfield_species.size();
-    const int ntot = nvar + m_pfield_varnames.size() * nspec;
+    const auto nvar = static_cast<int>(m_varnames_fields.size());
+    const auto nspec = static_cast<int>(m_pfield_species.size());
+    const auto ntot = static_cast<int>(nvar + m_pfield_varnames.size() * nspec);
 
     m_all_field_functors[lev].resize(ntot);
     // Fill vector of functors for all components except individual cylindrical modes.
