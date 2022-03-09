84e1b2204204b5e8e3111722362103d1e27cd6ed
tmsclark <thomas.clark@u-psud.fr>
2022-03-09 15:48:12 +0100

    corrections

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -665,6 +665,8 @@ WarpX::OneStep_sub1 (Real curtime)
     const int coarse_lev = 0;
 
     WARPX_ALWAYS_ASSERT_WITH_MESSAGE(finest_level == 1, "Must have exactly two levels");
+    
+    // TODO: we could save some charge depositions
 
     // i) Push particles and fields on the fine patch (first fine step)
     PushParticlesandDepose(fine_lev, curtime, DtType::FirstHalf);
