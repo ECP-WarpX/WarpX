c7e4c9d1d9107352183616d94a31e7ace6b41188
Ryan Sandberg <RSandberg@lbl.gov>
2022-04-11 22:04:16 -0400

    RZ + openPMD : don't transpose data, save data with shape (Nm, Nz, Nr) (#3030)
    * part_per_grid/cell : cartesian diag in RZ/openPMD
    
    * RZ+openPMD : flexible ZR or RZ order
    
    * fix logic if not in RZ
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix regression test, remove transpose and refactor
    
    * Update Source/Diagnostics/WarpXOpenPMD.cpp
    
    * Fix: Doxygen, Consistent Naming, Comments, Style
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Remi Lehe <remi.lehe@normalesup.org>
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- Examples/Tests/openpmd_rz/analysis_openpmd_rz.py
+++ Examples/Tests/openpmd_rz/analysis_openpmd_rz.py
@@ -10,12 +10,15 @@ ii = series.iterations[20]
 
 assert len(ii.meshes) == 7, 'improper number of meshes'
 
+# select j_t
 jt = ii.meshes['j']['t']
-(Nm, Nr, Nz) = jt.shape
+
+# this is in C (Python) order; r is the fastest varying index
+(Nm, Nz, Nr) = jt.shape
 
 assert Nm == 3, 'Wrong number of angular modes stored or possible incorrect ordering when flushed'
 assert Nr == 64, 'Wrong number of radial points stored or possible incorrect ordering when flushed'
 assert Nz == 512, 'Wrong number of z points stored or possible incorrect ordering when flushed'
 
-assert ii.meshes['part_per_grid'][io.Mesh_Record_Component.SCALAR].shape == [64,512], 'problem with part_per_grid'
-assert ii.meshes['rho_electrons'][io.Mesh_Record_Component.SCALAR].shape == [3, 64, 512], 'problem with rho_electrons'
+assert ii.meshes['part_per_grid'][io.Mesh_Record_Component.SCALAR].shape == [512,64], 'problem with part_per_grid'
+assert ii.meshes['rho_electrons'][io.Mesh_Record_Component.SCALAR].shape == [3, 512, 64], 'problem with rho_electrons'
