29b6985f06123a635cd4f1433f80d0571637530c
Kevin Z. Zhu <86268612+KZhu-ME@users.noreply.github.com>
2022-05-05 10:03:08 -0700

    Update nvidia key (#175)
    * Update nvidia signing keys
    
    * fixed nvidia key url

--- mewarpx/medocker/docker_shared/ubuntu_packages.sh
+++ mewarpx/medocker/docker_shared/ubuntu_packages.sh
@@ -61,7 +61,7 @@ if [ ${WARPXCOMPUTE} == CUDA ] ; then
     # install cuda toolkit
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
     mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+    apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
     add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
     apt-get update
     apt-get -y install cuda
