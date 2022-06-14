b208a376c5828922c5a36519112d343104f33748
Axel Huebl <axel.huebl@plasma.ninja>
2022-06-15 02:58:44 +0700

    AMReX/PICSAR: Weekly Update (#3177)
    * AMReX: Weekly Update
    
    * AMReX: Bump Once More
    
    to 1a2fc3dbee1ce0c168cdb66710458757e92a0605

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -241,7 +241,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "18d0a2861d31c52c65752a1d5856f54e08699ce3"
+set(WarpX_amrex_branch "833e61c076a708ae1680eab68713a2d40637085d"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
