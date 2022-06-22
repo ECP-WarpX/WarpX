4fb8b0b0ea427c34e0478e162abf3f2f660b54a9
Axel Huebl <axel.huebl@plasma.ninja>
2022-06-23 00:15:14 +0200

    AMReX/PICSAR: Weekly Update (#3192)
    * AMReX: Weekly Update
    
    * Reset: `reduced_diags_single_precision`
    
    * Reset: `background_mcc_dp_psp`

--- run_test.sh
+++ run_test.sh
@@ -71,7 +71,7 @@ python3 -m pip install --upgrade -r warpx/Regression/requirements.txt
 
 # Clone AMReX and warpx-data
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach 833e61c076a708ae1680eab68713a2d40637085d && cd -
+cd amrex && git checkout --detach 2d931f63cb4d611d0d23d694726889647f8a482d && cd -
 # warpx-data contains various required data sets
 git clone --depth 1 https://github.com/ECP-WarpX/warpx-data.git
 
