5b59fa07bccd6cc4408bdda08b53b1ff7fa060e1
RevathiJambunathan <revanathan@gmail.com>
2022-06-24 14:34:10 -0700

    move MovingWindow to start of PIC loop

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -119,6 +119,11 @@ WarpX::Evolve (int numsteps)
             }
         }
 
+        bool move_j = is_synchronized;
+        // If is_synchronized we need to shift j too so that next step we can evolve E by dt/2.
+        // We might need to move j because we are going to make a plotfile.
+        int num_moved = MoveWindow(step+1, move_j);
+
         // At the beginning, we have B^{n} and E^{n}.
         // Particles have p^{n} and x^{n}.
         // is_synchronized is true.
@@ -262,11 +267,6 @@ WarpX::Evolve (int numsteps)
             t_new[i] = cur_time;
         }
         multi_diags->FilterComputePackFlush( step, false, true );
-
-        bool move_j = is_synchronized;
-        // If is_synchronized we need to shift j too so that next step we can evolve E by dt/2.
-        // We might need to move j because we are going to make a plotfile.
-        int num_moved = MoveWindow(step+1, move_j);
 
         mypc->ContinuousFluxInjection(cur_time, dt[0]);
 
