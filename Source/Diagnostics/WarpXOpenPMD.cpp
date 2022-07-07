ec7ba17fa90fed2ddbd905c6baa990fb15a1d4a2
Neil Zaim <neilzaim@hotmail.com>
2022-07-07 17:55:20 +0200

    Species variables in diagnostics: deal with runtime components

--- Source/Diagnostics/WarpXOpenPMD.cpp
+++ Source/Diagnostics/WarpXOpenPMD.cpp
@@ -589,9 +589,7 @@ WarpXOpenPMDPlot::WriteOpenPMDParticles (const amrex::Vector<ParticleDiag>& part
     {
         real_names[x.second+PIdx::nattribs] = detail::snakeToCamel(x.first);
     }
-    // plot any "extra" fields by default
-    real_flags = particle_diags[i].plot_flags;
-    real_flags.resize(tmp.NumRealComps(), 1);
+    real_flags = particle_diags[i].m_plot_flags;
     // and the names
     int_names.resize(tmp.NumIntComps());
     auto runtime_inames = tmp.getParticleRuntimeiComps();
