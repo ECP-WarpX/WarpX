a61aef31d1c0a6c6dd398efcb8c357d1e25d75a4
Remi Lehe <remi.lehe@normalesup.org>
2022-07-11 08:06:34 -0700

    Progress in analysis script

--- Examples/Modules/nuclear_fusion/analysis_deuterium_tritium_fusion.py
+++ Examples/Modules/nuclear_fusion/analysis_deuterium_tritium_fusion.py
@@ -65,13 +65,13 @@ default_tol = 1.e-12 # Default relative tolerance
 reactant_species = ['deuterium', 'tritium']
 product_species = ['helium', 'neutron']
 
-from scipy.constants import m_u, m_p
 mass = {
-    'deuterium': 2.01410177812*m_u,
-    'tritium': 3.0160492779*m_u,
-    'helium': 4.00260325413*m_u,
-    'neutron': 1.0013784193052508*m_p
+    'deuterium': 2.01410177812*scc.m_u,
+    'tritium': 3.0160492779*scc.m_u,
+    'helium': 4.00260325413*scc.m_u,
+    'neutron': 1.0013784193052508*scc.m_p
 }
+m_reduced = np.product([mass[s] for s in reactant_species])/np.sum([mass[s] for s in reactant_species])
 
 ## Some physical parameters
 keV_to_Joule = scc.e*1e3
@@ -200,39 +200,11 @@ def check_id(data):
         complex_id = data[species_name + "_id_end"] + 1j*data[species_name + "_cpu_end"]
         assert(complex_id.shape == np.unique(complex_id).shape)
 
-def basic_product_particles_check(data):
-    ## For each nuclear fusion reaction in the code, we create 6 alpha macroparticles. So the
-    ## total number of alpha macroparticles must be a multiple of 6.
-    num_alpha = data["alpha_w_end"].shape[0]
-    assert(num_alpha%6 == 0)
-
-    ## The weight of the 6 macroparticles coming from a single fusion event should be the same.
-    ## We verify this here.
-    assert(np.array_equal(data["alpha_w_end"][::6], data["alpha_w_end"][1::6]))
-    assert(np.array_equal(data["alpha_w_end"][::6], data["alpha_w_end"][2::6]))
-    assert(np.array_equal(data["alpha_w_end"][::6], data["alpha_w_end"][3::6]))
-    assert(np.array_equal(data["alpha_w_end"][::6], data["alpha_w_end"][4::6]))
-    assert(np.array_equal(data["alpha_w_end"][::6], data["alpha_w_end"][5::6]))
-
-    ## When we create 6 macroparticles, the first has the exact same momentum as the second, the
-    ## third has the same as the fourth and the fifth has the same as the sixth. We verify this
-    ## here
-    assert(np.array_equal(data["alpha_px_end"][::6], data["alpha_px_end"][1::6]))
-    assert(np.array_equal(data["alpha_py_end"][::6], data["alpha_py_end"][1::6]))
-    assert(np.array_equal(data["alpha_pz_end"][::6], data["alpha_pz_end"][1::6]))
-    assert(np.array_equal(data["alpha_px_end"][2::6], data["alpha_px_end"][3::6]))
-    assert(np.array_equal(data["alpha_py_end"][2::6], data["alpha_py_end"][3::6]))
-    assert(np.array_equal(data["alpha_pz_end"][2::6], data["alpha_pz_end"][3::6]))
-    assert(np.array_equal(data["alpha_px_end"][4::6], data["alpha_px_end"][5::6]))
-    assert(np.array_equal(data["alpha_py_end"][4::6], data["alpha_py_end"][5::6]))
-    assert(np.array_equal(data["alpha_pz_end"][4::6], data["alpha_pz_end"][5::6]))
-
 def generic_check(data):
     check_particle_number_conservation(data)
     check_energy_conservation(data)
     check_momentum_conservation(data)
     check_id(data)
-    #basic_product_particles_check(data)
 
 def check_isotropy(data, relative_tolerance):
     ## Checks that the alpha particles are emitted isotropically
@@ -242,83 +214,22 @@ def check_isotropy(data, relative_tolerance):
     assert(is_close(average_px_sq, average_py_sq, rtol = relative_tolerance))
     assert(is_close(average_px_sq, average_pz_sq, rtol = relative_tolerance))
 
-def astrophysical_factor_lowE(E):
-    ## E is in keV
-    ## Returns astrophysical factor in MeV b using the low energy fit in the range E < 400 keV
-    ## described in equation (2) of W.M. Nevins and R. Swain, Nuclear Fusion, 40, 865 (2000)
-    C0 = 197.
-    C1 = 0.24
-    C2 = 2.31e-4
-    AL = 1.82e4
-    EL = 148.
-    dEL = 2.35
-    return C0 + C1*E + C2*E**2 + AL/((E-EL)**2 + dEL**2)
-
-def astrophysical_factor_midE(E):
-    ## E is in keV
-    ## Returns astrophysical factor in MeV b using the mid energy fit in the range
-    ## 400 keV < E < 642 keV described in equation (3) of W.M. Nevins and R. Swain,
-    ## Nuclear Fusion, 40, 865 (2000)
-    D0 = 330.
-    D1 = 66.1
-    D2 = -20.3
-    D5 = -1.58
-    E_400 = 400.
-    E_100 = 100.
-    E_norm = (E - E_400)/E_100
-    return D0 + D1*E_norm + D2*E_norm**2 + D5*E_norm**5
-
-def astrophysical_factor_highE(E):
-    ## E is in keV
-    ## Returns astrophysical factor in MeV b using the high energy fit in the range
-    ## 642 keV < E < 3500 keV described in equation (4) of W.M. Nevins and R. Swain,
-    ## Nuclear Fusion, 40, 865 (2000)
-    A0 = 2.57e6
-    A1 = 5.67e5
-    A2 = 1.34e5
-    A3 = 5.68e5
-    E0 = 581.3
-    E1 = 1083.
-    E2 = 2405.
-    E3 = 3344.
-    dE0 = 85.7
-    dE1 = 234.
-    dE2 = 138.
-    dE3 = 309.
-    B = 4.38
-    return A0/((E-E0)**2 + dE0**2) + A1/((E-E1)**2 + dE1**2) + \
-           A2/((E-E2)**2 + dE2**2) + A3/((E-E3)**2 + dE3**2) + B
-
-def astrophysical_factor(E):
-    ## E is in keV
-    ## Returns astrophysical factor in MeV b using the fits described in W.M. Nevins
-    ## and R. Swain, Nuclear Fusion, 40, 865 (2000)
-    conditions = [E <= 400, E <= 642, E > 642]
-    choices = [astrophysical_factor_lowE(E),
-               astrophysical_factor_midE(E),
-               astrophysical_factor_highE(E)]
-    return np.select(conditions, choices)
-
-def pb_cross_section_buck_fit(E):
-    ## E is in MeV
-    ## Returns cross section in b using a power law fit of the data presented in Buck et al.,
-    ## Nuclear Physics A, 398(2), 189-202 (1983) in the range E > 3.5 MeV.
-    E_start_fit = 3.5
-    ## Cross section at E = E_start_fit = 3.5 MeV
-    cross_section_start_fit = 0.2168440845211521
-    slope_fit = -2.661840717596765
-    return cross_section_start_fit*(E/E_start_fit)**slope_fit
-
-def pb_cross_section(E):
-    ## E is in keV
-    ## Returns cross section in b using the fits described in W.M. Nevins and R. Swain,
-    ## Nuclear Fusion, 40, 865 (2000) for E < 3.5 MeV and a power law fit of the data presented in
-    ## Buck et al., Nuclear Physics A, 398(2), 189-202 (1983) for E > 3.5 MeV.
-    E_MeV = E/1.e3
-    conditions = [E <= 3500, E > 3500]
-    choices = [astrophysical_factor(E)/E_MeV * np.exp(-np.sqrt(E_Gamow_MeV / E_MeV)),
-               pb_cross_section_buck_fit(E_MeV)]
-    return np.select(conditions, choices)
+def cross_section( E_keV ):
+    ## Returns cross section in b, using the analytical fits given
+    ## in H.-S. Bosch and G.M. Hale 1992 Nucl. Fusion 32 611
+    joule_to_keV = 1.e-3/scc.e
+    B_G = scc.pi * scc.alpha * np.sqrt( 2.*m_reduced * scc.c**2 * joule_to_keV );
+    A1 = 6.927e4;
+    A2 = 7.454e8;
+    A3 = 2.050e6;
+    A4 = 5.2002e4;
+    B1 = 6.38e1;
+    B2 = -9.95e-1;
+    B3 = 6.981e-5;
+    B4 = 1.728e-4;
+    astrophysical_factor = (A1 + E_keV*(A2 + E_keV*(A3 + E_keV*A4))) / (1 + E_keV*(B1 + E_keV*(B2 + E_keV*(B3 + E_keV*B4))));
+    millibarn_to_barn = 1.e-3;
+    return millibarn_to_barn * astrophysical_factor/E_keV * np.exp(-B_G/np.sqrt(E_keV))
 
 def E_com_to_p_sq_com(m1, m2, E):
     ## E is the total (kinetic+mass) energy of a two particle (with mass m1 and m2) system in
@@ -328,29 +239,30 @@ def E_com_to_p_sq_com(m1, m2, E):
            scc.c**6/(4.*E**2)*((m1**2 - m2**2)**2)
 
 def compute_relative_v_com(E):
-    ## E is the kinetic energy of proton+boron in the center of mass frame, in keV
-    ## Returns the relative velocity between proton and boron in this frame, in m/s
-    E_J  = E*keV_to_Joule + (m_p + m_b)*scc.c**2
-    p_sq = E_com_to_p_sq_com(m_p, m_b, E_J)
+    ## E is the kinetic energy of reactants in the center of mass frame, in keV
+    ## Returns the relative velocity between reactants in this frame, in m/s
+    m0 = mass[reactant_species[0]]
+    m1 = mass[reactant_species[1]]
+    E_J  = E*keV_to_Joule + (m0 + m1)*scc.c**2
+    p_sq = E_com_to_p_sq_com(m0, m1, E_J)
     p = np.sqrt(p_sq)
-    gamma_p = np.sqrt(1. + p_sq / (m_p*scc.c)**2)
-    gamma_b = np.sqrt(1. + p_sq / (m_b*scc.c)**2)
-    v_p = p/(gamma_p*m_p)
-    v_b = p/(gamma_b*m_b)
-    return v_p+v_b
+    gamma0 = np.sqrt(1. + p_sq / (m0*scc.c)**2)
+    gamma1 = np.sqrt(1. + p_sq / (m1*scc.c)**2)
+    v0 = p/(gamma0*m0)
+    v1 = p/(gamma1*m1)
+    return v0+v1
 
-def expected_alpha_weight_com(E_com, proton_density, boron_density, dV, dt):
-    ## Computes expected number of produced alpha particles as a function of energy E_com in the
+def expected_weight_com(E_com, reactant0_density, reactant1_density, dV, dt):
+    ## Computes expected number of product particles as a function of energy E_com in the
     ## center of mass frame. E_com is in keV.
     assert(np.all(E_com>=0))
     ## Case E_com == 0 is handled manually to avoid division by zero
     conditions = [E_com == 0, E_com > 0]
     ## Necessary to avoid division by 0 warning when pb_cross_section is evaluated
     E_com_never_zero = np.clip(E_com, 1.e-15, None)
-    choices = [0., pb_cross_section(E_com_never_zero)*compute_relative_v_com(E_com_never_zero)]
+    choices = [0., cross_section(E_com_never_zero)*compute_relative_v_com(E_com_never_zero)]
     sigma_times_vrel = np.select(conditions, choices)
-    ## Factor 3 is here because each fusion reaction produces 3 alphas
-    return 3.*proton_density*boron_density*sigma_times_vrel*barn_to_square_meter*dV*dt
+    return reactant0_density*reactant1_density*sigma_times_vrel*barn_to_square_meter*dV*dt
 
 def check_macroparticle_number(data, fusion_probability_target_value, num_pair_per_cell):
     ## Checks that the number of macroparticles is as expected for the first and second tests
@@ -361,10 +273,9 @@ def check_macroparticle_number(data, fusion_probability_target_value, num_pair_p
     ## equal to the parameter fusion_probability_target_value
     fusion_probability_per_pair = fusion_probability_target_value
     expected_fusion_number = numcells*num_pair_per_cell*fusion_probability_per_pair
-    ## Each fusion event produces 6 alpha macroparticles
-    expected_macroparticle_number = 6.*expected_fusion_number
-    std_macroparticle_number = 6.*np.sqrt(expected_fusion_number)
-    actual_macroparticle_number = data["alpha_w_end"].shape[0]
+    expected_macroparticle_number = 2*expected_fusion_number
+    std_macroparticle_number = 2*np.sqrt(expected_fusion_number)
+    actual_macroparticle_number = data[product_species[0] + "_w_end"].shape[0]
     # 5 sigma test that has an intrinsic probability to fail of 1 over ~2 millions
     assert(is_close(actual_macroparticle_number, expected_macroparticle_number, rtol = 0.,
                     atol = 5.*std_macroparticle_number))
@@ -372,17 +283,19 @@ def check_macroparticle_number(data, fusion_probability_target_value, num_pair_p
     ## used in subsequent function
     return expected_fusion_number
 
-def p_sq_boron_frame_to_E_COM_frame(p_proton_sq):
-    # Takes the proton square norm of the momentum in the boron rest frame and returns the total
+def p_sq_reactant1_frame_to_E_COM_frame(p_reactant0_sq):
+    # Takes the reactant0 square norm of the momentum in the reactant1 rest frame and returns the total
     # kinetic energy in the center of mass frame. Everything is in SI units.
+    m0 = mass[reactant_species[0]]
+    m1 = mass[reactant_species[1]]
 
     # Total (kinetic + mass) energy in lab frame
-    E_lab = np.sqrt(p_proton_sq*scc.c**2 + (m_p*scc.c**2)**2) + m_b*scc.c**2
+    E_lab = np.sqrt(p_reactant0_sq*scc.c**2 + (m0*scc.c**2)**2) + m1*scc.c**2
     # Use invariant E**2 - p**2c**2 of 4-momentum norm to compute energy in center of mass frame
-    E_com = np.sqrt(E_lab**2 - p_proton_sq*scc.c**2)
+    E_com = np.sqrt(E_lab**2 - p_reactant0_sq*scc.c**2)
     # Corresponding kinetic energy
-    E_com_kin = E_com - (m_b+scc.m_p)*scc.c**2
-    return E_com_kin*(p_proton_sq>0.)
+    E_com_kin = E_com - (m1+m0)*scc.c**2
+    return E_com_kin*(p_reactant0_sq>0.)
 
 def p_sq_to_kinetic_energy(p_sq, m):
     ## Returns the kinetic energy of a particle as a function of its squared momentum.
@@ -394,31 +307,33 @@ def compute_E_com1(data):
 
     ## Square norm of the momentum of proton/boron as a function of cell number in z direction
     p_sq = 2.*m_reduced*(Energy_step*np.arange(size_z)**2)
-    return p_sq_to_kinetic_energy(p_sq, m_b) + p_sq_to_kinetic_energy(p_sq, m_p)
+    Ekin = 0
+    for species_name in reactant_species:
+        Ekin += p_sq_to_kinetic_energy( p_sq, mass[species_name] )
+    return Ekin
 
 def compute_E_com2(data):
     ## Computes kinetic energy (in Joule) in the center of frame for the second test
 
     ## Square norm of the momentum of the proton as a function of cell number in z direction
-    p_proton_sq = 2.*m_p*(Energy_step*np.arange(size_z)**2)
-    return p_sq_boron_frame_to_E_COM_frame(p_proton_sq)
+    p_reactant0_sq = 2.*mass[reactant_species[0]]*(Energy_step*np.arange(size_z)**2)
+    return p_sq_reactant1_frame_to_E_COM_frame(p_reactant0_sq)
 
-def check_alpha_yield(data, expected_fusion_number, E_com, proton_density, boron_density):
+def check_fusion_yield(data, expected_fusion_number, E_com, reactant0_density, reactant1_density):
     ## Checks that the fusion yield is as expected for the first and second tests.
-    ## Proton and boron densities are in m^-3.
+    product_weight_theory = expected_weight_com(E_com/keV_to_Joule,
+        reactant0_density, reactant1_density, dV_slice, dt)
+    for species_name in product_species:
+        product_weight_simulation = np.histogram(data[species_name+"_z_end"],
+                bins=size_z, range=(0, size_z), weights = data[species_name+"_w_end"])[0]
 
-    alpha_weight_theory = expected_alpha_weight_com(E_com/keV_to_Joule, proton_density,
-                                                    boron_density, dV_slice, dt)
-    alpha_weight_simulation = np.histogram(data["alpha_z_end"], bins=size_z, range=(0, size_z),
-                                           weights = data["alpha_w_end"])[0]
+        ## -1 is here because the first slice 0 < z < 1 does not contribute to fusion
+        expected_fusion_number_per_slice = expected_fusion_number/(size_z-1)
+        relative_std_weight = 1./np.sqrt(expected_fusion_number_per_slice)
 
-    ## -1 is here because the first slice 0 < z < 1 does not contribute to alpha creation
-    expected_fusion_number_per_slice = expected_fusion_number/(size_z-1)
-    relative_std_alpha_weight = 1./np.sqrt(expected_fusion_number_per_slice)
-
-    # 5 sigma test that has an intrinsic probability to fail of 1 over ~2 millions
-    assert(np.all(is_close(alpha_weight_theory, alpha_weight_simulation,
-                           rtol = 5.*relative_std_alpha_weight)))
+        # 5 sigma test that has an intrinsic probability to fail of 1 over ~2 millions
+        assert(np.all(is_close(product_weight_theory, product_weight_simulation,
+                               rtol = 5.*relative_std_weight)))
 
 def check_initial_energy1(data, E_com):
     ## In WarpX, the initial momentum of the alphas is computed assuming that the fusion process
@@ -582,25 +497,25 @@ def check_xy_isotropy(data):
     assert(average_pz_sq > average_py_sq)
 
 def specific_check1(data):
-    check_isotropy(data, relative_tolerance = 3.e-2)
+    #check_isotropy(data, relative_tolerance = 3.e-2)
     expected_fusion_number = check_macroparticle_number(data,
                                                         fusion_probability_target_value = 0.002,
                                                         num_pair_per_cell = 10000)
     E_com = compute_E_com1(data)
-    check_alpha_yield(data, expected_fusion_number, E_com, proton_density = 1.,
-                                                           boron_density = 1.)
-    check_initial_energy1(data, E_com)
+    check_fusion_yield(data, expected_fusion_number, E_com, reactant0_density = 1.,
+                                                           reactant1_density = 1.)
+    #check_initial_energy1(data, E_com)
 
 def specific_check2(data):
-    check_xy_isotropy(data)
-    ## Only 900 particles pairs per cell here because we ignore the 10% of protons that are at rest
+    #check_xy_isotropy(data)
+    ## Only 900 particles pairs per cell here because we ignore the 10% of reactants that are at rest
     expected_fusion_number = check_macroparticle_number(data,
                                                         fusion_probability_target_value = 0.02,
                                                         num_pair_per_cell = 900)
     E_com = compute_E_com2(data)
-    check_alpha_yield(data, expected_fusion_number, E_com, proton_density = 1.e20,
-                                                           boron_density = 1.e26)
-    check_initial_energy2(data)
+    check_fusion_yield(data, expected_fusion_number, E_com, reactant0_density = 1.e20,
+                                                           reactant1_density = 1.e26)
+    #check_initial_energy2(data)
 
 def check_charge_conservation(rho_start, rho_end):
     assert(np.all(is_close(rho_start, rho_end, rtol=2.e-11)))
@@ -632,8 +547,7 @@ def main():
         generic_check(data)
 
         # Checks that are specific to test number i
-        # TODO: activate code below
-        # eval("specific_check"+str(i)+"(data)")
+        eval("specific_check"+str(i)+"(data)")
 
     rho_start = field_data_start["rho"].to_ndarray()
     rho_end = field_data_end["rho"].to_ndarray()
