a1171506a0a94701ec87fcf2ccb3f3c8fa7326a0
Remi Lehe <remi.lehe@normalesup.org>
2022-07-12 02:47:23 -0700

    Clean up the test script

--- Examples/Modules/nuclear_fusion/analysis_deuterium_tritium_fusion.py
+++ Examples/Modules/nuclear_fusion/analysis_deuterium_tritium_fusion.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# Copyright 2021 Neil Zaim
+# Copyright 2022 Neil Zaim, Remi Lehe
 #
 # This file is part of WarpX.
 #
@@ -15,41 +15,29 @@ import checksumAPI
 import numpy as np
 import scipy.constants as scc
 
-## This script performs various checks for the proton boron nuclear fusion module. The simulation
-## that we check is made of 5 different tests, each with different proton, boron and alpha species.
+## This script performs various checks for the fusion module. The simulation
+## that we check is made of 2 different tests, each with different reactant and product species.
 ##
-## The first test is performed in the proton-boron center of mass frame. It could correspond to the
-## physical case of a proton beam colliding with a boron beam. The kinetic energy of the colliding
+## The first test is performed in the center of mass frame of the reactant. It could correspond to the
+## physical case of two beams colliding with each other. The kinetic energy of the colliding
 ## particles depends on the cell number in the z direction and varies in the few keV to few MeV
 ## range. All the particles within a cell have the exact same momentum, which allows detailed
-## checks of the energy of produced alpha particles. The proton and boron species have the same
-## density and number of particles in this test. The number of produced alphas is much smaller than
-## the initial number of protons and borons.
+## checks of the energy of produced alpha particles. The reactant species have the same
+## density and number of particles in this test. The number of product species is much smaller than
+## the initial number of reactants
 ##
-## The second test is performed in the boron rest frame. It corresponds to the physical case of a
-## low density proton beam colliding with a high-density proton+boron target. The energy of the
-## proton beam is varied in the few keV to few MeV range, depending on the cell number in the z
+## The second test is performed in the rest frame of the second reactant. It corresponds to the
+## physical case of a low density beam colliding with a high-density mixed target. The energy of the
+## beam particles is varied in the few keV to few MeV range, depending on the cell number in the z
 ## direction. As in the previous case, all the particles within a cell have the exact same
-## momentum, which allows detailed checks of the energy of produced alpha particles. In this test,
-## there are 100 immobile boron and 100 immobile proton macroparticles per cell, as well as 900
-## beam proton macroparticles per cell. The density of the immobile particles is 6 orders of
+## momentum, which allows detailed checks of the energy of product particles. In this test,
+## there are 100 immobile macroparticles for each reactant per cell, as well as 900
+## of the beam reactant macroparticles per cell. The density of the immobile particles is 6 orders of
 ## magnitude higher than the number of beam particles, which means that they have a much higher
 ## weight. This test is similar to the example given in section 3 of Higginson et al.,
 ## Journal of Computation Physics, 388 439–453 (2019), which was found to be sensitive to the way
-## unsampled pairs are accounted for. As before, the number of produced alphas is much smaller than
-## the initial number of protons and borons.
-##
-## The third test corresponds to a Maxwellian plasma with a 44 keV temperature. The alpha yield is
-## directly compared to the analytical fits of W.M. Nevins and R. Swain, Nuclear Fusion, 40, 865
-## (2000) for a thermal plasma.
-##
-## The fourth test corresponds to a plasma with an extremely small boron density, so that all boron
-## macroparticles should have disappeared by the end of the simulation, which we verify.
-##
-## The fifth test is exactly the same as the fourth test, except that the
-## fusion_probability_threshold parameter is increased to an excessive value. Because of that, we
-## severely underestimate the fusion yield and boron macroparticles remain at the end of the
-## simulation, which we verify.
+## unsampled pairs are accounted for. As before, the number of product particles is much smaller than
+## the initial number of reactants.
 ##
 ## In all simulations, we check particle number, charge, momentum and energy conservation and
 ## perform basic checks regarding the produced particles. When possible, we also compare the number
@@ -120,8 +108,7 @@ def add_species_to_dict(yt_ad, data_dict, species_name, prefix, suffix):
         add_existing_species_to_dict(yt_ad, data_dict, species_name, prefix, suffix)
     except yt.utilities.exceptions.YTFieldNotFound:
         ## If species does not exist, we avoid python crash and add empty arrays to the
-        ## dictionnary. Currently, this happens for the boron species in test number 4, which
-        ## entirely fuses into alphas.
+        ## dictionnary.
         add_empty_species_to_dict(data_dict, species_name, prefix, suffix)
 
 def check_particle_number_conservation(data):
@@ -316,7 +303,7 @@ def p_sq_to_kinetic_energy(p_sq, m):
 def compute_E_com1(data):
     ## Computes kinetic energy (in Joule) in the center of frame for the first test
 
-    ## Square norm of the momentum of proton/boron as a function of cell number in z direction
+    ## Square norm of the momentum of reactant as a function of cell number in z direction
     p_sq = 2.*m_reduced*(Energy_step*np.arange(size_z)**2)
     Ekin = 0
     for species_name in reactant_species:
@@ -326,7 +313,7 @@ def compute_E_com1(data):
 def compute_E_com2(data):
     ## Computes kinetic energy (in Joule) in the center of frame for the second test
 
-    ## Square norm of the momentum of the proton as a function of cell number in z direction
+    ## Square norm of the momentum of reactant0 as a function of cell number in z direction
     p_reactant0_sq = 2.*mass[reactant_species[0]]*(Energy_step*np.arange(size_z)**2)
     return p_sq_reactant1_frame_to_E_COM_frame(p_reactant0_sq)
 
@@ -345,158 +332,6 @@ def check_fusion_yield(data, expected_fusion_number, E_com, reactant0_density, r
         assert(np.all(is_close(product_weight_theory, product_weight_simulation,
                                rtol = 5.*relative_std_weight)))
 
-def check_initial_energy1(data, E_com):
-    ## In WarpX, the initial momentum of the alphas is computed assuming that the fusion process
-    ## takes place in two steps:
-    ## (1): proton + boron 11 -> alpha + beryllium 8
-    ## (2): beryllium 8 -> alpha + alpha
-    ## The alpha generated in the first step (labeled alpha1) generally has a different initial
-    ## energy distribution than the alphas generated in the second step (labeled alpha2 and
-    ## alpha3).
-    ## In the first test, we are in the center of mass frame. Therefore, the momentum of alpha1 is
-    ## entirely determined by the energy in the center of mass frame, so we check in this function
-    ## that the energy of the alpha1 macroparticles is as expected. On the other hand, the energy
-    ## of alpha2 and alpha3 follows a continuous distribution within a given range. In this test,
-    ## we check that this range is as expected by comparing the maximum and minimum energy of the
-    ## obtained macroparticles to the theoretical maximum and minimum.
-    ## Note that in the simulations, 6 macroparticles are generated during for each fusion event.
-    ## The first and second macroparticles are alpha1 particles. The third and fourth are alpha2.
-    ## The fifth and sixth are alpha3.
-
-    energy_alpha_simulation = compute_energy_array(data, "alpha", "end", m_a)
-    z_alpha = data["alpha_z_end"]
-
-    # Loop over all slices (i.e. cells in the z direction)
-    for slice_number in range(1, size_z):
-        ## Kinetic energy in the lab frame before fusion
-        E_kinetic_com_before = E_com[slice_number]
-        ## Total (kinetic + mass) energy in the lab frame after
-        ## proton + boron 11 -> alpha + beryllium 8
-        E_total_com_after = E_kinetic_com_before + E_fusion + (m_a + m_be)*scc.c**2
-        ## Corresponding momentum norm squared of alpha1/beryllium
-        p_sq_after = E_com_to_p_sq_com(m_a, m_be, E_total_com_after)
-        ## Corresponding kinetic energy for alpha1
-        energy_alpha1_theory = p_sq_to_kinetic_energy(p_sq_after, m_a)
-        ## Corresponding kinetic energy for beryllium
-        energy_beryllium_theory = p_sq_to_kinetic_energy(p_sq_after, m_be)
-        ## Corresponding kinetic energy for alpha2 + alpha3 after beryllium decay
-        energy_alpha2_plus_3_theory = energy_beryllium_theory + E_decay
-        ## Compute the theoretical maximum and minimum energy of alpha2 and alpha3. This
-        ## calculation is done nonrelativistically, by noting that the maximum (minimum) energy
-        ## corresponds to an alpha emitted exactly in the (opposite) direction of the beryllium
-        ## in the center of mass frame. This calculation involves solving a polynomial equation of
-        ## order 2 in p_alpha23.
-        max_p_alpha23 = 0.5*(np.sqrt(p_sq_after) + \
-                        np.sqrt(4*m_a*energy_alpha2_plus_3_theory - p_sq_after))
-        min_p_alpha23 = 0.5*(np.sqrt(p_sq_after) - \
-                        np.sqrt(4*m_a*energy_alpha2_plus_3_theory - p_sq_after))
-        max_energy_alpha23 = max_p_alpha23**2/(2.*m_a)
-        min_energy_alpha23 = min_p_alpha23**2/(2.*m_a)
-
-        ## Get the energy of all alphas in the slice
-        energy_alpha_slice = energy_alpha_simulation[(z_alpha >= slice_number)* \
-                                                     (z_alpha < (slice_number + 1))]
-        ## Energy of alphas1 (here, first macroparticle of each fusion event) in the slice
-        energy_alpha1_simulation = energy_alpha_slice[::6]
-        ## Energy of alphas2 (here, third macroparticle of each fusion event) in the slice
-        energy_alpha2_simulation = energy_alpha_slice[2::6]
-        ## Energy of alphas3 (here, fifth macroparticle of each fusion event) in the slice
-        energy_alpha3_simulation = energy_alpha_slice[4::6]
-
-        assert(np.all(is_close(energy_alpha1_simulation, energy_alpha1_theory, rtol=5.e-8)))
-        assert(is_close(np.amax(energy_alpha2_simulation), max_energy_alpha23, rtol=1.e-2))
-        assert(is_close(np.amin(energy_alpha2_simulation), min_energy_alpha23, rtol=1.e-2))
-        assert(is_close(np.amax(energy_alpha3_simulation), max_energy_alpha23, rtol=1.e-2))
-        assert(is_close(np.amin(energy_alpha3_simulation), min_energy_alpha23, rtol=1.e-2))
-
-def check_initial_energy2(data):
-    ## In WarpX, the initial momentum of the alphas is computed assuming that the fusion process
-    ## takes place in two steps:
-    ## (1): proton + boron 11 -> alpha + beryllium 8
-    ## (2): beryllium 8 -> alpha + alpha
-    ## The alpha generated in the first step (labeled alpha1) generally has a different initial
-    ## energy distribution than the alphas generated in the second step (labeled alpha2 and
-    ## alpha3).
-    ## In the second test, we are in the boron rest frame. In this case, the momentum of each alpha
-    ## follows a continuous distribution within a given range. In this function, we verify that
-    ## this range is as expected by comparing the maximum and minimum energy of the obtained
-    ## macroparticles to the theoretical maximum and minimum. Be aware that the range for alpha1
-    ## is not the same as the range for alpha2 and alpha3 (typically alpha1 particles will carry
-    ## more energy).
-    ## Note that in the simulations, 6 macroparticles are generated during for each fusion event.
-    ## The first and second macroparticles are alpha1 particles. The third and fourth are alpha2.
-    ## The fifth and sixth are alpha3.
-
-    energy_alpha_simulation = compute_energy_array(data, "alpha", "end", m_a)
-    z_alpha = data["alpha_z_end"]
-
-    # Loop over all slices (i.e. cells in the z direction)
-    for slice_number in range(1, size_z):
-        ## For simplicity, all the calculations in this functino are done nonrelativistically
-        ## Proton kinetic energy in the lab frame before fusion
-        E_proton_nonrelativistic = Energy_step*slice_number**2
-        ## Corresponding square norm of proton momentum
-        p_proton_sq = 2.*scc.m_p*E_proton_nonrelativistic
-        ## Kinetic energy in the lab frame after
-        ## proton + boron 11 -> alpha + beryllium 8
-        E_after_fusion = E_proton_nonrelativistic + E_fusion
-
-        ## Compute the theoretical maximum and minimum energy of alpha1 in the lab frame. This
-        ## calculation is done by noting that the maximum (minimum) energy corresponds to an alpha
-        ## emitted exactly in the (opposite) direction of the proton in the lab frame. This
-        ## calculation involves solving a polynomial equation of order 2 in p_alpha1.
-        max_p_alpha1 = (m_a/m_be*np.sqrt(p_proton_sq) + \
-                       np.sqrt(-m_a/m_be*p_proton_sq + 2.*E_after_fusion*m_a*(m_a/m_be + 1.))) / \
-                       (m_a/m_be + 1.)
-        min_p_alpha1 = (m_a/m_be*np.sqrt(p_proton_sq) - \
-                       np.sqrt(-m_a/m_be*p_proton_sq + 2.*E_after_fusion*m_a*(m_a/m_be + 1.))) / \
-                       (m_a/m_be + 1.)
-        max_energy_alpha1 = max_p_alpha1**2/(2*m_a)
-        min_energy_alpha1 = min_p_alpha1**2/(2*m_a)
-
-        ## Corresponding max/min kinetic energy of Beryllium in the lab frame
-        max_E_beryllium = E_after_fusion - min_energy_alpha1
-        min_E_beryllium = E_after_fusion - max_energy_alpha1
-        ## Corresponding max/min momentum square of Beryllium in the lab frame
-        max_p_sq_beryllium = 2.*m_be*max_E_beryllium
-        min_p_sq_beryllium = 2.*m_be*min_E_beryllium
-        ## Corresponding max/min kinetic energy in the lab frame for alpha2 + alpha3 after
-        ## Beryllium decay
-        max_energy_alpha2_plus_3 = max_E_beryllium + E_decay
-        min_energy_alpha2_plus_3 = min_E_beryllium + E_decay
-
-        ## Compute the theoretical maximum and minimum energy of alpha2 and alpha3 in the lab
-        ## frame. This calculation is done by noting that the maximum (minimum) energy corresponds
-        ## to an alpha emitted exactly in the (opposite) direction of a beryllium with energy
-        ## max_E_beryllium (min_E_beryllium). This calculation involves solving a polynomial
-        ## equation of order 2 in p_alpha23.
-        max_p_alpha23 = 0.5*(np.sqrt(max_p_sq_beryllium) + \
-                             np.sqrt(4*m_a*max_energy_alpha2_plus_3 - max_p_sq_beryllium))
-        min_p_alpha23 = 0.5*(np.sqrt(min_p_sq_beryllium) - \
-                             np.sqrt(4*m_a*min_energy_alpha2_plus_3 - min_p_sq_beryllium))
-        max_energy_alpha23 = max_p_alpha23**2/(2*m_a)
-        min_energy_alpha23 = min_p_alpha23**2/(2*m_a)
-
-        ## Get the energy of all alphas in the slice
-        energy_alpha_slice = energy_alpha_simulation[(z_alpha >= slice_number)* \
-                                                     (z_alpha < (slice_number + 1))]
-        ## Energy of alphas1 (here, first macroparticle of each fusion event) in the slice
-        energy_alpha1_simulation = energy_alpha_slice[::6]
-        ## Energy of alphas2 (here, third macroparticle of each fusion event) in the slice
-        energy_alpha2_simulation = energy_alpha_slice[2::6]
-        ## Energy of alphas3 (here, fifth macroparticle of each fusion event) in the slice
-        energy_alpha3_simulation = energy_alpha_slice[4::6]
-
-        assert(is_close(np.amax(energy_alpha1_simulation), max_energy_alpha1, rtol=1.e-2))
-        assert(is_close(np.amin(energy_alpha1_simulation), min_energy_alpha1, rtol=1.e-2))
-        ## Tolerance is quite high below because we don't have a lot of alphas to produce good
-        ## statistics and an event like alpha1 emitted exactly in direction of proton & alpha2
-        ## emitted exactly in direction opposite to Beryllium is somewhat rare.
-        assert(is_close(np.amax(energy_alpha2_simulation), max_energy_alpha23, rtol=2.5e-1))
-        assert(is_close(np.amin(energy_alpha2_simulation), min_energy_alpha23, rtol=2.5e-1))
-        assert(is_close(np.amax(energy_alpha3_simulation), max_energy_alpha23, rtol=2.5e-1))
-        assert(is_close(np.amin(energy_alpha3_simulation), min_energy_alpha23, rtol=2.5e-1))
-
 def specific_check1(data):
     check_isotropy(data, relative_tolerance = 3.e-2)
     expected_fusion_number = check_macroparticle_number(data,
@@ -505,7 +340,6 @@ def specific_check1(data):
     E_com = compute_E_com1(data)
     check_fusion_yield(data, expected_fusion_number, E_com, reactant0_density = 1.,
                                                            reactant1_density = 1.)
-    #check_initial_energy1(data, E_com)
 
 def specific_check2(data):
     check_xy_isotropy(data)
@@ -516,7 +350,6 @@ def specific_check2(data):
     E_com = compute_E_com2(data)
     check_fusion_yield(data, expected_fusion_number, E_com, reactant0_density = 1.e20,
                                                            reactant1_density = 1.e26)
-    #check_initial_energy2(data)
 
 def check_charge_conservation(rho_start, rho_end):
     assert(np.all(is_close(rho_start, rho_end, rtol=2.e-11)))
