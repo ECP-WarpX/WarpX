5e18eec22336c484f628b1836a977000094caa69
Neil Zaim <neilzaim@hotmail.com>
2022-07-18 15:04:50 +0200

    Always output theta in RZ

--- Source/Diagnostics/ParticleDiag/ParticleDiag.cpp
+++ Source/Diagnostics/ParticleDiag/ParticleDiag.cpp
@@ -43,6 +43,13 @@ ParticleDiag::ParticleDiag(std::string diag_name, std::string name, WarpXParticl
         }
     }
 
+#ifdef WARPX_DIM_RZ
+    // Always write out theta, whether or not it's requested,
+    // to be consistent with always writing out r and z.
+    // TODO: openPMD does a reconstruction to Cartesian, so we can now skip force-writing this
+    m_plot_flags[pc->getParticleComps().at("theta")] = 1;
+#endif
+
     // build filter functors
     m_do_random_filter = queryWithParser(pp_diag_name_species_name, "random_fraction",
                                                                     m_random_fraction);
