57f6fb989d23bd225b7e8b3a750410446e394387
Dave Grote <grote1@llnl.gov>
2022-08-02 11:18:15 -0700

    Allow filtering in z with RZ FDTD

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -730,9 +730,9 @@ WarpX::ReadParameters ()
         }
         else // FDTD
         {
-            // Filter currently not working with FDTD solver in RZ geometry
+            // Filter currently not working with FDTD solver in RZ geometry along R
             // (see https://github.com/ECP-WarpX/WarpX/issues/1943)
-            WARPX_ALWAYS_ASSERT_WITH_MESSAGE(!use_filter,
+            WARPX_ALWAYS_ASSERT_WITH_MESSAGE(!use_filter || filter_npass_each_dir[0] == 0,
                 "Filter currently not working with FDTD solver in RZ geometry");
         }
 #endif
