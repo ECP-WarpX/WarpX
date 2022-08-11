651c74fd222642201830f99a2c8741e405f2db37
Remi Lehe <remi.lehe@normalesup.org>
2022-08-11 16:28:36 -0700

    Enable particle RZ BackTransformed diagnostics

--- Source/Diagnostics/MultiDiagnostics.cpp
+++ Source/Diagnostics/MultiDiagnostics.cpp
@@ -24,11 +24,7 @@ MultiDiagnostics::MultiDiagnostics ()
         if ( diags_types[i] == DiagTypes::Full ){
             alldiags[i] = std::make_unique<FullDiagnostics>(i, diags_names[i]);
         } else if ( diags_types[i] == DiagTypes::BackTransformed ){
-#ifdef WARPX_DIM_RZ
-            amrex::Abort(Utils::TextMsg::Err("BackTransformed diagnostics is currently not supported for RZ"));
-#else
             alldiags[i] = std::make_unique<BTDiagnostics>(i, diags_names[i]);
-#endif
         } else if ( diags_types[i] == DiagTypes::BoundaryScraping ){
             alldiags[i] = std::make_unique<BoundaryScrapingDiagnostics>(i, diags_names[i]);
         } else {
