7545b3c28acfa9d5ea8d3b8c600020a5bf4f3008
Remi Lehe <remi.lehe@normalesup.org>
2022-08-12 13:14:55 -0700

    Enable Particle RZ BackTransformed Diagnostics (#3309)
    * Enable particle RZ BackTransformed diagnostics
    
    * Add warning + compute theta
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- Source/Diagnostics/MultiDiagnostics.cpp
+++ Source/Diagnostics/MultiDiagnostics.cpp
@@ -4,7 +4,7 @@
 #include "Diagnostics/FullDiagnostics.H"
 #include "Diagnostics/BoundaryScrapingDiagnostics.H"
 #include "Utils/TextMsg.H"
-
+#include <ablastr/warn_manager/WarnManager.H>
 #include <AMReX_ParmParse.H>
 #include <AMReX.H>
 #include <AMReX_REAL.H>
@@ -24,10 +24,9 @@ MultiDiagnostics::MultiDiagnostics ()
         if ( diags_types[i] == DiagTypes::Full ){
             alldiags[i] = std::make_unique<FullDiagnostics>(i, diags_names[i]);
         } else if ( diags_types[i] == DiagTypes::BackTransformed ){
-#ifdef WARPX_DIM_RZ
-            amrex::Abort(Utils::TextMsg::Err("BackTransformed diagnostics is currently not supported for RZ"));
-#else
             alldiags[i] = std::make_unique<BTDiagnostics>(i, diags_names[i]);
+#ifdef WARPX_DIM_RZ
+            ablastr::warn_manager::WMRecordWarning("MultiDiagnostics", "BackTransformed diagnostics for fields is not yet fully implemented in RZ. Field output might be incorrect.");
 #endif
         } else if ( diags_types[i] == DiagTypes::BoundaryScraping ){
             alldiags[i] = std::make_unique<BoundaryScrapingDiagnostics>(i, diags_names[i]);
