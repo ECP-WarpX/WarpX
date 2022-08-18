fbaf5226ee92444b618029ad56722f8e54918448
Yin-YinjianZhao <yinjianzhao@lbl.gov>
2022-08-18 11:57:46 -0700

    The automated test of RZ passed, cleaning up needed.

--- Examples/Tests/LoadExternalField/analysis_rz.py
+++ Examples/Tests/LoadExternalField/analysis_rz.py
@@ -6,42 +6,27 @@
 #
 # License: BSD-3-Clause-LBNL
 
-
-# This script tests the particle pusher (HC)
-# using a force-free field,
-# in which position x should remain 0.
-# An initial velocity Vy corresponding to
-# Lorentz factor = 20 is used.
-# Bz is fixed at 1 T.
-# Ex = -Vy*Bz.
-
-# Possible errors:
-# Boris:     2321.3958529
-# Vay:       0.00010467
-# HC:        0.00011403
-# tolerance: 0.001
-# Possible running time: ~ 4.0 s
-
 import os
 import sys
 import numpy as np
 import openpmd_api as io
 from openpmd_viewer import OpenPMDTimeSeries
+import yt
 
 sys.path.insert(1, '../../../../warpx/Regression/Checksum/')
 import checksumAPI
 
 tolerance = 0.001
-x0 = 1.209762559606344934e-01 #1.140180338454055003e-1
-y0 = -2.730782429781069068e-02 #-4.908434106235705363e-2
-z0 = 4.363249204658920632e+00 #4.379090207432996706
+r0 = 0.12402005
+z0 = 4.3632492
 
 filename = sys.argv[1]
-ts = OpenPMDTimeSeries(filename)
-it = ts.iterations
-x,y,z = ts.get_particle(var_list=['x','y','z'], iteration=it[len(it)-1], species='proton')
+ds = yt.load( filename )
+ad = ds.all_data()
+r  = ad['proton','particle_position_x'].to_ndarray()
+z  = ad['proton','particle_position_y'].to_ndarray()
 
-error = np.sqrt((x-x0)**2+(y-y0)**2+(z-z0)**2)
+error = np.min(np.sqrt((r-r0)**2+(z-z0)**2))
 
 print('error = ', error)
 print('tolerance = ', tolerance)
