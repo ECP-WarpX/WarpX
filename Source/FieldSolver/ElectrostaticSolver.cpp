df0ef6403cb4c8dbbe6cbe95607ce16963175513
Dave Grote <grote1@llnl.gov>
2022-09-27 11:14:54 -0700

    Added tridiag solve for 1D

--- Source/FieldSolver/ElectrostaticSolver.cpp
+++ Source/FieldSolver/ElectrostaticSolver.cpp
@@ -242,10 +242,24 @@ WarpX::AddSpaceChargeFieldLabFrame ()
     setPhiBC(phi_fp);
 
     // Compute the potential phi, by solving the Poisson equation
-    if ( IsPythonCallBackInstalled("poissonsolver") ) ExecutePythonCallback("poissonsolver");
-    else computePhi( rho_fp, phi_fp, beta, self_fields_required_precision,
-                     self_fields_absolute_tolerance, self_fields_max_iters,
-                     self_fields_verbosity );
+    if (IsPythonCallBackInstalled("poissonsolver")) {
+
+        // Use the Python level solver (user specificed)
+        ExecutePythonCallback("poissonsolver");
+
+    } else {
+
+#if defined(WARPX_DIM_1D_Z)
+        // Use the tridiag solver with 1D
+        computePhiTriDiagonal(rho_fp, phi_fp);
+#else
+        // Use the AMREX MLMG solver otherwise
+        computePhi(rho_fp, phi_fp, beta, self_fields_required_precision,
+                   self_fields_absolute_tolerance, self_fields_max_iters,
+                   self_fields_verbosity);
+#endif
+
+    }
 
     // Compute the electric field. Note that if an EB is used the electric
     // field will be calculated in the computePhi call.
@@ -669,6 +683,153 @@ WarpX::computeB (amrex::Vector<std::array<std::unique_ptr<amrex::MultiFab>, 3> >
 #endif
         }
     }
+}
+
+/* \brief Compute the potential by solving Poisson's equation with
+          a 1D tridiagonal solve.
+
+   \param[in] rho The charge density a given species
+   \param[out] phi The potential to be computed by this function
+*/
+void
+WarpX::computePhiTriDiagonal (const amrex::Vector<std::unique_ptr<amrex::MultiFab> >& rho,
+                              amrex::Vector<std::unique_ptr<amrex::MultiFab> >& phi) const
+{
+
+    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(max_level == 0,
+        "The tridiagonal solver cannot be used with mesh refinement");
+
+    const int lev = 0;
+
+    const amrex::Real* dx = Geom(lev).CellSize();
+    const amrex::Real xmin = Geom(lev).ProbLo(0);
+    const amrex::Real xmax = Geom(lev).ProbHi(0);
+    const int nx_full_domain = static_cast<int>( (xmax - xmin)/dx[0] + 0.5_rt );
+
+    int nx_solve_min = 1;
+    int nx_solve_max = nx_full_domain - 1;
+
+    auto field_boundary_lo0 = WarpX::field_boundary_lo[0];
+    auto field_boundary_hi0 = WarpX::field_boundary_hi[0];
+    if (field_boundary_lo0 == FieldBoundaryType::None) {
+        // Neumann boundary condition
+        // Solve for the point on the lower boundary
+        nx_solve_min = 0;
+    }
+    if (field_boundary_hi0 == FieldBoundaryType::None) {
+        // Neumann boundary condition
+        // Solve for the point on the upper boundary
+        nx_solve_max = nx_full_domain;
+    }
+
+    // Create a 1-D MultiFab that covers all of x, including guard cells on each end.
+    // The tridiag solve will be done in this MultiFab and then copied out afterwards.
+    const amrex::IntVect lo_full_domain(AMREX_D_DECL(-1,0,0));
+    const amrex::IntVect hi_full_domain(AMREX_D_DECL(nx_full_domain+1,0,0));
+    const amrex::Box box_full_domain(lo_full_domain, hi_full_domain);
+    const BoxArray ba_full_domain(box_full_domain);
+    amrex::DistributionMapping dm_full_domain;
+    amrex::Vector<int> pmap = {0}; // The data will only be on processor 0
+    dm_full_domain.define(pmap);
+    const int ncomps1d = 1;
+    const amrex::IntVect nguard1d(AMREX_D_DECL(1,0,0));
+    const BoxArray ba_full_domain_node = amrex::convert(ba_full_domain, amrex::IntVect::TheNodeVector());
+
+    // Put the data in the pinned arena since the tridiag solver will be done on the CPU, but have
+    // the data readily accessible from the GPU.
+    auto phi1d_mf = MultiFab(ba_full_domain_node, dm_full_domain, ncomps1d, nguard1d, MFInfo().SetArena(The_Pinned_Arena()));
+    auto zwork1d_mf = MultiFab(ba_full_domain_node, dm_full_domain, ncomps1d, nguard1d, MFInfo().SetArena(The_Pinned_Arena()));
+    auto rho1d_mf = MultiFab(ba_full_domain_node, dm_full_domain, ncomps1d, nguard1d, MFInfo().SetArena(The_Pinned_Arena()));
+
+    // Multiplier on the charge density
+    const amrex::Real norm = dx[0]*dx[0]/PhysConst::ep0;
+
+    // Copy previous phi to get the boundary values
+    phi1d_mf.ParallelCopy(*phi[lev], 0, 0, 1, Geom(lev).periodicity());
+    rho1d_mf.ParallelCopy(*rho[lev], 0, 0, 1, Geom(lev).periodicity());
+    rho1d_mf.mult(norm);
+
+    const auto& phi1d_arr = phi1d_mf[0].array();
+    const auto& zwork1d_arr = zwork1d_mf[0].array();
+    const auto& rho1d_arr = rho1d_mf[0].array();
+
+    // The loops are always performed on the CPU
+
+    amrex::Real diag = 2._rt;
+
+    if (field_boundary_lo0 == FieldBoundaryType::PEC) {
+
+        phi1d_arr(1,0,0) = (phi1d_arr(0,0,0) + rho1d_arr(1,0,0))/diag;
+
+    } else if (field_boundary_lo0 == FieldBoundaryType::None) {
+
+        // Neumann boundary condition
+        phi1d_arr(0,0,0) = rho1d_arr(0,0,0)/diag;
+
+        zwork1d_arr(1,0,0) = -2._rt/diag;
+        diag = 2._rt - (-1._rt)*zwork1d_arr(1,0,0);
+        phi1d_arr(1,0,0) = (rho1d_arr(1,0,0) - (-1._rt)*phi1d_arr(1-1,0,0))/diag;
+
+    }
+
+    for (int i_up = 2 ; i_up < nx_solve_max ; i_up++) {
+
+        zwork1d_arr(i_up,0,0) = -1._rt/diag;
+        diag = 2._rt - (-1._rt)*zwork1d_arr(i_up,0,0);
+        phi1d_arr(i_up,0,0) = (rho1d_arr(i_up,0,0) - (-1._rt)*phi1d_arr(i_up-1,0,0))/diag;
+
+    }
+
+    int const imax = nx_solve_max;
+    if (field_boundary_hi0 == FieldBoundaryType::PEC) {
+
+        zwork1d_arr(imax,0,0) = -1._rt/diag;
+        diag = 2._rt - (-1._rt)*zwork1d_arr(imax,0,0);
+        phi1d_arr(imax,0,0) = (phi1d_arr(imax+1,0,0) + rho1d_arr(imax,0,0) - (-1._rt)*phi1d_arr(imax-1,0,0))/diag;
+
+    } else if (field_boundary_hi0 == FieldBoundaryType::None) {
+
+        // Neumann boundary condition
+        zwork1d_arr(imax,0,0) = -2._rt/diag;
+        diag = 2._rt - (-1._rt)*zwork1d_arr(imax,0,0);
+        phi1d_arr(imax,0,0) = (rho1d_arr(imax,0,0) - (-1._rt)*phi1d_arr(imax-1,0,0))/diag;
+
+    }
+
+    for (int i_down = nx_solve_max-1 ; i_down >= nx_solve_min ; i_down--) {
+
+        phi1d_arr(i_down,0,0) = phi1d_arr(i_down,0,0) - zwork1d_arr(i_down+1,0,0)*phi1d_arr(i_down+1,0,0);
+
+    }
+
+    // Extrapolate into the lower guard cell
+    if (field_boundary_lo0 == FieldBoundaryType::PEC) {
+
+        phi1d_arr(-1,0,0) = 2._rt*phi1d_arr(0,0,0) - phi1d_arr(1,0,0);
+
+    } else if (field_boundary_lo0 == FieldBoundaryType::None) {
+
+        // Neumann boundary condition
+        phi1d_arr(-1,0,0) = phi1d_arr(1,0,0);
+
+    }
+
+    // Extrapolate into the upper guard cell
+    if (field_boundary_hi0 == FieldBoundaryType::PEC) {
+
+        phi1d_arr(nx_full_domain+1,0,0) = 2._rt*phi1d_arr(nx_full_domain,0,0) - phi1d_arr(nx_full_domain-1,0,0);
+
+    } else if (field_boundary_hi0 == FieldBoundaryType::None) {
+
+        // Neumann boundary condition
+        phi1d_arr(nx_full_domain+1,0,0) = phi1d_arr(nx_full_domain-1,0,0);
+
+    }
+
+    // Copy phi1d to phi, including the x guard cell
+    const IntVect xghost(AMREX_D_DECL(1,0,0));
+    phi[lev]->ParallelCopy(phi1d_mf, 0, 0, 1, xghost, xghost, Geom(lev).periodicity());
+
 }
 
 void ElectrostaticSolver::PoissonBoundaryHandler::definePhiBCs ( )
