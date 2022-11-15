fa9f871ae0e17e7f0d1deeaa1c3117ef7d48c867
Axel Huebl <axel.huebl@plasma.ninja>
2022-11-15 09:58:24 -0600

    AMReX/PICSAR: Weekly Update (#3522)
    * AMReX: Weekly Update
    
    * CMake: Fix -fPIC (for all shared builds)
    
    openPMD currently side-injects shared build defaults, thus this surfaced.

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -92,7 +92,8 @@ macro(find_amrex)
         endif()
 
         # shared libs, i.e. for Python bindings, need relocatable code
-        if(WarpX_LIB OR ABLASTR_POSITION_INDEPENDENT_CODE)
+        #   openPMD: currently triggers shared libs (TODO)
+        if(WarpX_LIB OR ABLASTR_POSITION_INDEPENDENT_CODE OR BUILD_SHARED_LIBS OR WarpX_OPENPMD)
             set(AMReX_PIC ON CACHE INTERNAL "")
         endif()
 
@@ -239,7 +240,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "0d3deeb5c75cade14c381ef620921beaa2604c11"
+set(WarpX_amrex_branch "d8bc97f92a1a568b6e996db3b8d9715fced0464f"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
