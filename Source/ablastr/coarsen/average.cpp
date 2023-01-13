b3fdc8235cd563881507220eb6088e0390ee9fe3
Prabhat Kumar <89051199+prkkumar@users.noreply.github.com>
2023-01-12 17:14:35 -0800

    Update Source/ablastr/coarsen/average.cpp
    Co-authored-by: Weiqun Zhang <WeiqunZhang@lbl.gov>

--- Source/ablastr/coarsen/average.cpp
+++ Source/ablastr/coarsen/average.cpp
@@ -105,11 +105,7 @@ namespace ablastr::coarsen::average
                                            "source MultiFab and destination MultiFab have different IndexType");
 
         // Number of guard cells to fill on coarse patch and number of components
-        amrex::IntVect ngrow = mf_src.nGrowVect();
-        for (int l=0; l < AMREX_SPACEDIM; ++l) {
-             if (crse_ratio[l] > 1)  ngrow[l] = (ngrow[l] + 1) / crse_ratio[l];
-        }
-
+        const amrex::IntVect ngrow = (mf_src.nGrowVect() + crse_ratio-1) / crse_ratio; // round up int division
         const int ncomp = mf_src.nComp();
 
         Loop(mf_dst, mf_src, ncomp, ngrow, crse_ratio);
