8a9f9c4c42d84f5f1fff03fce3badb2db33e9af2
Axel Huebl <axel.huebl@plasma.ninja>
2023-01-29 17:22:53 -0800

    AMReX/PICSAR: Weekly Update (#3634)
    * AMReX: Weekly Update
    
    * 225c605680e825e1f0793780746b4112d4262381

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -240,7 +240,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "d5ddf3b22e949e7cc346a2f8660d1c73edfa391a"
+set(WarpX_amrex_branch "225c605680e825e1f0793780746b4112d4262381"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
--- run_test.sh
+++ run_test.sh
@@ -71,7 +71,7 @@ python3 -m pip install --upgrade -r warpx/Regression/requirements.txt
 
 # Clone AMReX and warpx-data
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach d5ddf3b22e949e7cc346a2f8660d1c73edfa391a && cd -
+cd amrex && git checkout --detach 225c605680e825e1f0793780746b4112d4262381 && cd -
 # warpx-data contains various required data sets
 git clone --depth 1 https://github.com/ECP-WarpX/warpx-data.git
 
