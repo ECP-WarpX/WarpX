6c30444437c60856876c108fd5395d018a908588
Edoardo Zoni <ezoni@lbl.gov>
2023-02-01 18:28:16 -0800

    Improve Python script to plot PSATD stencils

--- Tools/Algorithms/stencil.py
+++ Tools/Algorithms/stencil.py
@@ -19,6 +19,9 @@ from scipy.constants import c
 plt.style.use('tableau-colorblind10')
 plt.rcParams.update({'font.size': 14})
 
+sp = np.finfo(np.float32).eps
+dp = np.finfo(np.float64).eps
+
 def get_Fornberg_coeffs(order, staggered):
     """
     Compute the centered or staggered
@@ -37,22 +40,21 @@ def get_Fornberg_coeffs(order, staggered):
     coeffs : numpy.ndarray
         Array of centered or staggered Fornberg coefficients.
     """
-    m = order // 2
+    m = order//2
     coeffs = np.zeros(m+1)
 
     # Compute Fornberg coefficients by recurrence
     if staggered:
         prod = 1.
         for k in range(1, m+1):
-            prod = prod * (m+k) / (4*k)
-        coeffs[0] = 4 * m * prod**2
+            prod = prod*(m+k)/(4*k)
+        coeffs[0] = 4*m*prod**2
         for n in range(1, m+1):
-            coeffs[n] = - (((2*n-3) * (m+1-n))
-                        / ((2*n-1) * (m-1+n)) * coeffs[n-1])
+            coeffs[n] = -(((2*n-3)*(m+1-n))/((2*n-1)*(m-1+n))*coeffs[n-1])
     else:
         coeffs[0] = -2.
         for n in range(1, m+1):
-            coeffs[n] = - (m+1-n) / (m+n) * coeffs[n-1]
+            coeffs[n] = -(m+1-n)/(m+n)*coeffs[n-1]
 
     return coeffs
 
@@ -78,7 +80,7 @@ def modified_k(kx, dx, order, staggered):
     k_mod : numpy.ndarray
         Centered or staggered modified wave vector.
     """
-    m = order // 2
+    m = order//2
     coeffs = get_Fornberg_coeffs(order, staggered)
 
     # Array of values for n: from 1 to m
@@ -87,11 +89,9 @@ def modified_k(kx, dx, order, staggered):
     # Array of values of sin
     # (first axis corresponds to k and second axis to n)
     if staggered:
-        sin_kn = (np.sin(kx[:,np.newaxis]*(n[np.newaxis,:]-0.5) * dx)
-                 / ((n[np.newaxis,:]-0.5) * dx))
+        sin_kn = (np.sin(kx[:,np.newaxis]*(n[np.newaxis,:]-0.5)*dx)/((n[np.newaxis,:]-0.5)*dx))
     else:
-        sin_kn = (np.sin(kx[:,np.newaxis]*n[np.newaxis,:] * dx)
-                 / (n[np.newaxis,:] * dx))
+        sin_kn = (np.sin(kx[:,np.newaxis]*n[np.newaxis,:]*dx)/(n[np.newaxis,:]*dx))
 
     # Modified k
     k_mod = np.tensordot(sin_kn, coeffs[1:], axes=(-1,-1))
@@ -121,7 +121,7 @@ def func_cosine(om, w_c, dt):
         Leading spectral coefficient of the general PSATD equations.
     """
     theta_c = np.exp(1.j*w_c*dt*0.5)
-    coeff = theta_c**2 * np.cos(om*dt)
+    coeff = theta_c**2*np.cos(om*dt)
     return coeff
 
 def compute_stencils(coeff_nodal, coeff_stagg, axis):
@@ -209,9 +209,9 @@ def compute_all(dx, dy, dz, dt, nox, noy, noz, v_gal, nx=256, ny=256, nz=256):
         Nodal and staggered stencils along all directions.
     """
     # k vectors
-    kx_arr = 2 * np.pi * np.fft.fftfreq(nx, dx)
-    ky_arr = 2 * np.pi * np.fft.fftfreq(ny, dy)
-    kz_arr = 2 * np.pi * np.fft.fftfreq(nz, dz)
+    kx_arr = 2*np.pi*np.fft.fftfreq(nx, dx)
+    ky_arr = 2*np.pi*np.fft.fftfreq(ny, dy)
+    kz_arr = 2*np.pi*np.fft.fftfreq(nz, dz)
 
     # Centered modified k vectors
     kx_arr_c = modified_k(kx_arr, dx, nox, False) if nox != 'inf' else kx_arr
@@ -228,11 +228,11 @@ def compute_all(dx, dy, dz, dt, nox, noy, noz, v_gal, nx=256, ny=256, nz=256):
     kx_s, ky_s, kz_s = np.meshgrid(kx_arr_s, ky_arr_s, kz_arr_s)
 
     # Frequencies
-    kk_c = np.sqrt(kx_c**2 + ky_c**2 + kz_c**2)
-    kk_s = np.sqrt(kx_s**2 + ky_s**2 + kz_s**2)
-    om_c = c * kk_c
-    om_s = c * kk_s
-    w_c = v_gal * kz_c
+    kk_c = np.sqrt(kx_c**2+ky_c**2+kz_c**2)
+    kk_s = np.sqrt(kx_s**2+ky_s**2+kz_s**2)
+    om_c = c*kk_c
+    om_s = c*kk_s
+    w_c = v_gal*kz_c
 
     # Spectral coefficient
     coeff_nodal = func_cosine(om_c, w_c, dt)
@@ -246,7 +246,7 @@ def compute_all(dx, dy, dz, dt, nox, noy, noz, v_gal, nx=256, ny=256, nz=256):
 
     return stencils
 
-def compute_guard_cells(error, stencil):
+def compute_guard_cells(errmin, errmax, stencil):
     """
     Compute the minimum number of guard cells for a given
     error threshold (number of guard cells such that the
@@ -264,10 +264,13 @@ def compute_guard_cells(error, stencil):
     guard_cells : numpy.int64
         Number of cells.
     """
-    diff = stencil - error
-    minv = diff[diff > 0.].min()
-    guard_cells = np.argwhere(diff == minv)[0,0]
-    return guard_cells
+    diff = stencil - errmin
+    v = next(d for d in diff if d < 0)
+    gcmin = np.argwhere(diff == v)[0,0]
+    diff = stencil - errmax
+    v = next(d for d in diff if d < 0)
+    gcmax = np.argwhere(diff == v)[0,0] - 1
+    return (gcmin, gcmax)
 
 def plot_stencil(cells, stencil_nodal, stencil_stagg, label, path, name):
     """
@@ -287,27 +290,34 @@ def plot_stencil(cells, stencil_nodal, stencil_stagg, label, path, name):
     name : str
         Label for figure name.
     """
-    fig = plt.figure(dpi=100)
+    fig = plt.figure(figsize=[10,6])
     ax = fig.add_subplot(111)
-    ax.plot(cells, stencil_nodal, '-', label='nodal')
-    ax.plot(cells, stencil_stagg, '-', label='staggered, hybrid')
+    ax.plot(cells, stencil_nodal, linestyle='-', label='nodal')
+    ax.plot(cells, stencil_stagg, linestyle='-', label='staggered or hybrid')
+    # Plot single and double precision machine epsilons
+    ax.axhline(y=sp, c='grey', ls='dashed', label='machine epsilon (single precision)')
+    ax.axhline(y=dp, c='grey', ls='dotted', label='machine epsilon (double precision)')
+    # Shade regions between single and double precision machine epsilons
+    xmin, xmax = compute_guard_cells(sp, dp, stencil_nodal)
+    ax.fill_between(cells[xmin:xmax+1], stencil_nodal[xmin:xmax+1], alpha=0.5)
+    xmin, xmax = compute_guard_cells(sp, dp, stencil_stagg)
+    ax.fill_between(cells[xmin:xmax+1], stencil_stagg[xmin:xmax+1], alpha=0.5)
+    #
     ax.set_yscale('log')
     ax.set_xticks(cells, minor=True)
     ax.grid(which='minor', linewidth=0.2)
     ax.grid(which='major', linewidth=0.4)
     ax.legend()
     ax.set_xlabel('number of cells')
-    ax.set_ylabel(r'$\Gamma({:s})$'.format(label))
+    ax.set_ylabel('signal to be truncated')
     ax.set_title(r'Stencil extent along ${:s}$'.format(label))
     fig.tight_layout()
     fig_name = os.path.join(path, 'figure_stencil_' + label)
     if name:
         fig_name += '_' + name
-    fig.savefig(fig_name + '.pdf', dpi=100)
-    fig.savefig(fig_name + '.png', dpi=100)
+    fig.savefig(fig_name + '.png', dpi=150)
 
-def run_main(dx, dy, dz, dt, nox, noy, noz, gamma=1., galilean=False,
-             ex=1e-07, ey=1e-07, ez=1e-07, path='.', name=''):
+def run_main(dx, dy, dz, dt, nox, noy, noz, gamma=1., galilean=False, path='.', name=''):
     """
     Main function.
 
@@ -331,12 +341,6 @@ def run_main(dx, dy, dz, dt, nox, noy, noz, gamma=1., galilean=False,
         Lorentz factor.
     galilean : bool, optional (default = False)
         Galilean scheme.
-    ex : float, optional (default = 1e-07)
-        Error threshold along x.
-    ey : float, optional (default = 1e-07)
-        Error threshold along y.
-    ez : float, optional (default = 1e-07)
-        Error threshold along z.
     path : str, optional (default = '.')
         Path where figures are saved.
     name : str, optional (default = '')
@@ -356,32 +360,27 @@ def run_main(dx, dy, dz, dt, nox, noy, noz, gamma=1., galilean=False,
     # Galilean velocity (default = 0.)
     v_gal = 0.
     if galilean:
-        v_gal = - np.sqrt(1.-1./gamma**2) * c
+        v_gal = -np.sqrt(1.-1./gamma**2)*c
 
     # Display some output
-    print('\n >> Numerical Setup')
-    print('    ---------------')
-    print('\n    cell size:')
-    print('    - dx = {:g}'.format(dx))
-    print('    - dy = {:g}'.format(dy))
-    print('    - dz = {:g}'.format(dz))
-    print('    - dz/dx = {:g}'.format(dz / dx))
-    print('    - dz/dy = {:g}'.format(dz / dy))
-    print('\n    time step:')
-    print('    - dt = {:g}'.format(dt))
-    print('    - c*dt/dx = {:g}'.format(c * dt / dx))
-    print('    - c*dt/dy = {:g}'.format(c * dt / dy))
-    print('    - c*dt/dz = {:g}'.format(c * dt / dz))
-    print('\n    spectral order:')
-    print('    - nox = {:d}'.format(nox))
-    print('    - noy = {:d}'.format(noy))
-    print('    - noz = {:d}'.format(noz))
-    print('\n    Lorentz boost, Galilean velocity:')
-    print('    - gamma = {:g}'.format(gamma))
-    print('    - v_gal = {:g}'.format(v_gal))
-
-    print('\n >> Compute Stencil')
-    print('    ---------------')
+    print('\nCell size:')
+    print('- dx = {:g}'.format(dx))
+    print('- dy = {:g}'.format(dy))
+    print('- dz = {:g}'.format(dz))
+    print('- dz/dx = {:g}'.format(dz/dx))
+    print('- dz/dy = {:g}'.format(dz/dy))
+    print('\nTime step:')
+    print('- dt = {:g}'.format(dt))
+    print('- c*dt/dx = {:g}'.format(c*dt/dx))
+    print('- c*dt/dy = {:g}'.format(c*dt/dy))
+    print('- c*dt/dz = {:g}'.format(c*dt/dz))
+    print('\nSpectral order:')
+    print('- nox = {:d}'.format(nox))
+    print('- noy = {:d}'.format(noy))
+    print('- noz = {:d}'.format(noz))
+    print('\nLorentz boost, Galilean velocity:')
+    print('- gamma = {:g}'.format(gamma))
+    print('- v_gal = {:g}'.format(v_gal))
 
     stencils = compute_all(dx, dy, dz, dt, nox, noy, noz, v_gal)
 
@@ -407,51 +406,37 @@ def run_main(dx, dy, dz, dt, nox, noy, noz, gamma=1., galilean=False,
     # (number of guard cells such that the stencil measure
     # is not larger than the error threshold)
 
-    print('\n    error threshold:')
-    print('    - ex = {:g}'.format(ex))
-    print('    - ey = {:g}'.format(ey))
-    print('    - ez = {:g}'.format(ez))
-
-    # 1) Nodal solver
-    gc_nodal_x = compute_guard_cells(ex, stencils['x']['nodal'])
-    gc_nodal_y = compute_guard_cells(ey, stencils['y']['nodal'])
-    gc_nodal_z = compute_guard_cells(ez, stencils['z']['nodal'])
-
-    print('\n    nodal solver:')
-    print('    - {:d} guard cells along x'.format(gc_nodal_x))
-    print('    - {:d} guard cells along y'.format(gc_nodal_y))
-    print('    - {:d} guard cells along z'.format(gc_nodal_z))
-
-    # 2) Staggered solver
-    gc_stagg_x = compute_guard_cells(ex, stencils['x']['stagg'])
-    gc_stagg_y = compute_guard_cells(ey, stencils['y']['stagg'])
-    gc_stagg_z = compute_guard_cells(ez, stencils['z']['stagg'])
-
-    print('\n    staggered solver:')
-    print('    - {:d} guard cells along x'.format(gc_stagg_x))
-    print('    - {:d} guard cells along y'.format(gc_stagg_y))
-    print('    - {:d} guard cells along z'.format(gc_stagg_z))
-
     # Plot stencils
-    plot_stencil(cx, stencils['x']['nodal'], stencils['x']['stagg'],
-                 'x', path, name)
-    plot_stencil(cy, stencils['y']['nodal'], stencils['y']['stagg'],
-                 'y', path, name)
-    plot_stencil(cz, stencils['z']['nodal'], stencils['z']['stagg'],
-                 'z', path, name)
+    plot_stencil(cx, stencils['x']['nodal'], stencils['x']['stagg'], 'x', path, name)
+    plot_stencil(cy, stencils['y']['nodal'], stencils['y']['stagg'], 'y', path, name)
+    plot_stencil(cz, stencils['z']['nodal'], stencils['z']['stagg'], 'z', path, name)
 
-    print('\n >> Output Summary')
-    print('    --------------')
+    # Compute min and max numbers of guard cells
+    gcmin_x_nodal, gcmax_x_nodal = compute_guard_cells(sp, dp, stencils['x']['nodal'])
+    gcmin_y_nodal, gcmax_y_nodal = compute_guard_cells(sp, dp, stencils['y']['nodal'])
+    gcmin_z_nodal, gcmax_z_nodal = compute_guard_cells(sp, dp, stencils['z']['nodal'])
+    #
+    gcmin_x_stagg, gcmax_x_stagg = compute_guard_cells(sp, dp, stencils['x']['stagg'])
+    gcmin_y_stagg, gcmax_y_stagg = compute_guard_cells(sp, dp, stencils['y']['stagg'])
+    gcmin_z_stagg, gcmax_z_stagg = compute_guard_cells(sp, dp, stencils['z']['stagg'])
 
-    print('\n    path to figures: \'' + os.path.abspath(path) + '\'')
-
-    print('\n    stencil arrays:')
-    print('    - sx_nodal')
-    print('    - sx_stagg')
-    print('    - sy_nodal')
-    print('    - sy_stagg')
-    print('    - sz_nodal')
-    print('    - sz_stagg\n')
+    fig_path = os.path.abspath(path)
+    print(f'\nFigures saved in {fig_path}/.')
+    print('\nThe plots show the extent of the signal to be truncated (y-axis)'
+        + '\nby choosing a given number of cells (x-axis) for the ghost regions'
+        + '\nof each simulation grid, along x, y, and z.')
+    print('\nIt is recommended to choose a number of ghost cells that corresponds to'
+        + '\na truncation of the signal between single and double machine precision.'
+        + '\nThe more guard cells, the more accurate, yet expensive, results.')
+    print('\nFor a nodal simulation, choose:')
+    print(f'- between {gcmin_x_nodal} and {gcmax_x_nodal} ghost cells along x')
+    print(f'- between {gcmin_y_nodal} and {gcmax_y_nodal} ghost cells along y')
+    print(f'- between {gcmin_z_nodal} and {gcmax_z_nodal} ghost cells along z')
+    print('\nFor a staggered or hybrid simulation, choose:')
+    print(f'- between {gcmin_x_stagg} and {gcmax_x_stagg} ghost cells along x')
+    print(f'- between {gcmin_y_stagg} and {gcmax_y_stagg} ghost cells along y')
+    print(f'- between {gcmin_z_stagg} and {gcmax_z_stagg} ghost cells along z')
+    print()
 
     return stencils
 
@@ -474,11 +459,6 @@ if __name__ == '__main__':
     gamma = 30.
     # Galilean flag
     galilean = True
-    # Error threshold
-    # (might need to be smaller in z than (x,y) w/ Galilean algorithms)
-    ex = 1e-07
-    ey = 1e-07
-    ez = 1e-07
     # Output path
     path = '.'
     # Output name tag (can be empty: '')
@@ -487,13 +467,4 @@ if __name__ == '__main__':
 
     # Run main function (some arguments are optional,
     # see definition of run_main function for help)
-    stencils = run_main(dx, dy, dz, dt, nox, noy, noz, gamma,
-                        galilean, ex, ey, ez, path, name)
-
-    # Make stencil arrays available for inspection
-    sx_nodal = stencils['x']['nodal']
-    sx_stagg = stencils['x']['stagg']
-    sy_nodal = stencils['y']['nodal']
-    sy_stagg = stencils['y']['stagg']
-    sz_nodal = stencils['z']['nodal']
-    sz_stagg = stencils['z']['stagg']
+    stencils = run_main(dx, dy, dz, dt, nox, noy, noz, gamma, galilean, path, name)
