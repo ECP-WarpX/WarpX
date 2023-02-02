2525a9b68998f37b75198f988c5799e95a7f4621
Axel Huebl <axel.huebl@plasma.ninja>
2023-02-02 14:27:06 -0800

    Release 23.02 (#3667)
    * AMReX: 23.02
    
    * PICSAR: 23.02
    
    * WarpX: 23.02

--- Python/setup.py
+++ Python/setup.py
@@ -54,7 +54,7 @@ else:
     package_data = {}
 
 setup(name = 'pywarpx',
-      version = '23.01',
+      version = '23.02',
       packages = ['pywarpx'],
       package_dir = {'pywarpx': 'pywarpx'},
       description = """Wrapper of WarpX""",
