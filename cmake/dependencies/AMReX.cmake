f5fa305a86a35cf0f07ee798c601b5cee86a6996
Remi Lehe <remi.lehe@normalesup.org>
2023-02-10 14:49:56 -0800

    Update AMReX depsndency to use Bcast with complexs

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -240,7 +240,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "e81a9c232c2608de90b3514ece0f211687b41cfe"
+set(WarpX_amrex_branch "522f1bfb237c6967fe2ac228131e3b917242b7ea"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
