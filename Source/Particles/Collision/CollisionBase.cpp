aec32acb15f9644beb3a48bcef77fb4eae7d5b84
Arianna Formenti <ariannaformenti@lbl.gov>
2023-02-09 20:27:15 -0800

    removed print

--- Source/Particles/Collision/CollisionBase.cpp
+++ Source/Particles/Collision/CollisionBase.cpp
@@ -16,8 +16,7 @@ CollisionBase::CollisionBase (std::string collision_name)
     // read collision species
     amrex::ParmParse pp_collision_name(collision_name);
     pp_collision_name.getarr("species", m_species_names);
-    amrex::Print() << "PRINT COLL NAME " << collision_name << "\n";  // Print with 10 digits
-
+    
     // number of time steps between collisions
     m_ndt = 1;
     utils::parser::queryWithParser(
