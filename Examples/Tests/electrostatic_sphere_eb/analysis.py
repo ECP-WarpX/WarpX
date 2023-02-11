dfce4dd24073ca8eed0565c09ce8d5c00d6fcca5
Remi Lehe <remi.lehe@normalesup.org>
2023-02-11 14:57:35 -0800

    Update Python test

--- Examples/Tests/electrostatic_sphere_eb/analysis.py
+++ Examples/Tests/electrostatic_sphere_eb/analysis.py
@@ -14,12 +14,15 @@ import numpy as np
 from scipy.constants import epsilon_0
 
 data = np.loadtxt('diags/reducedfiles/eb_charge.txt')
+data_eighth = np.loadtxt('diags/reducedfiles/eb_charge_one_eighth.txt')
 q_sim = data[1,2]
+q_sim_eighth = data_eighth[1,2]
 # Theoretical charge on the embedded boundary, for sphere at potential phi_0
 phi_0 = 1. # V
 R = 0.3 # m
 q_th = -4*np.pi*epsilon_0*phi_0*R
 assert abs((q_sim-q_th)/q_th) < 0.03
+assert abs((q_sim_eighth-q_th/8)/(q_th/8)) < 0.03
 
 # The first step is the same as in inputs_3d
 my_check = checksumAPI.evaluate_checksum(
