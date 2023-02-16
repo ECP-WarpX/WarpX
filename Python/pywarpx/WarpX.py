6302671c33d8c7626326818ec6d5fc7246e50953
Dave Grote <grote1@llnl.gov>
2023-02-16 10:51:03 -0800

    Added FieldReduction and ParticleHistogram to ReducedDiagnostics

--- Python/pywarpx/WarpX.py
+++ Python/pywarpx/WarpX.py
@@ -84,7 +84,8 @@ class WarpX(Bucket):
         reduced_diagnostics.reduced_diags_names = reduced_diagnostics._diagnostics_dict.keys()
         argv += reduced_diagnostics.attrlist()
         for diagnostic in reduced_diagnostics._diagnostics_dict.values():
-            diagnostic.species = diagnostic._species_dict.keys()
+            if 'species' in diagnostic._species_dict:
+                diagnostic.species = diagnostic._species_dict.keys()
             argv += diagnostic.attrlist()
             for species_diagnostic in diagnostic._species_dict.values():
                 argv += species_diagnostic.attrlist()
