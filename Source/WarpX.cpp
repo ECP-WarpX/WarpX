07ab5538e0eb237ce94514ef04da6c843d36d9d3
kaplannp <kaplannp@whitman.edu>
2023-04-07 00:36:28 +0200

    Throw error on shared if no hip or cuda
    This commit makes the assumption that if you use shared, you must be
    using HIP or CUDA. This allows us to remove a bunch of macros that tried
    to quietly revert to non shared if you didn't use HIP/CUDA, and we now
    throw error if you try to run without HIP/CUDA

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -134,7 +134,7 @@ int WarpX::macroscopic_solver_algo;
 bool WarpX::do_single_precision_comms = false;
 
 bool WarpX::do_shared_mem_charge_deposition = false;
-#if defined(WARPX_DIM_3D) || defined(WARPX_DIM_2D)
+#if (defined(AMREX_USE_HIP) || defined(AMREX_USE_CUDA)) && (defined(WARPX_DIM_3D) || defined(WARPX_DIM_2D))
 bool WarpX::do_shared_mem_current_deposition = true;
 #else
 bool WarpX::do_shared_mem_current_deposition = false;
@@ -144,7 +144,7 @@ amrex::IntVect WarpX::shared_tilesize(AMREX_D_DECL(6,6,8));
 #elif defined(WARPX_DIM_2D)
 amrex::IntVect WarpX::shared_tilesize(AMREX_D_DECL(14,14));
 #else
-amrex::IntVect WarpX::shared_tilesize(AMREX_D_DECL(0,0,0));
+amrex::IntVect WarpX::shared_tilesize(AMREX_D_DECL(0,0,0)); //Not used
 #endif
 int WarpX::shared_mem_current_tpb = 128;
 
@@ -787,6 +787,10 @@ WarpX::ReadParameters ()
 #endif
         pp_warpx.query("do_shared_mem_charge_deposition", do_shared_mem_charge_deposition);
         pp_warpx.query("do_shared_mem_current_deposition", do_shared_mem_current_deposition);
+#if !(defined(AMREX_USE_HIP) || defined(AMREX_USE_CUDA))
+        WARPX_ALWAYS_ASSERT_WITH_MESSAGE(!do_shared_mem_current_deposition,
+                "requested shared memory for current deposition, but shared memory is only available for CUDA or HIP");
+#endif
         pp_warpx.query("shared_mem_current_tpb", shared_mem_current_tpb);
 
         // initialize the shared tilesize
