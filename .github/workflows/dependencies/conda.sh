671d1ee8a77f1f34591bb2b8d75204e9b1936dde
Axel Huebl <axel.huebl@plasma.ninja>
2023-04-07 13:17:38 -0700

    fix mpi4py

--- .github/workflows/dependencies/conda.sh
+++ .github/workflows/dependencies/conda.sh
@@ -11,4 +11,4 @@ conda update -n base conda
 conda install -n base conda-libmamba-solver
 conda config --set solver libmamba
 
-conda install -c conda-forge blaspp ccache cmake compilers git lapackpp "openpmd-api=*=mpi_mpich*" python "mpi4py=*=mpi_mpich*" numpy pandas scipy yt "fftw=*=mpi_mpich*" pkg-config setuptools matplotlib mpich pip virtualenv wheel
+conda install -c conda-forge blaspp ccache cmake compilers git lapackpp "openpmd-api=*=mpi_mpich*" python mpi4py numpy pandas scipy yt "fftw=*=mpi_mpich*" pkg-config setuptools matplotlib mpich pip virtualenv wheel
