a23d2aa2ce3a16470e5db083e420642615f30762
Axel Huebl <axel.huebl@plasma.ninja>
2023-04-10 15:53:06 -0700

    AMReX: Include Fix Roundoff Domain 3247

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -240,7 +240,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "8cae8024915ff8d31fe655665d17783fd34cda8f"
+set(WarpX_amrex_branch "bb47ec9a6e4fbaea36839d94cd2bf7bdba038168"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
--- run_test.sh
+++ run_test.sh
@@ -71,7 +71,7 @@ python3 -m pip install --upgrade -r warpx/Regression/requirements.txt
 
 # Clone AMReX and warpx-data
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach 8cae8024915ff8d31fe655665d17783fd34cda8f && cd -
+cd amrex && git checkout --detach bb47ec9a6e4fbaea36839d94cd2bf7bdba038168 && cd -
 # warpx-data contains various required data sets
 git clone --depth 1 https://github.com/ECP-WarpX/warpx-data.git
 
