9d35cf3f58b0138525c5b4b13e0b5a1059155ddf
Axel Huebl <axel.huebl@plasma.ninja>
2023-04-24 09:34:50 -0700

    AMReX: Weekly Update

--- run_test.sh
+++ run_test.sh
@@ -71,7 +71,7 @@ python3 -m pip install --upgrade -r warpx/Regression/requirements.txt
 
 # Clone AMReX and warpx-data
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach c1e8e73b63dc8b6732c6cdfe51914f7ae144575b && cd -
+cd amrex && git checkout --detach 9d2f762f4897b5f585544baf87a19edd0f0c9560 && cd -
 # warpx-data contains various required data sets
 git clone --depth 1 https://github.com/ECP-WarpX/warpx-data.git
 # openPMD-example-datasets contains various required data sets
