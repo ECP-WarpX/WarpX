847d0d951d2c347f627a35eadb4991355f3ceb5e
Kara-Mostefa Ilian <ilian.kara-mostefa@student-cs.fr>
2023-04-25 16:19:51 -0700

    Correct the implementation of the test with Gaussian profile, fix and clean up previous code

--- Examples/Tests/laser_injection_from_file/analysis.py
+++ Examples/Tests/laser_injection_from_file/analysis.py
@@ -29,7 +29,7 @@ from scipy.signal import hilbert
 import yt ; yt.funcs.mylog.setLevel(50)
 
 #Maximum acceptable error for this test
-relative_error_threshold = 0.065
+relative_error_threshold = 0.1
 
 #Physical parameters
 um = 1.e-6
@@ -40,52 +40,13 @@ c = 299792458
 wavelength = 1.*um
 w0 = 6.*um
 tt = 10.*fs
-x_c = 0.*um
 t_c = 20.*fs
-foc_dist = 10*um
-E_max = 1e12
-rot_angle = 0
 
-#Parameters of the tx grid
-x_l = -12.0*um
-x_r = 12.0*um
-x_points = 480
-t_l = 0.0*fs
-t_r = 40.0*fs
-t_points = 400
-tcoords = np.linspace(t_l, t_r, t_points)
-xcoords = np.linspace(x_l, x_r, x_points)
-
-def gauss(T,X,Y,opt):
-    """Compute the electric field for a Gaussian laser pulse.
-       This is used to write the binary input file.
-    """
-
-    k0 = 2.0*np.pi/wavelength
-    inv_tau2 = 1./tt/tt
-    osc_phase = k0*c*(T-t_c)
-
-    diff_factor = 1.0 + 1.0j* foc_dist * 2/(k0*w0*w0)
-    inv_w_2 = 1.0/(w0*w0*diff_factor)
-
-    pre_fact = np.exp(1.0j * osc_phase)
-
-    if opt == '3d':
-        pre_fact = pre_fact/diff_factor
-    else:
-        pre_fact = pre_fact/np.sqrt(diff_factor)
-
-    exp_arg = - (X*X + Y*Y)*inv_w_2 - inv_tau2 * (T-t_c)*(T-t_c)
-
-    return np.real(pre_fact * np.exp(exp_arg))
+E_max= 32599175904452.426
 
 # Function for the envelope
-def gauss_env(T,XX, YY, ZZ):
-    '''Function to compute the theory for the envelope
-    '''
-
-    X = np.cos(rot_angle)*XX + np.sin(rot_angle)*ZZ
-    Z = -np.sin(rot_angle)*XX + np.cos(rot_angle)*ZZ
+def gauss_env(T, X, Y, Z):
+    # Function to compute the theory for the envelope
 
     inv_tau2 = 1./tt/tt
     inv_w_2 = 1.0/(w0*w0)
@@ -94,7 +55,6 @@ def gauss_env(T,XX, YY, ZZ):
 
 def do_analysis(fname, compname, steps):
     ds = yt.load(fname)
-
     dt = ds.current_time.to_value()/steps
 
     # Define 3D meshes
@@ -110,7 +70,7 @@ def do_analysis(fname, compname, steps):
         ds.domain_left_edge[ds.dimensionality-1],
         ds.domain_right_edge[ds.dimensionality-1],
         ds.domain_dimensions[ds.dimensionality-1]).v
-    X, Y, Z = np.meshgrid(x, y, z, sparse=False, indexing='ijk')
+    X, Y, Z = np.meshgrid(x, y, z, sparse=False, indexing='ij')
 
     # Compute the theory for envelope
     env_theory = gauss_env(+t_c-ds.current_time.to_value(), X,Y,Z)+gauss_env(-t_c+ds.current_time.to_value(), X,Y,Z)
@@ -121,24 +81,28 @@ def do_analysis(fname, compname, steps):
     env = abs(hilbert(F_laser))
     extent = [ds.domain_left_edge[ds.dimensionality-1], ds.domain_right_edge[ds.dimensionality-1],
           ds.domain_left_edge[0], ds.domain_right_edge[0] ]
-
+    
+    F_slice= F_laser[:,F_laser.shape[1]//2, :]
+    env_slice= env[:,env.shape[1]//2, :]
+    env_theory_slice= env_theory[:,env_theory.shape[1]//2, :]
+    
     # Plot results
     plt.figure(figsize=(8,6))
     plt.subplot(221)
     plt.title('PIC field')
-    plt.imshow(F_laser, extent=extent)
+    plt.imshow(F_slice, extent=extent)
     plt.colorbar()
     plt.subplot(222)
     plt.title('PIC envelope')
-    plt.imshow(env, extent=extent)
+    plt.imshow(env_slice, extent=extent)
     plt.colorbar()
     plt.subplot(223)
     plt.title('Theory envelope')
-    plt.imshow(env_theory, extent=extent)
+    plt.imshow(env_theory_slice, extent=extent)
     plt.colorbar()
     plt.subplot(224)
     plt.title('Difference')
-    plt.imshow(env-env_theory, extent=extent)
+    plt.imshow(env_slice-env_theory_slice, extent=extent)
     plt.colorbar()
     plt.tight_layout()
     plt.savefig(compname, bbox_inches='tight')
@@ -147,16 +111,16 @@ def do_analysis(fname, compname, steps):
     print("Relative error envelope: ", relative_error_env)
     assert(relative_error_env < relative_error_threshold)
 
-    fft_F_laser = np.fft.fft2(F_laser)
+    fft_F_laser = np.fft.fftn(F_laser)
 
-    freq_rows = np.fft.fftfreq(F_laser.shape[0],dt)
-    freq_cols = np.fft.fftfreq(F_laser.shape[1],dt)
-
+    freq_x = np.fft.fftfreq(F_laser.shape[0],dt)
+    freq_y = np.fft.fftfreq(F_laser.shape[1],dt)
+    freq_z = np.fft.fftfreq(F_laser.shape[2],dt)
+    
     pos_max = np.unravel_index(np.abs(fft_F_laser).argmax(), fft_F_laser.shape)
 
-    freq = np.sqrt((freq_rows[pos_max[0]])**2 +  (freq_cols[pos_max[1]]**2))
+    freq = np.sqrt((freq_x[pos_max[0]])**2 +  (freq_y[pos_max[1]]**2) + (freq_z[pos_max[2]])**2)
     exp_freq = c/wavelength
-
     relative_error_freq = np.abs(freq-exp_freq)/exp_freq
     print("Relative error frequency: ", relative_error_freq)
     assert(relative_error_freq < relative_error_threshold)
@@ -165,7 +129,7 @@ def do_analysis(fname, compname, steps):
 
 def launch_analysis(executable):
     os.system("./" + executable + " inputs.3d_test_txye diag1.file_prefix=diags/plotfiles/plt")
-    do_analysis("diags/plotfiles/plt000250/", "comp_unf.pdf", 250)
+    do_analysis("diags/plotfiles/plt000251/", "comp_unf.pdf", 251)
 
 
 def main() :
@@ -177,10 +141,11 @@ def main() :
     laser_energy = 1.0  # J
     profile = GaussianProfile(wavelength, pol, laser_energy, w0, tt, t_peak=0)
     dim = "xyt"
-    lo = (-10e-6, -10e-6, -60e-15)
-    hi = (+10e-6, +10e-6, +60e-15)
+    lo = (-10e-6, -10e-6, -20e-15)
+    hi = (+10e-6, +10e-6, +20e-15)
     npoints = (100, 100, 100)
     laser = Laser(dim, lo, hi, npoints, profile)
+    laser.normalize(laser_energy, kind="energy")
     laser.write_to_file("gaussianlaser3d")
 
     executables = glob.glob("*.ex")
@@ -189,6 +154,6 @@ def main() :
     else :
         assert(False)
     print('Passed')
-
+    
 if __name__ == "__main__":
     main()
