28de772724ca01efa3ed5eaf56fd617548dd01ec
Kara-Mostefa Ilian <ilian.kara-mostefa@student-cs.fr>
2023-04-27 22:02:41 -0700

    Update LaserProfileFromTXYEFile.cpp for enabling condition if WarpX is compiled with OpenPMD support

--- Source/Laser/LaserProfilesImpl/LaserProfileFromTXYEFile.cpp
+++ Source/Laser/LaserProfilesImpl/LaserProfileFromTXYEFile.cpp
@@ -40,6 +40,7 @@
 #include <tuple>
 #include <utility>
 #include <vector>
+
 // example: data handling & print
 #include <vector>   // std::vector
 #include <iostream> // std::cout
@@ -50,9 +51,6 @@
     namespace io = openPMD;
 #endif
 
-
-
-
 using namespace amrex;
 
 void
@@ -60,11 +58,7 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::init (
     const amrex::ParmParse& ppl,
     CommonLaserParameters params)
 {
-
-#ifndef WARPX_USE_OPENPMD
-    amrex::Abort(Utils::TextMsg::Err("WarpX should be compiled with option OpenPMD : ON"));
-#endif
-
+#ifdef WARPX_USE_OPENPMD
     if (!std::numeric_limits< double >::is_iec559)
     {
         ablastr::warn_manager::WMRecordWarning("Laser",
@@ -72,7 +66,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::init (
             "things will happen parsing the X, Y and T profiles for the laser!)",
             ablastr::warn_manager::WarnPriority::high);
     }
-
     // Parse the TXYE file
     ppl.get("txye_file_name", m_params.txye_file_name);
     if(m_params.txye_file_name.empty())
@@ -80,7 +73,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::init (
         Abort("txye_file_name must be provided for txye_file laser profile!");
     }
     parse_txye_file(m_params.txye_file_name);
-
     //Set time_chunk_size
     m_params.time_chunk_size = m_params.nt;
     int temp = 1;
@@ -91,7 +83,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::init (
     if(m_params.time_chunk_size < 2){
         Abort("Error! time_chunk_size must be >= 2!");
     }
-
     //Reads the (optional) delay
     utils::parser::queryWithParser(ppl, "delay", m_params.t_delay);
 
@@ -102,23 +93,23 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::init (
 
     //Read first time chunck
     read_data_t_chuck(0, m_params.time_chunk_size);
-
     //Copy common params
     m_common_params = params;
+#else
+    amrex::Abort(Utils::TextMsg::Err("WarpX should be compiled with option OpenPMD : ON"));
+    amrex::ignore_unused(ppl, params);
+#endif
 }
 
 void
 WarpXLaserProfiles::FromTXYEFileLaserProfile::update (amrex::Real t)
 {
     t += m_params.t_coords[0] - m_params.t_delay;
-
     if(t >= m_params.t_coords.back())
         return;
-
     const auto idx_times = find_left_right_time_indices(t);
     const auto idx_t_left = idx_times.first;
     const auto idx_t_right = idx_times.second;
-
     //Load data chunck if needed
     if(idx_t_right >  m_params.last_time_index){
         read_data_t_chuck(idx_t_left, idx_t_left+m_params.time_chunk_size);
@@ -132,7 +123,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::fill_amplitude (
     Real t, Real * AMREX_RESTRICT const amplitude) const
 {
     t += m_params.t_coords[0] - m_params.t_delay;
-
     //Amplitude is 0 if time is out of range
     if(t < m_params.t_coords.front() ||  t > m_params.t_coords.back()){
         amrex::ParallelFor(np,
@@ -140,21 +130,19 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::fill_amplitude (
                 amplitude[i] = 0.0_rt;});
         return;
     }
-
     //Find left and right time indices
     const auto [idx_t_left, idx_t_right] = find_left_right_time_indices(t);
 
     if(idx_t_left <  m_params.first_time_index){
         Abort("Something bad has happened with the simulation time");
     }
-
     internal_fill_amplitude_uniform(idx_t_left, np, Xp, Yp, t, amplitude);
 }
 
 void
 WarpXLaserProfiles::FromTXYEFileLaserProfile::parse_txye_file(std::string txye_file_name)
 {
-
+#ifdef WARPX_USE_OPENPMD
     if(ParallelDescriptor::IOProcessor()){
 
         auto series = io::Series(txye_file_name, io::Access::READ_ONLY);
@@ -167,7 +155,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::parse_txye_file(std::string txye_f
         m_params.nt = extent[0];
         m_params.nx = extent[1];
         m_params.ny = extent[2];
-
         if(m_params.nt <= 1) Abort("nt in txye file must be >=2");
         if(m_params.nx <= 1) Abort("nx in txye file must be >=2");
 #if (defined(WARPX_DIM_3D) || (defined WARPX_DIM_RZ))
@@ -175,7 +162,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::parse_txye_file(std::string txye_f
 #elif defined(WARPX_DIM_XZ)
         if(m_params.ny != 1) Abort("ny in txye file must be 1 in 2D");
 #endif
-
         // Allocate arrays that contain min/max
         // of the grid in each direction
         m_params.t_coords.resize(2);
@@ -185,7 +171,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::parse_txye_file(std::string txye_f
 #elif defined(WARPX_DIM_XZ)
         m_params.h_y_coords.resize(1);
 #endif
-
         // Extract grid offset and grid spacing
         std::vector<double> offset = E.gridGlobalOffset();
         std::vector<double> position = E_laser.position<double>();
@@ -199,7 +184,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::parse_txye_file(std::string txye_f
         m_params.h_y_coords[0] = offset[2] + position[2]*spacing[2];
         m_params.h_y_coords[1] = m_params.h_y_coords[0] + (m_params.ny-1)*spacing[2];
     }
-
     //Broadcast grid size and coordinate sizes
     //When a non-uniform grid is used, nt, nx and ny are identical
     //to t_coords.size(), x_coords.size() and y_coords.size().
@@ -214,7 +198,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::parse_txye_file(std::string txye_f
     ParallelDescriptor::Bcast(t_sizes, 6,
         ParallelDescriptor::IOProcessorNumber());
     m_params.nt = t_sizes[0]; m_params.nx = t_sizes[1]; m_params.ny = t_sizes[2];
-
     //Broadcast coordinates
     if(!ParallelDescriptor::IOProcessor()){
         m_params.t_coords.resize(t_sizes[3]);
@@ -227,7 +210,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::parse_txye_file(std::string txye_f
         m_params.h_x_coords.size(), ParallelDescriptor::IOProcessorNumber());
     ParallelDescriptor::Bcast(m_params.h_y_coords.dataPtr(),
         m_params.h_y_coords.size(), ParallelDescriptor::IOProcessorNumber());
-
     m_params.d_x_coords.resize(m_params.h_x_coords.size());
     m_params.d_y_coords.resize(m_params.h_y_coords.size());
     Gpu::copyAsync(Gpu::hostToDevice,
@@ -237,6 +219,9 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::parse_txye_file(std::string txye_f
                    m_params.h_y_coords.begin(), m_params.h_y_coords.end(),
                    m_params.d_y_coords.begin());
     Gpu::synchronize();
+#else
+    amrex::ignore_unused(txye_file_name);
+#endif
 }
 
 std::pair<int,int>
@@ -247,54 +232,47 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::find_left_right_time_indices(amrex
     const auto t_max = m_params.t_coords.back();
     const auto temp_idx_t_right = static_cast<int>(std::ceil( (m_params.nt-1)*(t-t_min)/(t_max-t_min)));
     idx_t_right = max(min(temp_idx_t_right, m_params.nt-1),1);
-
     return std::make_pair(idx_t_right-1, idx_t_right);
 }
 
 void
 WarpXLaserProfiles::FromTXYEFileLaserProfile::read_data_t_chuck(int t_begin, int t_end)
 {
+#ifdef WARPX_USE_OPENPMD
     amrex::Print() << Utils::TextMsg::Info(
         "Reading [" + std::to_string(t_begin) + ", " + std::to_string(t_end) +
         ") data chunk from " + m_params.txye_file_name);
-
     //Indices of the first and last timestep to read
     auto i_first = max(0, t_begin);
     auto i_last = min(t_end-1, m_params.nt-1);
     if((i_last-i_first+1)*m_params.nx*m_params.ny > static_cast<int>(m_params.E_data.size()))
         Abort("Data chunk to read from file is too large");
-
     Vector<Complex> h_E_data(m_params.E_data.size());
-
     if(ParallelDescriptor::IOProcessor()){
-
         auto series = io::Series(m_params.txye_file_name, io::Access::READ_ONLY);
         auto i = series.iterations[0];
         auto E = i.meshes["laserEnvelope"];
         auto E_laser = E[io::RecordComponent::SCALAR];
-
         // alternatively, pass pre-allocated
         std::shared_ptr< std::complex<double> > x_data = E_laser.loadChunk< std::complex<double> >();
-
         const int read_size = (i_last - i_first + 1)*m_params.nx*m_params.ny;
-
         series.flush();
-
         for (int j=0; j<read_size; j++) {
             h_E_data[j] = Complex{ x_data.get()[j].real(), x_data.get()[j].imag() };
         }
     }
-
     //Broadcast E_data
     ParallelDescriptor::Bcast(h_E_data.dataPtr(),
         h_E_data.size(), ParallelDescriptor::IOProcessorNumber());
 
     Gpu::copyAsync(Gpu::hostToDevice,h_E_data.begin(),h_E_data.end(),m_params.E_data.begin());
     Gpu::synchronize();
-
     //Update first and last indices
     m_params.first_time_index = i_first;
     m_params.last_time_index = i_last;
+#else
+    amrex::ignore_unused(t_begin, t_end);
+#endif
 }
 
 void
@@ -327,7 +305,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::internal_fill_amplitude_uniform(
     const auto t_right = idx_t_right*
         (m_params.t_coords.back()-m_params.t_coords.front())/(m_params.nt-1) +
         m_params.t_coords.front();
-
 #if (defined WARPX_DIM_1D_Z)
     amrex::Abort(Utils::TextMsg::Err(
         "WarpXLaserProfiles::FromTXYEFileLaserProfile Not implemented for 1D"));
@@ -357,7 +334,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::internal_fill_amplitude_uniform(
             idx_x_left*(tmp_x_max-tmp_x_min)/(tmp_nx-1) + tmp_x_min;
         const auto x_1 =
             idx_x_right*(tmp_x_max-tmp_x_min)/(tmp_nx-1) + tmp_x_min;
-
 #if (defined(WARPX_DIM_3D) || (defined WARPX_DIM_RZ))
         //Find indices and coordinates along y
         const int temp_idx_y_right = static_cast<int>(
@@ -389,7 +365,6 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::internal_fill_amplitude_uniform(
             p_E_data[idx(idx_t_right, idx_x_right, idx_y_left)],
             p_E_data[idx(idx_t_right, idx_x_right, idx_y_right)],
             t, Xp[i], Yp[i]);
-
 #elif defined(WARPX_DIM_XZ)
         //Interpolate amplitude
         const auto idx = [=](int i_interp, int j_interp){
@@ -406,15 +381,12 @@ WarpXLaserProfiles::FromTXYEFileLaserProfile::internal_fill_amplitude_uniform(
         amrex::ignore_unused(Yp);
 #else
         // TODO: implement WARPX_DIM_1D_Z
-        Complex val = 0;
         amrex::ignore_unused(x_0, x_1, p_E_data, tmp_idx_first_time,
                              t_left, t_right, Xp, Yp, t, idx_x_left);
 #endif
-
         // The interpolated amplitude was only the envelope.
         // Here we add the laser oscillations.
         amplitude[i] = (val*exp_omega_t).real();
-
         }
     );
 }
