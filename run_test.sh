734fd70a3ae3b303b3854b92584a0e243be57df9
Axel Huebl <axel.huebl@plasma.ninja>
2023-05-02 19:35:44 -0700

    Release 23.05 (#3887)
    * AMReX: 23.05
    
    * PICSAR: 23.05
    
    * WarpX: 23.05
    
    * Update ES EB RZ Tests
    
    - slightly relaxed Phi
    - better selection of r outside cutcells

--- run_test.sh
+++ run_test.sh
@@ -71,7 +71,7 @@ python3 -m pip install --upgrade -r warpx/Regression/requirements.txt
 
 # Clone AMReX and warpx-data
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach 9d2f762f4897b5f585544baf87a19edd0f0c9560 && cd -
+cd amrex && git checkout --detach 23.05 && cd -
 # warpx-data contains various required data sets
 git clone --depth 1 https://github.com/ECP-WarpX/warpx-data.git
 # openPMD-example-datasets contains various required data sets
