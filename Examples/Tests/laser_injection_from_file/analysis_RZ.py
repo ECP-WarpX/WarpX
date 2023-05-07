eeacaba8add2affcdf5661ff992ecaf703b1bac8
Kara-Mostefa Ilian <ilian.kara-mostefa@student-cs.fr>
2023-05-06 17:21:32 -0700

    Implement RZ reading & test & fix 2D Checksum test

--- Examples/Tests/laser_injection_from_file/analysis_RZ.py
+++ Examples/Tests/laser_injection_from_file/analysis_RZ.py
@@ -49,58 +49,77 @@ t_c = 20.*fs
 E_max= 16282454014843.37
 
 # Function for the envelope
-def gauss_env(T,Z):
+def gauss_env(T, X, Y, Z):
     # Function to compute the theory for the envelope
     inv_tau2 = 1./tt/tt
-    exp_arg = - inv_tau2 / c/c * (Z-T*c)*(Z-T*c)
+    inv_w_2 = 1.0/(w0*w0)
+    exp_arg = - (X*X)*inv_w_2 - (Y*Y)*inv_w_2- inv_tau2 / c/c * (Z-T*c)*(Z-T*c)
     return E_max * np.real(np.exp(exp_arg))
 
 def do_analysis(fname, compname, steps):
     ds = yt.load(fname)
     dt = ds.current_time.to_value()/steps
 
+    # Define 3D meshes
+    x = np.linspace(
+        ds.domain_left_edge[0],
+        ds.domain_right_edge[0],
+        ds.domain_dimensions[0]).v
+    y = np.linspace(
+        ds.domain_left_edge[1],
+        ds.domain_right_edge[1],
+        ds.domain_dimensions[1]).v
     z = np.linspace(
-        ds.domain_left_edge[0].v,
-        ds.domain_right_edge[0].v,
-        ds.domain_dimensions[0])
+        ds.domain_left_edge[ds.dimensionality-1],
+        ds.domain_right_edge[ds.dimensionality-1],
+        ds.domain_dimensions[ds.dimensionality-1]).v
+    X, Y, Z = np.meshgrid(x, y, z, sparse=False, indexing='ij')
 
     # Compute the theory for envelope
-    env_theory = gauss_env(+t_c-ds.current_time.to_value(), z)+gauss_env(-t_c+ds.current_time.to_value(),z)
+    env_theory = gauss_env(+t_c-ds.current_time.to_value(), X,Y,Z)+gauss_env(-t_c+ds.current_time.to_value(), X,Y,Z)
 
     # Read laser field in PIC simulation, and compute envelope
     all_data_level_0 = ds.covering_grid(level=0,left_edge=ds.domain_left_edge, dims=ds.domain_dimensions)
-    F_laser = all_data_level_0['boxlib', 'Ey'].v.squeeze()
+    F_laser = all_data_level_0['boxlib', 'Et'].v.squeeze()
     env = abs(hilbert(F_laser))
+    extent = [ds.domain_left_edge[ds.dimensionality-1], ds.domain_right_edge[ds.dimensionality-1],
+          ds.domain_left_edge[0], ds.domain_right_edge[0] ]
+
+    env_theory_slice= env_theory[:,env_theory.shape[1]//2, :]
 
     # Plot results
-    plt.figure(figsize=(8,8))
+    plt.figure(figsize=(8,6))
     plt.subplot(221)
     plt.title('PIC field')
-    plt.plot(z, F_laser)
+    plt.imshow(F_laser, extent=extent)
+    plt.colorbar()
     plt.subplot(222)
     plt.title('PIC envelope')
-    plt.plot(z, env)
+    plt.imshow(env, extent=extent)
+    plt.colorbar()
     plt.subplot(223)
     plt.title('Theory envelope')
-    plt.plot(z,env_theory)
+    plt.imshow(env_theory_slice, extent=extent)
+    plt.colorbar()
     plt.subplot(224)
     plt.title('Difference')
-    plt.plot(z,env-env_theory)
-
+    plt.imshow(env-env_theory_slice, extent=extent)
+    plt.colorbar()
     plt.tight_layout()
     plt.savefig(compname, bbox_inches='tight')
 
-    relative_error_env = np.sum(np.abs(env-env_theory)) / np.sum(np.abs(env))
+    relative_error_env = np.sum(np.abs(env-env_theory_slice)) / np.sum(np.abs(env))
     print("Relative error envelope: ", relative_error_env)
     assert(relative_error_env < relative_error_threshold)
 
     fft_F_laser = np.fft.fftn(F_laser)
 
-    freq_z = np.fft.fftfreq(F_laser.shape[0],dt)
+    freq_x = np.fft.fftfreq(F_laser.shape[0],dt)
+    freq_z = np.fft.fftfreq(F_laser.shape[1],dt)
 
     pos_max = np.unravel_index(np.abs(fft_F_laser).argmax(), fft_F_laser.shape)
 
-    freq = freq_z[pos_max[0]]
+    freq = np.sqrt((freq_x[pos_max[0]])**2 + (freq_z[pos_max[1]])**2)
     exp_freq = c/wavelength
     relative_error_freq = np.abs(freq-exp_freq)/exp_freq
     print("Relative error frequency: ", relative_error_freq)
@@ -109,8 +128,8 @@ def do_analysis(fname, compname, steps):
 
 
 def launch_analysis(executable):
-    os.system("./" + executable + " inputs.1d_test_txye diag1.file_prefix=diags/plotfiles/plt")
-    do_analysis("diags/plotfiles/plt000251/", "comp_unf.pdf", 251)
+    os.system("./" + executable + " inputs.RZ_test_txye diag1.file_prefix=diags/plotfiles/plt")
+    do_analysis("diags/plotfiles/plt000252/", "comp_unf.pdf", 252)
 
 
 def main() :
@@ -136,7 +155,7 @@ def main() :
         assert(False)
 
     # Do the checksum test
-    filename_end = "diags/plotfiles/plt000251/"
+    filename_end = "diags/plotfiles/plt000252/"
     test_name = os.path.split(os.getcwd())[1]
     checksumAPI.evaluate_checksum(test_name, filename_end)
     print('Passed')
