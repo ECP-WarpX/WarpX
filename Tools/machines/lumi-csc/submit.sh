9391c17e2cb81e884c80a21d48da6c287c39447f
Luca Fedeli <luca.fedeli@cea.fr>
2023-05-22 16:00:13 +0200

    Update LUMI documentation

--- Tools/machines/lumi-csc/submit.sh
+++ Tools/machines/lumi-csc/submit.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
+#!/bin/bash -l
 
 #SBATCH -A <project id>
-#SBATCH -J warpx
-#SBATCH -o %x-%j.out
-#SBATCH -t 00:10:00
-# Early access to the GPU partition
-#SBATCH -p eap
+#SBATCH --job-name=warpx
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --partition=standard-g
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-node=8
-#SBATCH --gpu-bind=closest
+#SBATCH --time=00:10:00
 
 export MPICH_GPU_SUPPORT_ENABLED=1
 
@@ -27,4 +25,19 @@ export FI_MR_CACHE_MONITOR=memhooks  # alternative cache monitor
 export ROCFFT_RTC_CACHE_PATH=/dev/null
 
 export OMP_NUM_THREADS=1
-srun ../warpx inputs > outputs
+
+cat << EOF > select_gpu
+#!/bin/bash
+
+export ROCR_VISIBLE_DEVICES=\$SLURM_LOCALID
+exec \$*
+EOF
+
+chmod +x ./select_gpu
+
+CPU_BIND="map_cpu:48,56,16,24,1,8,32,40"
+
+export MPICH_GPU_SUPPORT_ENABLED=1
+
+srun --cpu-bind=${CPU_BIND} ./select_gpu ./warpx inputs
+rm -rf ./select_gpu
