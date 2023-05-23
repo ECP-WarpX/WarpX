37470cef823e4f17deb655d020fd4545907d90af
Axel Huebl <axel.huebl@plasma.ninja>
2023-05-22 21:30:40 -0700

    BLAS++/LAPACK++ for RZ+PSATD

--- .github/workflows/dependencies/clang14.sh
+++ .github/workflows/dependencies/clang14.sh
@@ -18,11 +18,37 @@ sudo apt-get install -y \
     ccache              \
     clang-14            \
     clang-tidy-14       \
+    libblas-dev         \
     libc++-14-dev       \
     libboost-math-dev   \
     libfftw3-dev        \
     libfftw3-mpi-dev    \
     libhdf5-openmpi-dev \
+    liblapack-dev       \
     libopenmpi-dev      \
     libomp-dev          \
     ninja-build
+
+# cmake-easyinstall
+#
+sudo curl -L -o /usr/local/bin/cmake-easyinstall https://raw.githubusercontent.com/ax3l/cmake-easyinstall/main/cmake-easyinstall
+sudo chmod a+x /usr/local/bin/cmake-easyinstall
+export CEI_SUDO="sudo"
+export CEI_TMP="/tmp/cei"
+
+# BLAS++ & LAPACK++
+cmake-easyinstall \
+  --prefix=/usr/local                      \
+  git+https://github.com/icl-utk-edu/blaspp.git \
+  -Duse_openmp=OFF                         \
+  -Dbuild_tests=OFF                        \
+  -DCMAKE_CXX_COMPILER_LAUNCHER=$(which ccache) \
+  -DCMAKE_VERBOSE_MAKEFILE=ON
+
+cmake-easyinstall \
+  --prefix=/usr/local                        \
+  git+https://github.com/icl-utk-edu/lapackpp.git \
+  -Duse_cmake_find_lapack=ON                 \
+  -Dbuild_tests=OFF                          \
+  -DCMAKE_CXX_COMPILER_LAUNCHER=$(which ccache) \
+  -DCMAKE_VERBOSE_MAKEFILE=ON
