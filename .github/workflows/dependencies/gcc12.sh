b300e8ec65cfde3267a91b24f19df345d84f4b7a
lucafedeli88 <luca.fedeli@cea.fr>
2023-06-03 00:27:11 +0200

    fix bug

--- .github/workflows/dependencies/gcc12.sh
+++ .github/workflows/dependencies/gcc12.sh
@@ -41,7 +41,7 @@ export CEI_TMP="/tmp/cei"
 cmake-easyinstall \
   --prefix=/usr/local                      \
   git+https://github.com/icl-utk-edu/blaspp.git \
-  -Duse_openmp=ON                         \
+  -Duse_openmp=OFF                        \
   -Dbuild_tests=OFF                        \
   -DCMAKE_CXX_COMPILER_LAUNCHER=$(which ccache) \
   -DCMAKE_VERBOSE_MAKEFILE=ON
