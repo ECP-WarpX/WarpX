684c2130c19bbf7277c264762f04f3e6e7c98bf2
Luca Fedeli <luca.fedeli@cea.fr>
2023-06-17 22:16:47 +0200

    add performance-faster-string-find check in clang-tidy (#4003)

--- Source/Diagnostics/WarpXOpenPMD.cpp
+++ Source/Diagnostics/WarpXOpenPMD.cpp
@@ -203,7 +203,7 @@ namespace detail
         std::string component_name = openPMD::RecordComponent::SCALAR;
 
         // we use "_" as separator in names to group vector records
-        const std::size_t startComp = fullName.find_last_of("_");
+        const std::size_t startComp = fullName.find_last_of('_');
         if( startComp != std::string::npos ) {  // non-scalar
             record_name = fullName.substr(0, startComp);
             component_name = fullName.substr(startComp + 1u);
