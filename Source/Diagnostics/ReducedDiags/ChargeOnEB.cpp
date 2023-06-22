830f780439a385c0639aa7784829bea5872bec3f
David Grote <grote1@llnl.gov>
2023-06-22 05:18:31 -0700

    For ReducedDiags, write header on restart if file does not exist (#4022)

--- Source/Diagnostics/ReducedDiags/ChargeOnEB.cpp
+++ Source/Diagnostics/ReducedDiags/ChargeOnEB.cpp
@@ -59,7 +59,7 @@ ChargeOnEB::ChargeOnEB (std::string rd_name)
 
     if (ParallelDescriptor::IOProcessor())
     {
-        if ( m_IsNotRestart )
+        if ( m_write_header )
         {
             // open file
             std::ofstream ofs{m_path + m_rd_name + "." + m_extension, std::ofstream::out};
