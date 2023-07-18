de2436ef3d910bf3a6e28614989ac60227847fb0
Axel Huebl <axel.huebl@plasma.ninja>
2023-07-18 13:44:12 -0700

    AMReX: Weekly Update (#4092)

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -243,7 +243,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "23.07"
+set(WarpX_amrex_branch "10b6cb26d0ac359922276ce08eeb728a14624b70"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
