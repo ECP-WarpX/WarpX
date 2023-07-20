afd93620a3ec489bbbd642a06300568cb3948398
Arianna Formenti <ariannaformenti@lbl.gov>
2023-07-20 15:28:19 -0700

    fixed position of reduce data eval

--- Source/Diagnostics/ReducedDiags/ParticleExtrema.cpp
+++ Source/Diagnostics/ReducedDiags/ParticleExtrema.cpp
@@ -468,10 +468,10 @@ void ParticleExtrema::ComputeDiags (int step)
                         }
                         return {chi,chi};
                     });
-                    auto val = reduce_data.value();
-                    chimin[lev] = get<0>(val);
-                    chimax[lev] = get<1>(val);
                 }
+                auto val = reduce_data.value();
+                chimin[lev] = get<0>(val);
+                chimax[lev] = get<1>(val);
                 chimin_f = *std::min_element(chimin.begin(), chimin.end());
                 chimax_f = *std::max_element(chimax.begin(), chimax.end());
             }
