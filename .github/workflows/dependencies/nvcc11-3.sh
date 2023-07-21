a9710c2ef9cdd3d8073bb0492581191d3a9325ee
Axel Huebl <axel.huebl@plasma.ninja>
2023-07-21 15:56:52 -0700

    CI: CUDA 11.3 is <= Ubuntu 20.04

--- .github/workflows/dependencies/nvcc11-3.sh
+++ .github/workflows/dependencies/nvcc11-3.sh
@@ -26,7 +26,7 @@ sudo apt-get install -y \
     pkg-config          \
     wget
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 
 sudo apt-get update
