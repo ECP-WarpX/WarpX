e3164f9e053d345b153d770ae107a7f68c4bb260
lucafedeli88 <luca.fedeli@cea.fr>
2023-07-26 00:56:44 +0200

    replace with equality

--- Source/Diagnostics/ComputeDiagFunctors/ParticleReductionFunctor.cpp
+++ Source/Diagnostics/ComputeDiagFunctors/ParticleReductionFunctor.cpp
@@ -93,7 +93,7 @@ ParticleReductionFunctor::operator() (amrex::MultiFab& mf_dst, const int dcomp, 
                 const amrex::ParticleReal ux = p.rdata(PIdx::ux) / PhysConst::c;
                 const amrex::ParticleReal uy = p.rdata(PIdx::uy) / PhysConst::c;
                 const amrex::ParticleReal uz = p.rdata(PIdx::uz) / PhysConst::c;
-                const bool filtered_out_flag = ((do_filter) && (filter_fn(xw, yw, zw, ux, uy, uz) <= 0.0));
+                const bool filtered_out_flag = ((do_filter) && (filter_fn(xw, yw, zw, ux, uy, uz) == 0.0));
                 const amrex::Real value = (filtered_out_flag) ? (0._rt):(map_fn(xw, yw, zw, ux, uy, uz));
                 amrex::Gpu::Atomic::AddNoRet(&out_array(ii, jj, kk, 0), (amrex::Real)(p.rdata(PIdx::w) * value));
             });
