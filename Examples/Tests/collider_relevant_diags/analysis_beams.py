8729da819c5d85ea9d86acba7e43dd81ceb6004f
Arianna Formenti <ariannaformenti@lbl.gov>
2023-08-03 15:23:12 -0700

    updated 3d beam test

--- Examples/Tests/collider_relevant_diags/analysis_beams.py
+++ Examples/Tests/collider_relevant_diags/analysis_beams.py
@@ -10,6 +10,7 @@ import openpmd_api as io
 from scipy.constants import alpha, c
 from scipy.constants import e as q_e
 from scipy.constants import hbar, m_e, physical_constants, pi
+import pandas as pd 
 
 r_e = physical_constants["classical electron radius"][0]
 
@@ -17,7 +18,8 @@ r_e = physical_constants["classical electron radius"][0]
 E_crit = m_e**2*c**3/(q_e*hbar)
 B_crit = m_e**2*c**2/(q_e*hbar)
 
-
+def rerr(a,b):
+    return np.abs(a-b)/np.abs(np.minimum(a,b))
 
 def parse_input_file(input_file):
     """
@@ -84,8 +86,6 @@ def dL_dt_full():
 
 def num_dens(x,y,z):
     return n0 * exp(-x**2/(2*sigmax**2))*exp(-y**2/(2*sigmay**2))*exp(-z**2/(2*sigmaz**2))
-
-
 
 
 '''
@@ -185,16 +185,18 @@ print('ColliderRelevant diag ele = ',  np.loadtxt(CollDiagFname)[:,12]/np.loadtx
 '''
 
 CollDiagFname='diags/reducedfiles/ColliderRelevant_beam_e_beam_p.txt'
+df = pd.read_csv(CollDiagFname, sep=" ", header=0)
+
+
 
 ##################
 ### LUMINOSITY ###
 ##################
-dL_dt_cr = np.loadtxt(CollDiagFname)[:,2]
-times = np.loadtxt(CollDiagFname)[:,1]
+dL_dt_cr = df[[col for col in df.columns if 'dL_dt' in col]].to_numpy()
+times = df[[col for col in df.columns if 'time' in col]].to_numpy() 
 L_cr = np.trapz(dL_dt_cr, times)
 coll_timestep = np.argmax(dL_dt_cr)
-
-
+coll_time = times[coll_timestep]
 
 x = np.linspace(-3*sigmax,3*sigmax,128)
 y = np.linspace(-3*sigmay,3*sigmay,128)
@@ -202,14 +204,18 @@ z = np.linspace(-3*sigmaz,3*sigmaz,128)
 dx, dy, dz = x[1]-x[0], y[1]-y[0], z[1]-z[0]
 X,Y,Z = np.meshgrid(x,y,z)
 D = num_dens(X,Y,Z)
-dL_dt = 2.*np.sum(D**2)*dx*dy*dz*c
+dL_dt_theory = 2.*np.sum(D**2)*dx*dy*dz*c
 
 # see formula (2.21) in Yokoya and Chen
 L_theory = N**2 / (4*pi*sigmax*sigmay)
 
-#assert np.isclose(L_theory, L_cr, rtol=0.1, atol=1e-12)
-#assert np.isclose(dL_dt, np.max(dL_dt_cr), rtol=1e-1, atol=1e-12)
-#assert np.allclose(dL_dt_full(), dL_dt_cr, rtol=1e-12, atol=1e-12)
+# from guinea-pig: lumi_fine
+L_gp = 7.95485e+30  
+
+assert np.isclose(L_cr, L_theory, rtol=0.02, atol=0.)
+assert np.isclose(L_cr, L_gp, rtol=0.003, atol=0,)
+assert np.isclose(np.max(dL_dt_cr), dL_dt_theory, rtol=0.003, atol=0,)
+assert np.allclose(dL_dt_full(), dL_dt_cr, rtol=1e-12, atol=0.)
 
 ###############
 ### CHI MIN ###
@@ -220,11 +226,12 @@ chimin_ele_pe = np.loadtxt(fname)[:,18]
 fname='diags/reducedfiles/ParticleExtrema_beam_p.txt'
 chimin_pos_pe = np.loadtxt(fname)[:,18]
 
-chimin_ele_cr = np.loadtxt(CollDiagFname)[:,8]
+chimin_ele_cr = np.loadtxt(CollDiagFname)[:,14]
 chimin_pos_cr = np.loadtxt(CollDiagFname)[:,3]
 
-assert np.allclose(chimin_pos_pe, chimin_pos_cr, rtol=1e-12, atol=1e-12)
-assert np.allclose(chimin_ele_pe, chimin_ele_cr, rtol=1e-12, atol=1e-12)
+assert np.allclose(chimin_pos_pe, chimin_pos_cr, rtol=1e-16, atol=0.)
+assert np.allclose(chimin_ele_pe, chimin_ele_cr, rtol=1e-16, atol=0.)
+assert np.allclose(chimin_ele_cr, chimin_ele_cr, rtol=1e-12, atol=0.)
 
 ###############
 ### CHI MAX ###
@@ -232,22 +239,22 @@ assert np.allclose(chimin_ele_pe, chimin_ele_cr, rtol=1e-12, atol=1e-12)
 # see formula (3.52) in Yokoya and Chen
 chimax_theory = 2*N*r_e**2*gamma/(alpha*sigmaz*(sigmax+sigmay))
 
+# from guinea-pig: upsmax (maximal value of the beamstrahlung paramater that occured)
+chimax_gp = 1.51733
+
 fname='diags/reducedfiles/ParticleExtrema_beam_e.txt'
 chimax_ele_pe = np.loadtxt(fname)[:,19]
 
 fname='diags/reducedfiles/ParticleExtrema_beam_p.txt'
 chimax_pos_pe = np.loadtxt(fname)[:,19]
 
-chimax_ele_cr = np.loadtxt(CollDiagFname)[:,10]
-chimax_pos_cr = np.loadtxt(CollDiagFname)[:,5]
+chimax_ele_cr = df[[col for col in df.columns if 'chimax_beam_e' in col]].to_numpy()
+chimax_pos_cr = df[[col for col in df.columns if 'chimax_beam_p' in col]].to_numpy()
 
-assert np.allclose(chimin_ele_pe, chimin_ele_cr, rtol=1e-12, atol=1e-12)
-assert np.allclose(chimin_pos_pe, chimin_pos_cr, rtol=1e-12, atol=1e-12)
-
-plt.plot(chimax_theory*np.ones_like(chimax_ele_cr))
-plt.plot(chimax_ele_cr)
-plt.plot(chimax_pos_cr)
-plt.show()
+assert np.allclose(chimax_pos_cr, chimax_pos_pe, rtol=1e-16, atol=0.)
+assert np.allclose(chimax_ele_cr, chimax_ele_pe, rtol=1e-16, atol=0.)
+assert np.isclose(chimax_ele_pe[coll_timestep], chimax_theory, rtol=0.2, atol=0.)
+assert np.isclose(chimax_pos_pe[coll_timestep], chimax_theory, rtol=0.2, atol=0.)
 
 ###############
 ### CHI AVE ###
@@ -255,76 +262,67 @@ plt.show()
 # see formula (3.52) in Yokoya and Chen
 chiave_theory = 5./12.*chimax_theory
 
-chiave_ele_cr = np.loadtxt(CollDiagFname)[:,9]
-chiave_pos_cr = np.loadtxt(CollDiagFname)[:,4]
+chiave_ele_cr = df[[col for col in df.columns if 'chiave_beam_e' in col]].to_numpy()
+chiave_pos_cr = df[[col for col in df.columns if 'chiave_beam_p' in col]].to_numpy()
 
-#plt.plot(chiave_theory*np.ones_like(chiave_pos_cr))
-plt.plot(chiave_pos_cr)
-plt.plot(chiave_ele_cr)
-plt.show()
-print(chiave_pos_cr)
-print(chiave_theory)
-
-##################
-### LUMINOSITY ###
-##################
-dL_dt_cr = np.loadtxt(CollDiagFname)[:,2]
-times = np.loadtxt(CollDiagFname)[:,1]
-L_cr = np.trapz(dL_dt_cr, times)
-coll_timestep = np.argmax(dL_dt_cr)
-
-
-
-x = np.linspace(-3*sigmax,3*sigmax,128)
-y = np.linspace(-3*sigmay,3*sigmay,128)
-z = np.linspace(-3*sigmaz,3*sigmaz,128)
-dx, dy, dz = x[1]-x[0], y[1]-y[0], z[1]-z[0]
-X,Y,Z = np.meshgrid(x,y,z)
-D = num_dens(X,Y,Z)
-dL_dt = 2.*np.sum(D**2)*dx*dy*dz*c
-
-# see formula (2.21) in Yokoya and Chen
-L_theory = N**2 / (4*pi*sigmax*sigmay)
-
-assert np.isclose(L_theory, L_cr, rtol=0.1, atol=1e-12)
-assert np.isclose(dL_dt, np.max(dL_dt_cr), rtol=1e-1, atol=1e-12)
-assert np.allclose(dL_dt_full(), dL_dt_cr, rtol=1e-12, atol=1e-12)
-
+assert np.isclose(chiave_ele_cr[coll_timestep], chiave_theory, rtol=0.2, atol=0.)
+assert np.isclose(chiave_pos_cr[coll_timestep], chiave_theory, rtol=0.2, atol=0.)
 
 ##################
 ### DISRUPTION ###
 ##################
-x_std_ele = np.loadtxt(CollDiagFname)[:,11]
-y_std_ele = np.loadtxt(CollDiagFname)[:,12]
-x_std_pos = np.loadtxt(CollDiagFname)[:,6]
-y_std_pos = np.loadtxt(CollDiagFname)[:,7]
+xave_ele = df[[col for col in df.columns if ']x_ave_beam_e' in col]].to_numpy()
+yave_ele = df[[col for col in df.columns if ']y_ave_beam_e' in col]].to_numpy()
+xstd_ele = df[[col for col in df.columns if ']x_std_beam_e' in col]].to_numpy()
+ystd_ele = df[[col for col in df.columns if ']y_std_beam_e' in col]].to_numpy()
+
+thetaxave_ele = df[[col for col in df.columns if 'thetax_ave_beam_e' in col]].to_numpy()
+thetayave_ele = df[[col for col in df.columns if 'thetay_ave_beam_e' in col]].to_numpy()
+thetaxstd_ele = df[[col for col in df.columns if 'thetax_std_beam_e' in col]].to_numpy()
+thetaystd_ele = df[[col for col in df.columns if 'thetay_std_beam_e' in col]].to_numpy()
+
+thetaxmax_ele = df[[col for col in df.columns if 'thetax_max_beam_e' in col]].to_numpy()
+thetaymax_ele = df[[col for col in df.columns if 'thetay_max_beam_e' in col]].to_numpy()
+
+ref_time = times[coll_timestep] + sqrt(3)*sigmaz/c
+ref_timestep = np.argmin(np.abs(times - ref_time)**2)
+
+
+theta0 = 2*N*r_e/(gamma*(sigmax+sigmay))
+print('theta! = ', theta0)
 
 #see formula (2.13) from Yokoya and Chen
 Dx = 2*N*r_e*sigmaz/(gamma*sigmax*(sigmax+sigmay))
 Dy = 2*N*r_e*sigmaz/(gamma*sigmay*(sigmax+sigmay))
 
-boh = (x_std_ele[coll_timestep]-x_std_ele[0])/x_std_ele[0]
-boh1 = (y_std_ele[coll_timestep]-y_std_ele[0])/y_std_ele[0]
-
-print(Dx, Dy, boh)
+print('Dx! = ', Dx)
 
 
+thetax_ave_ele_gp = 0.201504 # microradians
+thetay_ave_ele_gp = 0.0879509 # microradians
+thetax_std_ele_gp = 773.377 # microradians
+thetay_std_ele_gp = 773.696 # microradians
 
 
-plt.plot(Dx*np.ones_like(x_std_pos))
-print(Dx)
-plt.plot(np.abs(x_std_ele-x_std_ele[0])/x_std_ele[0])
-plt.plot(np.abs(x_std_pos-x_std_pos[0])/x_std_pos[0])
+##################
+#### PHOTONS #####
+##################
+
+chi = np.max(chiave_ele_cr)
+n_gamma = 5./2. * alpha**2 * sigmaz / r_e / gamma * chi / sqrt(1.+chi**(2./3.))
+print(chi, n_gamma)
 
 
-plt.plot(Dy*np.ones_like(x_std_pos))
-print(Dy)
-plt.plot(np.abs(y_std_ele-y_std_ele[0])/y_std_ele[0])
-plt.plot(np.abs(y_std_pos-y_std_pos[0])/y_std_pos[0])
+fname='diags/reducedfiles/ParticleNumber.txt'
+N_pho = np.loadtxt(fname)[:,9]
+N_ele = np.loadtxt(fname)[:,8]
+fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(5,5), dpi=300)
+ax.plot(times, N_pho/N_ele[0])
+print(N_pho[-1]/N_ele[0])
+#boh = np.abs(x_std_ele-x_std_ele[0])/x_std_ele[0]
+#boh1 = np.abs(y_std_ele-y_std_ele[0])/y_std_ele[0]
 
-plt.axvline(x=coll_timestep)
-#plt.show()
-
+#print(Dx, boh[coll_timestep])
 # see formula (2.35) in Yokoya and Chen
 #theta = 2*N*r_e/(gamma*(sigmax+sigmay))
 
@@ -335,20 +333,177 @@ plt.axvline(x=coll_timestep)
 #plt.close()
 
 
-fig, ax = plt.subplots(ncols=4, nrows=1, figsize=(12,5), dpi=300)
+fig, ax = plt.subplots(ncols=4, nrows=1, figsize=(15,5), dpi=300)
 
-ax[0].plot(times, chiave_pos_cr)
-ax[0].plot(times, chiave_ele_cr)
-ax[0].plot(times, chiave_theory*np.ones_like(chiave_pos_cr))
+ax[0].plot(times, chiave_pos_cr, label='pos cr')
+ax[0].plot(times, chiave_ele_cr, label='ele cr')
+ax[0].plot(times, chiave_theory*np.ones_like(chiave_pos_cr), label='theory')
+ax[0].set_title('chi ave')
+
+ax[3].plot(times, chimin_pos_pe, label='pos pe')
+ax[3].plot(times, chimin_pos_cr, label='pos cr')
+ax[3].plot(times, chimin_ele_pe, label='ele pe')
+ax[3].plot(times, chimin_ele_cr, label='ele cr')
+ax[3].set_title('chi min')
+
+ax[1].plot(times, chimax_pos_pe, label='pos pe')
+ax[1].plot(times, chimax_ele_pe, label='ele pe')
+ax[1].plot(times, chimax_pos_cr, label='pos cr')
+ax[1].plot(times, chimax_ele_cr, label='ele cr')
+ax[1].plot(times, chimax_theory*np.ones_like(chiave_pos_cr), label='theory')
+ax[1].set_title('chi max')
+
+ax[2].plot(times, dL_dt_cr, label='cr')
+ax[2].plot(times, dL_dt_theory*np.ones_like(dL_dt_cr), label='theory')
+ax[2].plot(times, dL_dt_full(), label='full')
+ax[2].set_title('dL/dt')
+
+for a in ax.reshape(-1):
+    a.legend() 
+    a.axvline(x=times[coll_timestep], color='black')
+    a.axvline(x=times[ref_timestep], color='magenta')
+
+fig.savefig('x.png', dpi=300.)
+plt.close()
 
 
-ax[1].plot(times, chimax_pos_cr)
-ax[1].plot(times, chimax_ele_cr)
-ax[1].plot(times, chimax_theory*np.ones_like(chiave_pos_cr))
+
+fig, ax = plt.subplots(ncols=3, nrows=1, figsize=(15,5), dpi=300)
+
+ax[0].plot(times, xstd_ele, label='x')
+ax[0].plot(times, ystd_ele, label='y')
+ax[0].set_title('pos std')
+
+ax[1].plot(times, thetaxstd_ele, label='x')
+ax[1].plot(times, thetaystd_ele, label='y')
+ax[1].set_title('theta std')
+
+ax[2].plot(times, thetaxmax_ele, label='x')
+ax[2].plot(times, thetaymax_ele, label='y')
+ax[2].set_title('theta max')
 
 
-ax[2].plot(times, dL_dt_cr)
-ax[2].plot(times, dL_dt*np.ones_like(dL_dt_cr))
-ax[2].plot(times, dL_dt_full())
+for a in ax.reshape(-1):
+    a.legend() 
+    a.axvline(x=times[coll_timestep], color='black')
+    a.axvline(x=times[ref_timestep], color='magenta')
 
+fig.savefig('y.png', dpi=300.)   
+plt.close()
+
+
+
+
+fig, ax = plt.subplots(ncols=3, nrows=1, figsize=(15,5), dpi=300)
+b = 2.*xstd_ele[:,0]
+t = times[:,0]
+
+db_dt = np.diff(b)/np.diff(t)
+
+ax[0].plot(t[:-1], db_dt/c)
+ax[0].plot(times, -Dx*np.ones_like(times))
+
+ax[1].plot(times, thetaxstd_ele, label='x')
+ax[1].plot(times, thetaystd_ele, label='y')
+ax[1].plot(times, theta0*np.ones_like(times))
+ax[1].plot(times, thetax_std_ele_gp*np.ones_like(times)/1e6)
+ax[1].plot(times, thetay_std_ele_gp*np.ones_like(times)/1e6)
+ax[1].set_title('theta std')
+
+ax[2].plot(times, thetaxave_ele, label='x')
+ax[2].plot(times, thetayave_ele, label='y')
+#ax[2].plot(times, theta0*np.ones_like(times))
+#ax[2].plot(times, thetax_ave_ele_gp*np.ones_like(times)/1e6)
+#ax[2].plot(times, thetay_ave_ele_gp*np.ones_like(times)/1e6)
+ax[2].set_title('theta ave')
+
+
+for a in ax.reshape(-1):
+    a.legend() 
+    a.axvline(x=times[coll_timestep], color='black')
+    a.axvline(x=times[ref_timestep], color='magenta')
+
+fig.savefig('z.png', dpi=300.)   
 plt.show()
+
+
+
+
+#ax[0].plot(times, -Dx*np.ones_like(times), label='Dx theory')
+#ax[0].plot(times, -Dx*c*(times-coll_time)/sigmaz, label='dx/x theory')
+
+
+
+
+#ax[1].plot(times, -(0.78+0.2*Dx)*np.ones_like(times))
+#ax[1].plot(times, -(r_e*N/gamma/sigmaz**2)*np.ones_like(times)* xstd_ele[0])
+
+#*c/sigmaz, label='dx/x theory')
+
+#print(-Dx*c/sigmaz)
+#print(db_dt[ref_timestep])
+#ax[1].plot(times[:-1], -r_e*N/(gamma*sigmaz**2)*np.ones_like(times[:-1]), label='factor')
+#ax[1].plot(times[:-1], par, label='bho')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ax[0].plot(times, theta0*np.ones_like(times), label='theory', color='red', zorder=11)
+
+#ax[0].plot(times, thetaxave_ele, label='y')
+#ax[0].set_title('theta ave')
+
+#ax[0].plot(times, xstd_ele)
+
+
+#ax[0].plot(times, (thetaxstd_ele-thetaxstd_ele[0])/thetaxstd_ele[0], label='thetax')
+
+
+#ax[0].plot(times, (thetaystd_ele-thetaystd_ele[0])/thetaystd_ele[0], label='thetay')
+#ax[0].plot(times, (ystd_ele - ystd_ele[0])/ystd_ele[0], label='y')
+
+
+#ax[0].plot(times, thetaxstd_ele, label='y')
+#ax[0].set_title('stds')
+
+#ax[0].plot(times, -Dx*c*(times-coll_time)/sigmaz, label='dx/x theory')
+
+
+#ax[2].plot(times, xave_ele, label='x ele')
+#ax[2].plot(times, yave_ele, label='y ele')
+#ax[2].set_title('ave pos')
+
+#ax[1].plot(times, xstd_ele, label='x ele')
+#ax[1].plot(times, ystd_ele, label='y ele')
+#ax[1].set_title('std pos')
+
+
+
+#ax[1].plot(times, -Dx*np.ones_like(times), label='Dx theory')
+#ax[1].plot(times, -Dx*c*(times-coll_time)/sigmaz, label='dx/x theory')
+
+
+
+#ax[2].plot(times, xstd_ele, label='std')
+
+#ax[3].fill_between(times, yave_ele-ystd_ele, yave_ele+ystd_ele, alpha=0.4)
+#ax[3].plot(times, yave_ele, label='ave')
+#ax[3].plot(times, ystd_ele, label='std')
+#ax[3].set_title('y')
+
+
+
