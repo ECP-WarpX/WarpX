5aae34ebfa6831f179d42cc50346b25ffb8d4991
johnson452 <grj@princeton.edu>
2023-08-07 10:55:46 -0700

    Bugfix/improvement: RZ test now correctly outputs and tests rho, J in addition to the electric field

--- Examples/Tests/langmuir_fluids/analysis_rz.py
+++ Examples/Tests/langmuir_fluids/analysis_rz.py
@@ -73,6 +73,39 @@ def Ez( z, r, epsilon, k0, w0, wp, t) :
             np.exp( -r**2/w0**2 ) * np.cos( k0*z ) * np.sin( wp*t )
     return( Ez_array )
 
+def Jr( z, r, epsilon, k0, w0, wp, t) :
+    """
+    Return the radial current density as an array
+    of the same length as z and r, in the half-plane theta=0
+    """
+    dt = t / 80 # SPECIFIC to config parameters!
+    Jr_array = \
+        - epsilon_0 * epsilon * m_e*c**2/e * 2*r/w0**2 * \
+            np.exp( -r**2/w0**2 ) * np.sin( k0*z ) * np.cos( wp*t -wp*dt/2) * wp #phase_error = wp*dt/2
+    return( Jr_array )
+
+def Jz( z, r, epsilon, k0, w0, wp, t) :
+    """
+    Return the longitudinal current density as an array
+    of the same length as z and r, in the half-plane theta=0
+    """
+    dt = t / 80 # SPECIFIC to config parameters!
+    Jz_array = \
+        epsilon_0 * epsilon * m_e*c**2/e * k0 * \
+            np.exp( -r**2/w0**2 ) * np.cos( k0*z ) * np.cos( wp*t -wp*dt/2) * wp  #phase_error = wp*dt/2
+    return( Jz_array )
+
+def rho( z, r, epsilon, k0, w0, wp, t) :
+    """
+    Return the charge density as an array
+    of the same length as z and r, in the half-plane theta=0
+    """
+    dt = t / 80 # SPECIFIC to config parameters!
+    rho_array = \
+        epsilon_0 * epsilon * m_e*c**2/e * np.sin( wp*t ) * np.sin( k0*z ) * np.exp( -r**2/w0**2 ) * \
+            ((4.0/(w0**2))*(1 - (r**2)/(w0**2)) + k0**2)  
+    return( rho_array )
+
 # Read the file
 ds = yt.load(fn)
 t0 = ds.current_time.to_value()
@@ -100,6 +133,24 @@ max_error = abs(Ez_sim-Ez_th).max()/abs(Ez_th).max()
 print('Ez: Max error: %.2e' %(max_error))
 overall_max_error = max( overall_max_error, max_error )
 
+Jr_sim = data[('boxlib','jr')].to_ndarray()[:,:,0]
+Jr_th = Jr(zz, rr, epsilon, k0, w0, wp, t0)
+max_error = abs(Jr_sim-Jr_th).max()/abs(Jr_th).max()
+print('Jr: Max error: %.2e' %(max_error))
+overall_max_error = max( overall_max_error, max_error )
+
+Jz_sim = data[('boxlib','jz')].to_ndarray()[:,:,0]
+Jz_th = Jz(zz, rr, epsilon, k0, w0, wp, t0)
+max_error = abs(Jz_sim-Jz_th).max()/abs(Jz_th).max()
+print('Jz: Max error: %.2e' %(max_error))
+overall_max_error = max( overall_max_error, max_error )
+
+rho_sim = data[('boxlib','rho')].to_ndarray()[:,:,0]
+rho_th = rho(zz, rr, epsilon, k0, w0, wp, t0)
+max_error = abs(rho_sim-rho_th).max()/abs(rho_th).max()
+print('rho: Max error: %.2e' %(max_error))
+overall_max_error = max( overall_max_error, max_error )
+
 # Plot the last field from the loop (Ez at iteration 40)
 plt.subplot2grid( (1,2), (0,0) )
 plt.imshow( Ez_sim )
@@ -114,7 +165,7 @@ plt.savefig(test_name+'_analysis.png')
 
 error_rel = overall_max_error
 
-tolerance_rel = 0.12
+tolerance_rel = 0.16
 
 print("error_rel    : " + str(error_rel))
 print("tolerance_rel: " + str(tolerance_rel))
