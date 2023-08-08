e8240675f67d6d9b0a3ed4974809d9b47491fe21
roelof-groenewald <regroenewald@gmail.com>
2023-08-07 22:14:35 -0700

    code cleanup - reduce code duplication

--- Source/Diagnostics/ComputeDiagFunctors/JFunctor.cpp
+++ Source/Diagnostics/ComputeDiagFunctors/JFunctor.cpp
@@ -7,11 +7,8 @@
 #include "JFunctor.H"
 
 #include "Particles/MultiParticleContainer.H"
-#include "Utils/TextMsg.H"
 #include "Utils/WarpXAlgorithmSelection.H"
 #include "WarpX.H"
-
-#include <ablastr/coarsen/sample.H>
 
 #include <AMReX.H>
 #include <AMReX_IntVect.H>
@@ -62,28 +59,6 @@ JFunctor::operator() (amrex::MultiFab& mf_dst, int dcomp, const int /*i_buffer*/
         amrex::MultiFab::Copy(*m_mf_src, *current_fp_temp[0][m_dir], 0, 0, 1, m_mf_src->nGrowVect());
     }
 
-#ifdef WARPX_DIM_RZ
-    if (m_convertRZmodes2cartesian) {
-        // In cylindrical geometry, sum real part of all modes of m_mf_src in
-        // temporary multifab mf_dst_stag, and cell-center it to mf_dst.
-        WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
-            nComp()==1,
-            "The RZ averaging over modes must write into 1 single component");
-        amrex::MultiFab mf_dst_stag(m_mf_src->boxArray(), warpx.DistributionMap(m_lev), 1, m_mf_src->nGrowVect());
-        // Mode 0
-        amrex::MultiFab::Copy(mf_dst_stag, *m_mf_src, 0, 0, 1, m_mf_src->nGrowVect());
-        for (int ic=1 ; ic < m_mf_src->nComp() ; ic += 2) {
-            // All modes > 0
-            amrex::MultiFab::Add(mf_dst_stag, *m_mf_src, ic, 0, 1, m_mf_src->nGrowVect());
-        }
-        ablastr::coarsen::sample::Coarsen( mf_dst, mf_dst_stag, dcomp, 0, nComp(), 0,  m_crse_ratio);
-    } else {
-        ablastr::coarsen::sample::Coarsen( mf_dst, *m_mf_src, dcomp, 0, nComp(), 0, m_crse_ratio);
-    }
-#else
-    // In cartesian geometry, coarsen and interpolate from simulation MultiFab, m_mf_src,
-    // to output diagnostic MultiFab, mf_dst.
-    ablastr::coarsen::sample::Coarsen(mf_dst, *m_mf_src, dcomp, 0, nComp(), mf_dst.nGrowVect(), m_crse_ratio);
-    amrex::ignore_unused(m_lev, m_convertRZmodes2cartesian);
-#endif
+    InterpolateToDst(mf_dst, *m_mf_src, dcomp, warpx.DistributionMap(m_lev),
+                     m_convertRZmodes2cartesian);
 }
