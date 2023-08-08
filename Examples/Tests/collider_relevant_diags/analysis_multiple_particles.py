24881f69698dd174dfd58aaf3dec4ea608d838a5
Edoardo Zoni <ezoni@lbl.gov>
2023-08-07 18:27:06 -0700

    Reuse input file parser from Tools

--- Examples/Tests/collider_relevant_diags/analysis_multiple_particles.py
+++ Examples/Tests/collider_relevant_diags/analysis_multiple_particles.py
@@ -11,41 +11,11 @@ from scipy.constants import c, e, hbar, m_e
 sys.path.append('../../../../warpx/Regression/Checksum/')
 import checksumAPI
 
+sys.path.append('../../../../warpx/Tools/Parser/')
+from input_file_parser import parse_input_file
+
 E_crit = m_e**2*c**3/(e*hbar)
 B_crit = m_e**2*c**2/(e*hbar)
-
-def parse_input_file(input_file):
-    """
-    Parse WarpX input file.
-
-    Parameters
-    ----------
-    input_file : string
-        Path to input file.
-
-    Returns
-    -------
-    input_dict : dictionary
-        Dictionary storing WarpX input parameters
-        (parameter's name stored as key, parameter's value stored as value).
-    """
-    input_dict = dict()
-    with open(input_file) as ff:
-        for line in ff:
-            sline = line.split('=')
-            # skip lines that are commented out, blank, or continuation of previous parameters
-            skip_line = sline[0].startswith('#') or sline[0].startswith('\n') or len(sline) == 1
-            if not skip_line:
-                key = sline[0].strip()
-                val = sline[1].split()
-                # The value corresponding to a given key of input_dict is a list
-                # of strings, from which we remove any leftover comments
-                for i in range(len(val)):
-                    if val[i].startswith('#'):
-                        val = val[:i]
-                        break
-                input_dict[key] = val
-    return input_dict
 
 def chi(ux, uy, uz, Ex, Ey, Ez, Bx, By, Bz):
     gamma = np.sqrt(1.+ux**2+uy**2+uz**2)
