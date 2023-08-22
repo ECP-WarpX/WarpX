4f09ac2c1217ed9bb784be40517136e3dfaa00cd
ariannaformenti@lbl.gov <arianna.formenti@gmail.com>
2023-08-22 16:41:56 -0700

    assert in RZ and removed fine ref levs

--- Source/Diagnostics/ReducedDiags/ColliderRelevant.cpp
+++ Source/Diagnostics/ReducedDiags/ColliderRelevant.cpp
@@ -67,12 +67,24 @@ ColliderRelevant::ColliderRelevant (std::string rd_name)
 
     WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
         m_beam_name.size() == 2u,
-        "Collider-relevant diagnostic must involve exactly two species");
+        "Collider-relevant diagnostics must involve exactly two species");
+
+    // RZ coordinate is not working
+#if (defined WARPX_DIM_RZ)
+    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(false,
+        "Collider-relevant diagnostics do not work in RZ geometry.");
+#endif
 
     ablastr::warn_manager::WMRecordWarning(
         "DIAGNOSTICS",
         "The collider-relevant reduced diagnostic is meant for \
         colliding species propagating along the z direction.",
+        ablastr::warn_manager::WarnPriority::low);
+
+    ablastr::warn_manager::WMRecordWarning(
+        "DIAGNOSTICS",
+        "The collider-relevant reduced diagnostic only considers the \
+        coarsest level of refinement for the calculations involving chi.",
         ablastr::warn_manager::WarnPriority::low);
 
     // get WarpX class object
@@ -107,7 +119,7 @@ ColliderRelevant::ColliderRelevant (std::string rd_name)
 
 #if (defined WARPX_DIM_3D)
     add_diag("dL_dt", "dL_dt(m^-2*s^-1)");
-#elif (defined(WARPX_DIM_XZ) || defined(WARPX_DIM_RZ))
+#elif (defined(WARPX_DIM_XZ)
     add_diag("dL_dt", "dL_dt(m^-1*s^-1)");
 #else
     add_diag("dL_dt", "dL_dt(s^-1)");
@@ -133,12 +145,12 @@ ColliderRelevant::ColliderRelevant (std::string rd_name)
         add_diag("thetax_ave_"+species_names[i_s], "theta_x_ave_"+species_names[i_s]+"(rad)");
         add_diag("thetax_max_"+species_names[i_s], "theta_x_max_"+species_names[i_s]+"(rad)");
         add_diag("thetax_std_"+species_names[i_s], "theta_x_std_"+species_names[i_s]+"(rad)");
-        add_diag("thetay_ave_"+species_names[i_s], "theta_y_ave_"+species_names[i_s]+"(rad)");
         add_diag("thetay_min_"+species_names[i_s], "theta_y_min_"+species_names[i_s]+"(rad)");
+        add_diag("thetay_ave_"+species_names[i_s], "theta_y_ave_"+species_names[i_s]+"(rad)");
         add_diag("thetay_max_"+species_names[i_s], "theta_y_max_"+species_names[i_s]+"(rad)");
         add_diag("thetay_std_"+species_names[i_s], "theta_y_std_"+species_names[i_s]+"(rad)");
 
-#elif (defined(WARPX_DIM_XZ) || defined(WARPX_DIM_RZ))
+#elif (defined(WARPX_DIM_XZ)
         add_diag("x_ave_"+species_names[i_s], "x_ave_"+species_names[i_s]+"(m)");
         add_diag("x_std_"+species_names[i_s], "x_std_"+species_names[i_s]+"(m)");
         add_diag("thetax_min_"+species_names[i_s], "theta_x_min_"+species_names[i_s]+"(rad)");
@@ -192,7 +204,7 @@ void ColliderRelevant::ComputeDiags (int step)
     amrex::Geometry const & geom = warpx.Geom(0);
 #if defined(WARPX_DIM_1D_Z)
         amrex::Real dV = geom.CellSize(0);
-#elif defined(WARPX_DIM_XZ) || defined(WARPX_DIM_RZ)
+#elif defined(WARPX_DIM_XZ)
         amrex::Real dV = geom.CellSize(0) * geom.CellSize(1);
 #elif defined(WARPX_DIM_3D)
         amrex::Real dV = geom.CellSize(0) * geom.CellSize(1) * geom.CellSize(2);
@@ -225,7 +237,7 @@ void ColliderRelevant::ComputeDiags (int step)
             });
         amrex::ParallelDescriptor::ReduceRealSum(wtot, amrex::ParallelDescriptor::IOProcessorNumber());
 
-#if defined(WARPX_DIM_XZ) || defined(WARPX_DIM_RZ)
+#if defined(WARPX_DIM_XZ)
         // thetax_min, thetax_max
         amrex::Real thetax_min = 0.0_rt;
         amrex::Real thetax_max = 0.0_rt;
@@ -411,9 +423,6 @@ void ColliderRelevant::ComputeDiags (int step)
 #endif
 
 #if (defined WARPX_QED)
-        // get number of level
-        const int level_number = WarpX::GetInstance().finestLevel();
-
         // get mass
         amrex::ParticleReal m = myspc.getMass();
         const bool is_photon = myspc.AmIA<PhysicalSpecies::photon>();
@@ -428,106 +437,96 @@ void ColliderRelevant::ComputeDiags (int step)
 
         if (myspc.DoQED())
         {
-            // declare chi arrays
-            std::vector<Real> chimin, chiave, chimax;
-            chimin.resize(level_number+1,0.0_rt);
-            chimax.resize(level_number+1,0.0_rt);
-            chiave.resize(level_number+1,0.0_rt);
-
             // define variables in preparation for field gatheeduce_data.value()ring
             const int n_rz_azimuthal_modes = WarpX::n_rz_azimuthal_modes;
             const int nox = WarpX::nox;
             const bool galerkin_interpolation = WarpX::galerkin_interpolation;
             const amrex::IntVect ngEB = warpx.getngEB();
 
-            // loop over refinement levels
-            for (int lev = 0; lev <= level_number; ++lev)
+            // TO DO: loop over refinement levels: for (int lev = 0; lev <= level_number; ++lev)
+            int lev = 0;
+
+            // define variables in preparation for field gathering
+            const std::array<amrex::Real,3>& dx = WarpX::CellSize(std::max(lev, 0));
+            const amrex::GpuArray<amrex::Real, 3> dx_arr = {dx[0], dx[1], dx[2]};
+            const amrex::MultiFab & Ex = warpx.getEfield(lev,0);
+            const amrex::MultiFab & Ey = warpx.getEfield(lev,1);
+            const amrex::MultiFab & Ez = warpx.getEfield(lev,2);
+            const amrex::MultiFab & Bx = warpx.getBfield(lev,0);
+            const amrex::MultiFab & By = warpx.getBfield(lev,1);
+            const amrex::MultiFab & Bz = warpx.getBfield(lev,2);
+
+            // declare reduce_op
+            ReduceOps<ReduceOpMin, ReduceOpMax, ReduceOpSum> reduce_op;
+            ReduceData<Real, Real, Real> reduce_data(reduce_op);
+            using ReduceTuple = typename decltype(reduce_data)::Type;
+
+            // Loop over boxes
+            for (WarpXParIter pti(myspc, lev); pti.isValid(); ++pti)
             {
+                const auto GetPosition = GetParticlePosition(pti);
+                // get particle arrays
+                amrex::ParticleReal* const AMREX_RESTRICT ux = pti.GetAttribs()[PIdx::ux].dataPtr();
+                amrex::ParticleReal* const AMREX_RESTRICT uy = pti.GetAttribs()[PIdx::uy].dataPtr();
+                amrex::ParticleReal* const AMREX_RESTRICT uz = pti.GetAttribs()[PIdx::uz].dataPtr();
+                amrex::ParticleReal* const AMREX_RESTRICT w = pti.GetAttribs()[PIdx::w].dataPtr();
+                // declare external fields
+                const int offset = 0;
+                const auto getExternalEB = GetExternalEBField(pti, offset);
                 // define variables in preparation for field gathering
-                const std::array<amrex::Real,3>& dx = WarpX::CellSize(std::max(lev, 0));
-                const amrex::GpuArray<amrex::Real, 3> dx_arr = {dx[0], dx[1], dx[2]};
-                const amrex::MultiFab & Ex = warpx.getEfield(lev,0);
-                const amrex::MultiFab & Ey = warpx.getEfield(lev,1);
-                const amrex::MultiFab & Ez = warpx.getEfield(lev,2);
-                const amrex::MultiFab & Bx = warpx.getBfield(lev,0);
-                const amrex::MultiFab & By = warpx.getBfield(lev,1);
-                const amrex::MultiFab & Bz = warpx.getBfield(lev,2);
+                amrex::Box box = pti.tilebox();
+                box.grow(ngEB);
+                const amrex::Dim3 lo = amrex::lbound(box);
+                const std::array<amrex::Real, 3>& xyzmin = WarpX::LowerCorner(box, lev, 0._rt);
+                const amrex::GpuArray<amrex::Real, 3> xyzmin_arr = {xyzmin[0], xyzmin[1], xyzmin[2]};
+                const amrex::Array4<const amrex::Real> & ex_arr = Ex[pti].array();
+                const amrex::Array4<const amrex::Real> & ey_arr = Ey[pti].array();
+                const amrex::Array4<const amrex::Real> & ez_arr = Ez[pti].array();
+                const amrex::Array4<const amrex::Real> & bx_arr = Bx[pti].array();
+                const amrex::Array4<const amrex::Real> & by_arr = By[pti].array();
+                const amrex::Array4<const amrex::Real> & bz_arr = Bz[pti].array();
+                const amrex::IndexType ex_type = Ex[pti].box().ixType();
+                const amrex::IndexType ey_type = Ey[pti].box().ixType();
+                const amrex::IndexType ez_type = Ez[pti].box().ixType();
+                const amrex::IndexType bx_type = Bx[pti].box().ixType();
+                const amrex::IndexType by_type = By[pti].box().ixType();
+                const amrex::IndexType bz_type = Bz[pti].box().ixType();
 
-                // declare reduce_op
-                ReduceOps<ReduceOpMin, ReduceOpMax, ReduceOpSum> reduce_op;
-                ReduceData<Real, Real, Real> reduce_data(reduce_op);
-                using ReduceTuple = typename decltype(reduce_data)::Type;
-
-                // Loop over boxes
-                for (WarpXParIter pti(myspc, lev); pti.isValid(); ++pti)
+                // evaluate reduce_op
+                reduce_op.eval(pti.numParticles(), reduce_data,
+                [=] AMREX_GPU_DEVICE (int i) -> ReduceTuple
                 {
-                    const auto GetPosition = GetParticlePosition(pti);
-                    // get particle arrays
-                    amrex::ParticleReal* const AMREX_RESTRICT ux = pti.GetAttribs()[PIdx::ux].dataPtr();
-                    amrex::ParticleReal* const AMREX_RESTRICT uy = pti.GetAttribs()[PIdx::uy].dataPtr();
-                    amrex::ParticleReal* const AMREX_RESTRICT uz = pti.GetAttribs()[PIdx::uz].dataPtr();
-                    amrex::ParticleReal* const AMREX_RESTRICT w = pti.GetAttribs()[PIdx::w].dataPtr();
-                    // declare external fields
-                    const int offset = 0;
-                    const auto getExternalEB = GetExternalEBField(pti, offset);
-                    // define variables in preparation for field gathering
-                    amrex::Box box = pti.tilebox();
-                    box.grow(ngEB);
-                    const amrex::Dim3 lo = amrex::lbound(box);
-                    const std::array<amrex::Real, 3>& xyzmin = WarpX::LowerCorner(box, lev, 0._rt);
-                    const amrex::GpuArray<amrex::Real, 3> xyzmin_arr = {xyzmin[0], xyzmin[1], xyzmin[2]};
-                    const amrex::Array4<const amrex::Real> & ex_arr = Ex[pti].array();
-                    const amrex::Array4<const amrex::Real> & ey_arr = Ey[pti].array();
-                    const amrex::Array4<const amrex::Real> & ez_arr = Ez[pti].array();
-                    const amrex::Array4<const amrex::Real> & bx_arr = Bx[pti].array();
-                    const amrex::Array4<const amrex::Real> & by_arr = By[pti].array();
-                    const amrex::Array4<const amrex::Real> & bz_arr = Bz[pti].array();
-                    const amrex::IndexType ex_type = Ex[pti].box().ixType();
-                    const amrex::IndexType ey_type = Ey[pti].box().ixType();
-                    const amrex::IndexType ez_type = Ez[pti].box().ixType();
-                    const amrex::IndexType bx_type = Bx[pti].box().ixType();
-                    const amrex::IndexType by_type = By[pti].box().ixType();
-                    const amrex::IndexType bz_type = Bz[pti].box().ixType();
+                    // get external fields
+                    amrex::ParticleReal xp, yp, zp;
+                    GetPosition(i, xp, yp, zp);
+                    amrex::ParticleReal ex = 0._rt, ey = 0._rt, ez = 0._rt;
+                    amrex::ParticleReal bx = 0._rt, by = 0._rt, bz = 0._rt;
+                    getExternalEB(i, ex, ey, ez, bx, by, bz);
 
-                    // evaluate reduce_op
-                    reduce_op.eval(pti.numParticles(), reduce_data,
-                    [=] AMREX_GPU_DEVICE (int i) -> ReduceTuple
-                    {
-                        // get external fields
-                        amrex::ParticleReal xp, yp, zp;
-                        GetPosition(i, xp, yp, zp);
-                        amrex::ParticleReal ex = 0._rt, ey = 0._rt, ez = 0._rt;
-                        amrex::ParticleReal bx = 0._rt, by = 0._rt, bz = 0._rt;
-                        getExternalEB(i, ex, ey, ez, bx, by, bz);
-
-                        // gather E and B
-                        doGatherShapeN(xp, yp, zp,
-                            ex, ey, ez, bx, by, bz,
-                            ex_arr, ey_arr, ez_arr, bx_arr, by_arr, bz_arr,
-                            ex_type, ey_type, ez_type,
-                            bx_type, by_type, bz_type,
-                            dx_arr, xyzmin_arr, lo,
-                            n_rz_azimuthal_modes, nox, galerkin_interpolation);
-                        // compute chi
-                        amrex::Real chi = 0.0_rt;
-                        if (is_photon) {
-                            chi = QedUtils::chi_photon(ux[i]*m, uy[i]*m, uz[i]*m,
-                                             ex, ey, ez, bx, by, bz);
-                        } else {
-                            chi = QedUtils::chi_ele_pos(ux[i]*m, uy[i]*m, uz[i]*m,
-                                             ex, ey, ez, bx, by, bz);
-                        }
-                        return {chi, chi, chi*w[i]};
-                    });
-                }
-                auto val = reduce_data.value();
-                chimin[lev] = get<0>(val);
-                chimax[lev] = get<1>(val);
-                chiave[lev] = get<2>(val);
+                    // gather E and B
+                    doGatherShapeN(xp, yp, zp,
+                        ex, ey, ez, bx, by, bz,
+                        ex_arr, ey_arr, ez_arr, bx_arr, by_arr, bz_arr,
+                        ex_type, ey_type, ez_type,
+                        bx_type, by_type, bz_type,
+                        dx_arr, xyzmin_arr, lo,
+                        n_rz_azimuthal_modes, nox, galerkin_interpolation);
+                    // compute chi
+                    amrex::Real chi = 0.0_rt;
+                    if (is_photon) {
+                        chi = QedUtils::chi_photon(ux[i]*m, uy[i]*m, uz[i]*m,
+                                            ex, ey, ez, bx, by, bz);
+                    } else {
+                        chi = QedUtils::chi_ele_pos(ux[i]*m, uy[i]*m, uz[i]*m,
+                                            ex, ey, ez, bx, by, bz);
+                    }
+                    return {chi, chi, chi*w[i]};
+                });
             }
-            chimin_f = *std::min_element(chimin.begin(), chimin.end());
-            chimax_f = *std::max_element(chimax.begin(), chimax.end());
-            chiave_f = chiave[0]; // FIXME mesh refinement
+            auto val = reduce_data.value();
+            chimin_f = get<0>(val);
+            chimax_f = get<1>(val);
+            chiave_f = get<2>(val); 
             amrex::ParallelDescriptor::ReduceRealMin(chimin_f, amrex::ParallelDescriptor::IOProcessorNumber());
             amrex::ParallelDescriptor::ReduceRealMax(chimax_f, amrex::ParallelDescriptor::IOProcessorNumber());
             amrex::ParallelDescriptor::ReduceRealSum(chiave_f, amrex::ParallelDescriptor::IOProcessorNumber());
