439766a3a4d25ddc2bd5a3a00b04d061c9155fcb
Axel Huebl <axel.huebl@plasma.ninja>
2023-08-28 15:13:14 -0700

    Lassen RHEL8
    Update the Lassen (LLNL) documentation for their RHEL8 transition
    and Python support.

---
+++ Tools/machines/lassen-llnl/install_v100_ml.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Copyright 2023 The WarpX Community
+#
+# This file is part of WarpX.
+#
+# Author: Axel Huebl
+# License: BSD-3-Clause-LBNL
+
+# Exit on first error encountered #############################################
+#
+set -eu -o pipefail
+
+
+# Check: ######################################################################
+#
+#   Was perlmutter_gpu_warpx.profile sourced and configured correctly?
+if [ -z ${proj-} ]; then echo "WARNING: The 'proj' variable is not yet set in your lassen_v100_warpx.profile file! Please edit its line 2 to continue!"; exit 1; fi
+
+
+# Remove old dependencies #####################################################
+#
+# remove common user mistakes in python, located in .local instead of a venv
+python3 -m pip uninstall -qqq -y torch 2>/dev/null || true
+
+
+# Python ML ###################################################################
+#
+# for basic python dependencies, see install_v100_dependencies.sh
+
+# optional: for libEnsemble - WIP: issues with nlopt
+# python3 -m pip install -r $HOME/src/warpx/Tools/LibEnsemble/requirements.txt
+
+# optional: for pytorch
+if [ -d ${HOME}/src/pytorch ]
+then
+  cd ${HOME}/src/pytorch
+  git fetch
+  git checkout .
+  git checkout v2.0.1
+  cd -
+else
+  git clone -b v2.0.1 --recurse-submodules https://github.com/pytorch/pytorch.git /ccs/proj/${proj}/${USER}/src/pytorch
+fi
+cd ${HOME}/src/pytorch
+rm -rf build
+python3 -m pip install -r requirements.txt
+#   patch to avoid compile issues
+#   https://github.com/pytorch/pytorch/issues/97497#issuecomment-1499069641
+#   https://github.com/pytorch/pytorch/pull/98511
+wget -q -O - https://github.com/pytorch/pytorch/pull/98511.patch | git apply
+USE_CUDA=1 BLAS=OpenBLAS MAX_JOBS=64 ATEN_AVX512_256=OFF BUILD_TEST=0 python3 setup.py develop
+#   (optional) If using torch.compile with inductor/triton, install the matching version of triton
+#make triton
+rm -rf build
+cd -
+
+# optional: optimas dependencies (based on libEnsemble & ax->botorch->gpytorch->pytorch)
+#   commented because scikit-learn et al. compile > 2 hrs
+#   please run manually on a login node if needed
+#python3 -m pip install -r $HOME/src/warpx/Tools/optimas/requirements.txt
