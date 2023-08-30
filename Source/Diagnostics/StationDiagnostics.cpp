3c09425868f2cbfc4596d244c034703b09480316
RevathiJambunathan <revanathan@gmail.com>
2023-08-30 11:05:00 -0700

    write particle data as vectors. (this commit has a compilation error. )

--- Source/Diagnostics/StationDiagnostics.cpp
+++ Source/Diagnostics/StationDiagnostics.cpp
@@ -40,11 +40,14 @@
 #include <AMReX_MultiFab.H>
 #include <AMReX_ParmParse.H>
 #include <AMReX_ParallelDescriptor.H>
+#include <AMReX_ParticleContainer.H>
+#include <AMReX_ParticleContainerBase.H>
 #include <AMReX_PlotFileUtil.H>
 #include <AMReX_REAL.H>
 #include <AMReX_RealBox.H>
 #include <AMReX_VisMF.H>
 #include <AMReX_Vector.H>
+#include <AMReX_VectorIO.H>
 
 #include <algorithm>
 #include <cmath>
@@ -291,6 +294,7 @@ StationDiagnostics::Flush (int i_buffer)
     if (! amrex::UtilCreateDirectory(filename, permission_flag_rwxrxrx) ) {
         amrex::CreateDirectoryFailed(filename);
     }
+    amrex::Print() << " finename : " << filename << "\n";
     WriteStationHeader(filename);
     for (int lev = 0; lev < 1; ++lev) {
         const std::string buffer_path = filename + amrex::Concatenate("/Level_",lev,1) + "/";
@@ -301,11 +305,14 @@ StationDiagnostics::Flush (int i_buffer)
         }
         std::string buffer_string = amrex::Concatenate("buffer-",m_flush_counter,m_file_min_digits);
         const std::string prefix = amrex::MultiFabFileFullPrefix(lev, filename, "Level_", buffer_string);
+        amrex::Print() << " Writing buffer " << buffer_string << "\n";
         amrex::VisMF::Write(m_mf_output[i_buffer][lev], prefix);
     }
 
-
-    FlushParticleBuffer();
+    for (int isp = 0; isp < m_particles_buffer[0].size(); ++isp) {
+        FlushParticleBuffer(filename, m_output_species_names[isp], isp, i_buffer);
+        m_totalParticles_in_buffer[i_buffer][isp] = 0;
+    }
 
     // reset counter
     m_slice_counter = 0;
@@ -341,7 +348,211 @@ StationDiagnostics::WriteStationHeader (const std::string& filename)
 }
 
 void
-StationDiagnostics::FlushParticleBuffer ()
+StationDiagnostics::FlushParticleBuffer (std::string path, std::string species_name, int isp, int i_buffer)
 {
 
+    amrex::Vector<std::string> real_names;
+    amrex::Vector<std::string> int_names;
+    amrex::Vector<int> int_flags;
+    amrex::Vector<int> real_flags;
+
+    real_names.push_back("weight");
+    real_names.push_back("momentum_x");
+    real_names.push_back("momentum_y");
+    real_names.push_back("momentum_z");
+
+    real_names.resize(m_particles_buffer[i_buffer][isp]->NumRealComps());
+    auto runtime_rnames = m_particles_buffer[i_buffer][isp]->getParticleRuntimeComps();
+    for (auto const& x : runtime_rnames) {real_names[x.second+PIdx::nattribs] = x.first;}
+
+    real_flags.resize(m_particles_buffer[i_buffer][isp]->NumRealComps(), 1);
+
+    int_names.resize(m_particles_buffer[i_buffer][isp]->NumIntComps());
+    auto runtime_inames = m_particles_buffer[i_buffer][isp]->getParticleRuntimeiComps();
+    for (auto const& x : runtime_inames) { int_names[x.second + 0] = x.first; }
+
+    int_flags.resize(m_particles_buffer[i_buffer][isp]->NumIntComps(), 1);
+
+    std::string species_dir = path + "/" + species_name;
+    // create directory for recorded species
+    if (amrex::ParallelDescriptor::IOProcessor())
+    {
+        if ( ! amrex::UtilCreateDirectory(species_dir, 0755))
+        {
+            amrex::CreateDirectoryFailed(species_dir);
+        }
+    }
+    amrex::ParallelDescriptor::Barrier();
+    // create direcotry for buffer
+    std::string buffer_string = amrex::Concatenate("pbuffer-", m_flush_counter, m_file_min_digits);
+    std::string buffer_dir = species_dir + "/" + buffer_string;
+    if (amrex::ParallelDescriptor::IOProcessor())
+    {
+        if ( ! amrex::UtilCreateDirectory(buffer_dir, 0755))
+        {
+            amrex::CreateDirectoryFailed(buffer_dir);
+        }
+    }
+    amrex::ParallelDescriptor::Barrier();
+
+    if (amrex::ParallelDescriptor::IOProcessor())
+    {
+        WriteHeaderFile(buffer_dir, real_names, int_names, isp);
+        WriteParticleData(buffer_dir, real_names, int_names, real_flags, int_flags, isp);
+    }
+}
+
+void
+StationDiagnostics::WriteHeaderFile (std::string pdir, amrex::Vector<std::string> real_names,
+                                     amrex::Vector<std::string> int_names, int isp)
+{
+    std::string HdrFileName = pdir;
+    if ( ! HdrFileName.empty() && HdrFileName[HdrFileName.size()-1] != '/') {
+        HdrFileName += '/';
+    }
+
+    HdrFileName += "Header";
+
+    std::ofstream HdrFile;
+
+    HdrFile.open(HdrFileName.c_str(), std::ios::out|std::ios::trunc);
+
+    if ( ! HdrFile.good()) { amrex::FileOpenFailed(HdrFileName); }
+
+    HdrFile << AMREX_SPACEDIM << "\n";
+
+    // number of real parameters
+    HdrFile << real_names.size() << "\n";
+
+    // Real component names
+    for (int i = 0; i < real_names.size(); ++i)
+    {
+        HdrFile << real_names[i] << "\n";
+    }
+
+    // number of int parameters
+    HdrFile << int_names.size() << "\n";
+
+    // int component names
+    for (int i = 0; i < int_names.size(); ++i)
+    {
+        HdrFile << int_names[i] << "\n";
+    }
+
+    // number of particles
+    HdrFile << m_totalParticles_in_buffer[0][isp] << "\n";
+
+    // finest level
+    HdrFile << m_particles_buffer[0][isp]->finestLevel() << "\n";
+
+    // Box Array size
+    for (int lev = 0; lev <= m_particles_buffer[0][isp]->finestLevel(); ++lev)
+    {
+        HdrFile << m_particles_buffer[0][isp]->ParticleBoxArray(lev).size() << "\n";
+    }
+
+    HdrFile.flush();
+    HdrFile.close();
+    if ( ! HdrFile.good())
+    {
+        amrex::Abort("ParticleContainer::Checkpoint(): problem writing HdrFile");
+    }
+}
+
+void
+StationDiagnostics::WriteParticleData (std::string pdir, amrex::Vector<std::string> real_names,
+                                       amrex::Vector<std::string> int_names,
+                                       amrex::Vector<int> real_flags, amrex::Vector<int> int_flags,
+                                       int isp)
+{
+    const int NProcs = amrex::ParallelDescriptor::NProcs();
+    const int IOProcNumber = amrex::ParallelDescriptor::IOProcessorNumber();
+
+    // Writing data out in parallel
+    // Allow upto nOutFiles activate writers at a time
+    int nOutFiles = 256;
+    nOutFiles = std::max(1, std::min(nOutFiles, NProcs));
+
+    bool gotsomeParticles = (m_particles_buffer[0][isp] > 0);
+    amrex::Print() << " got some particles " << gotsomeParticles << "\n";
+    const int lev = 0;
+
+    // flags to determine which particles to output
+    amrex::Vector<std::map<std::pair<int,int>, amrex::Vector<int> > > particle_io_flags(m_particles_buffer[0][isp]->GetParticles().size());
+    const auto pmap = m_particles_buffer[0][isp]->GetParticles(lev);
+    for (const auto & kv : pmap)
+    {
+        auto& flags = particle_io_flags[lev][kv.first];
+        const auto np = kv.second.numParticles(); 
+        flags.resize(np,1);
+    }
+    amrex::Gpu::Device::streamSynchronize();
+
+    amrex::MFInfo info;
+    info.SetAlloc(false);
+    amrex::MultiFab state(m_particles_buffer[0][isp]->ParticleBoxArray(lev),
+                          m_particles_buffer[0][isp]->ParticleDistributionMap(lev),
+                          1,0,info);
+    // We eventually want to write out the file name and the offset
+    // into that file into which each grid of particles is written.
+    amrex::Vector<int>  which(state.size(),0);
+    amrex::Vector<int > count(state.size(),0);
+    amrex::Vector<amrex::Long> where(state.size(),0);
+
+    if (gotsomeParticles) {
+        std::string LevelDir = pdir;
+        if ( ! LevelDir.empty() && LevelDir[LevelDir.size()-1] != '/') { LevelDir += '/'; }
+        LevelDir = amrex::Concatenate(LevelDir.append("Level_"), lev, 1);
+        if (amrex::ParallelDescriptor::IOProcessor())
+        {
+            if ( ! amrex::UtilCreateDirectory(LevelDir, 0755)) {
+                amrex::CreateDirectoryFailed(LevelDir);
+            }
+        }
+        amrex::ParallelDescriptor::Barrier();
+
+        std::string filePrefix = LevelDir;
+        filePrefix += '/';
+        filePrefix += amrex::ParticleContainerBase::DataPrefix();
+        bool groupSets(false), setBuf(true);
+
+        for ( amrex::NFilesIter nfi(nOutFiles, filePrefix, groupSets, setBuf); nfi.ReadyToWrite(); ++nfi)
+        {
+            auto& myStream = (std::ofstream&) nfi.Stream();
+//           pc.WriteParticles(lev, myStream, nfi.FileNumber(), which, count, where,
+//                             write_real_comp, write_int_comp, particle_io_flags, is_checkpoint);
+
+            std::map<int, amrex::Vector<int> > tile_map;
+
+            for (const auto& kv : m_particles_buffer[0][isp]->GetParticles(lev))
+            {
+                const int grid = kv.first.first;
+                const int tile = kv.first.second;
+                tile_map[grid].push_back(tile);
+                const auto flags = particle_io_flags[lev].at(kv.first);
+
+                count[grid] += amrex::particle_detail::countFlags(flags);
+            }
+
+
+            for (amrex::MFIter mfi(state); mfi.isValid(); ++mfi)
+            {
+                const int grid = mfi.index();
+
+                if (count[grid] == 0) continue;
+                amrex::Vector<int> istuff;
+                amrex::Vector<amrex::ParticleReal> rstuff;
+                amrex::particle_detail::packIOData(istuff, rstuff, m_particles_buffer[0][isp].get(), lev, grid,
+                                                   real_flags, int_flags,
+                                                   particle_io_flags, tile_map[grid], count[grid], true);
+
+                amrex::writeIntData(istuff.dataPtr(), istuff.size(), myStream);
+                myStream.flush();
+
+                amrex::writeDoubleData( (double*) rstuff.dataPtr(), rstuff.size(), myStream);
+                myStream.flush();
+            }
+
+        }
+    }
 }
