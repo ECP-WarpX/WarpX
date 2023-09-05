5227d69453831aceae854121509562ea7bebebad
Luca Fedeli <luca.fedeli@cea.fr>
2023-09-05 15:46:18 +0200

    address some of the issues found with clang-tidy

--- Source/ablastr/parallelization/MPIInitHelpers.cpp
+++ Source/ablastr/parallelization/MPIInitHelpers.cpp
@@ -32,18 +32,18 @@ namespace ablastr::parallelization
     int
     mpi_thread_required ()
     {
-        int thread_required = -1;
 #ifdef AMREX_USE_MPI
-        thread_required = MPI_THREAD_SINGLE;  // equiv. to MPI_Init
+        return MPI_THREAD_SINGLE;  // equiv. to MPI_Init
 #   ifdef AMREX_USE_OMP
-        thread_required = MPI_THREAD_FUNNELED;
+        return MPI_THREAD_FUNNELED;
 #   endif
 #   ifdef AMREX_MPI_THREAD_MULTIPLE  // i.e. for async_io
-        thread_required = MPI_THREAD_MULTIPLE;
+        return MPI_THREAD_MULTIPLE;
 #   endif
+#else
+        return -1;
 #endif
-        return thread_required;
-    }
+}
 
     std::pair< int, int >
     mpi_init (int argc, char* argv[])
