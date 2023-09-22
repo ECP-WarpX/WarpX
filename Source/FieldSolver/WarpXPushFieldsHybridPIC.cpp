a8f685551e70a8b20322b71d6a708eff820d215b
roelof-groenewald <regroenewald@gmail.com>
2023-09-22 15:04:09 -0700

    add external current support to the hybrid-PIC solver

--- Source/FieldSolver/WarpXPushFieldsHybridPIC.cpp
+++ Source/FieldSolver/WarpXPushFieldsHybridPIC.cpp
@@ -98,6 +98,7 @@ void WarpX::HybridPICEvolveFields ()
     // momentum equation
     for (int sub_step = 0; sub_step < sub_steps; sub_step++)
     {
+        m_hybrid_pic_model->GetCurrentExternal(m_edge_lengths);
         m_hybrid_pic_model->CalculateCurrentAmpere(Bfield_fp, m_edge_lengths);
         m_hybrid_pic_model->HybridPICSolveE(
             Efield_fp, current_fp_temp, Bfield_fp, rho_fp_temp, m_edge_lengths,
@@ -126,6 +127,7 @@ void WarpX::HybridPICEvolveFields ()
     // Now push the B field from t=n+1/2 to t=n+1 using the n+1/2 quantities
     for (int sub_step = 0; sub_step < sub_steps; sub_step++)
     {
+        m_hybrid_pic_model->GetCurrentExternal(m_edge_lengths);
         m_hybrid_pic_model->CalculateCurrentAmpere(Bfield_fp, m_edge_lengths);
         m_hybrid_pic_model->HybridPICSolveE(
             Efield_fp, current_fp, Bfield_fp, rho_fp_temp, m_edge_lengths,
@@ -158,6 +160,7 @@ void WarpX::HybridPICEvolveFields ()
     m_hybrid_pic_model->CalculateElectronPressure(DtType::Full);
 
     // Update the E field to t=n+1 using the extrapolated J_i^n+1 value
+    m_hybrid_pic_model->GetCurrentExternal(m_edge_lengths);
     m_hybrid_pic_model->CalculateCurrentAmpere(Bfield_fp, m_edge_lengths);
     m_hybrid_pic_model->HybridPICSolveE(
         Efield_fp, current_fp_temp, Bfield_fp, rho_fp, m_edge_lengths,
