5b4dfdcaad210ebb69af1f095aa077e449509cc8
Axel Huebl <axel.huebl@plasma.ninja>
2023-11-02 19:17:09 -0700

    Python 3.12: Refactor out Distutils (#4392)
    * CI: Unconstrain Some Upper Py Versions
    
    * Replace distutils
    
    * Update Docs
    
    * no `clean` commmand
    
    https://github.com/pypa/setuptools/discussions/2838
    
    * Remove `SETUPTOOLS_USE_DISTUTILS`
    
    Old workaround for old mpi4py.
    
    * Update Doc String
    
    * Update CI
    
    * Update Tools/machines/lassen-llnl/install_v100_ml.sh
    
    Co-authored-by: Roelof Groenewald <40245517+roelof-groenewald@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Roelof Groenewald <40245517+roelof-groenewald@users.noreply.github.com>

--- Tools/machines/lassen-llnl/install_v100_dependencies_toss3.sh
+++ Tools/machines/lassen-llnl/install_v100_dependencies_toss3.sh
@@ -117,6 +117,8 @@ python3 -m venv ${SW_DIR}/venvs/warpx-lassen-toss3
 source ${SW_DIR}/venvs/warpx-lassen-toss3/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip cache purge
+python3 -m pip install --upgrade build
+python3 -m pip install --upgrade packaging
 python3 -m pip install --upgrade wheel
 python3 -m pip install --upgrade setuptools
 # Older version for h4py
--- Tools/machines/quartz-llnl/install_dependencies.sh
+++ Tools/machines/quartz-llnl/install_dependencies.sh
@@ -99,6 +99,8 @@ python3 -m venv ${SW_DIR}/venvs/warpx-quartz
 source ${SW_DIR}/venvs/warpx-quartz/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip cache purge
+python3 -m pip install --upgrade build
+python3 -m pip install --upgrade packaging
 python3 -m pip install --upgrade wheel
 python3 -m pip install --upgrade setuptools
 python3 -m pip install --upgrade cython
