a1050ecd24787dfeb1ab8380703aa6b59734bfbf
eyadammak <eya.dammak@ensta-paris.fr>
2023-12-01 16:11:54 -0800

    some changes

--- Examples/Tests/point_of_contact_EB/analysis.py
+++ Examples/Tests/point_of_contact_EB/analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from openpmd_viewer import OpenPMDTimeSeries
 from scipy.optimize import curve_fit
@@ -16,7 +17,7 @@ x,y,z=ts_full.get_particle( ['x','y','z'], species='electron', iteration=it_stam
 print('Coordinates of the point of contact:')
 print('x=%5.3f, y=%5.9f, z=%5.9f' % (x, y, z))
 x_round = round(x, 3)
-y_cut = truncate(y, 3)
+y_cut = float('%5.4f' %y)
 
 assert (x_round!=-0.1983) and (y_cut!=0.0258), 'Test point of contact at EB did not pass'
 
