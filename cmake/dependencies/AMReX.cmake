200a92ae3e61b78c90b9f8bdb93d3d47b25fb3ef
Axel Huebl <axel.huebl@plasma.ninja>
2023-12-12 10:51:48 -0800

    AMReX/pyAMReX/PICSAR: Weekly Update (#4503)
    * AMReX: Weekly Update
    
    * pyAMReX: Weekly Update

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -269,7 +269,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "edb4c25027efbbc465c88d453441dcd7115d8651"
+set(WarpX_amrex_branch "ecaa46d0be4b5c79b8806e48e3469000d8bb7252"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
