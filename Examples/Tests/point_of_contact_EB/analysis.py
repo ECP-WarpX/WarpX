6df7c1482beb53a2c1c524d54774cfd1288eafcc
eyadammak <eya.dammak@ensta-paris.fr>
2023-12-19 15:47:18 -0800

    addind checkout + change of some details

--- Examples/Tests/point_of_contact_EB/analysis.py
+++ Examples/Tests/point_of_contact_EB/analysis.py
@@ -3,16 +3,32 @@ import numpy as np
 from openpmd_viewer import OpenPMDTimeSeries
 from scipy.optimize import curve_fit
 
+import os
+import sys
+import yt
+
+yt.funcs.mylog.setLevel(0)
+sys.path.insert(1, '../../../../warpx/Regression/Checksum/')
+import checksumAPI
+
+# Open plotfile specified in command line
+filename = sys.argv[1]
+ds = yt.load( filename )
+test_name = os.path.split(os.getcwd())[1]
+checksumAPI.evaluate_checksum(test_name, filename)
 
 ts_scraping = OpenPMDTimeSeries('../../../../warpx/Examples/Tests/point_of_contact_EB/diags/diag3/particles_at_eb/')
-x,y,z=ts_scraping.get_particle( ['x','y','z'], species='electron', iteration=it_stamp )
+
+
+it=ts_scraping.iterations
+x,y,z=ts_scraping.get_particle( ['x','y','z'], species='electron', iteration=it )
 
 x_analytic=-0.1983
 y_analytic=0.02584
 z_analytic=0.0000
 
 print('NUMERICAL coordinates of the point of contact:')
-print('x=%5.4f, y=%5.4f, z=%5.4f' % (x, y, z))
+print('x=%5.4f, y=%5.4f, z=%5.4f' % (x[0], y[0], z[0]))
 print('\n')
 print('ANALYTICAL coordinates of the point of contact:')
 print('x=%5.4f, y=%5.4f, z=%5.4f' % (x_analytic, y_analytic, z_analytic))
@@ -20,14 +36,10 @@ print('x=%5.4f, y=%5.4f, z=%5.4f' % (x_analytic, y_analytic, z_analytic))
 tolerance=0.001
 print("tolerance = "+ str(tolerance *100) + '%')
 
-diff_x=np.abs((x-x_analytic)/x_analytic)
-diff_y=np.abs((y-y_analytic)/y_analytic)
-diff_z=np.abs((z-z_analytic)/z_analytic)
+diff_x=np.abs((x[0]-x_analytic)/x_analytic)
+diff_y=np.abs((y[0]-y_analytic)/y_analytic)
 
-print("pourcentage error for x = "+ str(diff_x *100) + '%')
-print("pourcentage error for y = "+ str(diff_y*100) + '%')
-print("pourcentage error for z = "+ str(diff_z*100) + '%')
+print("percentage error for x = %5.4f %%" %(diff_x *100))
+print("percentage error for y = %5.4f %%" %(diff_y *100))
 
-assert (diff_x < tolerance) and (diff_y < tolerance) and (diff_z < tolerance), 'Test point_of_contact did not pass'
-
-
+assert (diff_x < tolerance) and (diff_y < tolerance) , 'Test point_of_contact did not pass'
