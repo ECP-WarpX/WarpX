2c4d125439b7a535806cb54cd40b7226fb20c044
eyadammak <eya.dammak@ensta-paris.fr>
2023-12-19 10:05:44 -0800

    adding a test for a tolerance of 0.1%

--- Examples/Tests/point_of_contact_EB/analysis.py
+++ Examples/Tests/point_of_contact_EB/analysis.py
@@ -1,26 +1,33 @@
-<<<<<<< HEAD
-import math
-
-import numpy as np
-
-=======
->>>>>>> ef1e962595decf0b2be4c24004b44886094e2cc5
 import matplotlib.pyplot as plt
 import numpy as np
 from openpmd_viewer import OpenPMDTimeSeries
 from scipy.optimize import curve_fit
 
-ts_scraping = OpenPMDTimeSeries('../../../../warpx/Examples/Physics_applications/point_of_contact_EB/diags/diag3/particles_at_eb/')
-ts_full = OpenPMDTimeSeries('../../../../warpx/Examples/Physics_applications/point_of_contact_EB/diags/diag2/')
-it_stamp=ts_scraping.get_particle( ['timestamp'], species='electron' )
-x,y,z=ts_full.get_particle( ['x','y','z'], species='electron', iteration=it_stamp )
+
+ts_scraping = OpenPMDTimeSeries('../../../../warpx/Examples/Tests/point_of_contact_EB/diags/diag3/particles_at_eb/')
+x,y,z=ts_scraping.get_particle( ['x','y','z'], species='electron', iteration=it_stamp )
+
+x_analytic=-0.1983
+y_analytic=0.02584
+z_analytic=0.0000
+
+print('NUMERICAL coordinates of the point of contact:')
+print('x=%5.4f, y=%5.4f, z=%5.4f' % (x, y, z))
+print('\n')
+print('ANALYTICAL coordinates of the point of contact:')
+print('x=%5.4f, y=%5.4f, z=%5.4f' % (x_analytic, y_analytic, z_analytic))
+
+tolerance=0.001
+print("tolerance = "+ str(tolerance *100) + '%')
+
+diff_x=np.abs((x-x_analytic)/x_analytic)
+diff_y=np.abs((y-y_analytic)/y_analytic)
+diff_z=np.abs((z-z_analytic)/z_analytic)
+
+print("pourcentage error for x = "+ str(diff_x *100) + '%')
+print("pourcentage error for y = "+ str(diff_y*100) + '%')
+print("pourcentage error for z = "+ str(diff_z*100) + '%')
+
+assert (diff_x < tolerance) and (diff_y < tolerance) and (diff_z < tolerance), 'Test point_of_contact did not pass'
 
 
-
-
-print('Coordinates of the point of contact:')
-print('x=%5.3f, y=%5.9f, z=%5.9f' % (x, y, z))
-x_round = round(x, 3)
-y_cut = float('%5.4f' %y)
-
-assert (x_round!=-0.1983) and (y_cut!=0.0258), 'Test point of contact at EB did not pass'
