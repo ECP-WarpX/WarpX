edac726a8dbc2953fe336d68d672388ab6aa96bf
Rickard Holmberg <rho@jfpower.se>
2024-01-24 11:55:55 +0100

    Remove cartesian external openpmd fields

--- Source/Particles/Gather/GetExternalFields.cpp
+++ Source/Particles/Gather/GetExternalFields.cpp
@@ -100,51 +100,7 @@ GetExternalEBField::GetExternalEBField (const WarpXParIter& a_pti, long a_offset
         auto axisLabels = F.getAttribute("axisLabels").get<std::vector<std::string>>();
         auto fileGeom = F.getAttribute("geometry").get<std::string>();
 
-        if (fileGeom == "cartesian")
-        {
-            WARPX_ALWAYS_ASSERT_WITH_MESSAGE(axisLabels[0] == "x" && axisLabels[1] == "y" && axisLabels[2] == "z",
-                                            "3D expects axisLabels {x, y, z}");
-
-            const auto offset = F.gridGlobalOffset();
-            const auto gridSpacing = F.gridSpacing<long double>();
-            auto FCx = F["x"];
-            auto FCy = F["y"];
-            auto FCz = F["z"];
-            const auto extent = FCx.getExtent();
-
-            const openPMD::Offset chunk_offset = {0,0,0};
-            const openPMD::Extent chunk_extent = {extent[0], extent[1], extent[2]};
-
-            auto FCx_chunk_data = FCx.loadChunk<amrex::Real>(chunk_offset,chunk_extent);
-            auto FCy_chunk_data = FCy.loadChunk<amrex::Real>(chunk_offset,chunk_extent);
-            auto FCz_chunk_data = FCz.loadChunk<amrex::Real>(chunk_offset,chunk_extent);
-            series.flush();
-            auto FCx_data_host = FCx_chunk_data.get();
-            auto FCy_data_host = FCy_chunk_data.get();
-            auto FCz_data_host = FCz_chunk_data.get();
-            const size_t total_extent = size_t(extent[0]) * extent[1] * extent[2];
-            amrex::Gpu::DeviceVector<amrex::Real> FCx_data_gpu(total_extent);
-            amrex::Gpu::DeviceVector<amrex::Real> FCy_data_gpu(total_extent);
-            amrex::Gpu::DeviceVector<amrex::Real> FCz_data_gpu(total_extent);
-
-            auto FCx_data = FCx_data_gpu.data();
-            auto FCy_data = FCy_data_gpu.data();
-            auto FCz_data = FCz_data_gpu.data();
-
-            amrex::Gpu::copy(amrex::Gpu::hostToDevice, FCx_data_host, FCx_data_host + total_extent, FCx_data);
-            amrex::Gpu::copy(amrex::Gpu::hostToDevice, FCy_data_host, FCy_data_host + total_extent, FCy_data);
-            amrex::Gpu::copy(amrex::Gpu::hostToDevice, FCz_data_host, FCz_data_host + total_extent, FCz_data);
-
-            const amrex::Array4<amrex::Real> fcx_array(FCx_data, {0,0,0}, {extent[2], extent[1], extent[0]}, 1);
-            const amrex::Array4<amrex::Real> fcy_array(FCy_data, {0,0,0}, {extent[2], extent[1], extent[0]}, 1);
-            const amrex::Array4<amrex::Real> fcz_array(FCz_data, {0,0,0}, {extent[2], extent[1], extent[0]}, 1);
-
-            Bfield_file_external_particle_cart = new ExternalFieldFromFile3DCart(
-                amrex::RealVect {gridSpacing[0], gridSpacing[1], gridSpacing[2] }, 
-                amrex::RealVect {offset[0], offset[1], offset[2] }, 
-                fcx_array, fcy_array, fcz_array
-            );
-        } else if (fileGeom == "thetaMode")
+    if (fileGeom == "thetaMode")
         {
             WARPX_ALWAYS_ASSERT_WITH_MESSAGE(axisLabels[0] == "r" && axisLabels[1] == "z",
                                             "RZ expects axisLabels {r, z}");
@@ -182,7 +138,7 @@ GetExternalEBField::GetExternalEBField (const WarpXParIter& a_pti, long a_offset
                 fcr_array, fcz_array
             );
         } else {
-            WARPX_ALWAYS_ASSERT_WITH_MESSAGE(false, "3D can only read from files with cartesian or thetaMode geometry");
+            WARPX_ALWAYS_ASSERT_WITH_MESSAGE(false, "3D can only read external fields from files with thetaMode geometry");
         }
     }
 #endif
