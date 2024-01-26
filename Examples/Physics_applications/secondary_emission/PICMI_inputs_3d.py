20c1a932c764f827705849890021754fdbc3d428
eyadammak <eya.dammak@ensta-paris.fr>
2024-01-26 11:52:05 -0800

    adding a 3d case

---
+++ Examples/Physics_applications/secondary_emission/PICMI_inputs_3d.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+import numpy as np
+
+from pywarpx import callbacks, libwarpx, particle_containers, picmi
+
+# Create the parser and add the argument
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-u', '--unique', action='store_true',
+    help="Whether injected particles should be treated as unique"
+)
+
+# Parse the input
+args, left = parser.parse_known_args()
+sys.argv = sys.argv[:1] + left
+
+##########################
+# numerics parameters
+##########################
+
+dt = 1.0e-11
+
+# --- Nb time steps
+
+max_steps = 100
+diagnostic_interval = 10
+
+# --- grid
+
+nx = 64
+ny= 64
+nz=64
+
+xmin = -3.0
+xmax = 3.0
+ymin = -3.0
+ymax = 3.0
+zmin = -3.0
+zmax = 3.0
+
+##########################
+# numerics components
+##########################
+
+grid = picmi.Cartesian3DGrid(
+    number_of_cells = [nx, ny, nz],
+    lower_bound = [xmin, ymin, zmin],
+    upper_bound = [xmax, ymax, zmax],
+    lower_boundary_conditions = ['dirichlet', 'dirichlet', 'dirichlet'],
+    upper_boundary_conditions =  ['dirichlet', 'dirichlet', 'dirichlet'],
+    lower_boundary_conditions_particles = ['reflecting', 'reflecting', 'reflecting'],
+    upper_boundary_conditions_particles =  ['reflecting', 'reflecting', 'reflecting'],
+)
+
+
+solver = picmi.ElectrostaticSolver(
+    grid=grid, method='Multigrid',
+    warpx_absolute_tolerance=1e-7
+)
+
+embedded_boundary = picmi.EmbeddedBoundary(
+    implicit_function="-(x**2+y**2+z**2-radius**2)",
+    radius = 0.3277
+)
+
+##########################
+# physics components
+##########################
+
+
+n = 7.0e9 #plasma density #particles/m^3
+Te = 85 #Electron temp in eV
+qe = picmi.constants.q_e #elementary charge
+m_e = picmi.constants.m_e #electron mass
+v_eth = (qe * Te / m_e) ** 0.5
+
+# nothing to change in the distribution function?
+e_dist = picmi.UniformDistribution(density = n, rms_velocity=[v_eth, v_eth, v_eth] )
+
+electrons = picmi.Species(
+    particle_type='electron', name='electrons', initial_distribution=e_dist, warpx_save_particles_at_eb=1
+)
+
+##########################
+# diagnostics
+##########################
+
+part_diag = picmi.ParticleDiagnostic(name = 'diag1',
+    period = diagnostic_interval,
+    species = [electrons],
+    warpx_format = 'openpmd'
+)
+
+##########################
+# simulation setup
+##########################
+
+sim = picmi.Simulation(
+    solver=solver,
+    time_step_size = dt,
+    max_steps = max_steps,
+    warpx_embedded_boundary=embedded_boundary,
+    warpx_amrex_the_arena_is_managed=1,
+)
+
+sim.add_species(
+    electrons,
+    layout = picmi.GriddedLayout(
+        n_macroparticle_per_cell=[10, 1, 1], grid=grid
+    )
+)
+sim.add_diagnostic(part_diag)
+
+
+sim.initialize_inputs()
+sim.initialize_warpx()
+
+
+##########################
+# python particle data access
+##########################
+
+#angle=
+#damping_coef=
+#number=
+
+
+
+#nb_steps_scraping=0
+def mirror_reflection():
+    #global nb_steps_scraping
+    buffer = particle_containers.ParticleBoundaryBufferWrapper()
+    if (len(buffer.get_particle_boundary_buffer("electrons", 'eb', 'step_scraped', 0))!=0): #otherwise np.concatenate doesnt work
+        time_steps = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'step_scraped', 0))
+        #np_steps_scraping+=len(time_steps)
+        #step 1: extract the different parameters of the scraping buffer (normal, time, position)
+        x = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'x', 0)) 
+        y = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'y', 0)) 
+        z = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'z', 0))
+        ux = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'ux', 0))
+        uy = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'uy', 0))
+        uz = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'uz', 0))
+        w = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'w', 0))
+        nx = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'nx', 0))
+        ny = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'ny', 0))
+        nz = np.concatenate(buffer.get_particle_boundary_buffer("electrons", 'eb', 'nz', 0))
+
+        #step 2: use these parameters to inject from the same position electrons in the plasma
+        elect_pc = particle_containers.ParticleContainerWrapper('electrons')
+   
+        un=ux*nx+uy*ny+uz*nz
+        ux_reflect=2*un*nx-ux
+        uy_reflect=2*un*ny-uy
+        uz_reflect=2*un*nz-uz
+
+        elect_pc.add_particles(
+            #for a "mirror reflection" u(sym)=2(u.n)n-u
+            x=x, y=y, z=z, ux=ux_reflect, uy=uy_reflect, uz=uz_reflect,
+            w=w,
+            unique_particles=args.unique
+            )
+    
+        buffer.clear_buffer()
+
+
+callbacks.installafterstep(mirror_reflection)
+
+##########################
+# simulation run
+##########################
+
+sim.step(max_steps)
