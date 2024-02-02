2e0aec3a0d94622d870d8e75be236edc521f2e6c
Axel Huebl <axel.huebl@plasma.ninja>
2024-02-01 20:33:15 -0800

    AMReX & pyAMReX: Latest `development`
    More pure SoA and id handling goodness.

--- cmake/dependencies/AMReX.cmake
+++ cmake/dependencies/AMReX.cmake
@@ -269,7 +269,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "24.02"
+set(WarpX_amrex_branch "296ed40e16ae1877640f5b78e9162dbd4ba1c279"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 
