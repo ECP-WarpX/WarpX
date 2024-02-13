43130f96a59c61ee1721ec1b2e5a6fce41ec8afa
Eya D <81635404+EyaDammak@users.noreply.github.com>
2024-02-13 13:04:35 -0800

    Update ParticleBoundaryBuffer.cpp

--- Source/Particles/ParticleBoundaryBuffer.cpp
+++ Source/Particles/ParticleBoundaryBuffer.cpp
@@ -378,7 +378,7 @@ void ParticleBoundaryBuffer::gatherParticles (MultiParticleContainer& mypc,
 
                           amrex::filterAndTransformParticles(ptile_buffer, ptile,
                                                              predicate,
-                                                             CopyAndTimestamp{step_scraped_index, timestep, dt},
+                                                             CopyAndTimestamp{timestamp_index, timestep, dt},
                                                              0, dst_index);
                         }
                     }
