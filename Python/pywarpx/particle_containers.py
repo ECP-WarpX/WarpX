189786baba42e7c7945ca85d4511ba0ac4e5507c
Eya D <81635404+EyaDammak@users.noreply.github.com>
2024-02-15 13:00:35 -0800

    Update particle_containers.py

--- Python/pywarpx/particle_containers.py
+++ Python/pywarpx/particle_containers.py
@@ -772,9 +772,9 @@ class ParticleBoundaryBufferWrapper(object):
                 form x/y/z_hi/lo or eb.
 
             comp_name      : str
-                The component of the array data that will be returned.
-                "x", "y", "z", "ux", "uy", "uz", "w"
-                "step_scraped","time_scraped", "nx", "ny", "nz"
+                The component of the array data that will be returned. If
+                "step_scraped" the special attribute holding the timestep at
+                which a particle was scraped will be returned.
 
             level          : int
                 Which AMR level to retrieve scraped particle data from.
@@ -784,9 +784,20 @@ class ParticleBoundaryBufferWrapper(object):
         part_container = self.particle_buffer.get_particle_container(
             species_name, self._get_boundary_number(boundary)
         )
-        comp = part_container.int_comp_names[comp_name]
-        comp = part_container.real_comp_names[comp_name]
-        return comp
+        data_array = []
+        if comp_name == 'step_scraped':
+            # the step scraped is always the final integer component
+            comp_idx = part_container.num_int_comps - 1
+            for ii, pti in enumerate(libwarpx.libwarpx_so.BoundaryBufferParIter(part_container, level)):
+                soa = pti.soa()
+                data_array.append(xp.array(soa.get_int_data(comp_idx), copy=False))
+        else:
+            container_wrapper = ParticleContainerWrapper(species_name)
+            comp_idx = container_wrapper.particle_container.get_comp_index(comp_name)
+            for ii, pti in enumerate(libwarpx.libwarpx_so.BoundaryBufferParIter(part_container, level)):
+                soa = pti.soa()
+                data_array.append(xp.array(soa.get_real_data(comp_idx), copy=False))
+        return data_array
 
 
     def clear_buffer(self):
