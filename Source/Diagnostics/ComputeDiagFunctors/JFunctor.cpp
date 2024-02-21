70cc5093452c840a0c799064815bfe5ffc4c9641
Luca Fedeli <luca.fedeli@cea.fr>
2024-02-21 10:44:42 +0100

    introduce AMREX_ASSUME

--- Source/Diagnostics/ComputeDiagFunctors/JFunctor.cpp
+++ Source/Diagnostics/ComputeDiagFunctors/JFunctor.cpp
@@ -10,6 +10,7 @@
 #include "WarpX.H"
 
 #include <AMReX.H>
+#include <AMReX_Extension.H>
 #include <AMReX_IntVect.H>
 #include <AMReX_MultiFab.H>
 
@@ -60,12 +61,10 @@ JFunctor::operator() (amrex::MultiFab& mf_dst, int dcomp, const int /*i_buffer*/
         }
     }
 
-    if (m_mf_src != nullptr){
-        InterpolateMFForDiag(
-            mf_dst, *m_mf_src, dcomp,
-            warpx.DistributionMap(m_lev), m_convertRZmodes2cartesian);
-    }
-    else{
-        WARPX_ABORT_WITH_MESSAGE("m_mf_src can't be a nullptr.");
-    }
+    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(m_mf_src != nullptr, "m_mf_src can't be a nullptr.");
+    AMREX_ASSUME(m_mf_src != nullptr);
+
+    InterpolateMFForDiag(
+        mf_dst, *m_mf_src, dcomp,
+        warpx.DistributionMap(m_lev), m_convertRZmodes2cartesian);
 }
