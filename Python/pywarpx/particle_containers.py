9a017a67e5495263223da42db47657693b25bbd2
Eya D <81635404+EyaDammak@users.noreply.github.com>
2024-02-23 21:55:45 -0800

    Update of the time saved for scraped particles on the boundaries (#4710)
    * modifications of the names and change of timeScraped into delta
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ParticleBoundaryBuffer.cpp
    
    * Update analysis.py
    
    * updates
    
    * Update parameters.rst
    
    * Update parameters.rst
    
    * Update parameters.rst
    
    * Update parameters.rst
    
    * Update parameters.rst
    
    * Update parameters.rst
    
    * Update parameters.rst
    
    * Update analysis.py
    
    * Update parameters.rst
    
    * Update particle_containers.py
    
    * Update ParticleBoundaryBuffer.cpp
    
    * Update analysis.py
    
    * Update ParticleBoundaryBuffer.cpp
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ParticleBoundaryBuffer.cpp
    
    * Update ParticleBoundaryBuffer.cpp
    
    * update name
    
    * Update Source/Particles/ParticleBoundaryBuffer.cpp
    
    Co-authored-by: Revathi  Jambunathan <41089244+RevathiJambunathan@users.noreply.github.com>
    
    * Update Source/Particles/ParticleBoundaryBuffer.cpp
    
    Co-authored-by: Revathi  Jambunathan <41089244+RevathiJambunathan@users.noreply.github.com>
    
    * Update ParticleBoundaryBuffer.cpp
    
    * Update ParticleBoundaryBuffer.cpp
    
    * Update Docs/source/usage/parameters.rst
    
    Co-authored-by: Remi Lehe <remi.lehe@normalesup.org>
    
    * Update analysis.py
    
    * Update analysis.py
    
    * Update particle_containers.py
    
    * Update particle_containers.py
    
    * Update analysis.py
    
    * Update analysis.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Revathi  Jambunathan <41089244+RevathiJambunathan@users.noreply.github.com>
    Co-authored-by: Remi Lehe <remi.lehe@normalesup.org>

--- Python/pywarpx/particle_containers.py
+++ Python/pywarpx/particle_containers.py
@@ -774,7 +774,7 @@ class ParticleBoundaryBufferWrapper(object):
             comp_name      : str
                 The component of the array data that will be returned.
                 "x", "y", "z", "ux", "uy", "uz", "w"
-                "step_scraped","time_scraped",
+                "stepScraped","deltaTimeScraped",
                 if boundary='eb': "nx", "ny", "nz"
 
             level          : int
