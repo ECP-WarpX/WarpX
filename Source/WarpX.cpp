766a521bea1a840b9ff25ef328dd971dfaa0f9f5
Roelof Groenewald <40245517+roelof-groenewald@users.noreply.github.com>
2024-03-05 16:24:23 -0800

    Enable `WarpX::ComputeEdgeLengths` in RZ (#4749)
    * Enable `WarpX::ComputeEdgeLengths` in RZ
    
    * Update exit message when using EBs in RZ with fully electromagnetic solvers.

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -752,8 +752,9 @@ WarpX::ReadParameters ()
 
 #if defined(AMREX_USE_EB) && defined(WARPX_DIM_RZ)
         WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
-        electromagnetic_solver_id==ElectromagneticSolverAlgo::None,
-        "Currently, the embedded boundary in RZ only works for electrostatic solvers (or no solver).");
+        electromagnetic_solver_id==ElectromagneticSolverAlgo::None
+        || electromagnetic_solver_id==ElectromagneticSolverAlgo::HybridPIC,
+        "Currently, the embedded boundary in RZ only works for electrostatic solvers, the Ohm's law solver or with no solver installed.");
 #endif
 
         if (electrostatic_solver_id == ElectrostaticSolverAlgo::LabFrame ||
