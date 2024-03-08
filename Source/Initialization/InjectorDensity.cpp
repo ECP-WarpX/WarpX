40702de9d0a085c9a2b6759f9a2a8b5aa3db3c8f
Sarah Vickers <svickers@lbl.gov>
2024-03-08 13:49:57 -0800

    works but doesn't use interpolation or paralleleization

--- Source/Initialization/InjectorDensity.cpp
+++ Source/Initialization/InjectorDensity.cpp
@@ -174,40 +174,31 @@ InjectorDensityFromFile::InjectorDensityFromFile (std::string const & a_species_
 
     // Read external field openPMD data
     io::Series series = io::Series(external_density_path, io::Access::READ_ONLY);
-    io::Iteration i0  = series.iterations[0];
+    //io::Iteration i0  = series.iterations[0];
     //io::Iteration i1  = series.iterations[1];
-    //auto iseries = series.iterations.begin()->second;
+    auto iseries = series.iterations.begin()->second;
 
-    io::Mesh P = i0.meshes[density];
+    io::Mesh P = iseries.meshes[density];
     io::MeshRecordComponent p_scalar0 = P[io::RecordComponent::SCALAR];
     auto all_data0 = p_scalar0.loadChunk<double>();
 
     auto axisLabels = P.getAttribute("axisLabels").get<std::vector<std::string>>();
     auto fileGeom = P.getAttribute("geometry").get<std::string>();
 
-    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(P.getAttribute("dataOrder").get<std::string>() == "C",
-                                     "Reading from files with non-C dataOrder is not implemented");
-
-//    #if defined(WARPX_DIM_3D)
-//    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(fileGeom == "cartesian", "3D can only read from files with cartesian geometry");
-//    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(axisLabels[0] == "x" && axisLabels[1] == "y" && axisLabels[2] == "z",
-//                                     "3D expects axisLabels {x, y, z}");
-//    #endif
-
     std::cout << "all_data size is : " << sizeof(all_data0);
 
-    io::ParticleSpecies particle = i0.particles[a_species_name];
-    std::shared_ptr<double> charge = particle["charge"][openPMD::RecordComponent::SCALAR].loadChunk<double>();
+    io::ParticleSpecies particle = iseries.particles[a_species_name];
+    charge = particle["charge"][openPMD::RecordComponent::SCALAR].loadChunk<double>();
     std::cout << "[5] electron charge is " << charge << '\n';
     //series.flush();
 
     //const auto fileGeom = P.getAttribute("geometry").get< std::string >();
-    const std::vector<double> offset = P.gridGlobalOffset();
+    offset = P.gridGlobalOffset();
     const auto offset0 = static_cast<amrex::Real>(offset[0]);
     const auto offset1 = static_cast<amrex::Real>(offset[1]);
     const auto offset2 = static_cast<amrex::Real>(offset[2]);
     spacing = P.gridSpacing< long double >();
-    const std::vector<std::uint64_t> extent = p_scalar0.getExtent();
+    extent = p_scalar0.getExtent();
     const int extent0 = static_cast<int>(extent[0]);
     const int extent1 = static_cast<int>(extent[1]);
     const int extent2 = static_cast<int>(extent[2]);
@@ -219,44 +210,10 @@ InjectorDensityFromFile::InjectorDensityFromFile (std::string const & a_species_
     const long double file_dy = static_cast<amrex::Real>(spacing[1]);
     const long double file_dz = static_cast<amrex::Real>(spacing[2]);
 
-//    std::cout << "Full E/x starts with:\n\t{";
-//    for (size_t col = 0; col < extent[0] && col < 10; ++col)
-//        std::cout << all_data0.get()[col] << ", ";
-//    std::cout << "...}\n";
-//    for (size_t col = 0; col < extent[1] && col < 10; ++col)
-//        std::cout << all_data0.get()[col] << ", ";
-//    std::cout << "...}\n";
-//    for (size_t col = 0; col < extent[2] && col < 10; ++col)
-//        std::cout << all_data0.get()[col] << ", ";
-//    std::cout << "...}\n";
-
-      //std::cout << all_data0.get()[1] << '\n';
-
-//    for (size_t row = 0; row < extent0 && row < 5;  ++row){
-//        for (size_t depth = 0; depth < extent2 && depth < 5; ++depth) {
-//            for (size_t col = 0; col < extent1 && col < 5; ++col) {
-//                std::cout << "for (" << row << ", " << col << ", " << depth << ") we have "
-//                          << all_data0.get()[col * row * depth] /charge.get()[0] << "\n";
-//            }
-//        }
-//    }
-
-//    std::cout << "Full E/x1 starts with:\n\t{";
-//    for (size_t col = 0; col < extent[0] && col < 10; ++col)
-//        std::cout << all_data1.get()[col] << ", ";
-//    std::cout << "...}\n";
-//    for (size_t col = 0; col < extent[1] && col < 10; ++col)
-//        std::cout << all_data1.get()[col] << ", ";
-//    std::cout << "...}\n";
-//    for (size_t col = 0; col < extent[2] && col < 10; ++col)
-//        std::cout << all_data1.get()[col] << ", ";
-//    std::cout << "...}\n";
-
     const io::Offset chunk_offset =  {0, 0, 0};  //{0,0,0};
     const io::Extent chunk_extent = {extent[0], extent[1], extent[2]};
-    //std::cout << chunk_extent[0] << " " << chunk_extent[1] << " " << chunk_extent[2] << '\n';
 
-    auto P_chunk_data = p_scalar0.loadChunk<double>(chunk_offset,chunk_extent);
+    P_chunk_data = p_scalar0.loadChunk<double>(chunk_offset,chunk_extent);
     series.flush();
     auto *P_data_host = P_chunk_data.get();
 
@@ -265,24 +222,33 @@ InjectorDensityFromFile::InjectorDensityFromFile (std::string const & a_species_
     amrex::Gpu::DeviceVector<double> P_data_gpu(total_extent);
     double *P_data = P_data_gpu.data();
     amrex::Gpu::copy(amrex::Gpu::hostToDevice, P_data_host, P_data_host + total_extent, P_data);
+//
+//    for (size_t row = 0; row < chunk_extent[0] && row < 5; ++row)
+//    {
+//        for (size_t col = 0; col < chunk_extent[1] && col < 5; ++col) {
+//            for (size_t depth = 0; depth < chunk_extent[2] && depth < 5; ++depth) {
+//                std::cout << "\t" << '(' << row + chunk_offset[0] << '|'
+//                          << col + chunk_offset[1] << '|' << depth + chunk_offset[2] << ")\t"
+//                          << P_chunk_data.get()[row * col * depth] << ")\t" << all_data0.get()[row * col * depth];
+//            }
+//        }
+//        std::cout << '\n';
+//    }
+//
+//
+//    for (int i = 0; i <= extent0; ++i){
+//
+//        for (int j = 0; j<= extent1; ++j){
+//
+//            for (int )
+//        }
+//    }
 
-    //std::cout << P_data_host[1] << '\n';
-    std::cout << " size of P-data is : " << sizeof(P_data) << '\n';
 
-    std::cout << "[6] P_chunk_data is :" << '\n';
-    for (size_t row = 0; row < chunk_extent[0] && row < 5; ++row)
-    {
-        for (size_t col = 0; col < chunk_extent[1] && col < 5; ++col) {
-            for (size_t depth = 0; depth < chunk_extent[2] && depth < 5; ++depth) {
-                std::cout << "\t" << '(' << row + chunk_offset[0] << '|'
-                          << col + chunk_offset[1] << '|' << depth + chunk_offset[2] << ")\t"
-                          << P_chunk_data.get()[row * col * depth] << ")\t" << all_data0.get()[row * col * depth];
-            }
-        }
-        std::cout << '\n';
-    }
 
-   //const amrex::Array4<double> fc_array(P_data, {0,0,0}, {extent0, extent1, extent2}, 1);
+
+
+    const amrex::Array4<double> fc_array(P_data, {0,0,0}, {extent0, extent1, extent2}, 1);
 
 
     // iterate over the external field and find the index that corresponds to the grid points
@@ -293,13 +259,10 @@ InjectorDensityFromFile::InjectorDensityFromFile (std::string const & a_species_
         //const amrex::Array4<double> mffab = mf->array(mfi);
         mffab = mf.array(mfi);
 
-        //std::cout << " inside for loop" << '\n';
-
         amrex::ParallelFor (tb,
-            [box, this, dx, offset, file_dx, file_dy, file_dz, P_data, extent0, extent1, extent2, charge] AMREX_GPU_DEVICE (int i, int j, int k) {
+            [box, this, dx,file_dx, file_dy, file_dz, P_data, extent0, extent1, extent2] AMREX_GPU_DEVICE (int i, int j, int k) {
                 // i,j,k denote x,y,z indices in 3D xyz.
                 // i,j denote r,z indices in 2D rz; k is just 0
-                //std::cout << " inside parallel for" << '\n';
 
                 //for 3D only!!
                 const int ii = i;
@@ -345,36 +308,17 @@ InjectorDensityFromFile::InjectorDensityFromFile (std::string const & a_species_
                      f000, f001, f010, f011, f100, f101, f110, f111,
                      x0, x1, x2)/charge.get()[0]);
 //#endif
-                //std::cout << fc_array << '\n';
-                //std::cout << "charge of the particle is " << charge << '\n';
-                //double d = fc_array(iz, iy, ix) / charge.get()[0];
-                //double d = fc_array(ix, iy, iz) / charge.get()[0];
-//                double d = fc_array(i, j, k) / charge.get()[0];
-//                mffab(i,j,k) = static_cast<amrex::Real>(utils::algorithms::bilinear_interp<double>
-//                        (xx0, xx0+file_dr, xx1, xx1+file_dz,
-//                         f00, f01, f10, f11,
-//                         x0, x1));
-//                double test = fc_array(iz, iy, ix);
-//                if (ix < 1 && iy < 1 && iz < extent2) {
-//                    std::cout << "for (" << ix << ", " << iy << ", " << iz << ") we have " << test << '\n';
-//                }
-
-
-                //mffab(i,j,k) = static_cast<amrex::Real> (d);
-                //std::cout << mffab(i,j,k) << "  ";
-//                mffab(i,j,k) = static_cast<amrex::Real> (3.3*10**26)
 
         }
         ); //end ParallelFor
     }
 
     std::cout << "extent0, extent1, extent2 is : ( " << extent0 << ", " << extent1 << ", " << extent2  << " )" << '\n';
-    //std::cout << "size of mffab is : " << sizeof(mffab) << "and size of fc_array is : " << sizeof(fc_array) << '\n';
     for (size_t row = 0; row < extent0 && row < 4; ++row) {
         for (size_t col = 0; col < extent1 && col < 4; ++col) {
             for (size_t depth = 0; depth < extent2 && depth < 8; ++depth) {
-                std::cout << "for (" << row << ", " << col << ", " << depth << ") we have mffab: " << mffab(row, col, depth)
-                        << " and for all_data: " << all_data0.get()[row * col * depth] /charge.get()[0]<< '\n';
+                std::cout << "for (" << row << ", " << col << ", " << depth << ") we have mffab: " << mffab(row, col, depth) << " and for fc_array: " << fc_array(row, col, depth) <<
+                        " and for chunk_data: " << P_chunk_data.get()[(row*extent1*extent2) + col*extent2 + depth] /charge.get()[0]<< "and for all_data: " << all_data0.get()[(row*extent1*extent2) + col*extent2 + depth] /charge.get()[0] <<'\n';
             }
         }
     }
