6212cbcd4f29af29fe30a86f7d316d1c4bf87858
Remi Lehe <remi.lehe@normalesup.org>
2024-03-30 06:09:25 -0700

    Update Examples/Tests/particle_thermal_boundary/analysis_2d.py

--- Examples/Tests/particle_thermal_boundary/analysis_2d.py
+++ Examples/Tests/particle_thermal_boundary/analysis_2d.py
@@ -32,6 +32,6 @@ PE_rdiag = './diags/reducedfiles/EN.txt'
 init_Penergy = np.loadtxt(PE_rdiag)[0,2]
 final_Penergy = np.loadtxt(PE_rdiag)[-1,2]
 assert( abs(final_Penergy - init_Penergy)/init_Penergy < 0.02)
-
+filename = sys.argv[1]
 test_name = os.path.split(os.getcwd())[1]
 checksumAPI.evaluate_checksum(test_name, filename)
