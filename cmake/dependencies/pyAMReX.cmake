a91cf05a41cd0561417bdcc51f8b45643f74aa9c
Axel Huebl <axel.huebl@plasma.ninja>
2024-04-08 09:13:00 -0700

    pyAMReX: Weekly Update

--- cmake/dependencies/pyAMReX.cmake
+++ cmake/dependencies/pyAMReX.cmake
@@ -79,7 +79,7 @@ option(WarpX_pyamrex_internal "Download & build pyAMReX" ON)
 set(WarpX_pyamrex_repo "https://github.com/AMReX-Codes/pyamrex.git"
     CACHE STRING
     "Repository URI to pull and build pyamrex from if(WarpX_pyamrex_internal)")
-set(WarpX_pyamrex_branch "24.04"
+set(WarpX_pyamrex_branch "9b0a4d82bc14756530efe4e0c9ce3463ca5df742"
     CACHE STRING
     "Repository branch for WarpX_pyamrex_repo if(WarpX_pyamrex_internal)")
 
