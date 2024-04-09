9033f309496974a7421218ed9de71e04f3591467
Remi Lehe <remi.lehe@normalesup.org>
2024-04-09 12:39:03 -0700

    Apply suggestions from code review

--- Source/Parallelization/WarpXSumGuardCells.cpp
+++ Source/Parallelization/WarpXSumGuardCells.cpp
@@ -19,7 +19,7 @@ WarpXSumGuardCells(amrex::MultiFab& mf, const amrex::Periodicity& period,
                    const amrex::IntVect& src_ngrow,
                    const int icomp, const int ncomp)
 {
-    amrex::IntVect n_updated_guards = mf.nGrowVect();
+    amrex::IntVect const n_updated_guards = mf.nGrowVect();
     ablastr::utils::communication::SumBoundary(mf, icomp, ncomp, src_ngrow, n_updated_guards, WarpX::do_single_precision_comms, period);
 }
 
@@ -30,7 +30,7 @@ WarpXSumGuardCells(amrex::MultiFab& dst, amrex::MultiFab& src,
                    const amrex::IntVect& src_ngrow,
                    const int icomp, const int ncomp)
 {
-    amrex::IntVect n_updated_guards = dst.nGrowVect();
+    amrex::IntVect const n_updated_guards = dst.nGrowVect();
 
     dst.setVal(0., icomp, ncomp, n_updated_guards);
     dst.ParallelAdd(src, 0, icomp, ncomp, src_ngrow, n_updated_guards, period);
