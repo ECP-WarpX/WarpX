05991dfcebdd6042435c314bff8838539e473405
Luca Fedeli <luca.fedeli@cea.fr>
2024-04-29 13:28:13 +0200

    change numbers of retries

--- .github/workflows/dependencies/clang17.sh
+++ .github/workflows/dependencies/clang17.sh
@@ -15,7 +15,7 @@ apt-get -y install sudo
 # `man apt.conf`:
 #   Number of retries to perform. If this is non-zero APT will retry
 #   failed files the given number of times.
-echo 'Acquire::Retries "3";' | sudo tee /etc/apt/apt.conf.d/80-retries
+echo 'Acquire::Retries "6";' | sudo tee /etc/apt/apt.conf.d/80-retries
 
 sudo apt-get -qqq update
 sudo apt-get install -y \
