155336b513bbb455fec33624b84242df73f2eae3
Axel Huebl <axel.huebl@plasma.ninja>
2024-04-29 13:27:28 -0700

    AMReX/pyAMReX/PICSAR: Weekly Update (#4904)
    * AMReX: Weekly Update
    
    * pyAMReX: Weekly Update

--- run_test.sh
+++ run_test.sh
@@ -68,7 +68,7 @@ python3 -m pip install --upgrade -r warpx/Regression/requirements.txt
 
 # Clone AMReX and warpx-data
 git clone https://github.com/AMReX-Codes/amrex.git
-cd amrex && git checkout --detach b752027c1aebdfb4be339b1e30932b4108286a7a && cd -
+cd amrex && git checkout --detach d857968968ae2cdf18f39ee50df96529e326ff39 && cd -
 # warpx-data contains various required data sets
 git clone --depth 1 https://github.com/ECP-WarpX/warpx-data.git
 # openPMD-example-datasets contains various required data sets
