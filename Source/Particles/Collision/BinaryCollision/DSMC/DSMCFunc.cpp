3abdde18c045458ce47deec0a58978c4b568619f
Thomas Marks <marksta@umich.edu>
2024-05-21 14:07:40 -0400

    remove commented-out check

--- Source/Particles/Collision/BinaryCollision/DSMC/DSMCFunc.cpp
+++ Source/Particles/Collision/BinaryCollision/DSMC/DSMCFunc.cpp
@@ -23,9 +23,6 @@ DSMCFunc::DSMCFunc (
 {
     using namespace amrex::literals;
 
-    // WARPX_ALWAYS_ASSERT_WITH_MESSAGE( (std::is_same<amrex::ParticleReal, double>::value),
-    // "Particle precision must be double for DSMC collisions.");
-
     const amrex::ParmParse pp_collision_name(collision_name);
 
     // query for a list of collision processes
