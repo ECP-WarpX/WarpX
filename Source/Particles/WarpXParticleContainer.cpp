db20264730d64e71279b53cc3c46579e8af92d28
Andrew Myers <atmyers2@gmail.com>
2024-06-04 12:51:20 -0700

    remove GPUCLOCK and change DenseBins type to match changes in development

--- Source/Particles/WarpXParticleContainer.cpp
+++ Source/Particles/WarpXParticleContainer.cpp
@@ -536,22 +536,22 @@ WarpXParticleContainer::DepositCurrent (WarpXParIter& pti,
                     GetPosition, wp.dataPtr() + offset, uxp.dataPtr() + offset,
                     uyp.dataPtr() + offset, uzp.dataPtr() + offset, ion_lev,
                     jx_fab, jy_fab, jz_fab, np_to_deposit, dt, relative_time, dx,
-                    xyzmin, lo, q, WarpX::n_rz_azimuthal_modes, cost,
-                    WarpX::load_balance_costs_update_algo, bins, box, geom, max_tbox_size);
+                    xyzmin, lo, q, WarpX::n_rz_azimuthal_modes,
+                    bins, box, geom, max_tbox_size);
             } else if (WarpX::nox == 2){
                 doEsirkepovDepositionSharedShapeN<2>(
                     GetPosition, wp.dataPtr() + offset, uxp.dataPtr() + offset,
                     uyp.dataPtr() + offset, uzp.dataPtr() + offset, ion_lev,
                     jx_fab, jy_fab, jz_fab, np_to_deposit, dt, relative_time, dx,
-                    xyzmin, lo, q, WarpX::n_rz_azimuthal_modes, cost,
-                    WarpX::load_balance_costs_update_algo, bins, box, geom, max_tbox_size);
+                    xyzmin, lo, q, WarpX::n_rz_azimuthal_modes,
+                    bins, box, geom, max_tbox_size);
             } else if (WarpX::nox == 3){
                 doEsirkepovDepositionSharedShapeN<3>(
                     GetPosition, wp.dataPtr() + offset, uxp.dataPtr() + offset,
                     uyp.dataPtr() + offset, uzp.dataPtr() + offset, ion_lev,
                     jx_fab, jy_fab, jz_fab, np_to_deposit, dt, relative_time, dx,
-                    xyzmin, lo, q, WarpX::n_rz_azimuthal_modes, cost,
-                    WarpX::load_balance_costs_update_algo, bins, box, geom, max_tbox_size);
+                    xyzmin, lo, q, WarpX::n_rz_azimuthal_modes,
+                    bins, box, geom, max_tbox_size);
             }
             WARPX_PROFILE_VAR_STOP(esirkepov_current_dep_kernel);
         }
