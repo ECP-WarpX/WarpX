8431e4c626e90a46acc725993f17df49a327e171
Remi Lehe <remi.lehe@normalesup.org>
2024-07-02 14:06:45 -0700

    Update default behavior for gathering with direct deposition

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -1284,8 +1284,12 @@ WarpX::ReadParameters ()
             }
         }
 
-        // Use same shape factors in all directions, for gathering
-        if (field_gathering_algo == GatheringAlgo::MomentumConserving) { galerkin_interpolation = false; }
+        // Use same shape factors in all directions
+        // - with momentum-conserving field gathering
+        // - with direct current deposition
+        if ((field_gathering_algo == GatheringAlgo::MomentumConserving) || (current_deposition_algo == CurrentDepositionAlgo::Direct)) {
+            galerkin_interpolation = false;
+        }
 
         {
             const ParmParse pp_interpolation("interpolation");
