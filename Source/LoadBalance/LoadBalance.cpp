fe394745f4abf9f47c794b6f0e7ecad7fc809de9
Luca Fedeli <luca.fedeli@cea.fr>
2024-07-03 14:52:54 +0200

    create LoadBalance folder, create LoadBalance.H/cpp files, update CMakeLists and Make files

---
+++ Source/LoadBalance/LoadBalance.cpp
@@ -0,0 +1,10 @@
+/* Copyright 2024 Andrew Myers, Ann Almgren, Axel Huebl
+ * David Grote, Luca Fedeli, Maxence Thevenet, Michael Rowan
+ * Remi Lehe, Weiqun Zhang, levinem, Revathi Jambunathan
+ *
+ * This file is part of WarpX.
+ *
+ * License: BSD-3-Clause-LBNL
+ */
+
+#include "LoadBalance.H"
