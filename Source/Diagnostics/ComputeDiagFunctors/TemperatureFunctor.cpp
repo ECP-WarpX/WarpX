25111a433cda8ca1ac1d411e1887efb1498a547b
Thomas Marks <marksta@umich.edu>
2024-08-14 11:54:45 -0400

    fix typo

--- Source/Diagnostics/ComputeDiagFunctors/TemperatureFunctor.cpp
+++ Source/Diagnostics/ComputeDiagFunctors/TemperatureFunctor.cpp
@@ -54,7 +54,7 @@ TemperatureFunctor::operator() (amrex::MultiFab& mf_dst, const int dcomp, const 
                 amrex::GpuArray<amrex::Real,AMREX_SPACEDIM> const& dxi)
             {
                 // Get position in AMReX convention to calculate corresponding index.
-                const auto cellIndex = ParticleUtils::getParticleCellIndex(p, plo, dx).dim3();
+                const auto cellIndex = ParticleUtils::getParticleCellIndex(p, plo, dxi).dim3();
                 const int ii = cellIndex.x;
                 const int jj = cellIndex.y;
                 const int kk = cellIndex.z;
