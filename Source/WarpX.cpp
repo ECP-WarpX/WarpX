2cce89e2a2a231f392f84c51ae818aae74d6a433
David Grote <grote1@llnl.gov>
2024-08-20 17:07:49 -0700

    Bug fixes

--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -2976,7 +2976,7 @@ WarpX::LowerCorner(const Box& bx, const int lev, const amrex::Real time_shift_de
     return { grid_min[0] + galilean_shift[0], std::numeric_limits<Real>::lowest(), grid_min[1] + galilean_shift[2] };
 
 #elif defined(WARPX_DIM_RCYLINDER) || defined(WARPX_DIM_RSPHERE)
-    return { xyzmin[0] + galilean_shift[0], std::numeric_limits<Real>::lowest(), std::numeric_limits<Real>::lowest() };
+    return { grid_min[0] + galilean_shift[0], std::numeric_limits<Real>::lowest(), std::numeric_limits<Real>::lowest() };
 
 #elif defined(WARPX_DIM_1D_Z)
     return { std::numeric_limits<Real>::lowest(), std::numeric_limits<Real>::lowest(), grid_min[0] + galilean_shift[2] };
@@ -3004,7 +3004,7 @@ WarpX::UpperCorner(const Box& bx, const int lev, const amrex::Real time_shift_de
     return { grid_max[0] + galilean_shift[0], std::numeric_limits<Real>::max(), grid_max[1] + galilean_shift[1] };
 
 #elif defined(WARPX_DIM_RCYLINDER) || defined(WARPX_DIM_RSPHERE)
-    return { xyzmax[0] + galilean_shift[0], std::numeric_limits<Real>::max(), std::numeric_limits<Real>::max() };
+    return { grid_max[0] + galilean_shift[0], std::numeric_limits<Real>::max(), std::numeric_limits<Real>::max() };
 
 #elif defined(WARPX_DIM_1D_Z)
     return { std::numeric_limits<Real>::max(), std::numeric_limits<Real>::max(), grid_max[0] + galilean_shift[0] };
