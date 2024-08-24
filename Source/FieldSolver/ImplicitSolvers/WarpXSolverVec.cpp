40cb43a25c8ae297fc0ed9a9a64924255e78df95
Justin Angus <angus1@llnl.gov>
2024-08-24 15:46:18 -0700

    dotMask now owned by WarpX. Defined only when needed.

--- Source/FieldSolver/ImplicitSolvers/WarpXSolverVec.cpp
+++ Source/FieldSolver/ImplicitSolvers/WarpXSolverVec.cpp
@@ -29,7 +29,6 @@ void WarpXSolverVec::Define ( WarpX*                    a_WarpX,
     m_field_type = a_field_type;
     m_is_defined = true;
     SetWarpXPointer(a_WarpX);
-    SetDotMask();
 }
 
 void WarpXSolverVec::Copy ( const warpx::fields::FieldType  a_field_type )
@@ -58,50 +57,22 @@ void WarpXSolverVec::SetWarpXPointer( WarpX*  a_WarpX )
     m_warpx_ptr_defined = true;
 }
 
-void WarpXSolverVec::SetDotMask()
-{
-    if (m_dot_mask_defined) { return; }
-    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
-        IsDefined(),
-        "WarpXSolverVec::SetDotMask() called from undefined instance ");
-    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
-        m_warpx_ptr_defined,
-        "WarpXSolverVec::SetDotMask() called before SetWarpXPointer() ");
-
-    const amrex::Vector<amrex::Geometry>& Geom = m_WarpX->Geom();
-    m_dotMask.resize(m_num_amr_levels);
-    for ( int n = 0; n < 3; n++) {
-        const amrex::BoxArray& grids = m_field_vec[0][n]->boxArray();
-        const amrex::MultiFab tmp( grids, m_field_vec[0][n]->DistributionMap(),
-                                   1, 0, amrex::MFInfo().SetAlloc(false) );
-        const amrex::Periodicity& period = Geom[0].periodicity();
-        m_dotMask[0][n] = tmp.OwnerMask(period);
-    }
-    m_dot_mask_defined = true;
-
-    // If the function below is not called, then the following
-    // error message occurs after the simulation finishes:
-    // malloc_consolidate(): unaligned fastbin chunk detected
-    amrex::ExecOnFinalize(WarpXSolverVec::clearDotMask);
-}
-
 [[nodiscard]] amrex::Real WarpXSolverVec::dotProduct ( const WarpXSolverVec&  a_X ) const
 {
-    WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
-        m_dot_mask_defined,
-        "WarpXSolverVec::dotProduct called with m_dotMask not yet defined");
     WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
         a_X.m_field_type==m_field_type,
         "WarpXSolverVec::dotProduct(X) called with solver vecs of different types");
 
     amrex::Real result = 0.0;
-    const int lev = 0;
     const bool local = true;
-    for (int n = 0; n < 3; ++n) {
-        auto rtmp = amrex::MultiFab::Dot( *m_dotMask[lev][n],
-                                          *m_field_vec[lev][n], 0,
-                                          *a_X.getVec()[lev][n], 0, 1, 0, local);
-        result += rtmp;
+    for (int lev = 0; lev < m_num_amr_levels; ++lev) {
+        for (int n = 0; n < 3; ++n) {
+            const amrex::iMultiFab* dotMask = m_WarpX->getFieldDotMaskPointer(m_field_type,lev,n);
+            auto rtmp = amrex::MultiFab::Dot( *dotMask,
+                                              *m_field_vec[lev][n], 0,
+                                              *a_X.getVec()[lev][n], 0, 1, 0, local);
+            result += rtmp;
+        }
     }
     amrex::ParallelAllReduce::Sum(result, amrex::ParallelContext::CommunicatorSub());
     return result;
--- Source/WarpX.cpp
+++ Source/WarpX.cpp
@@ -327,6 +327,10 @@ WarpX::WarpX ()
     Efield_fp.resize(nlevs_max);
     Bfield_fp.resize(nlevs_max);
 
+    Efield_dotMask.resize(nlevs_max);
+    Bfield_dotMask.resize(nlevs_max);
+    phi_dotMask.resize(nlevs_max);
+
     // Only allocate vector potential arrays when using the Magnetostatic Solver
     if (electrostatic_solver_id == ElectrostaticSolverAlgo::LabFrameElectroMagnetostatic)
     {
@@ -2095,6 +2099,9 @@ WarpX::ClearLevel (int lev)
         Efield_fp [lev][i].reset();
         Bfield_fp [lev][i].reset();
 
+        Efield_dotMask [lev][i].reset();
+        Bfield_dotMask [lev][i].reset();
+
         current_store[lev][i].reset();
 
         if (do_current_centering)
@@ -2140,6 +2147,8 @@ WarpX::ClearLevel (int lev)
     F_cp  [lev].reset();
     G_cp  [lev].reset();
     rho_cp[lev].reset();
+
+    phi_dotMask[lev].reset();
 
 #ifdef WARPX_USE_FFT
     if (WarpX::electromagnetic_solver_id == ElectromagneticSolverAlgo::PSATD) {
@@ -3669,4 +3678,46 @@ WarpX::getMultiLevelField(warpx::fields::FieldType field_type) const
             WARPX_ABORT_WITH_MESSAGE("Invalid field type");
             return Efield_fp;
     }
+}
+
+const amrex::iMultiFab*
+WarpX::getFieldDotMaskPointer (warpx::fields::FieldType field_type, const int lev, const int dir) const
+{
+    switch(field_type)
+    {
+        case FieldType::Efield_fp :
+            if (Efield_dotMask[lev][dir] == nullptr) {
+                SetDotMask( Efield_dotMask[lev][dir], field_type, lev, dir );
+            }
+            return Efield_dotMask[lev][dir].get();
+        case FieldType::Bfield_fp :
+            if (Bfield_dotMask[lev][dir] == nullptr) {
+                SetDotMask( Bfield_dotMask[lev][dir], field_type, lev, dir );
+            }
+            return Bfield_dotMask[lev][dir].get();
+        case FieldType::phi_fp :
+            if (phi_dotMask[lev] == nullptr) {
+                SetDotMask( phi_dotMask[lev], field_type, lev, 0 );
+            }
+            return phi_dotMask[lev].get();
+        default:
+            WARPX_ABORT_WITH_MESSAGE("Invalid field type for dotMask");
+            return Efield_dotMask[lev][dir].get();
+    }
+}
+
+void WarpX::SetDotMask( std::unique_ptr<amrex::iMultiFab>&  field_dotMask,
+                        warpx::fields::FieldType  field_type,
+                        const int  lev, const int  dir ) const
+{
+    // Define the dot mask for this field_type needed to properly compute dotProduct()
+    // for field values that have shared locations on different MPI ranks
+
+    const amrex::MultiFab* this_field = getFieldPointer(field_type,lev,dir);
+    const amrex::BoxArray& this_ba = this_field->boxArray();
+    const amrex::MultiFab tmp( this_ba, this_field->DistributionMap(),
+                               1, 0, amrex::MFInfo().SetAlloc(false) );
+    const amrex::Periodicity& period = Geom(lev).periodicity();
+    field_dotMask = tmp.OwnerMask(period);
+
 }
