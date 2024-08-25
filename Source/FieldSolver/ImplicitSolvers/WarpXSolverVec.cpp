69e3b737fbe454ea71e268721ae531c391b746c7
Justin Angus <angus1@llnl.gov>
2024-08-24 23:43:32 -0700

    WarpXSolverVec can now be used with scalar field quantities.

--- Source/FieldSolver/ImplicitSolvers/WarpXSolverVec.cpp
+++ Source/FieldSolver/ImplicitSolvers/WarpXSolverVec.cpp
@@ -7,44 +7,74 @@
 #include "FieldSolver/ImplicitSolvers/WarpXSolverVec.H"
 #include "WarpX.H"
 
-void WarpXSolverVec::Define ( WarpX*                    a_WarpX,
-                        const warpx::fields::FieldType  a_field_type )
+void WarpXSolverVec::Define ( WarpX*     a_WarpX,
+                              FieldType  a_field_type,
+                              FieldType  a_scalar_type )
 {
     WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
         !IsDefined(),
         "WarpXSolverVec::Define(a_vec, a_type) called on already defined WarpXSolverVec");
 
-    const amrex::Vector<std::array<std::unique_ptr<amrex::MultiFab>,3>>& this_vec(
-                                                            a_WarpX->getMultiLevelField(a_field_type));
-    m_field_vec.resize(m_num_amr_levels);
-    for (int lev=0; lev<m_num_amr_levels; ++lev) {
-        for (int n=0; n<3; n++) {
-            const amrex::MultiFab& mf_model = *this_vec[lev][n];
-            m_field_vec[lev][n] = std::make_unique<amrex::MultiFab>( mf_model.boxArray(),
-                                                                     mf_model.DistributionMap(),
-                                                                     mf_model.nComp(),
-                                                                     amrex::IntVect::TheZeroVector() );
-        }
-    }
     m_field_type = a_field_type;
+    m_scalar_type = a_scalar_type;
+
+    m_field_vec.resize(m_num_amr_levels);
+    m_scalar_vec.resize(m_num_amr_levels);
+
+    // Define the 3D vector field data container
+    if (m_field_type != FieldType::None) {
+
+        for (int lev = 0; lev < m_num_amr_levels; ++lev) {
+            for (int n = 0; n < 3; n++) {
+                const amrex::MultiFab* mf_model = a_WarpX->getFieldPointer(m_field_type,lev,n);
+                m_field_vec[lev][n] = std::make_unique<amrex::MultiFab>( mf_model->boxArray(),
+                                                                         mf_model->DistributionMap(),
+                                                                         mf_model->nComp(),
+                                                                         amrex::IntVect::TheZeroVector() );
+            }
+        }
+
+    }
+
+    // Define the scalar data container
+    if (m_scalar_type != FieldType::None) {
+
+        for (int lev = 0; lev < m_num_amr_levels; ++lev) {
+            const amrex::MultiFab* mf_model = a_WarpX->getFieldPointer(m_scalar_type,lev,0);
+            m_scalar_vec[lev] = std::make_unique<amrex::MultiFab>( mf_model->boxArray(),
+                                                                   mf_model->DistributionMap(),
+                                                                   mf_model->nComp(),
+                                                                   amrex::IntVect::TheZeroVector() );
+        }
+
+    }
+
     m_is_defined = true;
     SetWarpXPointer(a_WarpX);
 }
 
-void WarpXSolverVec::Copy ( const warpx::fields::FieldType  a_field_type )
+void WarpXSolverVec::Copy ( FieldType  a_field_type,
+                            FieldType  a_scalar_type )
 {
     WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
         IsDefined(),
         "WarpXSolverVec::Copy() called on undefined WarpXSolverVec");
     WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
-        a_field_type==m_field_type,
+        a_field_type==m_field_type &&
+        a_scalar_type==m_scalar_type,
         "WarpXSolverVec::Copy() called with vecs of different types");
 
-    const amrex::Vector<std::array<std::unique_ptr<amrex::MultiFab>,3>>& this_vec(
-                                               m_WarpX->getMultiLevelField(a_field_type));
-    for (int lev=0; lev<m_num_amr_levels; ++lev) {
-        for (int n=0; n<3; ++n) {
-            amrex::MultiFab::Copy( *m_field_vec[lev][n], *this_vec[lev][n], 0, 0, m_ncomp,
+    for (int lev = 0; lev < m_num_amr_levels; ++lev) {
+        if (m_field_type != FieldType::None) {
+            for (int n = 0; n < 3; ++n) {
+                const amrex::MultiFab* this_field = m_WarpX->getFieldPointer(m_field_type,lev,n);
+                amrex::MultiFab::Copy( *m_field_vec[lev][n], *this_field, 0, 0, m_ncomp,
+                                       amrex::IntVect::TheZeroVector() );
+            }
+        }
+        if (m_scalar_type != FieldType::None) {
+            const amrex::MultiFab* this_scalar = m_WarpX->getFieldPointer(m_scalar_type,lev,0);
+            amrex::MultiFab::Copy( *m_scalar_vec[lev], *this_scalar, 0, 0, m_ncomp,
                                    amrex::IntVect::TheZeroVector() );
         }
     }
@@ -60,17 +90,27 @@ void WarpXSolverVec::SetWarpXPointer( WarpX*  a_WarpX )
 [[nodiscard]] amrex::Real WarpXSolverVec::dotProduct ( const WarpXSolverVec&  a_X ) const
 {
     WARPX_ALWAYS_ASSERT_WITH_MESSAGE(
-        a_X.m_field_type==m_field_type,
+        a_X.getFieldVecType()==m_field_type &&
+        a_X.getScalarVecType()==m_scalar_type,
         "WarpXSolverVec::dotProduct(X) called with solver vecs of different types");
 
     amrex::Real result = 0.0;
     const bool local = true;
     for (int lev = 0; lev < m_num_amr_levels; ++lev) {
-        for (int n = 0; n < 3; ++n) {
-            const amrex::iMultiFab* dotMask = m_WarpX->getFieldDotMaskPointer(m_field_type,lev,n);
+        if (m_field_type != FieldType::None) {
+            for (int n = 0; n < 3; ++n) {
+                const amrex::iMultiFab* dotMask = m_WarpX->getFieldDotMaskPointer(m_field_type,lev,n);
+                auto rtmp = amrex::MultiFab::Dot( *dotMask,
+                                                  *m_field_vec[lev][n], 0,
+                                                  *a_X.getFieldVec()[lev][n], 0, 1, 0, local);
+                result += rtmp;
+            }
+        }
+        if (m_scalar_type != FieldType::None) {
+            const amrex::iMultiFab* dotMask = m_WarpX->getFieldDotMaskPointer(m_scalar_type,lev,0);
             auto rtmp = amrex::MultiFab::Dot( *dotMask,
-                                              *m_field_vec[lev][n], 0,
-                                              *a_X.getVec()[lev][n], 0, 1, 0, local);
+                                              *m_scalar_vec[lev], 0,
+                                              *a_X.getScalarVec()[lev], 0, 1, 0, local);
             result += rtmp;
         }
     }
