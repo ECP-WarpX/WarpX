0c6525f59f1fbe4819265e30d4fb5ed968dbe7a8
Remi Lehe <remi.lehe@normalesup.org>
2024-08-26 12:06:04 -0700

    Add automated test

--- Examples/Tests/diff_lumi_diag/analysis.py
+++ Examples/Tests/diff_lumi_diag/analysis.py
@@ -1,45 +1,48 @@
 #!/usr/bin/env python3
+# This test checks the differential luminosity of the beam-beam interaction
+# in the case of two Gaussian beams crossing rigidly at the interaction point.
+# The beams have a Gaussian distribution both in energy and in transverse positions.
+# In that case, the differential luminosity can be calculated analytically.
 
 import os
-import re
 import sys
 
 import numpy as np
-from scipy.constants import e, eV
+from read_raw_data import read_reduced_diags_histogram
+from scipy.constants import eV
 
 sys.path.insert(1, "../../../../warpx/Regression/Checksum/")
 import checksumAPI
 
-sigmaz = 300e-6
-sigmax = 500e-9
-sigmay = 10e-9
-Q = 1.2e10 * e
-GeV = 1e9 * eV
+# Extract the differential luminosity from the file
+_, _, E_bin, bin_data = read_reduced_diags_histogram(
+    "./diags/reducedfiles/DifferentialLuminosity_beam1_beam2.txt"
+)
+dL_dE_sim = bin_data[-1]  # Differential luminosity at the end of the simulation
 
-fn = sys.argv[1]
+# Beam parameters
+N = 1.2e10
+E_beam = 125e9 * eV
+sigma_x = 500e-9
+sigma_y = 10e-9
 
+# Compute the analytical differential luminosity for 2 Gaussian beams
+sigma_E1 = 0.02 * E_beam
+sigma_E2 = 0.03 * E_beam
+sigma_E = np.sqrt(
+    sigma_E1**2 + sigma_E2**2
+)  # Expected width of the differential luminosity
+dL_dE_th = (
+    N**2
+    / (2 * (2 * np.pi) ** 1.5 * sigma_x * sigma_y * sigma_E)
+    * np.exp(-((E_bin - 2 * E_beam) ** 2) / (2 * sigma_E**2))
+)
 
-def get_bins(fname):
-    my_bins = []
-    with open(fname) as f:
-        for line in f:
-            if line.startswith("#"):
-                data = line.split()
-                for d in data[2:]:
-                    m = re.search("=(.*)\(\)", d)
-                    my_bins.append(m.group(1))
-    return np.asarray(my_bins, dtype=float)
-
-
-fname = "diags/reducedfiles/DifferentialLuminosity_beam1_beam2.txt"
-Ecom = (get_bins(fname) - 1) / GeV  # GeV
-dEcom = Ecom[1] - Ecom[0]  # GeV
-dL_dEcom = np.loadtxt(fname)[-1, 2:]  # s^-1 m^-2 J^-1
-dL_dEcom *= 1e-2 * GeV  # s^-1 cm^-2 J^-1
-
-print(dL_dEcom[np.nonzero(dL_dEcom)])
+# Check that the simulation result and analytical result match
+assert abs(dL_dE_sim - dL_dE_th).max() / abs(dL_dE_th).max() < 9e-3
 
 # Get name of the test
+fn = sys.argv[1]
 test_name = os.path.split(os.getcwd())[1]
 
 # Run checksum regression test
