fa021529a6200db106cec6a32e0aa7a126905c88
Edoardo Zoni <ezoni@lbl.gov>
2024-08-28 10:42:44 -0700

    Fix restart tests

--- Examples/analysis_default_regression.py
+++ Examples/analysis_default_regression.py
@@ -13,17 +13,9 @@ fn = sys.argv[1]
 # Get name of the test
 test_name = os.path.split(os.getcwd())[1]
 
-restart = "restart" in test_name
-
-if restart:
-    test_name = test_name.replace("_restart", "")
-
 # Run checksum regression test
 if re.search("single_precision", fn):
     checksumAPI.evaluate_checksum(test_name, fn, rtol=2.0e-6)
 else:
-    if restart:
-        checksumAPI.evaluate_checksum(test_name, fn, rtol=1e-12)
-    else:
-        # using default relative tolerance
-        checksumAPI.evaluate_checksum(test_name, fn)
+    # using default relative tolerance
+    checksumAPI.evaluate_checksum(test_name, fn)
