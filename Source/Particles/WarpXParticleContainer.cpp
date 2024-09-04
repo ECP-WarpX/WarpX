6d6df500e48f614be24c495b024415fa552ec0c4
Remi Lehe <remi.lehe@normalesup.org>
2024-09-04 12:12:16 -0700

    Refactor maxVelocity function

--- Source/Particles/WarpXParticleContainer.cpp
+++ Source/Particles/WarpXParticleContainer.cpp
@@ -1420,24 +1420,32 @@ std::array<ParticleReal, 3> WarpXParticleContainer::meanParticleVelocity(bool lo
     return mean_v;
 }
 
-amrex::ParticleReal WarpXParticleContainer::maxParticleVelocity(bool local) const {
+amrex::ParticleReal WarpXParticleContainer::maxParticleVelocity(bool local) {
 
     const amrex::ParticleReal inv_clight_sq = 1.0_prt/(PhysConst::c*PhysConst::c);
+    ReduceOps<ReduceOpMax> reduce_op;
+    ReduceData<ParticleReal> reduce_data(reduce_op);
 
-    using PType = typename WarpXParticleContainer::SuperParticleType;
-    amrex::ReduceOps<amrex::ReduceOpMax> reduce_ops;
-    const auto reduce_res = amrex::ParticleReduce<amrex::ReduceData<amrex::ParticleReal>>(
-            *this,
-            [=] AMREX_GPU_HOST_DEVICE(const PType &p) noexcept -> amrex::GpuTuple<amrex::ParticleReal>
-            {
-                const auto ux = p.rdata(PIdx::ux);
-                const auto uy = p.rdata(PIdx::uy);
-                const auto uz = p.rdata(PIdx::uz);
-                return {(ux*ux + uy*uy + uz*uz) * inv_clight_sq,};
-            },
-            reduce_ops);
+    const int nLevels = finestLevel();
 
-    const amrex::ParticleReal max_usq = amrex::get<0>(reduce_res);
+#ifdef AMREX_USE_OMP
+#pragma omp parallel if (amrex::Gpu::notInLaunchRegion())
+#endif
+    for (int lev = 0; lev <= nLevels; ++lev) {
+        for (WarpXParIter pti(*this, lev); pti.isValid(); ++pti)
+        {
+            auto *const ux = pti.GetAttribs(PIdx::ux).data();
+            auto *const uy = pti.GetAttribs(PIdx::uy).data();
+            auto *const uz = pti.GetAttribs(PIdx::uz).data();
+
+            reduce_op.eval(pti.numParticles(), reduce_data,
+                [=] AMREX_GPU_DEVICE (int ip)
+                { return (ux[ip]*ux[ip] + uy[ip]*uy[ip] + uz[ip]*uz[ip]) * inv_clight_sq; });
+        }
+    }
+
+    const amrex::ParticleReal max_usq = amrex::get<0>(reduce_data.value());
+
     const amrex::ParticleReal gaminv = 1.0_prt/std::sqrt(1.0_prt + max_usq);
     amrex::ParticleReal max_v = gaminv * std::sqrt(max_usq) * PhysConst::c;
 
