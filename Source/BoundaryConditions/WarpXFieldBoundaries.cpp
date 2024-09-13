4dc9bb039b7813b877c50a2068ea58cc496e4d13
Axel Huebl <axel.huebl@plasma.ninja>
2024-09-12 17:06:39 -0700

    Edoardo: Fix PML copy `Efield_fp`
    Co-authored-by: Edoardo Zoni <59625522+EZoni@users.noreply.github.com>

--- Source/BoundaryConditions/WarpXFieldBoundaries.cpp
+++ Source/BoundaryConditions/WarpXFieldBoundaries.cpp
@@ -135,10 +135,10 @@ void WarpX::ApplyBfieldBoundary (const int lev, PatchType patch_type, DtType a_d
     if (lev == 0) {
         if (a_dt_type == DtType::FirstHalf) {
             if(::isAnyBoundary<FieldBoundaryType::Absorbing_SilverMueller>(field_boundary_lo, field_boundary_hi)){
-                auto Efield_fp_new = m_fields.get_mr_levels_alldirs("Efield_fp",max_level); // JRA, new to prevent shadow
+                auto Efield_fp = m_fields.get_mr_levels_alldirs("Efield_fp",max_level); // JRA, new to prevent shadow
                 auto Bfield_fp = m_fields.get_mr_levels_alldirs("Bfield_fp",max_level);
                 m_fdtd_solver_fp[0]->ApplySilverMuellerBoundary(
-                Efield_fp_new[lev], Bfield_fp[lev],
+                Efield_fp[lev], Bfield_fp[lev],
                 Geom(lev).Domain(), dt[lev],
                 field_boundary_lo, field_boundary_hi);
             }
