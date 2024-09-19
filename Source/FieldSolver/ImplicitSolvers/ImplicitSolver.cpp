5c45dc17508d4c9a327e9acc4421b7d382968835
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2024-09-19 14:27:06 -0700

    clang-tidy recommendations

--- Source/FieldSolver/ImplicitSolvers/ImplicitSolver.cpp
+++ Source/FieldSolver/ImplicitSolvers/ImplicitSolver.cpp
@@ -32,6 +32,7 @@ const Array<LinOpBCType,AMREX_SPACEDIM> ImplicitSolver::GetLinOpBCHi () const
 Array<LinOpBCType,AMREX_SPACEDIM> ImplicitSolver::convertFieldBCToLinOpBC (const Array<FieldBoundaryType,AMREX_SPACEDIM>& a_fbc) const
 {
     Array<LinOpBCType, AMREX_SPACEDIM> lbc;
+    for (auto& bc : lbc) { bc = LinOpBCType::interior; }
     for (int i = 0; i < AMREX_SPACEDIM; i++) {
         if (a_fbc[i] == FieldBoundaryType::PML) {
             WARPX_ABORT_WITH_MESSAGE("LinOpBCType not set for this FieldBoundaryType");
