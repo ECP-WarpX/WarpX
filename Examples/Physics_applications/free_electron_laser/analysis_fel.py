6aa13829107d7ca50f60d38e14e3e23907d4660b
Remi Lehe <remi.lehe@normalesup.org>
2024-09-29 09:47:14 -0700

    Update scripts

--- Examples/Physics_applications/free_electron_laser/analysis_fel.py
+++ Examples/Physics_applications/free_electron_laser/analysis_fel.py
@@ -17,7 +17,6 @@ are the expected ones. The check is performed both in the
 lab-frame diagnostics and boosted-frame diagnostics.
 """
 
-import os
 import sys
 
 import numpy as np
@@ -25,7 +24,6 @@ from openpmd_viewer import OpenPMDTimeSeries
 from scipy.constants import c, e, m_e
 
 sys.path.insert(1, "../../../../warpx/Regression/Checksum/")
-import checksumAPI
 
 # Physical parameters of the test
 gamma_bunch = 100.6
@@ -37,6 +35,54 @@ gamma_boost = (
     gamma_bunch / (1 + K * K / 2) ** 0.5
 )  # Lorentz factor of the ponderomotive frame
 beta_boost = (1 - 1.0 / gamma_boost**2) ** 0.5
+
+
+# Analyze the diagnostics showing quantities in the lab frame
+ts_lab = OpenPMDTimeSeries("diags/diag_labframe")
+
+
+# Extract the growth of the peak electric field
+def extract_peak_E_lab(iteration):
+    """
+    Extract the position of the peak electric field
+    """
+    Ex, info = ts_lab.get_field("E", "x", iteration=iteration)
+    Ex_max = abs(Ex).max()
+    z_max = info.z[abs(Ex).argmax()]
+    return z_max, Ex_max
+
+
+# Loop through all iterations
+# Since the radiation power is proportional to the square of the peak electric field,
+# the log of the power is equal to the log of the square of the peak electric field,
+# up to an additive constant.
+z_lab_peak, E_lab_peak = ts_lab.iterate(extract_peak_E_lab)
+log_P_peak = np.log(E_lab_peak**2)
+
+# Pick the iterations between which the growth of the log of the power is linear
+# (i.e. the growth of the power is exponential) and fit a line to extract the
+# gain length.
+i_start = 6
+i_end = 23
+# Perform linear fit
+p = np.polyfit(z_lab_peak[i_start:i_end], log_P_peak[i_start:i_end], 1)
+# Extract the gain length
+Lg = 1 / p[0]
+Lg_expected = 0.22  # Expected gain length from https://arxiv.org/pdf/2009.13645
+print("Gain length: ", Lg)
+assert abs(Lg - Lg_expected) / Lg_expected < 0.15
+
+# Check that the radiation wavelength is the expected one
+iteration_check = 14
+Ex, info = ts_lab.get_field("E", "x", iteration=iteration_check)
+Nz = len(info.z)
+fft_E = abs(np.fft.fft(Ex))
+lambd = 1.0 / np.fft.fftfreq(Nz, d=info.dz)
+lambda_radiation_lab = lambd[fft_E[: Nz // 2].argmax()]
+lambda_expected = lambda_u / (2 * gamma_boost**2)
+print("lambda_radiation_lab", lambda_radiation_lab)
+print("lambda_expected", lambda_expected)
+assert abs(lambda_radiation_lab - lambda_expected) / lambda_expected < 0.01
 
 # Analyze the diagnostics showing quantities in the boosted frame
 ts = OpenPMDTimeSeries("diags/diag_boostedframe")
@@ -51,8 +97,8 @@ def extract_peak_E_boost(iteration):
     Ex, info = ts.get_field("E", "x", iteration=iteration)
     By, info = ts.get_field("B", "y", iteration=iteration)
     E_lab = gamma_boost * (Ex + c * beta_boost * By)
-    E_lab_peak = E_lab.max()
-    z_boost_peak = info.z[E_lab.argmax()]
+    E_lab_peak = abs(E_lab).max()
+    z_boost_peak = info.z[abs(E_lab).argmax()]
     t_boost_peak = ts.current_t
     z_lab_peak = gamma_boost * (z_boost_peak + beta_boost * c * t_boost_peak)
     return z_lab_peak, E_lab_peak
@@ -61,9 +107,6 @@ def extract_peak_E_boost(iteration):
 # Loop through all iterations
 z_lab_peak, E_lab_peak = ts.iterate(extract_peak_E_boost)
 log_P_peak = np.log(E_lab_peak**2)
-# Since the radiation power is proportional to the square of the peak electric field,
-# the log of the power is equal to the log of the square of the peak electric field,
-# up to an additive constant.
 
 # Pick the iterations between which the growth of the log of the power is linear
 # (i.e. the growth of the power is exponential) and fit a line to extract the
@@ -91,49 +134,7 @@ lambda_radiation_lab = lambda_radiation_boost / (2 * gamma_boost)
 lambda_expected = lambda_u / (2 * gamma_boost**2)
 assert abs(lambda_radiation_lab - lambda_expected) / lambda_expected < 0.01
 
-# Analyze the diagnostics showing quantities in the lab frame
-ts_lab = OpenPMDTimeSeries("diags/diag_labframe")
-
-
-# Extract the growth of the peak electric field
-def extract_peak_E_lab(iteration):
-    """
-    Extract the position of the peak electric field
-    """
-    Ex, info = ts_lab.get_field("E", "x", iteration=iteration)
-    return info.zmax, np.nanmax(Ex)
-
-
-# Loop through all iterations
-z_lab_peak, E_lab_peak = ts_lab.iterate(extract_peak_E_lab)
-log_P_peak = np.log(E_lab_peak**2)
-
-# Pick the iterations between which the growth of the log of the power is linear
-# (i.e. the growth of the power is exponential) and fit a line to extract the
-# gain length.
-i_start = 6
-i_end = 20
-# Perform linear fit
-p = np.polyfit(z_lab_peak[i_start:i_end], log_P_peak[i_start:i_end], 1)
-# Extract the gain length
-Lg = 1 / p[0]
-Lg_expected = 0.22  # Expected gain length from https://arxiv.org/pdf/2009.13645
-print("Gain length: ", Lg)
-assert abs(Lg - Lg_expected) / Lg_expected < 0.15
-
-# Check that the radiation wavelength is the expected one
-iteration_check = 14
-Ex, info = ts_lab.get_field("E", "x", iteration=iteration_check)
-Nz = len(info.z)
-fft_E = abs(np.fft.fft(Ex))
-lambd = 1.0 / np.fft.fftfreq(Nz, d=info.dz)
-lambda_radiation_lab = lambd[fft_E[: Nz // 2].argmax()]
-lambda_expected = lambda_u / (2 * gamma_boost**2)
-print("lambda_radiation_lab", lambda_radiation_lab)
-print("lambda_expected", lambda_expected)
-assert abs(lambda_radiation_lab - lambda_expected) / lambda_expected < 0.01
-
 # this will be the name of the plot file
-filename = sys.argv[1]
-test_name = os.path.split(os.getcwd())[1]
-checksumAPI.evaluate_checksum(test_name, filename, output_format="openpmd")
+# filename = sys.argv[1]
+# test_name = os.path.split(os.getcwd())[1]
+# checksumAPI.evaluate_checksum(test_name, filename, output_format="openpmd")
