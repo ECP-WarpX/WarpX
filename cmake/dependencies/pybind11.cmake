217ada9ecd94dfc50411f61bb521dbf912fcc328
Axel Huebl <axel.huebl@plasma.ninja>
2024-10-11 16:59:40 -0700

    `PYBIND11_FINDPYTHON=ON` (#5390)
    Reuse our `find_package(Python ...)` call and use new CMake logic in
    pybind11.
    
    https://pybind11.readthedocs.io/en/stable/compiling.html#modules-with-cmake
    
    https://cmake.org/cmake/help/latest/command/find_package.html#config-mode-version-selection
    
    X-ref:
    https://github.com/openPMD/openPMD-api/pull/1677#issuecomment-2407767112
    
    Fix #5159
    
    Signed-off-by: Axel Huebl <axel.huebl@plasma.ninja>

--- cmake/dependencies/pybind11.cmake
+++ cmake/dependencies/pybind11.cmake
@@ -10,6 +10,11 @@ function(find_pybind11)
         message(STATUS "pybind11 repository: ${WarpX_pybind11_repo} (${WarpX_pybind11_branch})")
         include(FetchContent)
     endif()
+
+    # rely on our find_package(Python ...) call
+    # https://pybind11.readthedocs.io/en/stable/compiling.html#modules-with-cmake
+    set(PYBIND11_FINDPYTHON ON)
+
     if(WarpX_pybind11_internal OR WarpX_pybind11_src)
         set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 
--- setup.py
+++ setup.py
@@ -84,7 +84,15 @@ class CMakeBuild(build_ext):
         r_dim = re.search(r"warpx_(1|2|rz|3)(?:d*)", ext.name)
         dims = r_dim.group(1).upper()
 
+        pyv = sys.version_info
         cmake_args = [
+            # Python: use the calling interpreter in CMake
+            # https://cmake.org/cmake/help/latest/module/FindPython.html#hints
+            # https://cmake.org/cmake/help/latest/command/find_package.html#config-mode-version-selection
+            f"-DPython_ROOT_DIR={sys.prefix}",
+            f"-DPython_FIND_VERSION={pyv.major}.{pyv.minor}.{pyv.micro}",
+            "-DPython_FIND_VERSION_EXACT=TRUE",
+            "-DPython_FIND_STRATEGY=LOCATION",
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + os.path.join(extdir, "pywarpx"),
             "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=" + extdir,
             "-DWarpX_DIMS=" + dims,
