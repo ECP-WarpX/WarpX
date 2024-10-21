e96455ed3aa95f482f42a0ddb81cdf0d6928bf2c
Edoardo Zoni <ezoni@lbl.gov>
2024-10-21 09:14:49 -0700

    Pull latest pyAMReX commit

--- cmake/dependencies/pyAMReX.cmake
+++ cmake/dependencies/pyAMReX.cmake
@@ -74,7 +74,7 @@ option(WarpX_pyamrex_internal "Download & build pyAMReX" ON)
 set(WarpX_pyamrex_repo "https://github.com/AMReX-Codes/pyamrex.git"
     CACHE STRING
     "Repository URI to pull and build pyamrex from if(WarpX_pyamrex_internal)")
-set(WarpX_pyamrex_branch "fdd9c8cc8f9092ddbd5f29275393cf1329d7ac14"
+set(WarpX_pyamrex_branch "d96b4948cc5812be82dbff1df5d62927c866ae07"
     CACHE STRING
     "Repository branch for WarpX_pyamrex_repo if(WarpX_pyamrex_internal)")
 
